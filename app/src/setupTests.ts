927a1554349c22455532549f56c922e50a805207
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-02-24 11:06:12 -0800

    formatting

--- app/src/setupTests.ts
+++ app/src/setupTests.ts
@@ -16,10 +16,10 @@ import '@testing-library/jest-dom/extend-expect';
 */
 const createElementNSOrig = global.document.createElementNS;
 
-global.document.createElementNS = function(namespaceURI, qualifiedName) {
+global.document.createElementNS = function (namespaceURI, qualifiedName) {
   if (namespaceURI === 'http://www.w3.org/2000/svg' && qualifiedName === 'svg') {
     const element = createElementNSOrig.apply(this, arguments);
-    element.createSVGRect = function() {
+    element.createSVGRect = function () {
       // This is intentional
     };
 
