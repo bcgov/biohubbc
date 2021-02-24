59a384b73f271b601c9657da557214fe6338eac5
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-02-24 11:01:45 -0800

    fix tests issues

--- app/src/setupTests.ts
+++ app/src/setupTests.ts
@@ -16,10 +16,10 @@ import '@testing-library/jest-dom/extend-expect';
 */
 const createElementNSOrig = global.document.createElementNS;
 
-global.document.createElementNS = (namespaceURI, qualifiedName) => {
-  if (namespaceURI === 'https://www.w3.org/2000/svg' && qualifiedName === 'svg') {
+global.document.createElementNS = function(namespaceURI, qualifiedName) {
+  if (namespaceURI === 'http://www.w3.org/2000/svg' && qualifiedName === 'svg') {
     const element = createElementNSOrig.apply(this, arguments);
-    element.createSVGRect = () => {
+    element.createSVGRect = function() {
       // This is intentional
     };
 
