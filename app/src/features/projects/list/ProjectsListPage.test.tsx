2c2fbf6ba7c4c40e6f4a90950a2f0e9734c71a6e
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2021-03-09 12:48:30 -0800

    put the project list in a list folder

--- app/src/features/projects/list/ProjectsListPage.test.tsx
+++ app/src/features/projects/list/ProjectsListPage.test.tsx
@@ -5,8 +5,8 @@ import ProjectsPage from './ProjectsListPage';
 
 // https://stackoverflow.com/questions/58524183/how-to-mock-history-push-with-the-new-react-router-hooks-using-jest/59451956
 
-describe('ProjectsPage.test', () => {
-  test('ProjectsPage renders with the create project button', () => {
+describe('ProjectsListPage.test', () => {
+  test('ProjectsListPage renders with the create project button', () => {
     const { baseElement } = render(
       <MemoryRouter>
         <ProjectsPage />
