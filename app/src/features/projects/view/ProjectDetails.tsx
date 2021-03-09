b5b998284ddc98b83f860df82192afe768de74e7
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2021-03-08 16:32:37 -0800

    basic data for project general info

--- app/src/features/projects/view/ProjectDetails.tsx
+++ app/src/features/projects/view/ProjectDetails.tsx
@@ -1,5 +1,8 @@
 import { Box, Paper, Typography } from '@material-ui/core';
+//import React, { useState } from 'react';
 import React from 'react';
+//import { IProject } from 'interfaces/project-interfaces';
+//import { useParams } from 'react-router';
 
 export interface IProjectDetailsProps {
   projectData: any;
@@ -10,7 +13,14 @@ export interface IProjectDetailsProps {
  *
  * @return {*}
  */
-const ProjectDetails: React.FC<IProjectDetailsProps> = () => {
+const ProjectDetails: React.FC<IProjectDetailsProps> = (props) => {
+
+  const projectData= props.projectData;
+
+  // TODO this is using IProject in the mean time, but will eventually need something like IProjectRecord
+  //const [project ] = useState<IProject | null>(null);
+
+
   return (
     <>
       <Box mb={5}>
@@ -18,12 +28,19 @@ const ProjectDetails: React.FC<IProjectDetailsProps> = () => {
       </Box>
       <Box mb={3}>
         <Paper>
-          <Box p={3}>Project details component 1 placeholder</Box>
+          <Box p={3}>Project objectives placeholder</Box>
         </Paper>
       </Box>
       <Box mb={3}>
         <Paper>
-          <Box p={3}>Project details component 2 placeholder</Box>
+          <Box p={3}>{JSON.stringify(projectData)}</Box>
+          <Box p={3}>General Information</Box>
+          <Box p={3}>Name: {projectData?.name}</Box>
+          <Box p={3}>Type: placeholder</Box>
+          <Box p={3}>Timeline: {projectData?.start_date?.substring(0,10)} - {projectData?.end_date?.substring(0,10)}</Box>
+          <Box p={3}>Activities: placeholder</Box>
+          <Box p={3}>Climate change initiatives: placeholder</Box>
+
         </Paper>
       </Box>
     </>
