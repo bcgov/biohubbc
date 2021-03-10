42d41028e3d9cc65370cace65e3b3396d85164e4
charlie garrett-jones <charlie.garrettjones@bayseumcom>
2021-03-09 19:59:23 -0800

    database release.0.7

---
+++ database/src/migrations/20210225205948_biohub_release_0.7.ts
@@ -0,0 +1,89 @@
+import * as fs from 'fs';
+import Knex from 'knex';
+import path from 'path';
+
+const DB_USER_API_PASS = process.env.DB_USER_API_PASS;
+const DB_USER_API = process.env.DB_USER_API;
+
+/**
+ * Apply biohub release 0.7 changes.
+ *
+ * @export
+ * @param {Knex} knex
+ * @return {*}  {Promise<void>}
+ */
+export async function up(knex: Knex): Promise<void> {
+  const create_spatial_extensions = fs.readFileSync(path.join(__dirname, 'release.0.7', 'create_spatial_extensions.psql'));
+  const biohub_0_7 = fs.readFileSync(path.join(__dirname, 'release.0.7', 'biohub.0.7.sql'));
+  const populate_user_identity_source = fs.readFileSync(path.join(__dirname, 'release.0.7', 'populate_user_identity_source.sql'));
+  const api_set_context = fs.readFileSync(path.join(__dirname, 'release.0.7', 'api_set_context.sql'));
+  const tr_audit_trigger = fs.readFileSync(path.join(__dirname, 'release.0.7', 'tr_audit_trigger.sql'));
+  const project_audit_triggers = fs.readFileSync(path.join(__dirname, 'release.0.7', 'project_audit_triggers.sql'));
+  const api_get_context_user_id = fs.readFileSync(path.join(__dirname, 'release.0.7', 'api_get_context_user_id.sql'));
+  const tr_journal_trigger = fs.readFileSync(path.join(__dirname, 'release.0.7', 'tr_journal_trigger.sql'));
+  const project_journal_triggers = fs.readFileSync(path.join(__dirname, 'release.0.7', 'project_journal_triggers.sql'));
+  const tr_project_funding_source = fs.readFileSync(path.join(__dirname, 'release.0.7', 'tr_project_funding_source.sql'));
+
+  const populate_first_nations = fs.readFileSync(path.join(__dirname, 'release.0.7', 'populate_first_nations.sql'));
+  const populate_climate_change_initiatives = fs.readFileSync(path.join(__dirname, 'release.0.7', 'populate_climate_change_initiatives.sql'));
+  const populate_management_action_type = fs.readFileSync(path.join(__dirname, 'release.0.7', 'populate_management_action_type.sql'));
+  const populate_funding_source = fs.readFileSync(path.join(__dirname, 'release.0.7', 'populate_funding_source.sql'));
+  const populate_investment_action_category = fs.readFileSync(path.join(__dirname, 'release.0.7', 'populate_investment_action_category.sql'));
+  const populate_project_type = fs.readFileSync(path.join(__dirname, 'release.0.7', 'populate_project_type.sql'));
+  const populate_activity = fs.readFileSync(path.join(__dirname, 'release.0.7', 'populate_activity.sql'));
+  const populate_icun_classifications = fs.readFileSync(path.join(__dirname, 'release.0.7', 'populate_icun_classifications.sql'));
+
+  const project_dapi_views = fs.readFileSync(path.join(__dirname, 'release.0.7', 'project_dapi_views.sql'));
+  
+  await knex.raw(`
+    -- set up spatial extensions
+    ${create_spatial_extensions}
+
+    -- set up project management schema
+    create schema if not exists project_management;
+    set search_path = project_management, public;
+
+    ${biohub_0_7}
+    ${populate_user_identity_source}
+    ${api_set_context}
+    ${tr_audit_trigger}
+    ${project_audit_triggers}
+    ${api_get_context_user_id}
+    ${tr_journal_trigger}
+    ${project_journal_triggers}
+    ${tr_project_funding_source}
+
+    -- populate look up tables
+    set search_path = project_management;
+    ${populate_first_nations}
+    ${populate_climate_change_initiatives}
+    ${populate_management_action_type}
+    ${populate_funding_source}
+    ${populate_investment_action_category}
+    ${populate_project_type}
+    ${populate_activity}
+    ${populate_icun_classifications}
+
+    -- setup biohub api schema
+    create schema if not exists biohub_dapi_v1;
+    set search_path = biohub_dapi_v1;
+    ${project_dapi_views}
+
+    -- setup api user
+    create user ${DB_USER_API} password '${DB_USER_API_PASS}';
+    grant usage on schema biohub_dapi_v1 to ${DB_USER_API};
+    grant usage on schema project_management to ${DB_USER_API};
+    grant all on all tables in schema biohub_dapi_v1 to ${DB_USER_API};
+    alter DEFAULT PRIVILEGES in SCHEMA biohub_dapi_v1 grant ALL on tables to ${DB_USER_API};
+    --grant postgis_reader to ${DB_USER_API};
+    alter role ${DB_USER_API} set search_path to biohub_dapi_v1, project_management, public, topology;
+
+    set search_path = project_management;
+    grant execute on function api_set_context(idir_user_id system_user.user_identifier%type, bceid_user_id system_user.user_identifier%type) to ${DB_USER_API};
+
+  `);
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.raw(``);
+}
---
+++ database/src/migrations/release.0.7/smoketest_release.0.7.sql
@@ -0,0 +1,65 @@
+-- smoketest_release.0.7.sql
+
+-- following to be run as postgres user
+set search_path=biohub;
+insert into system_user (uis_id, user_identifier, record_effective_date, create_date, create_user)
+  select id, 'myIDIR', now(), now(), 1 from user_identity_source 
+    where name = 'IDIR'
+    and record_end_date is null;
+
+select count(1) from audit_log;
+
+-- following to be run as biohub_api user
+select api_set_context('myIDIR', null);
+
+do $$
+declare
+  __id integer;
+begin
+  insert into project (pt_id
+    , name
+    , objectives
+    , start_date
+    , coordinator_first_name
+    , coordinator_last_name
+    , coordinator_email_address
+    , coordinator_agency_name
+    , coordinator_public
+    , geometry
+    ) values ((select id from project_type where name = 'Wildlife')
+    , 'project 10'
+    , 'my objectives'
+    , now()
+    , 'coordinator_first_name'
+    , 'coordinator_last_name'
+    , 'coordinator_email_address'
+    , 'coordinator_agency_name'
+    , 'Y'
+    , ST_Transform(ST_GeomFromKML('<Polygon><outerBoundaryIs><LinearRing><coordinates>-124.320874799971,48.9077923120772 -124.322396203914,48.9065111298094 -124.324678309828,48.905390095325 -124.327360785201,48.9057904647837 -124.32844178274,48.9074319795644 -124.328962263036,48.9093937899119 -124.32912241082,48.9102746027211 -124.326880341851,48.9101544918834 -124.32359731229,48.9088733096156 -124.320874799971,48.9077923120772</coordinates></LinearRing></outerBoundaryIs></Polygon>'), 3005)
+    ) returning id into __id;
+
+  insert into focal_species (p_id, name) values (__id, 'test');
+  insert into ancillary_species (p_id, name) values (__id, 'test');
+  insert into stakeholder_partnership (p_id, name) values (__id, 'test');
+  insert into project_activity (p_id, a_id) values (__id, (select id from activity where name = 'Monitoring'));
+  insert into project_climate_initiative (p_id, cci_id) values (__id, (select id from climate_change_initiative where name = 'Monitoring'));
+  insert into project_region (p_id, name) values (__id, 'test');
+  insert into project_permit (p_id, number, sampling_conducted) values (__id, random(), 'Y');
+  insert into project_management_actions (p_id, mat_id) values (__id, (select id from management_action_type where name = 'Recovery Action'));
+  insert into project_funding_source (p_id, iac_id, funding_amount, funding_start_date, funding_end_date, funding_source_project_id) values (__id, (select id from investment_action_category where name = 'Action 1'), '$1,000.00', now(), now(), 'test');
+  insert into project_iucn_action_classificaton (p_id, iucn2_id) values (__id, (select id from iucn_conservation_action_level_2_subclassification where name = 'Species Stewardship'));
+end
+$$;
+
+select count(1) from project;
+select count(1) from focal_species;
+select count(1) from ancillary_species;
+select count(1) from stakeholder_partnership;
+select count(1) from project_activity;
+select count(1) from project_climate_initiative;
+select count(1) from project_region;
+select count(1) from project_permit;
+select count(1) from project_management_actions;
+select count(1) from project_funding_source;
+select count(1) from project_iucn_action_classificaton;
+
