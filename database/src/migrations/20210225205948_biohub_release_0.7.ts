a72254050f6ebf9ee0e2b7604e0158a2ff865d1a
charlie garrett-jones <charlie.garrettjones@bayseumcom>
2021-03-10 11:04:24 -0800

    fixed wrong source

--- database/src/migrations/20210225205948_biohub_release_0.7.ts
+++ database/src/migrations/20210225205948_biohub_release_0.7.ts
@@ -13,35 +13,49 @@ const DB_USER_API = process.env.DB_USER_API;
  * @return {*}  {Promise<void>}
  */
 export async function up(knex: Knex): Promise<void> {
-  const create_spatial_extensions = fs.readFileSync(path.join(__dirname, 'release.0.7', 'create_spatial_extensions.psql'));
+  const create_spatial_extensions = fs.readFileSync(
+    path.join(__dirname, 'release.0.7', 'create_spatial_extensions.psql')
+  );
   const biohub_0_7 = fs.readFileSync(path.join(__dirname, 'release.0.7', 'biohub.0.7.sql'));
-  const populate_user_identity_source = fs.readFileSync(path.join(__dirname, 'release.0.7', 'populate_user_identity_source.sql'));
+  const populate_user_identity_source = fs.readFileSync(
+    path.join(__dirname, 'release.0.7', 'populate_user_identity_source.sql')
+  );
   const api_set_context = fs.readFileSync(path.join(__dirname, 'release.0.7', 'api_set_context.sql'));
   const tr_audit_trigger = fs.readFileSync(path.join(__dirname, 'release.0.7', 'tr_audit_trigger.sql'));
   const project_audit_triggers = fs.readFileSync(path.join(__dirname, 'release.0.7', 'project_audit_triggers.sql'));
   const api_get_context_user_id = fs.readFileSync(path.join(__dirname, 'release.0.7', 'api_get_context_user_id.sql'));
   const tr_journal_trigger = fs.readFileSync(path.join(__dirname, 'release.0.7', 'tr_journal_trigger.sql'));
   const project_journal_triggers = fs.readFileSync(path.join(__dirname, 'release.0.7', 'project_journal_triggers.sql'));
-  const tr_project_funding_source = fs.readFileSync(path.join(__dirname, 'release.0.7', 'tr_project_funding_source.sql'));
+  const tr_project_funding_source = fs.readFileSync(
+    path.join(__dirname, 'release.0.7', 'tr_project_funding_source.sql')
+  );
 
   const populate_first_nations = fs.readFileSync(path.join(__dirname, 'release.0.7', 'populate_first_nations.sql'));
-  const populate_climate_change_initiatives = fs.readFileSync(path.join(__dirname, 'release.0.7', 'populate_climate_change_initiatives.sql'));
-  const populate_management_action_type = fs.readFileSync(path.join(__dirname, 'release.0.7', 'populate_management_action_type.sql'));
+  const populate_climate_change_initiatives = fs.readFileSync(
+    path.join(__dirname, 'release.0.7', 'populate_climate_change_initiatives.sql')
+  );
+  const populate_management_action_type = fs.readFileSync(
+    path.join(__dirname, 'release.0.7', 'populate_management_action_type.sql')
+  );
   const populate_funding_source = fs.readFileSync(path.join(__dirname, 'release.0.7', 'populate_funding_source.sql'));
-  const populate_investment_action_category = fs.readFileSync(path.join(__dirname, 'release.0.7', 'populate_investment_action_category.sql'));
+  const populate_investment_action_category = fs.readFileSync(
+    path.join(__dirname, 'release.0.7', 'populate_investment_action_category.sql')
+  );
   const populate_project_type = fs.readFileSync(path.join(__dirname, 'release.0.7', 'populate_project_type.sql'));
   const populate_activity = fs.readFileSync(path.join(__dirname, 'release.0.7', 'populate_activity.sql'));
-  const populate_icun_classifications = fs.readFileSync(path.join(__dirname, 'release.0.7', 'populate_icun_classifications.sql'));
+  const populate_icun_classifications = fs.readFileSync(
+    path.join(__dirname, 'release.0.7', 'populate_icun_classifications.sql')
+  );
 
   const project_dapi_views = fs.readFileSync(path.join(__dirname, 'release.0.7', 'project_dapi_views.sql'));
-  
+
   await knex.raw(`
     -- set up spatial extensions
     ${create_spatial_extensions}
 
-    -- set up project management schema
-    create schema if not exists project_management;
-    set search_path = project_management, public;
+    -- set up biohub schema
+    create schema if not exists biohub;
+    set search_path = biohub, public;
 
     ${biohub_0_7}
     ${populate_user_identity_source}
@@ -54,7 +68,7 @@ export async function up(knex: Knex): Promise<void> {
     ${tr_project_funding_source}
 
     -- populate look up tables
-    set search_path = project_management;
+    set search_path = biohub;
     ${populate_first_nations}
     ${populate_climate_change_initiatives}
     ${populate_management_action_type}
@@ -72,13 +86,13 @@ export async function up(knex: Knex): Promise<void> {
     -- setup api user
     create user ${DB_USER_API} password '${DB_USER_API_PASS}';
     grant usage on schema biohub_dapi_v1 to ${DB_USER_API};
-    grant usage on schema project_management to ${DB_USER_API};
+    grant usage on schema biohub to ${DB_USER_API};
     grant all on all tables in schema biohub_dapi_v1 to ${DB_USER_API};
     alter DEFAULT PRIVILEGES in SCHEMA biohub_dapi_v1 grant ALL on tables to ${DB_USER_API};
     --grant postgis_reader to ${DB_USER_API};
-    alter role ${DB_USER_API} set search_path to biohub_dapi_v1, project_management, public, topology;
+    alter role ${DB_USER_API} set search_path to biohub_dapi_v1, biohub, public, topology;
 
-    set search_path = project_management;
+    set search_path = biohub;
     grant execute on function api_set_context(idir_user_id system_user.user_identifier%type, bceid_user_id system_user.user_identifier%type) to ${DB_USER_API};
 
   `);
