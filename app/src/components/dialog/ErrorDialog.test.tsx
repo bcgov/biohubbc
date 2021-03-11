03a25a5d93b5114af51e6b098edca7197d30e11b
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-03-11 12:02:28 -0800

    fix snapshot test

--- app/src/components/dialog/ErrorDialog.test.tsx
+++ app/src/components/dialog/ErrorDialog.test.tsx
@@ -3,32 +3,36 @@ import React from 'react';
 import { ErrorDialog } from './ErrorDialog';
 
 describe('ErrorDialog', () => {
-  it('matches the snapshot with no error message', () => {
-    const { asFragment } = render(
-      <ErrorDialog
-        dialogTitle="This is dialog title"
-        dialogText="This is dialog text"
-        open={true}
-        onClose={() => jest.fn()}
-        onOk={() => jest.fn()}
-      />
+  it('matches the snapshot with no error message', async () => {
+    const { baseElement } = render(
+      <div id="root">
+        <ErrorDialog
+          dialogTitle="This is dialog title"
+          dialogText="This is dialog text"
+          open={true}
+          onClose={() => jest.fn()}
+          onOk={() => jest.fn()}
+        />
+      </div>
     );
 
-    expect(asFragment()).toMatchSnapshot();
+    expect(baseElement).toMatchSnapshot();
   });
 
   it('matches the snapshot with an error message', () => {
-    const { asFragment } = render(
-      <ErrorDialog
-        dialogTitle="This is dialog title"
-        dialogText="This is dialog text"
-        dialogError="This is dialog error"
-        open={true}
-        onClose={() => jest.fn()}
-        onOk={() => jest.fn()}
-      />
+    const { baseElement } = render(
+      <div id="root">
+        <ErrorDialog
+          dialogTitle="This is dialog title"
+          dialogText="This is dialog text"
+          dialogError="This is dialog error"
+          open={true}
+          onClose={() => jest.fn()}
+          onOk={() => jest.fn()}
+        />
+      </div>
     );
 
-    expect(asFragment()).toMatchSnapshot();
+    expect(baseElement).toMatchSnapshot();
   });
 });
