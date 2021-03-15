bb82f21c55ac093d1fc35fa98106274e8964fca9
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-03-15 14:26:48 -0700

    remove

--- app/src/features/projects/components/ProjectObjectivesForm.tsx
+++ app/src/features/projects/components/ProjectObjectivesForm.tsx
@@ -31,8 +31,6 @@ const ProjectObjectivesForm = () => {
 
   const { values, touched, errors, handleChange, handleSubmit } = formikProps;
 
-  console.log(values.objectives);
-
   return (
     <form onSubmit={handleSubmit}>
       <Grid container spacing={3}>
