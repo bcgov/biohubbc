5dd4cea64d3a6f011c6dbf79529f751fb7bd327c
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-03-15 16:18:30 -0700

    Remove unused code (#129)
    * remove unused code
    
    * remove more files unused
    
    * more

--- api/src/openapi/components/tags.ts
+++
@@ -1,8 +0,0 @@
-export const tags = {
-  description: 'Tags to idenfity the record',
-  type: 'array',
-  items: {
-    type: 'string',
-    enum: ['project', 'plant', 'animal', 'aquatic', 'terrestrial', 'invasive']
-  }
-};
