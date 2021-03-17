7bcf43f704cc8a8ba9583d0147e8557b00be291c
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-03-17 10:34:31 -0700

    more refactoring

--- app/src/features/projects/view/ProjectDetails.test.tsx
+++ app/src/features/projects/view/ProjectDetails.test.tsx
@@ -1,24 +1,8 @@
 import { render } from '@testing-library/react';
-import { IGetAllCodesResponse } from 'interfaces/useBioHubApi-interfaces';
+import { codes } from 'test-helpers/projectCodes';
 import { projectWithDetailsData } from 'test-helpers/projectWithDetailsData';
 import React from 'react';
 import ProjectDetails from './ProjectDetails';
-
-const codes: IGetAllCodesResponse = {
-  coordinator_agency: [],
-  management_action_type: [],
-  climate_change_initiative: [{ id: 1, name: 'climate code' }],
-  first_nations: [],
-  funding_source: [],
-  investment_action_category: [],
-  activity: [{ id: 1, name: 'activity code' }],
-  project_type: [],
-  region: [],
-  species: [],
-  iucn_conservation_action_level_1_classification: [],
-  iucn_conservation_action_level_2_subclassification: [],
-  iucn_conservation_action_level_3_subclassification: []
-};
 
 describe('ProjectDetails', () => {
   it('renders correctly', () => {
--- app/src/features/projects/view/components/GeneralInformation.test.tsx
+++ app/src/features/projects/view/components/GeneralInformation.test.tsx
@@ -1,24 +1,8 @@
 import { render } from '@testing-library/react';
 import { projectWithDetailsData } from 'test-helpers/projectWithDetailsData';
-import { IGetAllCodesResponse } from 'interfaces/useBioHubApi-interfaces';
+import { codes } from 'test-helpers/projectCodes';
 import React from 'react';
 import GeneralInformation from './GeneralInformation';
-
-const codes: IGetAllCodesResponse = {
-  coordinator_agency: [],
-  management_action_type: [],
-  climate_change_initiative: [{ id: 1, name: 'climate code' }],
-  first_nations: [],
-  funding_source: [],
-  investment_action_category: [],
-  activity: [{ id: 1, name: 'activity code' }],
-  project_type: [],
-  region: [],
-  species: [],
-  iucn_conservation_action_level_1_classification: [],
-  iucn_conservation_action_level_2_subclassification: [],
-  iucn_conservation_action_level_3_subclassification: []
-};
 
 describe('GeneralInformation', () => {
   it('renders correctly', () => {
