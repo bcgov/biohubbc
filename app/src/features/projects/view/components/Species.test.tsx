6e07df375118ade37df7f71fbf5895f1a4a7f386
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2021-03-25 16:34:14 -0700

    updated existing tests

--- app/src/features/projects/view/components/Species.test.tsx
+++ app/src/features/projects/view/components/Species.test.tsx
@@ -2,13 +2,23 @@ import { render } from '@testing-library/react';
 import React from 'react';
 import { getProjectForViewResponse } from 'test-helpers/project-helpers';
 import Species from './Species';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import { codes } from 'test-helpers/code-helpers';
+const history = createMemoryHistory();
 
 describe('Species', () => {
   it('renders correctly with default empty values', () => {
     const { asFragment } = render(
-      <Species
-        projectForViewData={{ ...getProjectForViewResponse, species: { focal_species: [], ancillary_species: [] } }}
-      />
+      <Router history={history}>
+        <Species
+          projectForViewData={{
+            ...getProjectForViewResponse,
+            species: { focal_species: [], ancillary_species: [] }
+          }}
+          codes={codes}
+        />
+      </Router>
     );
 
     expect(asFragment()).toMatchSnapshot();
@@ -16,19 +26,26 @@ describe('Species', () => {
 
   it('renders correctly with invalid null values', () => {
     const { asFragment } = render(
-      <Species
-        projectForViewData={{
-          ...getProjectForViewResponse,
-          species: { focal_species: (null as unknown) as string[], ancillary_species: (null as unknown) as string[] }
-        }}
-      />
+      <Router history={history}>
+        <Species
+          projectForViewData={{
+            ...getProjectForViewResponse,
+            species: { focal_species: (null as unknown) as string[], ancillary_species: (null as unknown) as string[] }
+          }}
+          codes={codes}
+        />
+      </Router>
     );
 
     expect(asFragment()).toMatchSnapshot();
   });
 
   it('renders correctly with existing species values', () => {
-    const { asFragment } = render(<Species projectForViewData={{ ...getProjectForViewResponse }} />);
+    const { asFragment } = render(
+      <Router history={history}>
+        <Species projectForViewData={{ ...getProjectForViewResponse }} codes={codes} />
+      </Router>
+    );
 
     expect(asFragment()).toMatchSnapshot();
   });
