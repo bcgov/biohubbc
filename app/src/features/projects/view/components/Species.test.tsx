dc3deef9ff90a0a2b5872c85a5693cbb67b51f0c
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2021-03-25 17:07:01 -0700

    tests for new functions

--- app/src/features/projects/view/components/Species.test.tsx
+++ app/src/features/projects/view/components/Species.test.tsx
@@ -1,11 +1,23 @@
-import { render } from '@testing-library/react';
+import { render, waitFor, fireEvent } from '@testing-library/react';
 import React from 'react';
 import { getProjectForViewResponse } from 'test-helpers/project-helpers';
 import Species from './Species';
 import { createMemoryHistory } from 'history';
 import { Router } from 'react-router-dom';
 import { codes } from 'test-helpers/code-helpers';
+import { useBiohubApi } from 'hooks/useBioHubApi';
 const history = createMemoryHistory();
+
+jest.mock('../../../../hooks/useBioHubApi');
+const mockUseBiohubApi = {
+  project: {
+    getProjectForUpdate: jest.fn<Promise<object>, []>()
+  }
+};
+
+const mockBiohubApi = ((useBiohubApi as unknown) as jest.Mock<typeof mockUseBiohubApi>).mockReturnValue(
+  mockUseBiohubApi
+);
 
 describe('Species', () => {
   it('renders correctly with default empty values', () => {
@@ -48,5 +60,73 @@ describe('Species', () => {
     );
 
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('editing the partnerships works in the dialog', async () => {
+    mockBiohubApi().project.getProjectForUpdate.mockResolvedValue({
+      species: {
+        focal_species: ['species 1', 'species 2'],
+        ancillary_species: ['species 1', 'species 2']
+      }
+    });
+
+    const { getByText } = render(
+      <Router history={history}>
+        <Species projectForViewData={{ ...getProjectForViewResponse }} codes={codes} />
+      </Router>
+    );
+    await waitFor(() => {
+      expect(getByText('Species')).toBeVisible();
+    });
+    fireEvent.click(getByText('EDIT'));
+    await waitFor(() => {
+      expect(getByText('Edit focal and ancillary species')).toBeVisible();
+    });
+
+    fireEvent.click(getByText('Cancel'));
+
+    await waitFor(() => {
+      expect(getByText('Edit focal and ancillary species')).not.toBeVisible();
+    });
+
+    fireEvent.click(getByText('EDIT'));
+
+    await waitFor(() => {
+      expect(getByText('Edit focal and ancillary species')).toBeVisible();
+    });
+
+    fireEvent.click(getByText('Save Changes'));
+
+    await waitFor(() => {
+      expect(history.location.pathname).toEqual(`/projects/${getProjectForViewResponse.id}/details`);
+    });
+  });
+
+  it('displays an error dialog when fetching the update data fails', async () => {
+    mockBiohubApi().project.getProjectForUpdate.mockResolvedValue({
+      partnerships: null
+    });
+
+    const { getByText } = render(
+      <Router history={history}>
+        <Species projectForViewData={{ ...getProjectForViewResponse }} codes={codes} />
+      </Router>
+    );
+
+    await waitFor(() => {
+      expect(getByText('Species')).toBeVisible();
+    });
+
+    fireEvent.click(getByText('EDIT'));
+
+    await waitFor(() => {
+      expect(getByText('Failed to Fetch Edit Data')).toBeVisible();
+    });
+
+    fireEvent.click(getByText('Ok'));
+
+    await waitFor(() => {
+      expect(getByText('Failed to Fetch Edit Data')).not.toBeVisible();
+    });
   });
 });
