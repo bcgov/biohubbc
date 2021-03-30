ad6b8e52b14330cfb0debb611f5d2b81382fb59f
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2021-03-30 14:21:50 -0700

    test for the DollarAmount Field

---
+++ app/src/components/fields/DollarAmountField.test.tsx
@@ -0,0 +1,24 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import DollarAmountField from './DollarAmountField';
+import { Formik } from 'formik';
+
+describe('DollarAmountField', () => {
+  it('matches the snapshot', () => {
+    const { asFragment } = render(
+      <Formik initialValues={[]} onSubmit={async () => {}}>
+        {() => (
+          <DollarAmountField
+            id="id"
+            label="label"
+            required= {true}
+            name= "name"
+            value= {123}
+          />
+        )}
+      </Formik>
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
