38e068e6502a639a7b33ac9dc4f20a288fc239da
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-03-30 14:43:26 -0700

    reduce files changed

--- app/src/features/projects/view/components/IUCNClassification.test.tsx
+++ app/src/features/projects/view/components/IUCNClassification.test.tsx
@@ -161,7 +161,7 @@ describe('IUCNClassification', () => {
     });
   });
 
-  it('shows error dialog with API error message when getting species data for update fails', async () => {
+  it('shows error dialog with API error message when getting IUCN data for update fails', async () => {
     mockBiohubApi().project.getProjectForUpdate = jest.fn(() => Promise.reject(new Error('API Error is Here')));
 
     const { getByText, queryByText, getAllByRole } = renderContainer();
@@ -185,7 +185,7 @@ describe('IUCNClassification', () => {
     });
   });
 
-  it('shows error dialog with API error message when updating species data fails', async () => {
+  it('shows error dialog with API error message when updating IUCN data fails', async () => {
     mockBiohubApi().project.getProjectForUpdate.mockResolvedValue({
       iucn: {
         classificationDetails: [
@@ -226,73 +226,6 @@ describe('IUCNClassification', () => {
     // Get the backdrop, then get the firstChild because this is where the event listener is attached
     //@ts-ignore
     fireEvent.click(getAllByRole('presentation')[0].firstChild);
-
-    await waitFor(() => {
-      expect(queryByText('API Error is Here')).toBeNull();
-    });
-  });
-
-  it('shows error dialog with API error message when getting species data for update fails', async () => {
-    mockBiohubApi().project.getProjectForUpdate = jest.fn(() => Promise.reject(new Error('API Error is Here')));
-
-    const { getByText, queryByText } = renderContainer();
-
-    await waitFor(() => {
-      expect(getByText('IUCN Classification')).toBeVisible();
-    });
-
-    fireEvent.click(getByText('EDIT'));
-
-    await waitFor(() => {
-      expect(queryByText('API Error is Here')).toBeInTheDocument();
-    });
-
-    fireEvent.click(getByText('Ok'));
-
-    await waitFor(() => {
-      expect(queryByText('API Error is Here')).toBeNull();
-    });
-  });
-
-  it('shows error dialog with API error message when updating species data fails', async () => {
-    mockBiohubApi().project.getProjectForUpdate.mockResolvedValue({
-      iucn: {
-        classificationDetails: [
-          {
-            classification: 1,
-            subClassification1: 1,
-            subClassification2: 1
-          }
-        ]
-      }
-    });
-    mockBiohubApi().project.updateProject = jest.fn(() => Promise.reject(new Error('API Error is Here')));
-
-    const { getByText, queryByText } = renderContainer();
-
-    await waitFor(() => {
-      expect(getByText('IUCN Classification')).toBeVisible();
-    });
-
-    fireEvent.click(getByText('EDIT'));
-
-    await waitFor(() => {
-      expect(mockBiohubApi().project.getProjectForUpdate).toBeCalledWith(getProjectForViewResponse.id, [
-        UPDATE_GET_ENTITIES.iucn
-      ]);
-    });
-
-    await waitFor(() => {
-      expect(getByText('Edit IUCN Classification')).toBeVisible();
-    });
-
-    fireEvent.click(getByText('Save Changes'));
-
-    await waitFor(() => {
-      expect(queryByText('API Error is Here')).toBeInTheDocument();
-    });
-
-    fireEvent.click(getByText('Ok'));
 
     await waitFor(() => {
       expect(queryByText('API Error is Here')).toBeNull();
