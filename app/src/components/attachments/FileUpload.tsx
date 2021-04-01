df09e7c996e628760d02e6fc0786a4cd9d9967f3
Nick Phura <nickphura@gmail.com>
2021-04-01 14:38:02 -0700

    BHBC-964: Initial structure

---
+++ app/src/components/attachments/FileUpload.tsx
@@ -0,0 +1,91 @@
+import { Box, Button, makeStyles, Theme, Typography } from '@material-ui/core';
+import { useBiohubApi } from 'hooks/useBioHubApi';
+import { DropzoneArea } from 'material-ui-dropzone';
+import React, { useState } from 'react';
+
+const useStyles = makeStyles((Theme: Theme) => ({
+  actionButton: {
+    minWidth: '6rem',
+    '& + button': {
+      marginLeft: '0.5rem'
+    }
+  }
+}));
+
+export interface IFileUploadProps {
+  projectId: number;
+}
+
+export const FileUpload: React.FC<IFileUploadProps> = (props) => {
+  const classes = useStyles();
+
+  const biohubApi = useBiohubApi();
+
+  const [files, setFiles] = useState<File[]>([]);
+  const [dropzoneText, setDropzoneText] = useState<string>('Select files');
+  const [dropzoneInstanceKey, setDropzoneInstanceKey] = useState<number>(0);
+
+  const handleUpload = async () => {
+    if (!files || !files.length) {
+      setDropzoneText('Select files');
+      return;
+    }
+
+    try {
+      setDropzoneText('Uploading ...');
+      const uploadResponse = await biohubApi.project.uploadProjectArtifacts(props.projectId, files);
+
+      if (!uploadResponse) {
+        setDropzoneText('Failed to upload, please try again');
+      } else {
+        setDropzoneText('Success. ' + files.length + ' file' + (files.length > 1 ? 's' : '') + ' uploaded');
+
+        // clear the files state
+        setFiles([]);
+
+        // implement the hack to reset the internal state of dropzone
+        // https://github.com/react-dropzone/react-dropzone/issues/881
+        setDropzoneInstanceKey(dropzoneInstanceKey > 0 ? 0 : 1);
+      }
+    } catch (error) {
+      console.log('Error', error);
+    }
+  };
+
+  const handleDeleteFile = async (f: File) => {
+    let newFiles = files;
+
+    setFiles(
+      newFiles.filter(function (elem: File) {
+        return elem !== f;
+      })
+    );
+  };
+
+  return (
+    <Box>
+      <Box key={dropzoneInstanceKey}>
+        <DropzoneArea
+          dropzoneText={dropzoneText}
+          filesLimit={10}
+          onChange={(e) => {
+            setFiles(e);
+          }}
+          onDelete={(f) => {
+            handleDeleteFile(f);
+          }}
+          showFileNames={true}
+          useChipsForPreview={true}
+          showAlerts={['error']}
+        />
+      </Box>
+      <Box>
+        <Button variant="contained" color="primary" onClick={handleUpload} className={classes.actionButton}>
+          <Typography variant="body1">Upload</Typography>
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default FileUpload;
--- app/src/features/projects/view/ProjectAttachments.tsx
+++ app/src/features/projects/view/ProjectAttachments.tsx
@@ -1,30 +1,12 @@
-import { Box, Button, CircularProgress, makeStyles, Paper, Typography } from '@material-ui/core';
-import { IErrorDialogProps } from 'components/dialog/ErrorDialog';
-import { UploadProjectArtifactsI18N } from 'constants/i18n';
-import { APIError } from 'hooks/api/useAxios';
+import { Box, Button, CircularProgress, Paper, Typography } from '@material-ui/core';
+import { mdiTrayPlus } from '@mdi/js';
+import Icon from '@mdi/react';
+import FileUpload from 'components/attachments/FileUpload';
+import ComponentDialog from 'components/dialog/ComponentDialog';
 import { useBiohubApi } from 'hooks/useBioHubApi';
 import { IGetProjectForViewResponse } from 'interfaces/useProjectApi.interface';
-import { DropzoneArea } from 'material-ui-dropzone';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-
-const useStyles = makeStyles((theme) => ({
-  breadCrumbLink: {
-    display: 'flex',
-    alignItems: 'center',
-    cursor: 'pointer'
-  },
-  breadCrumbLinkIcon: {
-    marginRight: '0.25rem'
-  },
-  actionButton: {
-    marginTop: theme.spacing(1),
-    minWidth: '6rem',
-    '& + button': {
-      marginLeft: '0.5rem'
-    }
-  }
-}));
 
 export interface IProjectAttachmentsProps {
   projectForViewData: IGetProjectForViewResponse;
@@ -38,75 +20,17 @@ export interface IProjectAttachmentsProps {
 const ProjectAttachments: React.FC<IProjectAttachmentsProps> = () => {
   const urlParams = useParams();
 
+  const projectId = urlParams['id'];
+
   const biohubApi = useBiohubApi();
-
-  const classes = useStyles();
-
-  const [files, setFiles] = useState<File[]>([]);
-  const [dropzoneText, setDropzoneText] = useState<string>('Select files');
-  const [dropzoneInstanceKey, setDropzoneInstanceKey] = useState<number>(0);
-
-  // Whether or not to show the text dialog
-  const [openErrorDialogProps, setOpenErrorDialogProps] = useState<IErrorDialogProps>({
-    dialogTitle: UploadProjectArtifactsI18N.uploadErrorTitle,
-    dialogText: UploadProjectArtifactsI18N.uploadErrorText,
-    open: false,
-    onClose: () => {
-      setOpenErrorDialogProps({ ...openErrorDialogProps, open: false });
-    },
-    onOk: () => {
-      setOpenErrorDialogProps({ ...openErrorDialogProps, open: false });
-    }
-  });
 
   const [project, setProject] = useState<IGetProjectForViewResponse | null>(null);
 
-  const handleUpload = async () => {
-    const showErrorDialog = (textDialogProps?: Partial<IErrorDialogProps>) => {
-      setOpenErrorDialogProps({ ...openErrorDialogProps, ...textDialogProps, open: true });
-    };
-
-    if (!files || !files.length) {
-      setDropzoneText('Select files');
-      return;
-    }
-
-    try {
-      setDropzoneText('Uploading ...');
-      const uploadResponse = await biohubApi.project.uploadProjectArtifacts(urlParams['id'], files);
-
-      if (!uploadResponse) {
-        setDropzoneText('Failed to upload, please try again');
-        showErrorDialog({ dialogError: 'Server responded with null.' });
-      } else {
-        setDropzoneText('Success. ' + files.length + ' file' + (files.length > 1 ? 's' : '') + ' uploaded');
-
-        // clear the files state
-        setFiles([]);
-
-        // implement the hack to reset the internal state of dropzone
-        // https://github.com/react-dropzone/react-dropzone/issues/881
-        setDropzoneInstanceKey(dropzoneInstanceKey > 0 ? 0 : 1);
-      }
-    } catch (error) {
-      const apiError = error as APIError;
-      showErrorDialog({ dialogError: apiError?.message, dialogErrorDetails: apiError?.errors });
-    }
-  };
-
-  const handleDeleteFile = async (f: File) => {
-    let newFiles = files;
-
-    setFiles(
-      newFiles.filter(function (elem: File) {
-        return elem !== f;
-      })
-    );
-  };
+  const [openUploadAttachments, setOpenUploadAttachments] = useState(false);
 
   useEffect(() => {
     const getProject = async () => {
-      const projectResponse = await biohubApi.project.getProjectForView(urlParams['id']);
+      const projectResponse = await biohubApi.project.getProjectForView(projectId);
 
       if (!projectResponse) {
         // TODO error handling/messaging
@@ -119,7 +43,7 @@ const ProjectAttachments: React.FC<IProjectAttachmentsProps> = () => {
     if (!project) {
       getProject();
     }
-  }, [urlParams, biohubApi, project, files]);
+  }, [urlParams, biohubApi, project]);
 
   if (!project) {
     return <CircularProgress></CircularProgress>;
@@ -127,38 +51,28 @@ const ProjectAttachments: React.FC<IProjectAttachmentsProps> = () => {
 
   return (
     <>
+      <ComponentDialog
+        open={openUploadAttachments}
+        dialogTitle="Upload Attachments"
+        onClose={() => setOpenUploadAttachments(false)}>
+        <FileUpload projectId={projectId} />
+      </ComponentDialog>
       <Box mb={5}>
-        <Typography variant="h2">Project Attachments</Typography>
-      </Box>
-      <Box mb={3}>
-        <Paper>
-          <Box p={3}>Project attachments component 1 placeholder</Box>
-        </Paper>
-      </Box>
-      <Box mb={3}>
-        <Paper>
-          <Box p={3}>
-            <Box key={dropzoneInstanceKey}>
-              <DropzoneArea
-                dropzoneText={dropzoneText}
-                filesLimit={10}
-                onChange={(e) => {
-                  setFiles(e);
-                }}
-                onDelete={(f) => {
-                  handleDeleteFile(f);
-                }}
-                showFileNames={true}
-                useChipsForPreview={true}
-                showAlerts={['error']}
-              />
-            </Box>
-            <Box>
-              <Button variant="contained" color="primary" onClick={handleUpload} className={classes.actionButton}>
-                <Typography variant="body1">Upload</Typography>
-              </Button>
-            </Box>
+        <Box display="flex" justifyContent="space-between">
+          <Box>
+            <Typography variant="h2">Project Attachments</Typography>
           </Box>
+          <Box>
+            <Button variant="outlined" onClick={() => setOpenUploadAttachments(true)}>
+              <Icon path={mdiTrayPlus} size={1} />
+              <Typography>Upload</Typography>
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+      <Box mb={3}>
+        <Paper>
+          <Box p={3}>Attachments list placeholder</Box>
         </Paper>
       </Box>
     </>
