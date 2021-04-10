90279e3519cf3008d62baf818cba8044de2bbaf2
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-04-09 17:02:50 -0700

    Add missing attachments tests (#217)
    * backend tests for attachments
    
    * frontend tests for attachments
    
    * remove
    
    * fixes
    
    * coverage

---
+++ api/src/utils/file-utils.test.ts
@@ -0,0 +1,19 @@
+import { expect } from 'chai';
+import { describe } from 'mocha';
+import { deleteFileFromS3, getS3SignedURL } from './file-utils';
+
+describe('deleteFileFromS3', () => {
+  it('returns null when no key specified', async () => {
+    const result = await deleteFileFromS3((null as unknown) as string);
+
+    expect(result).to.be.null;
+  });
+});
+
+describe('getS3SignedURL', () => {
+  it('returns null when no key specified', async () => {
+    const result = await getS3SignedURL((null as unknown) as string);
+
+    expect(result).to.be.null;
+  });
+});
