a36b79aaf5148362a7656e5f430943d5ac636090
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-04-14 15:46:54 -0700

    add missing tests (#234)
    Co-authored-by: Nick Phura <Nick.Phura@quartech.com>

--- api/src/utils/shared-api-docs.test.ts
+++ api/src/utils/shared-api-docs.test.ts
@@ -1,10 +1,32 @@
 import { expect } from 'chai';
 import { describe } from 'mocha';
-import { getAttachmentApiDocObject } from './shared-api-docs';
+import {
+  getAttachmentApiDocObject,
+  deleteFundingSourceApiDocObject,
+  addFundingSourceApiDocObject
+} from './shared-api-docs';
 
 describe('getAttachmentApiResponseObject', () => {
   it('returns a valid response object', () => {
     const result = getAttachmentApiDocObject('basic', 'success');
+
+    expect(result).to.not.be.null;
+    expect(result?.description).to.equal('basic');
+  });
+});
+
+describe('deleteFundingSourceApiDocObject', () => {
+  it('returns a valid response object', () => {
+    const result = deleteFundingSourceApiDocObject('basic', 'success');
+
+    expect(result).to.not.be.null;
+    expect(result?.description).to.equal('basic');
+  });
+});
+
+describe('addFundingSourceApiDocObject', () => {
+  it('returns a valid response object', () => {
+    const result = addFundingSourceApiDocObject('basic', 'success');
 
     expect(result).to.not.be.null;
     expect(result?.description).to.equal('basic');
