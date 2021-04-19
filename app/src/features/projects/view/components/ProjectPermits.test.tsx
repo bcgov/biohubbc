e064b5b740f917d75f6721d77cba8f025488cce8
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-04-18 19:23:49 -0700

    add test case

--- app/src/features/projects/view/components/ProjectPermits.test.tsx
+++ app/src/features/projects/view/components/ProjectPermits.test.tsx
@@ -5,8 +5,25 @@ import { getProjectForViewResponse } from 'test-helpers/project-helpers';
 import ProjectPermits from './ProjectPermits';
 
 describe('ProjectPermits', () => {
-  it('renders correctly', () => {
+  it('renders correctly with sampling conducted true', () => {
     const { asFragment } = render(<ProjectPermits projectForViewData={getProjectForViewResponse} codes={codes} />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('renders correctly with sampling conducted false', () => {
+    const { asFragment } = render(
+      <ProjectPermits
+        projectForViewData={{
+          ...getProjectForViewResponse,
+          permit: {
+            ...getProjectForViewResponse.permit,
+            permits: [{ permit_number: '123', sampling_conducted: false }]
+          }
+        }}
+        codes={codes}
+      />
+    );
 
     expect(asFragment()).toMatchSnapshot();
   });
