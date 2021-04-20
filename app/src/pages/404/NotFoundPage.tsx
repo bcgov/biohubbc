8b28e59844a815e9b50a641c30c6698cf69ca84f
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-04-19 19:11:37 -0700

    User denied access to BioHub (#245)
    improved access-denied page, and page-not-found page

--- app/src/pages/404/NotFoundPage.tsx
+++ app/src/pages/404/NotFoundPage.tsx
@@ -1,17 +1,27 @@
 import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { mdiHelpCircleOutline } from '@mdi/js';
+import Icon from '@mdi/react';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { useHistory } from 'react-router';
 
 const NotFoundPage = () => {
+  const history = useHistory();
+
   return (
     <Container>
-      <Grid container direction="row">
-        <Grid item>
-          <h1>Page not found</h1>
-          <Link to="/projects">Go back to the projects page</Link>
-        </Grid>
-      </Grid>
+      <Box pt={6} textAlign="center">
+        <Icon path={mdiHelpCircleOutline} size={2} color="#ff5252" />
+        <h1>Page Not Found</h1>
+        <Typography>Sorry, the page you are trying to access does not exist.</Typography>
+        <Box pt={4}>
+          <Button onClick={() => history.push('/')} type="submit" size="large" variant="contained" color="primary">
+            Return Home
+          </Button>
+        </Box>
+      </Box>
     </Container>
   );
 };
