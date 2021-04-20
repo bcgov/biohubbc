bffc7f360246920d85cb3b0699d8ac1ca813e262
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2021-04-20 10:55:57 -0700

    access-request linked to the forbidden page

--- app/src/pages/403/AccessDenied.tsx
+++ app/src/pages/403/AccessDenied.tsx
@@ -5,8 +5,10 @@ import { mdiAlertCircleOutline } from '@mdi/js';
 import Icon from '@mdi/react';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import { useHistory } from 'react-router';
 
 const AccessDenied = () => {
+  const history = useHistory();
   return (
     <Container>
       <Box pt={6} textAlign="center">
@@ -14,7 +16,12 @@ const AccessDenied = () => {
         <h1>Access Denied</h1>
         <Typography>You do not have permissions to access this application.</Typography>
         <Box pt={4}>
-          <Button type="submit" size="large" variant="contained" color="primary">
+          <Button
+            onClick={() => history.push('/access-request')}
+            type="submit"
+            size="large"
+            variant="contained"
+            color="primary">
             Request Access
           </Button>
         </Box>
