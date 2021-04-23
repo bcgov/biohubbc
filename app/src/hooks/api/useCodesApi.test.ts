baa1665aeebae8468eb31c02cbfac6146db3e5b7
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-04-23 16:56:47 -0700

    awesome tests

---
+++ app/src/hooks/api/useCodesApi.test.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import useCodesApi from './useCodesApi';
+
+describe('useCodesApi', () => {
+  let mock: any;
+
+  beforeEach(() => {
+    mock = new MockAdapter(axios);
+  });
+
+  afterEach(() => {
+    mock.restore();
+  });
+
+  it('getAllCodeSets works as expected', async () => {
+    mock.onGet('/api/codes/').reply(200, {
+      system_roles: [{ id: 1, name: 'Role 1' }],
+      regional_offices: [{ id: 1, name: 'Office 1' }]
+    });
+
+    const result = await useCodesApi(axios).getAllCodeSets();
+
+    expect(result.system_roles).toEqual([{ id: 1, name: 'Role 1' }]);
+    expect(result.regional_offices).toEqual([{ id: 1, name: 'Office 1' }]);
+  });
+});
