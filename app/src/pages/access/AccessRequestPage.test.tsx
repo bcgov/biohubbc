2b22b5dbd118014c1725ad4b747317b74159b862
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-04-23 11:36:03 -0700

    add test

--- app/src/pages/access/AccessRequestPage.test.tsx
+++ app/src/pages/access/AccessRequestPage.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
 import React from 'react';
 import { Router } from 'react-router';
@@ -15,5 +15,47 @@ describe('AccessRequestPage', () => {
     );
 
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  describe('Logout functionality', (): void => {
+    const { location } = window;
+
+    beforeAll((): void => {
+      // @ts-ignore
+      delete window.location;
+
+      // @ts-ignore
+      window.location = {
+        href: ''
+      };
+    });
+
+    afterAll((): void => {
+      window.location = location;
+    });
+
+    it('should change the location.href appropriately on logout success', async () => {
+      const target = 'https://example.com/';
+
+      window.location.href = target;
+
+      expect(window.location.href).toBe(target);
+
+      const { getByText } = render(
+        <Router history={history}>
+          <AccessRequestPage />
+        </Router>
+      );
+
+      fireEvent.click(getByText('Log out'));
+
+      await waitFor(() => {
+        expect(window.location.href).toEqual(
+          'https://dev.oidc.gov.bc.ca/auth/realms/35r1iman/protocol/openid-connect/logout?redirect_uri=' +
+            encodeURI(window.location.origin) +
+            '%2Faccess-request'
+        );
+      });
+    });
   });
 });
