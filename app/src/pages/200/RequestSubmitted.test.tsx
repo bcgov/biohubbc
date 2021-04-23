05b26a8b5dcfc95c468d6573aef612e593dec0ef
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-04-23 11:14:46 -0700

    awesome fe tests

--- app/src/pages/200/RequestSubmitted.test.tsx
+++ app/src/pages/200/RequestSubmitted.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import React from 'react';
 import RequestSubmitted from './RequestSubmitted';
 
@@ -7,5 +7,43 @@ describe('RequestSubmitted', () => {
     const { asFragment } = render(<RequestSubmitted />);
 
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  describe('Logout functionality', (): void => {
+    const { location } = window;
+
+    beforeAll((): void => {
+      // @ts-ignore
+      delete window.location;
+
+      // @ts-ignore
+      window.location = {
+        href: ''
+      };
+    });
+
+    afterAll((): void => {
+      window.location = location;
+    });
+
+    it('should change the location.href appropriately on logout success', async () => {
+      const target = 'https://example.com/';
+
+      window.location.href = target;
+
+      expect(window.location.href).toBe(target);
+
+      const { getByText } = render(<RequestSubmitted />);
+
+      fireEvent.click(getByText('Logout'));
+
+      await waitFor(() => {
+        expect(window.location.href).toEqual(
+          'https://dev.oidc.gov.bc.ca/auth/realms/35r1iman/protocol/openid-connect/logout?redirect_uri=' +
+            encodeURI(window.location.origin) +
+            '%2Faccess-request'
+        );
+      });
+    });
   });
 });
