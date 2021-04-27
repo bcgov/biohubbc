a942ba23314aba44c309088946764a52a0bd60a9
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2021-04-27 10:02:53 -0700

    first pass

--- api/src/security/auth-utils.test.ts
+++ api/src/security/auth-utils.test.ts
@@ -1,6 +1,14 @@
-import { expect } from 'chai';
+import chai,{ expect } from 'chai';
 import { describe } from 'mocha';
-import { userHasValidSystemRoles } from './auth-utils';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import { userHasValidSystemRoles, authorize } from './auth-utils';
+import * as auth_utils from './auth-utils';
+// import { getDBConnection } from '../database/db';
+// import pg from 'pg';
+//import { getDBConnection, IDBConnection } from '../database/db';
+
+chai.use(sinonChai);
 
 describe('userHasValidSystemRoles', () => {
   describe('validSystemRoles is a string', () => {
@@ -111,3 +119,78 @@ describe('userHasValidSystemRoles', () => {
     });
   });
 });
+
+//const assert = require('assert');
+
+describe('authorize', function () {
+  //const getSystemUserStub = sinon.stub(auth_utils,'getSystemUser');
+
+  beforeEach(function () {
+    // const fake = {
+    //   a: sinon.stub().resolves({ rows: [] }),
+    //   b: sinon.stub().resolves({ hello: 'world' })
+    // };
+    //getSystemUserStub = sinon.stub(auth_utils,'getSystemUser');
+  });
+
+  afterEach(function () {
+    //getSystemUserStub.restore;
+  });
+
+  it('throws HTTP403 when keycloak_token is empty', async function () {
+    try {
+      await authorize({ keycloak_token: '' }, ['abc']);
+    } catch (e) {
+      expect(JSON.stringify(e)).to.contain('Access Denied');
+    }
+  });
+
+  it('returns true without scopes', async function () {
+    const ret = await authorize({ keycloak_token: 'some token' }, []);
+    expect(ret).to.be.true;
+  });
+
+
+
+  it('throws HTTP403 when stubbed getSystemUser returns null', async function () {
+    const getSystemUserStub = sinon.stub(auth_utils, 'getSystemUser');
+
+    try {
+      await authorize({ keycloak_token: 'some token' }, ['abc']);
+    } catch (e) {
+      expect(JSON.stringify(e)).to.contain('Access Denied');
+    }
+
+    expect(getSystemUserStub).to.have.been.calledWith('some token');
+    expect(getSystemUserStub).to.have.been.calledOnce;
+    expect(getSystemUserStub).is.not.null;
+  });
+});
+
+
+
+
+
+
+
+
+
+// describe('a test', () => {
+//   afterEach(() => {
+//     sinon.restore();
+//   });
+//   it('should pass', async () => {
+//     const mPool = { query: sinon.stub().resolves({ rows: [] }) };
+//     const poolStub = sinon.stub(pg, 'Pool').callsFake(() => mPool);
+//     const connection = getDBConnection({ token: 'keycloakToken' });
+//     const actual = await connection.systemUserId;
+//     const open = await connection.open;
+//     const result = await connection.query('SELECT now();');
+//     expect(actual).is.not.null;
+//     expect(open).is.not.null;
+//     expect(result).is.not.null;
+//     //sinon.assert.calledOnce(poolStub);
+//     expect(poolStub).not.to.be.called;
+//     //sinon.assert.calledOnce(mPool.query);
+//   });
+// });
