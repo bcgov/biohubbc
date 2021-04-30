5517752e9781c3a7eb8e66aa732c15fc9f20a02d
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-04-30 13:45:30 -0700

    wip

--- database/src/migrations/release.0.12/populate_management_action_type.sql
+++
@@ -1,3 +0,0 @@
--- management_action_type.sql
-insert into management_action_type (name, record_effective_date) VALUES ('Recovery Action', now());
-insert into management_action_type (name, record_effective_date) VALUES ('Wildlife Management Action', now());
--- database/src/migrations/release.0.12/populate_project_type.sql
+++
@@ -1,5 +0,0 @@
--- populate_project_type.sql
-insert into project_type (name, record_effective_date) values ('Fisheries', now());
-insert into project_type (name, record_effective_date) values ('Wildlife', now());
-insert into project_type (name, record_effective_date) values ('Aquatic Habitat', now());
-insert into project_type (name, record_effective_date) values ('Terrestrial Habitat', now());
