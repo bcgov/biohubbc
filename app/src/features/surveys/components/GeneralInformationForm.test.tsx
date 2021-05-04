6d6a75b2bd5e1fdad8d0dfb9e1147128593e15ce
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-05-04 14:03:59 -0700

    fix tests

--- app/src/features/surveys/components/GeneralInformationForm.test.tsx
+++ app/src/features/surveys/components/GeneralInformationForm.test.tsx
@@ -5,26 +5,16 @@ import GeneralInformationForm, {
   GeneralInformationYupSchema
 } from 'features/surveys/components/GeneralInformationForm';
 import React from 'react';
-import { IMultiAutocompleteFieldOption } from 'components/fields/MultiAutocompleteFieldVariableSize';
 
 const handleSaveAndNext = jest.fn();
 
-const species: IMultiAutocompleteFieldOption[] = [
-  {
-    value: 1,
-    label: 'species 1'
-  },
-  {
-    value: 2,
-    label: 'species 2'
-  }
-];
+const species: string[] = ['species 1', 'species 2'];
 
 const generalInformationFilledValues = {
   survey_name: 'survey name',
   start_date: '2021-04-09 11:53:53',
   end_date: '2021-05-10 11:53:53',
-  species: ['species 1', 'species 2'],
+  species: 'species 1',
   survey_purpose: 'purpose',
   biologist_first_name: 'first',
   biologist_last_name: 'last'
