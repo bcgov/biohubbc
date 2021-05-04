f154b7f5c87df5c61abeca3d0be919e39413fe82
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-05-04 16:08:34 -0700

    tests galore

--- app/src/hooks/api/useProjectApi.test.ts
+++ app/src/hooks/api/useProjectApi.test.ts
@@ -1,5 +1,14 @@
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
+import { IProjectCoordinatorForm } from 'features/projects/components/ProjectCoordinatorForm';
+import { IProjectDetailsForm } from 'features/projects/components/ProjectDetailsForm';
+import { IProjectFundingForm } from 'features/projects/components/ProjectFundingForm';
+import { IProjectIUCNForm } from 'features/projects/components/ProjectIUCNForm';
+import { IProjectLocationForm } from 'features/projects/components/ProjectLocationForm';
+import { IProjectObjectivesForm } from 'features/projects/components/ProjectObjectivesForm';
+import { IProjectPartnershipsForm } from 'features/projects/components/ProjectPartnershipsForm';
+import { IProjectPermitForm } from 'features/projects/components/ProjectPermitForm';
+import { IProjectSpeciesForm } from 'features/projects/components/ProjectSpeciesForm';
 import { ICreateProjectSurveyRequest, UPDATE_GET_ENTITIES } from 'interfaces/useProjectApi.interface';
 import { getProjectForViewResponse } from 'test-helpers/project-helpers';
 import useProjectApi from './useProjectApi';
@@ -169,5 +178,56 @@ describe('useProjectApi', () => {
     const result = await useProjectApi(axios).uploadProjectAttachments(projectId, [file]);
 
     expect(result).toEqual(['result 1', 'result 2']);
+  });
+
+  it('createPermitNoSampling works as expected', async () => {
+    const permitData = {
+      permit: {
+        permits: [
+          {
+            permit_number: 'number',
+            permit_type: 'type',
+            sampling_conducted: 'true'
+          }
+        ]
+      },
+      coordinator: {
+        first_name: 'first',
+        last_name: 'last',
+        email_address: 'email@example.com',
+        coordinator_agency: 'agency',
+        share_contact_details: 'true'
+      }
+    };
+
+    mock.onPost('/api/permit-no-sampling').reply(200, {
+      ids: [1, 2, 3]
+    });
+
+    const result = await useProjectApi(axios).createPermitNoSampling(permitData);
+
+    expect(result).toEqual({ ids: [1, 2, 3] });
+  });
+
+  it('createProject works as expected', async () => {
+    const projectData = {
+      coordinator: (null as unknown) as IProjectCoordinatorForm,
+      permit: (null as unknown) as IProjectPermitForm,
+      project: (null as unknown) as IProjectDetailsForm,
+      objectives: (null as unknown) as IProjectObjectivesForm,
+      species: (null as unknown) as IProjectSpeciesForm,
+      location: (null as unknown) as IProjectLocationForm,
+      iucn: (null as unknown) as IProjectIUCNForm,
+      funding: (null as unknown) as IProjectFundingForm,
+      partnerships: (null as unknown) as IProjectPartnershipsForm
+    };
+
+    mock.onPost('/api/project').reply(200, {
+      id: 1
+    });
+
+    const result = await useProjectApi(axios).createProject(projectData);
+
+    expect(result).toEqual({ id: 1 });
   });
 });
