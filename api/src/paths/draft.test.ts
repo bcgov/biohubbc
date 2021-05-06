c9cb304988cad190fd180dfed12314202c1efa7d
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-05-06 15:14:49 -0700

    better code, awesome tests

--- api/src/paths/draft.test.ts
+++ api/src/paths/draft.test.ts
@@ -33,7 +33,12 @@ describe('draft', () => {
   };
 
   const sampleReq = {
-    keycloak_token: {}
+    keycloak_token: {},
+    body: {
+      id: 1,
+      name: 'name',
+      data: {}
+    }
   } as any;
 
   let actualResult = {
@@ -90,6 +95,52 @@ describe('draft', () => {
       }
     });
 
+    it('should throw a 400 error when missing request body param name', async () => {
+      sinon.stub(db, 'getDBConnection').returns({
+        ...dbConnectionObj,
+        systemUserId: () => {
+          return 20;
+        }
+      });
+
+      try {
+        const result = draft.createDraft();
+
+        await result(
+          { ...sampleReq, body: { ...sampleReq.body, name: null } },
+          (null as unknown) as any,
+          (null as unknown) as any
+        );
+        expect.fail();
+      } catch (actualError) {
+        expect(actualError.status).to.equal(400);
+        expect(actualError.message).to.equal('Missing required param name');
+      }
+    });
+
+    it('should throw a 400 error when missing request body param data', async () => {
+      sinon.stub(db, 'getDBConnection').returns({
+        ...dbConnectionObj,
+        systemUserId: () => {
+          return 20;
+        }
+      });
+
+      try {
+        const result = draft.createDraft();
+
+        await result(
+          { ...sampleReq, body: { ...sampleReq.body, data: null } },
+          (null as unknown) as any,
+          (null as unknown) as any
+        );
+        expect.fail();
+      } catch (actualError) {
+        expect(actualError.status).to.equal(400);
+        expect(actualError.message).to.equal('Missing required param data');
+      }
+    });
+
     it('should throw a 400 error when no id in result', async () => {
       sinon.stub(db, 'getDBConnection').returns({
         ...dbConnectionObj,
@@ -121,7 +172,33 @@ describe('draft', () => {
       }
     });
 
-    it('should return the draft id and create/update date on success', async () => {
+    it('should throw a 400 error when no result', async () => {
+      sinon.stub(db, 'getDBConnection').returns({
+        ...dbConnectionObj,
+        systemUserId: () => {
+          return 20;
+        },
+        query: async () => {
+          return {
+            rows: null
+          } as any;
+        }
+      });
+
+      sinon.stub(draft_queries, 'postDraftSQL').returns(SQL`some query`);
+
+      try {
+        const result = draft.createDraft();
+
+        await result(sampleReq, sampleRes as any, (null as unknown) as any);
+        expect.fail();
+      } catch (actualError) {
+        expect(actualError.status).to.equal(400);
+        expect(actualError.message).to.equal('Failed to save draft');
+      }
+    });
+
+    it('should return the draft id and update date on success', async () => {
       sinon.stub(db, 'getDBConnection').returns({
         ...dbConnectionObj,
         systemUserId: () => {
@@ -151,6 +228,35 @@ describe('draft', () => {
       expect(actualResult.date).to.equal('2020/05/05');
     });
 
+    it('should return the draft id and create date on success', async () => {
+      sinon.stub(db, 'getDBConnection').returns({
+        ...dbConnectionObj,
+        systemUserId: () => {
+          return 20;
+        },
+        query: async () => {
+          return {
+            rowCount: 1,
+            rows: [
+              {
+                id: 1,
+                create_date: '2020/04/04'
+              }
+            ]
+          } as QueryResult<any>;
+        }
+      });
+
+      sinon.stub(draft_queries, 'postDraftSQL').returns(SQL`some query`);
+
+      const result = draft.createDraft();
+
+      await result(sampleReq, sampleRes as any, (null as unknown) as any);
+
+      expect(actualResult.id).to.equal(1);
+      expect(actualResult.date).to.equal('2020/04/04');
+    });
+
     it('should throw an error when a failure occurs', async () => {
       const expectedError = new Error('cannot process query');
 
@@ -169,6 +275,217 @@ describe('draft', () => {
       } catch (actualError) {
         expect(actualError.message).to.equal(expectedError.message);
       }
+    });
+  });
+
+  describe('updateDraft', () => {
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it('should throw a 400 error when missing request body param id', async () => {
+      sinon.stub(db, 'getDBConnection').returns({
+        ...dbConnectionObj,
+        systemUserId: () => {
+          return 20;
+        }
+      });
+
+      try {
+        const result = draft.updateDraft();
+
+        await result(
+          { ...sampleReq, body: { ...sampleReq.body, id: null } },
+          (null as unknown) as any,
+          (null as unknown) as any
+        );
+        expect.fail();
+      } catch (actualError) {
+        expect(actualError.status).to.equal(400);
+        expect(actualError.message).to.equal('Missing required param id');
+      }
+    });
+
+    it('should throw a 400 error when missing request body param name', async () => {
+      sinon.stub(db, 'getDBConnection').returns({
+        ...dbConnectionObj,
+        systemUserId: () => {
+          return 20;
+        }
+      });
+
+      try {
+        const result = draft.updateDraft();
+
+        await result(
+          { ...sampleReq, body: { ...sampleReq.body, name: null } },
+          (null as unknown) as any,
+          (null as unknown) as any
+        );
+        expect.fail();
+      } catch (actualError) {
+        expect(actualError.status).to.equal(400);
+        expect(actualError.message).to.equal('Missing required param name');
+      }
+    });
+
+    it('should throw a 400 error when missing request body param data', async () => {
+      sinon.stub(db, 'getDBConnection').returns({
+        ...dbConnectionObj,
+        systemUserId: () => {
+          return 20;
+        }
+      });
+
+      try {
+        const result = draft.updateDraft();
+
+        await result(
+          { ...sampleReq, body: { ...sampleReq.body, data: null } },
+          (null as unknown) as any,
+          (null as unknown) as any
+        );
+        expect.fail();
+      } catch (actualError) {
+        expect(actualError.status).to.equal(400);
+        expect(actualError.message).to.equal('Missing required param data');
+      }
+    });
+
+    it('should throw a 400 error when no sql statement produced', async () => {
+      sinon.stub(db, 'getDBConnection').returns({
+        ...dbConnectionObj,
+        systemUserId: () => {
+          return 20;
+        }
+      });
+      sinon.stub(draft_queries, 'putDraftSQL').returns(null);
+
+      try {
+        const result = draft.updateDraft();
+
+        await result(sampleReq, (null as unknown) as any, (null as unknown) as any);
+        expect.fail();
+      } catch (actualError) {
+        expect(actualError.status).to.equal(400);
+        expect(actualError.message).to.equal('Failed to build SQL update statement');
+      }
+    });
+
+    it('should throw a 400 error when no id in result', async () => {
+      sinon.stub(db, 'getDBConnection').returns({
+        ...dbConnectionObj,
+        systemUserId: () => {
+          return 20;
+        },
+        query: async () => {
+          return {
+            rowCount: 1,
+            rows: [
+              {
+                id: null
+              }
+            ]
+          } as QueryResult<any>;
+        }
+      });
+
+      sinon.stub(draft_queries, 'putDraftSQL').returns(SQL`some query`);
+
+      try {
+        const result = draft.updateDraft();
+
+        await result(sampleReq, sampleRes as any, (null as unknown) as any);
+        expect.fail();
+      } catch (actualError) {
+        expect(actualError.status).to.equal(400);
+        expect(actualError.message).to.equal('Failed to update draft');
+      }
+    });
+
+    it('should throw a 400 error when no result', async () => {
+      sinon.stub(db, 'getDBConnection').returns({
+        ...dbConnectionObj,
+        systemUserId: () => {
+          return 20;
+        },
+        query: async () => {
+          return {
+            rows: null
+          } as any;
+        }
+      });
+
+      sinon.stub(draft_queries, 'putDraftSQL').returns(SQL`some query`);
+
+      try {
+        const result = draft.updateDraft();
+
+        await result(sampleReq, sampleRes as any, (null as unknown) as any);
+        expect.fail();
+      } catch (actualError) {
+        expect(actualError.status).to.equal(400);
+        expect(actualError.message).to.equal('Failed to update draft');
+      }
+    });
+
+    it('should return the draft id and update date on success', async () => {
+      sinon.stub(db, 'getDBConnection').returns({
+        ...dbConnectionObj,
+        systemUserId: () => {
+          return 20;
+        },
+        query: async () => {
+          return {
+            rowCount: 1,
+            rows: [
+              {
+                id: 1,
+                create_date: '2020/04/04',
+                update_date: '2020/05/05'
+              }
+            ]
+          } as QueryResult<any>;
+        }
+      });
+
+      sinon.stub(draft_queries, 'putDraftSQL').returns(SQL`some query`);
+
+      const result = draft.updateDraft();
+
+      await result(sampleReq, sampleRes as any, (null as unknown) as any);
+
+      expect(actualResult.id).to.equal(1);
+      expect(actualResult.date).to.equal('2020/05/05');
+    });
+
+    it('should return the draft id and create date on success', async () => {
+      sinon.stub(db, 'getDBConnection').returns({
+        ...dbConnectionObj,
+        systemUserId: () => {
+          return 20;
+        },
+        query: async () => {
+          return {
+            rowCount: 1,
+            rows: [
+              {
+                id: 1,
+                create_date: '2020/04/04'
+              }
+            ]
+          } as QueryResult<any>;
+        }
+      });
+
+      sinon.stub(draft_queries, 'putDraftSQL').returns(SQL`some query`);
+
+      const result = draft.updateDraft();
+
+      await result(sampleReq, sampleRes as any, (null as unknown) as any);
+
+      expect(actualResult.id).to.equal(1);
+      expect(actualResult.date).to.equal('2020/04/04');
     });
   });
 });
