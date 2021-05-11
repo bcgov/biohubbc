cf35d680fa430d2d92f5ad5b83902533e79b1893
Nick Phura <Nick.Phura@quartech.com>
2021-05-11 15:23:52 -0700

    BHBC-992: Log out / Log in (#294)
    * BHBC-992: Logout
    - Add util for siteminder + keycloak logout
    - Add username + logout button to header
    - Add log in page
    - Update routing to allow for un-authenticated pages
    
    * BHBC-992: Add siteminder url to env/pipeline/etc
    
    * Trigger build
    
    * BHBC-992: Add siteminder env to app config (accidentally added to api config)
    
    * BHBC-992: Review updates + add tests + fix existing tests
    
    * BHBC-992: Fix issue where submitting an access request would not route you correctly to the request submitted page
    - Added a `refresh` function to the keycloak wrapper
    
    * Update app/src/pages/login/LogInPage.tsx
    
    * BHBC-992: Remove auto-redirect to access request page
    
    Co-authored-by: Anissa Agahchen <anissa.agahchen@akimilabs.com>
    Co-authored-by: Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>

--- app/src/components/layout/Header.tsx
+++ app/src/components/layout/Header.tsx
@@ -1,15 +1,20 @@
 import AppBar from '@material-ui/core/AppBar';
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
+import Divider from '@material-ui/core/Divider';
 import { Theme } from '@material-ui/core/styles/createMuiTheme';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import Toolbar from '@material-ui/core/Toolbar';
+import { mdiAccountCircle } from '@mdi/js';
+import Icon from '@mdi/react';
 import headerImageLarge from 'assets/images/gov-bc-logo-horiz.png';
 import headerImageSmall from 'assets/images/gov-bc-logo-vert.png';
 import { SYSTEM_ROLE } from 'constants/roles';
 import { AuthStateContext } from 'contexts/authStateContext';
+import { ConfigContext } from 'contexts/configContext';
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { logOut } from 'utils/Utils';
 
 const useStyles = makeStyles((theme: Theme) => ({
   govHeader: {
@@ -50,6 +55,21 @@ const useStyles = makeStyles((theme: Theme) => ({
       }
     }
   },
+  usernameAndLogout: {
+    color: theme.palette.primary.contrastText,
+    fontSize: '1rem',
+    '& hr': {
+      backgroundColor: theme.palette.primary.contrastText,
+      height: '1rem'
+    },
+    '& a': {
+      color: 'inherit',
+      textDecoration: 'none'
+    },
+    '& a:hover': {
+      textDecoration: 'underline'
+    }
+  },
   mainNav: {
     backgroundColor: '#38598a'
   },
@@ -77,35 +97,88 @@ const useStyles = makeStyles((theme: Theme) => ({
 const Header: React.FC = () => {
   const classes = useStyles();
 
+  const config = useContext(ConfigContext);
+
+  const location = useLocation();
+
   const { keycloakWrapper } = useContext(AuthStateContext);
+
+  const LoggedInUser: React.FC = () => {
+    if (!keycloakWrapper?.keycloak?.authenticated || !keycloakWrapper?.hasLoadedAllUserInfo) {
+      return <></>;
+    }
+
+    const loggedInUserDisplayName = `${keycloakWrapper?.getIdentitySource()} / ${keycloakWrapper?.getUserIdentifier()}`.toUpperCase();
+
+    return (
+      <Box display="flex" className={classes.usernameAndLogout} my="auto" alignItems="center">
+        <Icon path={mdiAccountCircle} size={1.25} />
+        <Box ml={1}>{loggedInUserDisplayName}</Box>
+        <Box px={2}>
+          <Divider orientation="vertical" />
+        </Box>
+        <Link
+          to={location.pathname}
+          onClick={() => {
+            if (!config) {
+              return;
+            }
+
+            logOut(config);
+          }}
+          id="menu_log_out">
+          Log Out
+        </Link>
+      </Box>
+    );
+  };
+
+  const SecureLink: React.FC<{ to: string; label: string; validRoles: string[]; id: string }> = (props) => {
+    if (!keycloakWrapper?.hasSystemRole(props.validRoles)) {
+      return <></>;
+    }
+
+    return (
+      <Link to={props.to} color={'inherit'} id={props.id}>
+        {props.label}
+      </Link>
+    );
+  };
 
   return (
     <AppBar position="sticky" style={{ boxShadow: 'none' }}>
       <Box className={classes.govHeader}>
         <Container maxWidth="xl" className={classes.govHeaderContainer}>
           <Toolbar className={classes.govHeaderToolbar}>
-            <Link to="/projects" className={classes.brand} aria-label="Go to BioHub Home">
-              <picture>
-                <source srcSet={headerImageLarge} media="(min-width: 1200px)"></source>
-                <source srcSet={headerImageSmall} media="(min-width: 600px)"></source>
-                <img src={headerImageSmall} alt={'Government of British Columbia'} />
-              </picture>
-              BioHub
-            </Link>
+            <Box display="flex" justifyContent="space-between" width="100%">
+              <Link to="/projects" className={classes.brand} aria-label="Go to BioHub Home">
+                <picture>
+                  <source srcSet={headerImageLarge} media="(min-width: 1200px)"></source>
+                  <source srcSet={headerImageSmall} media="(min-width: 600px)"></source>
+                  <img src={headerImageSmall} alt={'Government of British Columbia'} />
+                </picture>
+                BioHub
+              </Link>
+              <LoggedInUser />
+            </Box>
           </Toolbar>
         </Container>
       </Box>
       <Box className={classes.mainNav}>
         <Container maxWidth="xl" className={classes.mainNavContainer}>
           <Toolbar variant="dense" className={classes.mainNavToolbar} role="navigation" aria-label="Main Navigation">
-            <Link to="/projects" color={'inherit'} id="menu_projects">
-              Projects
-            </Link>
-            {keycloakWrapper?.hasSystemRole([SYSTEM_ROLE.SYSTEM_ADMIN]) && (
-              <Link to="/admin/users" color={'inherit'} id="menu_admin_users">
-                Manage Users
-              </Link>
-            )}
+            <SecureLink
+              to="/projects"
+              label="Projects"
+              validRoles={[SYSTEM_ROLE.SYSTEM_ADMIN, SYSTEM_ROLE.PROJECT_ADMIN]}
+              id="menu_projects"
+            />
+            <SecureLink
+              to="/admin/users"
+              label="Manage Users"
+              validRoles={[SYSTEM_ROLE.SYSTEM_ADMIN]}
+              id="menu_admin_users"
+            />
           </Toolbar>
         </Container>
       </Box>
