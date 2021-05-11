1e26153dc399459b1fdd4477f137e4facd31c5c1
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-05-11 09:07:36 -0700

    missing tests for survey stuff (part 1 of many) (#295)
    * missing test
    
    * test 2

---
+++ api/src/models/survey-view-update.test.ts
@@ -0,0 +1,105 @@
+import { expect } from 'chai';
+import { describe } from 'mocha';
+import { GetSurveyData } from './survey-view-update';
+
+describe('GetSurveyData', () => {
+  describe('No values provided', () => {
+    let data: GetSurveyData;
+
+    before(() => {
+      data = new GetSurveyData();
+    });
+
+    it('sets survey_name', () => {
+      expect(data.survey_name).to.equal('');
+    });
+
+    it('sets survey_purpose', () => {
+      expect(data.survey_purpose).to.equal('');
+    });
+
+    it('sets species', () => {
+      expect(data.species).to.equal('');
+    });
+
+    it('sets start_date', () => {
+      expect(data.start_date).to.equal('');
+    });
+
+    it('sets end_date', () => {
+      expect(data.end_date).to.equal('');
+    });
+
+    it('sets biologist_first_name', () => {
+      expect(data.biologist_first_name).to.equal('');
+    });
+
+    it('sets biologist_last_name', () => {
+      expect(data.biologist_last_name).to.equal('');
+    });
+
+    it('sets survey_area_name', () => {
+      expect(data.survey_area_name).to.equal('');
+    });
+
+    it('sets revision_count', () => {
+      expect(data.revision_count).to.equal(null);
+    });
+  });
+
+  describe('all values provided', () => {
+    const surveyData = {
+      name: 'survey name',
+      objectives: 'purpose of survey',
+      start_date: '2020-04-20T07:00:00.000Z',
+      end_date: '2020-05-20T07:00:00.000Z',
+      revision_count: 1,
+      species: 'species',
+      lead_first_name: 'lead',
+      lead_last_name: 'last',
+      location_name: 'area'
+    };
+
+    let data: GetSurveyData;
+
+    before(() => {
+      data = new GetSurveyData(surveyData);
+    });
+
+    it('sets survey_name', () => {
+      expect(data.survey_name).to.equal(surveyData.name);
+    });
+
+    it('sets survey_purpose', () => {
+      expect(data.survey_purpose).to.equal(surveyData.objectives);
+    });
+
+    it('sets species', () => {
+      expect(data.species).to.equal(surveyData.species);
+    });
+
+    it('sets start_date', () => {
+      expect(data.start_date).to.equal(surveyData.start_date);
+    });
+
+    it('sets end_date', () => {
+      expect(data.end_date).to.equal(surveyData.end_date);
+    });
+
+    it('sets biologist_first_name', () => {
+      expect(data.biologist_first_name).to.equal(surveyData.lead_first_name);
+    });
+
+    it('sets biologist_last_name', () => {
+      expect(data.biologist_last_name).to.equal(surveyData.lead_last_name);
+    });
+
+    it('sets survey_area_name', () => {
+      expect(data.survey_area_name).to.equal(surveyData.location_name);
+    });
+
+    it('sets revision_count', () => {
+      expect(data.revision_count).to.equal(surveyData.revision_count);
+    });
+  });
+});
