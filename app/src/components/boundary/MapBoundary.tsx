c6fcc19b2c47ea574c761e8066c723ffc8701596
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-05-12 11:51:38 -0700

    add map to survey study area, refactoring

---
+++ app/src/components/boundary/MapBoundary.tsx
@@ -0,0 +1,104 @@
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Tooltip from '@material-ui/core/Tooltip';
+import Typography from '@material-ui/core/Typography';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { v4 as uuidv4 } from 'uuid';
+import MapContainer from 'components/map/MapContainer';
+import { Feature } from 'geojson';
+import { handleKMLUpload, handleShapefileUpload } from 'utils/mapBoundaryUploadHelpers';
+
+const useStyles = makeStyles({
+  bold: {
+    fontWeight: 'bold'
+  },
+  uploadButton: {
+    border: '2px solid',
+    textTransform: 'capitalize',
+    fontWeight: 'bold'
+  }
+});
+
+export interface IMapBoundaryProps {
+  title: string;
+  mapId: string;
+  isLoading: boolean;
+  setIsLoading: (isLoading: boolean) => void;
+  uploadError: string;
+  setUploadError: (error: string) => void;
+  values: any;
+  bounds: any[];
+  setFieldValue: (key: string, value: any) => void;
+}
+
+/**
+ * Shared component for map boundary component
+ *
+ * @return {*}
+ */
+const MapBoundary: React.FC<IMapBoundaryProps> = (props) => {
+  const classes = useStyles();
+  const { title, mapId, isLoading, setIsLoading, uploadError, setUploadError, values, bounds, setFieldValue } = props;
+
+  return (
+    <Grid item xs={12}>
+      <Typography className={classes.bold}>{title}</Typography>
+      <Box display="flex" mt={3}>
+        <Tooltip arrow color="secondary" title="Will only accept kml files, kmz files not accepted.">
+          <Button
+            variant="outlined"
+            component="label"
+            size="medium"
+            color="primary"
+            disabled={isLoading}
+            onClick={() => setUploadError('')}
+            className={classes.uploadButton}>
+            <input
+              key={uuidv4()}
+              data-testid="file-upload"
+              type="file"
+              hidden
+              onChange={(e) => handleKMLUpload(e, setIsLoading, setUploadError, values, setFieldValue)}
+            />
+            Upload KML
+          </Button>
+        </Tooltip>
+        <Tooltip arrow color="secondary" title="Will only accept zipped shapefiles of a known projection.">
+          <Button
+            variant="outlined"
+            component="label"
+            size="medium"
+            color="primary"
+            disabled={isLoading}
+            className={classes.uploadButton}
+            style={{ marginLeft: '1rem' }}>
+            <input
+              key={uuidv4()}
+              data-testid="shp-upload"
+              type="file"
+              hidden
+              onChange={(e) => handleShapefileUpload(e, values, setFieldValue)}
+            />
+            Upload Shapefile
+          </Button>
+        </Tooltip>
+      </Box>
+      <Box mt={2}>{uploadError && <Typography style={{ color: '#db3131' }}>{uploadError}</Typography>}</Box>
+      <Box mt={5} height={500}>
+        <MapContainer
+          mapId={mapId}
+          //@ts-ignore
+          geometryState={{
+            geometry: values.geometry,
+            setGeometry: (newGeo: Feature[]) => setFieldValue('geometry', newGeo)
+          }}
+          bounds={bounds}
+        />
+      </Box>
+    </Grid>
+  );
+};
+
+export default MapBoundary;
---
+++ app/src/utils/mapBoundaryUploadHelpers.ts
@@ -0,0 +1,109 @@
+//@ts-ignore
+import { kml } from '@tmcw/togeojson';
+import shp from 'shpjs';
+import { Feature } from 'geojson';
+import bbox from '@turf/bbox';
+
+/**
+ * Convert a zipped shapefile to geojson
+ * @param e The file upload event
+ */
+export const handleShapefileUpload = (e: any, values: any, setFieldValue: (key: string, value: any) => void) => {
+  // Only accept one file
+  const file = e.target.files[0];
+
+  // Back out if not a zipped file
+  if (!file?.type.match(/zip/)) {
+    return;
+  }
+
+  // Create a file reader to extract the binary data
+  const reader = new FileReader();
+  reader.readAsArrayBuffer(file);
+
+  // When the file is loaded run the conversion
+  reader.onload = async (event: any) => {
+    // The converter wants a buffer
+    const zip: Buffer = event?.target?.result as Buffer;
+
+    // Exit out if no zip
+    if (!zip) {
+      return;
+    }
+
+    // Run the conversion
+    const geojson = await shp(zip);
+    const features = (geojson as any).features;
+    setFieldValue('geometry', [...features, ...values.geometry]);
+  };
+};
+
+/**
+ *
+ * @param e The file upload event
+ * @param setIsLoading change state of isLoading
+ * @param setUploadError change state of upload error
+ * @param values current form values
+ * @param setFieldValue change form values
+ */
+export const handleKMLUpload = async (
+  e: any,
+  setIsLoading: (isLoading: boolean) => void,
+  setUploadError: (uploadError: string) => void,
+  values: any,
+  setFieldValue: (key: string, value: any) => void
+) => {
+  setIsLoading(true);
+
+  const file = e.target.files[0];
+  const fileAsString = await file?.text().then((xmlString: string) => {
+    return xmlString;
+  });
+
+  if (file?.type !== 'application/vnd.google-earth.kml+xml' && !fileAsString?.includes('</kml>')) {
+    setUploadError('You must upload a KML file, please try again.');
+    setIsLoading(false);
+    return;
+  }
+
+  const domKml = new DOMParser().parseFromString(fileAsString, 'application/xml');
+  const geojson = kml(domKml);
+
+  let sanitizedGeoJSON: Feature[] = [];
+  geojson.features.forEach((feature: Feature) => {
+    if (feature.geometry) {
+      sanitizedGeoJSON.push(feature);
+    }
+  });
+
+  setFieldValue('geometry', [...sanitizedGeoJSON, ...values.geometry]);
+};
+
+/**
+ * @param values current form values
+ * @param setBounds change bounds on map
+ */
+export const updateMapBounds = (values: any, setBounds: (bounds: any[]) => void) => {
+  /*
+    If no geometries, we do not need to set bounds
+
+    If there is only one geometry and it is a point, we cannot do the bound setting
+    because leaflet does not know how to handle that and tries to zoom in way too much
+
+    If there are multiple points or a polygon and a point, this is not an issue
+  */
+  if (!values || !values.geometry || !values.geometry.length || (values.geometry.length === 1 && values.geometry[0].geometry.type === 'Point')) {
+    return;
+  }
+
+  const allGeosFeatureCollection = {
+    type: 'FeatureCollection',
+    features: [...values.geometry]
+  };
+  const bboxCoords = bbox(allGeosFeatureCollection);
+
+  setBounds([
+    [bboxCoords[1], bboxCoords[0]],
+    [bboxCoords[3], bboxCoords[2]]
+  ]);
+};
