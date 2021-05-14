2bd7676013e844a8498ccff65ba79a76753d0851
Nick Phura <nickphura@gmail.com>
2021-05-14 09:17:37 -0700

    Update dialog context jsdoc

--- app/src/contexts/dialogContext.tsx
+++ app/src/contexts/dialogContext.tsx
@@ -5,11 +5,50 @@ import YesNoDialog, { IYesNoDialogProps } from 'components/dialog/YesNoDialog';
 import React, { createContext, useState } from 'react';
 
 export interface IDialogContext {
+  /**
+   * Set the yes no dialog props.
+   *
+   * Note: Any props that are not provided, will default to whatever value was previously set (or the default value)
+   *
+   * @memberof IDialogContext
+   */
   setYesNoDialog: (props: Partial<IYesNoDialogProps>) => void;
+  /**
+   * The current yes no dialog props.
+   *
+   * @type {IYesNoDialogProps}
+   * @memberof IDialogContext
+   */
   yesNoDialogProps: IYesNoDialogProps;
+  /**
+   * Set the error dialog props.
+   *
+   * Note: Any props that are not provided, will default to whatever value was previously set (or the default value)
+   *
+   * @memberof IDialogContext
+   */
   setErrorDialog: (props: Partial<IErrorDialogProps>) => void;
+  /**
+   * The current error dialog props.
+   *
+   * @type {IErrorDialogProps}
+   * @memberof IDialogContext
+   */
   errorDialogProps: IErrorDialogProps;
+  /**
+   * Set the snackbar props.
+   *
+   * Note: Any props that are not provided, will default to whatever value was previously set (or the default value)
+   *
+   * @memberof IDialogContext
+   */
   setSnackbar: (props: Partial<ISnackbarProps>) => void;
+  /**
+   * The current snackbar props.
+   *
+   * @type {ISnackbarProps}
+   * @memberof IDialogContext
+   */
   snackbarProps: ISnackbarProps;
 }
 
@@ -71,6 +110,12 @@ export const DialogContext = createContext<IDialogContext>({
   snackbarProps: defaultSnackbarProps
 });
 
+/**
+ * Wraps the provided children in a context that provides various modal dialogs/popups.
+ *
+ * @param {*} props
+ * @return {*}
+ */
 export const DialogContextProvider: React.FC = (props) => {
   const [yesNoDialogProps, setYesNoDialogProps] = useState<IYesNoDialogProps>(defaultYesNoDialogProps);
 
