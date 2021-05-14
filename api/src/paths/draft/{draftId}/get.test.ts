a8d2fd0ccc526b086d03426126594cb4a70bdbc5
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2021-05-13 21:59:21 -0700

    tests got get draft project

---
+++ api/src/paths/draft/{draftId}/get.test.ts
@@ -0,0 +1,127 @@
+import chai, { expect } from 'chai';
+import { describe } from 'mocha';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import * as viewDraftProject from './get';
+import * as db from '../../../database/db';
+import * as draft_queries from '../../../queries/draft-queries';
+import SQL from 'sql-template-strings';
+
+chai.use(sinonChai);
+
+describe('delete a draft project', () => {
+  const dbConnectionObj = {
+    systemUserId: () => {
+      return null;
+    },
+    open: async () => {
+      // do nothing
+    },
+    release: () => {
+      // do nothing
+    },
+    commit: async () => {
+      // do nothing
+    },
+    rollback: async () => {
+      // do nothing
+    },
+    query: async () => {
+      // do nothing
+    }
+  };
+
+  const sampleReq = {
+    keycloak_token: {},
+    body: {},
+    params: {
+      draftId: 1
+    }
+  } as any;
+
+  let actualResult: any = null;
+
+  const sampleRes = {
+    status: () => {
+      return {
+        json: (result: any) => {
+          actualResult = result;
+        }
+      };
+    }
+  };
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+
+
+  it('should throw a 400 error when no sql statement returned for getDraftSQL', async () => {
+    sinon.stub(db, 'getDBConnection').returns({
+      ...dbConnectionObj,
+      systemUserId: () => {
+        return 20;
+      }
+    });
+
+    sinon.stub(draft_queries, 'getDraftSQL').returns(null);
+
+    try {
+      const result = viewDraftProject.getSingleDraft();
+
+      await result(sampleReq, (null as unknown) as any, (null as unknown) as any);
+      expect.fail();
+    } catch (actualError) {
+      expect(actualError.status).to.equal(400);
+      expect(actualError.message).to.equal('Failed to build SQL get statement');
+    }
+  });
+
+  it('should return the draft project on success', async () => {
+
+    const mockQuery = sinon.stub();
+
+    mockQuery.resolves({ rows: [{ id: 1 }] });
+
+    sinon.stub(db, 'getDBConnection').returns({
+      ...dbConnectionObj,
+      systemUserId: () => {
+        return 20;
+      },
+      query: mockQuery
+    });
+
+    sinon.stub(draft_queries, 'getDraftSQL').returns(SQL`something`);
+
+    const result = viewDraftProject.getSingleDraft();
+
+    await result(sampleReq, sampleRes as any, (null as unknown) as any);
+
+    expect(actualResult).to.eql({id: 1});
+  });
+
+  it('should return null if the draft project does not exist', async () => {
+
+    const mockQuery = sinon.stub();
+
+    mockQuery.resolves({ rows: undefined});
+
+    sinon.stub(db, 'getDBConnection').returns({
+      ...dbConnectionObj,
+      systemUserId: () => {
+        return 20;
+      },
+      query: mockQuery
+    });
+
+    sinon.stub(draft_queries, 'getDraftSQL').returns(SQL`something`);
+
+    const result = viewDraftProject.getSingleDraft();
+
+    await result(sampleReq, sampleRes as any, (null as unknown) as any);
+
+    expect(actualResult).to.eql(null);
+  });
+
+});
