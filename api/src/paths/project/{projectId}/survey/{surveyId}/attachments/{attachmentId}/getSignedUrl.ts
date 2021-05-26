c504b3b6ed2ea15f061ad10fc75d271fe2b9465c
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-05-26 10:08:08 -0700

    add test case

--- api/src/paths/project/{projectId}/survey/{surveyId}/attachments/{attachmentId}/getSignedUrl.ts
+++ api/src/paths/project/{projectId}/survey/{surveyId}/attachments/{attachmentId}/getSignedUrl.ts
@@ -46,7 +46,7 @@ GET.apiDoc = {
   ]
 };
 
-function getSingleAttachmentURL(): RequestHandler {
+export function getSingleAttachmentURL(): RequestHandler {
   return async (req, res) => {
     defaultLog.debug({ label: 'Get single attachment url', message: 'params', req_params: req.params });
 
