be1abb4ce242e99cc86f1b350fc2fcfd11832b47
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-05-25 22:42:38 -0700

    Unit tests!!! (#332)
    * api tests
    
    * add tests

--- api/src/queries/survey/survey-view-queries.test.ts
+++ api/src/queries/survey/survey-view-queries.test.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 import { describe } from 'mocha';
-import { getSurveyProprietorSQL, getSurveyListSQL } from './survey-view-queries';
+import { getSurveyProprietorSQL, getSurveyListSQL, getSurveyForViewSQL } from './survey-view-queries';
 
 describe('getSurveyProprietorSQL', () => {
   it('returns null when null survey id param provided', () => {
@@ -25,6 +25,20 @@ describe('getSurveyListSQL', () => {
 
   it('returns a non null response when valid params passed in', () => {
     const response = getSurveyListSQL(1);
+
+    expect(response).to.not.be.null;
+  });
+});
+
+describe('getSurveyForViewSQL', () => {
+  it('returns a null response when null survey id param provided', () => {
+    const response = getSurveyForViewSQL((null as unknown) as number);
+
+    expect(response).to.be.null;
+  });
+
+  it('returns a non null response when valid params passed in', () => {
+    const response = getSurveyForViewSQL(1);
 
     expect(response).to.not.be.null;
   });
