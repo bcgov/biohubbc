f3255aefc30f2bd13517a9fdbfef3c898bb71af5
charlie garrett-jones <charlie.garrettjones@bayseumcom>
2021-06-03 13:36:57 -0700

    - missing semi-colon

--- database/src/migrations/20210225205948_biohub_release.ts
+++ database/src/migrations/20210225205948_biohub_release.ts
@@ -5,7 +5,7 @@ import path from 'path';
 const DB_USER_API_PASS = process.env.DB_USER_API_PASS;
 const DB_USER_API = process.env.DB_USER_API;
 
-const DB_RELEASE = 'release.0.17'
+const DB_RELEASE = 'release.0.17';
 
 /**
  * Apply biohub release changes.
