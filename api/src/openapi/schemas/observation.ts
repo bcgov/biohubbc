e858390948a2824adc0bceea90202a443c4bf012
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-06-15 14:33:18 -0700

    get observation for update endpoint

---
+++ api/src/openapi/schemas/observation.ts
@@ -0,0 +1,8 @@
+/**
+ * Response object for observation update GET request
+ */
+export const observationUpdateGetResponseObject = {
+  title: 'Observation get response object, for update purposes',
+  type: 'object',
+  properties: {}
+};
