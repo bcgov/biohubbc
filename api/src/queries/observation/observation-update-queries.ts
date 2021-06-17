afda135740f12e8ba64c0ae1270fb709cec7346c
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-06-17 15:04:35 -0700

    format fix

--- api/src/queries/observation/observation-update-queries.ts
+++ api/src/queries/observation/observation-update-queries.ts
@@ -48,7 +48,7 @@ export const getBlockObservationSQL = (observationId: number): SQLStatement | nu
  * @param {any} observationData
  * @returns {SQLStatement} sql query object
  */
- export const updateBlockObservationSQL = (observationId: number, observationData: any): SQLStatement | null => {
+export const updateBlockObservationSQL = (observationId: number, observationData: any): SQLStatement | null => {
   defaultLog.debug({
     label: 'updateBlockObservationSQL',
     message: 'params',
