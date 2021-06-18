dd3cc9791cd6a46d4c338fdcf85a3313e5cd93b6
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-06-18 11:54:51 -0700

    add test case coverage to 100% in file

--- api/src/paths/project/{projectId}/survey/{surveyId}/attachments/upload.test.ts
+++ api/src/paths/project/{projectId}/survey/{surveyId}/attachments/upload.test.ts
@@ -5,6 +5,8 @@ import sinonChai from 'sinon-chai';
 import * as upload from './upload';
 import * as db from '../../../../../../database/db';
 import * as file_utils from '../../../../../../utils/file-utils';
+import * as survey_attachment_queries from '../../../../../../queries/survey/survey-attachments-queries';
+import SQL from 'sql-template-strings';
 
 chai.use(sinonChai);
 
@@ -157,5 +159,196 @@ describe('uploadMedia', () => {
     );
 
     expect(actualResult).to.eql(['1/1/test.txt']);
+  });
+});
+
+describe('upsertSurveyAttachment', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  const dbConnectionObj = {
+    systemUserId: () => {
+      return 20;
+    },
+    open: async () => {
+      // do nothing
+    },
+    release: () => {
+      // do nothing
+    },
+    commit: async () => {
+      // do nothing
+    },
+    rollback: async () => {
+      // do nothing
+    },
+    query: async () => {
+      // do nothing
+    }
+  };
+
+  const file = {
+    fieldname: 'media',
+    originalname: 'test.txt',
+    encoding: '7bit',
+    mimetype: 'text/plain',
+    size: 340
+  } as any;
+
+  const projectId = 1;
+  const surveyId = 2;
+
+  it('should throw an error when failed to generate SQL get statement', async () => {
+    sinon.stub(survey_attachment_queries, 'getSurveyAttachmentByFileNameSQL').returns(null);
+
+    try {
+      await upload.upsertSurveyAttachment(file, projectId, surveyId, dbConnectionObj);
+
+      expect.fail();
+    } catch (actualError) {
+      expect(actualError.status).to.equal(400);
+      expect(actualError.message).to.equal('Failed to build SQL get statement');
+    }
+  });
+
+  it('should throw an error when failed to generate SQL put statement', async () => {
+    const mockQuery = sinon.stub();
+
+    mockQuery.onFirstCall().resolves({
+      rowCount: 1
+    });
+
+    sinon.stub(survey_attachment_queries, 'getSurveyAttachmentByFileNameSQL').returns(SQL`something`);
+    sinon.stub(survey_attachment_queries, 'putSurveyAttachmentSQL').returns(null);
+
+    try {
+      await upload.upsertSurveyAttachment(file, projectId, surveyId, { ...dbConnectionObj, query: mockQuery });
+
+      expect.fail();
+    } catch (actualError) {
+      expect(actualError.status).to.equal(400);
+      expect(actualError.message).to.equal('Failed to build SQL update statement');
+    }
+  });
+
+  it('should throw an error when failed to update survey attachment data', async () => {
+    const mockQuery = sinon.stub();
+
+    mockQuery
+      .onFirstCall()
+      .resolves({
+        rowCount: 1
+      })
+      .onSecondCall()
+      .resolves({
+        rowCount: null
+      });
+
+    sinon.stub(survey_attachment_queries, 'getSurveyAttachmentByFileNameSQL').returns(SQL`something`);
+    sinon.stub(survey_attachment_queries, 'putSurveyAttachmentSQL').returns(SQL`something`);
+
+    try {
+      await upload.upsertSurveyAttachment(file, projectId, surveyId, { ...dbConnectionObj, query: mockQuery });
+
+      expect.fail();
+    } catch (actualError) {
+      expect(actualError.status).to.equal(400);
+      expect(actualError.message).to.equal('Failed to update survey attachment data');
+    }
+  });
+
+  it('should return the rowCount of records updated on success (update)', async () => {
+    const mockQuery = sinon.stub();
+
+    mockQuery
+      .onFirstCall()
+      .resolves({
+        rowCount: 1
+      })
+      .onSecondCall()
+      .resolves({
+        rowCount: 1
+      });
+
+    sinon.stub(survey_attachment_queries, 'getSurveyAttachmentByFileNameSQL').returns(SQL`something`);
+    sinon.stub(survey_attachment_queries, 'putSurveyAttachmentSQL').returns(SQL`something`);
+
+    const result = await upload.upsertSurveyAttachment(file, projectId, surveyId, {
+      ...dbConnectionObj,
+      query: mockQuery
+    });
+
+    expect(result).to.equal(1);
+  });
+
+  it('should throw an error when failed to generate SQL insert statement', async () => {
+    const mockQuery = sinon.stub();
+
+    mockQuery.onFirstCall().resolves({
+      rowCount: null
+    });
+
+    sinon.stub(survey_attachment_queries, 'getSurveyAttachmentByFileNameSQL').returns(SQL`something`);
+    sinon.stub(survey_attachment_queries, 'postSurveyAttachmentSQL').returns(null);
+
+    try {
+      await upload.upsertSurveyAttachment(file, projectId, surveyId, { ...dbConnectionObj, query: mockQuery });
+
+      expect.fail();
+    } catch (actualError) {
+      expect(actualError.status).to.equal(400);
+      expect(actualError.message).to.equal('Failed to build SQL insert statement');
+    }
+  });
+
+  it('should throw an error when insert result has no id', async () => {
+    const mockQuery = sinon.stub();
+
+    mockQuery
+      .onFirstCall()
+      .resolves({
+        rowCount: null
+      })
+      .onSecondCall()
+      .resolves({
+        rows: [{ id: null }]
+      });
+
+    sinon.stub(survey_attachment_queries, 'getSurveyAttachmentByFileNameSQL').returns(SQL`something`);
+    sinon.stub(survey_attachment_queries, 'postSurveyAttachmentSQL').returns(SQL`something`);
+
+    try {
+      await upload.upsertSurveyAttachment(file, projectId, surveyId, { ...dbConnectionObj, query: mockQuery });
+
+      expect.fail();
+    } catch (actualError) {
+      expect(actualError.status).to.equal(400);
+      expect(actualError.message).to.equal('Failed to insert survey attachment data');
+    }
+  });
+
+  it('should return the id of record inserted on success (insert)', async () => {
+    const mockQuery = sinon.stub();
+
+    mockQuery
+      .onFirstCall()
+      .resolves({
+        rowCount: null
+      })
+      .onSecondCall()
+      .resolves({
+        rows: [{ id: 12 }]
+      });
+
+    sinon.stub(survey_attachment_queries, 'getSurveyAttachmentByFileNameSQL').returns(SQL`something`);
+    sinon.stub(survey_attachment_queries, 'postSurveyAttachmentSQL').returns(SQL`something`);
+
+    const result = await upload.upsertSurveyAttachment(file, projectId, surveyId, {
+      ...dbConnectionObj,
+      query: mockQuery
+    });
+
+    expect(result).to.equal(12);
   });
 });
