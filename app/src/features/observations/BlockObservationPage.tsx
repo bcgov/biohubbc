17360afff90056d4e10b44ede1c92921ea22c377
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2021-06-21 15:48:28 -0700

    adding cows with 1 or 2 calves

--- app/src/features/observations/BlockObservationPage.tsx
+++ app/src/features/observations/BlockObservationPage.tsx
@@ -189,6 +189,18 @@ const BlockObservationPage = () => {
     return <CircularProgress className="pageProgress" size={40} />;
   }
 
+  const addupMooseData = (tableData: any[][]): number => {
+    let sum = 0;
+    for (let row = 0; row < tableData.length; row++) {
+      for (let col = 1; col < 9; col++) {
+        if (tableData[row][col]) {
+          sum += col === 5 ? tableData[row][col] * 2 : col === 6 ? tableData[row][col] * 3 : tableData[row][col];
+        }
+      }
+    }
+    return sum;
+  };
+
   const handleSaveAndExit = async () => {
     if (!formikRef?.current) {
       return;
@@ -217,7 +229,7 @@ const BlockObservationPage = () => {
         block_name: formikRef.current.values.block_name,
         start_datetime: moment(`${formikRef.current.values.date} ${formikRef.current.values.start_time}`).toISOString(),
         end_datetime: moment(`${formikRef.current.values.date} ${formikRef.current.values.end_time}`).toISOString(),
-        observation_count: 50,
+        observation_count: addupMooseData(tableData),
         observation_data: {
           metaData: formikRef.current.values,
           tableData: {
