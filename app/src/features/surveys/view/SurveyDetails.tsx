509dc7ce523fa4a421dd2d1845e754e6c9ac5136
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-06-21 13:57:18 -0700

    add delete button on fe

--- app/src/features/surveys/view/SurveyDetails.tsx
+++ app/src/features/surveys/view/SurveyDetails.tsx
@@ -5,11 +5,15 @@ import Typography from '@material-ui/core/Typography';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import SurveyGeneralInformation from 'features/surveys/view/components/SurveyGeneralInformation';
 import SurveyProprietaryData from 'features/surveys/view/components/SurveyProprietaryData';
-import React from 'react';
+import React, { useContext } from 'react';
 import { IGetProjectForViewResponse } from 'interfaces/useProjectApi.interface';
 import { IGetSurveyForViewResponse } from 'interfaces/useSurveyApi.interface';
 import { IGetAllCodeSetsResponse } from 'interfaces/useCodesApi.interface';
 import SurveyStudyArea from './components/SurveyStudyArea';
+import Button from '@material-ui/core/Button';
+import Icon from '@mdi/react';
+import { mdiDelete } from '@mdi/js';
+import { DialogContext } from 'contexts/dialogContext';
 
 export interface ISurveyDetailsProps {
   surveyForViewData: IGetSurveyForViewResponse;
@@ -37,14 +41,46 @@ const useStyles = makeStyles((theme: Theme) => ({
  * @return {*}
  */
 const SurveyDetails: React.FC<ISurveyDetailsProps> = (props) => {
-  const classes = useStyles();
-
   const { surveyForViewData, codes, refresh, projectForViewData } = props;
+
+  const classes = useStyles();
+  const dialogContext = useContext(DialogContext);
+
+  const defaultYesNoDialogProps = {
+    dialogTitle: 'Delete Survey',
+    dialogText: 'Are you sure you want to delete this survey, its attachments and associated observations?',
+    open: false,
+    onClose: () => dialogContext.setYesNoDialog({ open: false }),
+    onNo: () => dialogContext.setYesNoDialog({ open: false }),
+    onYes: () => dialogContext.setYesNoDialog({ open: false })
+  };
+
+  const showDeleteSurveyDialog = () => {
+    dialogContext.setYesNoDialog({
+      ...defaultYesNoDialogProps,
+      open: true,
+      onYes: () => {
+        deleteSurvey();
+        dialogContext.setYesNoDialog({ open: false });
+      }
+    });
+  };
+
+  const deleteSurvey = () => {
+    console.log('delete survey');
+  };
 
   return (
     <>
-      <Box mb={6}>
+      <Box mb={5} display="flex" justifyContent="space-between">
         <Typography variant="h2">Survey Details</Typography>
+        <Button
+          variant="outlined"
+          color="primary"
+          startIcon={<Icon path={mdiDelete} size={1} />}
+          onClick={showDeleteSurveyDialog}>
+          Delete Survey
+        </Button>
       </Box>
 
       <Box component={Paper} p={4}>
