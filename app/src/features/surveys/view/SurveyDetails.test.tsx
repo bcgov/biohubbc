f0bcbb55acba5f4b81f2ca8dd5aad3259f96294d
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-06-21 15:45:38 -0700

    fix test fail

--- app/src/features/surveys/view/SurveyDetails.test.tsx
+++ app/src/features/surveys/view/SurveyDetails.test.tsx
@@ -4,18 +4,24 @@ import SurveyDetails from './SurveyDetails';
 import { getSurveyForViewResponse } from 'test-helpers/survey-helpers';
 import { codes } from 'test-helpers/code-helpers';
 import { getProjectForViewResponse } from 'test-helpers/project-helpers';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+
+const history = createMemoryHistory();
 
 describe('SurveyDetails', () => {
   const mockRefresh = jest.fn();
 
   it('renders correctly', () => {
     const { asFragment } = render(
-      <SurveyDetails
-        surveyForViewData={getSurveyForViewResponse}
-        codes={codes}
-        projectForViewData={getProjectForViewResponse}
-        refresh={mockRefresh}
-      />
+      <Router history={history}>
+        <SurveyDetails
+          surveyForViewData={getSurveyForViewResponse}
+          codes={codes}
+          projectForViewData={getProjectForViewResponse}
+          refresh={mockRefresh}
+        />
+      </Router>
     );
 
     expect(asFragment()).toMatchSnapshot();
