4219978eade9cb47a907948a172919d8393a3931
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2021-06-25 14:15:53 -0700

    db migration to add publish column

---
+++ database/src/migrations/20210625115350_project_add_publishdate.ts
@@ -0,0 +1,28 @@
+import Knex from 'knex';
+
+const DB_SCHEMA = process.env.DB_SCHEMA;
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw(`
+    set schema '${DB_SCHEMA}';
+    set search_path = ${DB_SCHEMA},public;
+
+    ALTER TABLE project ADD COLUMN publish_date timestamptz(6);
+  `);
+}
+
+/**
+ * Drop the `api_delete_survey` procedure.
+ *
+ * @export
+ * @param {Knex} knex
+ * @return {*}  {Promise<void>}
+ */
+export async function down(knex: Knex): Promise<void> {
+  await knex.raw(`
+    set schema '${DB_SCHEMA}';
+    set search_path = ${DB_SCHEMA},public;
+
+    ALTER TABLE project DROP COLUMN publish_date;
+  `);
+}
