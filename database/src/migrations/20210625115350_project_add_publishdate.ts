3ae1b3343fb115844e23c8151507f818e8bf1ef0
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2021-06-28 11:50:41 -0700

    db migration to add publish column (#389)
    * db migration to add publish column
    
    * clean up
    
    * clean up

---
+++ database/src/migrations/20210625115350_project_add_publishdate.ts
@@ -0,0 +1,29 @@
+import Knex from 'knex';
+
+const DB_SCHEMA = process.env.DB_SCHEMA;
+
+/**
+ * Add the publish_timestamp.
+ *
+ * @export
+ * @param {Knex} knex
+ * @return {*}  {Promise<void>}
+ */
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw(`
+    set schema '${DB_SCHEMA}';
+    set search_path = ${DB_SCHEMA},public;
+
+    ALTER TABLE project ADD COLUMN publish_timestamp timestamptz(6);
+  `);
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.raw(`
+    set schema '${DB_SCHEMA}';
+    set search_path = ${DB_SCHEMA},public;
+
+    ALTER TABLE project DROP COLUMN publish_timestamp;
+  `);
+}
