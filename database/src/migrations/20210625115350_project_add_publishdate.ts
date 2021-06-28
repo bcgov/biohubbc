622e5e2ca4d53771744e5ffc5b0156b0e6f6a71e
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2021-06-28 11:19:47 -0700

    clean up

--- database/src/migrations/20210625115350_project_add_publishdate.ts
+++ database/src/migrations/20210625115350_project_add_publishdate.ts
@@ -2,27 +2,29 @@ import Knex from 'knex';
 
 const DB_SCHEMA = process.env.DB_SCHEMA;
 
-export async function up(knex: Knex): Promise<void> {
-  await knex.raw(`
-    set schema '${DB_SCHEMA}';
-    set search_path = ${DB_SCHEMA},public;
-
-    ALTER TABLE project ADD COLUMN publish_date timestamptz(6);
-  `);
-}
-
 /**
- * Drop the `api_delete_survey` procedure.
+ * Add the publish_timestamp.
  *
  * @export
  * @param {Knex} knex
  * @return {*}  {Promise<void>}
  */
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw(`
+    set schema '${DB_SCHEMA}';
+    set search_path = ${DB_SCHEMA},public;
+
+    ALTER TABLE project ADD COLUMN publish_timestamp timestamptz(6);
+  `);
+}
+
+
 export async function down(knex: Knex): Promise<void> {
   await knex.raw(`
     set schema '${DB_SCHEMA}';
     set search_path = ${DB_SCHEMA},public;
 
-    ALTER TABLE project DROP COLUMN publish_date;
+    ALTER TABLE project DROP COLUMN publish_timestamp;
   `);
 }
