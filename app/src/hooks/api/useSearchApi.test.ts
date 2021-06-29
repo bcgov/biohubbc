1f919f02517297ee74aa390ee24ad4e4ec7a2561
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-06-29 14:41:28 -0700

    add in tests

---
+++ app/src/hooks/api/useSearchApi.test.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import useSearchApi from './useSearchApi';
+
+describe('useSearchApi', () => {
+  let mock: any;
+
+  beforeEach(() => {
+    mock = new MockAdapter(axios);
+  });
+
+  afterEach(() => {
+    mock.restore();
+  });
+
+  it('getSearchResults works as expected', async () => {
+    const res = [
+      {
+        id: '1',
+        name: 'name',
+        objectives: 'objectives',
+        geometry: []
+      }
+    ];
+
+    mock.onGet('api/search').reply(200, res);
+
+    const result = await useSearchApi(axios).getSearchResults();
+
+    expect(result[0].id).toEqual('1');
+  });
+});
