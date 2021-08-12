eece2a118c1dedf4d61ea266127b76fc1fbd605b
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-08-12 16:08:27 -0700

    update snaps

--- app/src/components/map/MapContainer.tsx
+++ app/src/components/map/MapContainer.tsx
@@ -465,13 +465,13 @@ const MapContainer: React.FC<IMapContainerProps> = (props) => {
         <LayersControl.BaseLayer checked name="BC Government">
           <TileLayer url="https://maps.gov.bc.ca/arcgis/rest/services/province/roads_wm/MapServer/tile/{z}/{y}/{x}" />
         </LayersControl.BaseLayer>
-      </LayersControl>
-      <LayersControl.BaseLayer name="Esri Imagery">
+        <LayersControl.BaseLayer name="Esri Imagery">
           <TileLayer
             attribution='&copy; <a href="https://www.esri.com/en-us/arcgis/products/location-services/services/basemaps">ESRI Basemap</a>'
             url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
           />
         </LayersControl.BaseLayer>
+      </LayersControl>
     </LeafletMapContainer>
   );
 };
