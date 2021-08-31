da0cf1b20c97c27ad5807fe176967f2adcf1553a
charlie garrett-jones <charlie.garrettjones@bayseumcom>
2021-08-31 15:52:32 -0700

    replacing populate_submission_message_type.sql

--- database/src/migrations/release.0.24/populate_submission_message_type.sql
+++ database/src/migrations/release.0.24/populate_submission_message_type.sql
@@ -5,7 +5,13 @@ insert into submission_message_type (name, record_effective_date, description, s
 insert into submission_message_type (name, record_effective_date, description, submission_message_class_id) values ('Unknown Header', now(), 'An unknown header term was detected.', (select submission_message_class_id from submission_message_class where name = 'Warning'));
 insert into submission_message_type (name, record_effective_date, description, submission_message_class_id) values ('Missing Required Header', now(), 'A required header term was not detected.', (select submission_message_class_id from submission_message_class where name = 'Error'));
 insert into submission_message_type (name, record_effective_date, description, submission_message_class_id) values ('Missing Recommended Header', now(), 'A recommended header term was detected.', (select submission_message_class_id from submission_message_class where name = 'Warning'));
+insert into submission_message_type (name, record_effective_date, description, submission_message_class_id) values ('Miscellaneous', now(), 'A (temporary) miscellaneous header/row term was detected.', (select submission_message_class_id from submission_message_class where name = 'Error'));
+
+
+
 -- field related
 insert into submission_message_type (name, record_effective_date, description, submission_message_class_id) values ('Missing Required Field', now(), 'A required field was not detected.', (select submission_message_class_id from submission_message_class where name = 'Error'));
 insert into submission_message_type (name, record_effective_date, description, submission_message_class_id) values ('Unexpected Format', now(), 'A field with an unexpected format was detected.', (select submission_message_class_id from submission_message_class where name = 'Error'));
 insert into submission_message_type (name, record_effective_date, description, submission_message_class_id) values ('Out of Range', now(), 'A field with an out of range value was detected.', (select submission_message_class_id from submission_message_class where name = 'Error'));
+insert into submission_message_type (name, record_effective_date, description, submission_message_class_id) values ('Invalid Value', now(), 'A field with a value not from the prescribed list.', (select submission_message_class_id from submission_message_class where name = 'Error'));
+
