55ee9eaf82bb39b50f77f0688f4ffec42284db59
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2021-09-15 13:39:45 -0700

    bit more

--- app/src/features/resources/ResourcesPage.tsx
+++ app/src/features/resources/ResourcesPage.tsx
@@ -14,7 +14,6 @@ import React from 'react';
 import { mdiFileExcelOutline } from '@mdi/js';
 import Icon from '@mdi/react';
 
-
 /**
  * Page to display a list of resources.
  *
@@ -26,69 +25,86 @@ const ResourcesPage: React.FC = () => {
       id: '1',
       name: 'Moose SRB or Composition Survey Skeena',
       url: 'https://nrs.objectstore.gov.bc.ca/gblhvt/templates/Moose_SRB_or_Composition_Survey_Skeena.xlsx',
-      type: mdiFileExcelOutline
+      type: mdiFileExcelOutline,
+      lastModified: 'Today',
+      fileSize: '82 KB'
     },
     {
       id: '2',
       name: 'Moose SRB or Composition Survey Omineca',
       url: 'https://nrs.objectstore.gov.bc.ca/gblhvt/templates/Moose_SRB_or_Composition_Survey_Omineca.xlsx',
-      type: mdiFileExcelOutline
+      type: mdiFileExcelOutline,
+      lastModified: 'Today',
+      fileSize: '64 KB'
     },
     {
       id: '3',
       name: 'Moose SRB or Composition Survey Cariboo',
       url: 'https://nrs.objectstore.gov.bc.ca/gblhvt/templates/Moose_SRB_or_Composition_Survey_Cariboo.xlsx',
-      type: mdiFileExcelOutline
+      type: mdiFileExcelOutline,
+      lastModified: 'Today',
+      fileSize: '60 KB'
     },
     {
       id: '4',
       name: 'Moose SRB or Composition Survey Okanagan',
       url: 'https://nrs.objectstore.gov.bc.ca/gblhvt/templates/Moose_SRB_or_Composition_Survey_Okanagan.xlsx',
-      type: mdiFileExcelOutline
+      type: mdiFileExcelOutline,
+      lastModified: 'Today',
+      fileSize: '63 KB'
     },
     {
       id: '5',
       name: 'Moose SRB or Composition Survey Kootenay',
       url: 'https://nrs.objectstore.gov.bc.ca/gblhvt/templates/Moose_SRB_or_Composition_Survey_Kootenay.xlsx',
-      type: mdiFileExcelOutline
+      type: mdiFileExcelOutline,
+      lastModified: 'Today',
+      fileSize: '63 KB'
     },
     {
       id: '6',
       name: 'Moose Recruitment Survey',
-      url:
-        'https://nrs.objectstore.gov.bc.ca/gblhvt/templates/Moose_Recruitment_Survey.xlsx?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=nr-biohubbc-dlv%2F20210914%2Fca-central-1%2Fs3%2Faws4_request&X-Amz-Date=20210914T212011Z&X-Amz-Expires=300000&X-Amz-Signature=6645f28fe7f569f3d473090d07d735a32219556a1a3b79a6d466d017a7b8a157&X-Amz-SignedHeaders=host',
-      type: mdiFileExcelOutline
+      url: 'https://nrs.objectstore.gov.bc.ca/gblhvt/templates/Moose_Recruitment_Survey.xlsx',
+      type: mdiFileExcelOutline,
+      lastModified: 'Today',
+      fileSize: '36 KB'
     }
   ];
 
-
   const getResourcesList = () => {
+    return (
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Last Modified</TableCell>
+              <TableCell>File Size</TableCell>
+              <TableCell>Type</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody data-testid="resources-table">
+            {resources?.map((row) => (
+              <TableRow key={row.id}>
+                <TableCell>
+                  <Link underline="always" component="button" variant="body2">
+                    <a href={row.url}>{row.name}</a>
+                  </Link>
+                </TableCell>
 
-      return (
-        <TableContainer>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Name</TableCell>
-                <TableCell>Type</TableCell>
+                <TableCell>{row.lastModified}</TableCell>
+                <TableCell>{row.fileSize}</TableCell>
+                <TableCell>
+                  {' '}
+                  <Icon path={row.type} size={1} />
+                </TableCell>
               </TableRow>
-            </TableHead>
-            <TableBody data-testid="resources-table">
-              {resources?.map((row) => (
-                <TableRow key={row.id}>
-                  <TableCell>
-                    <Link underline="always" component="button" variant="body2">
-                      <a href={row.url}>{row.name}</a>
-                    </Link>
-                  </TableCell>
-                  <TableCell> <Icon path={row.type} size={1} /></TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      );
-    };
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  };
   //};
 
   /**
@@ -101,8 +117,7 @@ const ResourcesPage: React.FC = () => {
           <Typography variant="h1">Resources</Typography>
         </Box>
         <Paper>
-          <Box display="flex" alignItems="center" justifyContent="space-between" p={2}>
-          </Box>
+          <Box display="flex" alignItems="center" justifyContent="space-between" p={2}></Box>
           <Divider></Divider>
           {getResourcesList()}
         </Paper>
