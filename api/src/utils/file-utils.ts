6530ddc6e652b2b6104258327daecce8f15171b5
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-09-22 15:50:02 -0700

    fix

--- api/src/utils/file-utils.ts
+++ api/src/utils/file-utils.ts
@@ -4,7 +4,7 @@ import { S3_ROLE } from '../constants/roles';
 import clamd from 'clamdjs';
 
 const scanner =
-  process.env.ENABLE_FILE_VIRUS_SCAN !== 'true'
+  process.env.ENABLE_FILE_VIRUS_SCAN === 'true'
     ? clamd.createScanner(process.env.CLAMAV_HOST || 'clamav', Number(process.env.CLAMAV_PORT) || 3310)
     : null;
 const OBJECT_STORE_BUCKET_NAME = process.env.OBJECT_STORE_BUCKET_NAME || '';
