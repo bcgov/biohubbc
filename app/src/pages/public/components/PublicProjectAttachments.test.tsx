69cd8df3d12f53d88583f9c9b8093cc443193630
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2021-09-27 12:25:32 -0700

    clean up

--- app/src/pages/public/components/PublicProjectAttachments.test.tsx
+++ app/src/pages/public/components/PublicProjectAttachments.test.tsx
@@ -1,4 +1,4 @@
-import { cleanup, fireEvent, render, waitFor } from '@testing-library/react';
+import { cleanup, render, waitFor } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
 import { useBiohubApi } from 'hooks/useBioHubApi';
 import React from 'react';
@@ -7,7 +7,6 @@ import { getProjectForViewResponse } from 'test-helpers/project-helpers';
 import PublicProjectAttachments from './PublicProjectAttachments';
 
 const history = createMemoryHistory();
-
 jest.mock('../../../hooks/useBioHubApi');
 const mockUseBiohubApi = {
   public: {
