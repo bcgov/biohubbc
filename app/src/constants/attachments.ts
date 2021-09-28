4e458964768dce67bf749bed588823964cca30b6
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-09-28 09:59:47 -0700

    fix

--- app/src/constants/attachments.ts
+++ app/src/constants/attachments.ts
@@ -14,5 +14,5 @@ export enum ProjectAttachmentValidExtensions {
   REPORT = '.doc, .docx, .png',
   SPATIAL = '.kml, .gdb, .lyr, .shp, .las, .laz, .zlas, .gml, .gpx, .json',
   VIDEO = '.mp4, .mov, .wmv, .ave',
-  OTHER = '.doc, .docx, .pdf, .xls, .xlsx, .xlsm, .xlsb, .accdb, .mdb, .ods, .shp, .gdb, .kml, .json'
+  OTHER = '.wav, .mp3, .mp4, .wma, .txt, .xls, .xlsx, .xlsm, .xlsb, .accdb, .mdb, .ods, .csv, .gif, .png, .jpg, .jpeg, .svg, .tiff, .bmp, .tif, .doc, .docx, .kml, .gdb, .lyr, .shp, .las, .laz, .zlas, .gml, .gpx, .json, .mp4, .mov, .wmv, .ave'
 }
