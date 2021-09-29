4d178c1c4c383f65aca755bcfa9625203000c738
Roland Stens <roland.stens@gmail.com>
2021-09-29 11:31:14 -0700

    Update auto secure

--- database/src/migrations/20210909150006_add_trigger_to_secured_objects.ts
+++ database/src/migrations/20210909150006_add_trigger_to_secured_objects.ts
@@ -24,12 +24,22 @@ export async function up(knex: Knex): Promise<void> {
   -- *******************************************************************
   declare
     v_security_rule_id integer;
-    v_record_id integer;
     v_identity_column character varying;
+    v_record_id integer;
   begin
+    -- Find the right entry in the security rules table for the system rule related to the table
     execute format('select security_rule_id from ${DB_SCHEMA}.security_rule where rule_definition ->> ''target'' = lower(''%1$s'') and system_rule=true', TG_TABLE_NAME) into v_security_rule_id;
-    execute format('select ${DB_SCHEMA}.api_secure_record( %3$s, lower(''%1$s''), %2$s, ${DB_SCHEMA}.api_get_context_user_id())',TG_TABLE_NAME, v_security_rule_id,NEW.project_attachment_id);
-    update ${DB_SCHEMA}.security set project_id = NEW.project_id where security_token = NEW.security_token;
+
+    SELECT "1" into v_record_id FROM (SELECT NEW.*) as t("1");
+
+    -- Secure the record
+    execute format('select ${DB_SCHEMA}.api_secure_record( %3$s, lower(''%1$s''), %2$s, ${DB_SCHEMA}.api_get_context_user_id())',TG_TABLE_NAME, v_security_rule_id,v_record_id);
+
+   -- todo
+   -- if NEW.project_id then
+   --   update ${DB_SCHEMA}.security set project_id = NEW.project_id where security_token = NEW.security_token;
+   -- end if;
+
     return NEW;
   end;
 
@@ -50,6 +60,11 @@ export async function up(knex: Knex): Promise<void> {
       FOR EACH ROW
       EXECUTE PROCEDURE ${DB_SCHEMA}.tr_secure_record_trigger();
 
+      CREATE TRIGGER secure_record
+      AFTER INSERT
+      ON ${DB_SCHEMA}.survey_attachment
+      FOR EACH ROW
+      EXECUTE PROCEDURE ${DB_SCHEMA}.tr_secure_record_trigger();
   `);
 }
 
