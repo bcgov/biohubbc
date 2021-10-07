f9b056ac1c0ea3d08cbb00ae5a563459fd97fb98
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-10-07 11:03:38 -0700

    wip

--- app/src/layouts/PublicLayout.tsx
+++ app/src/layouts/PublicLayout.tsx
@@ -1,15 +1,37 @@
 import Box from '@material-ui/core/Box';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import Alert from '@material-ui/lab/Alert';
 import Footer from 'components/layout/Footer';
 import Header from 'components/layout/Header';
 import { DialogContextProvider } from 'contexts/dialogContext';
-import React from 'react';
+import React, { useState } from 'react';
 
 const PublicLayout: React.FC = (props) => {
+  const [showUnsupportedBrowserAlert, setShowUnsupportedBrowserAlert] = useState<boolean>(true);
+
+  function isSupportedBrowser() {
+    if (
+      navigator.userAgent.indexOf('Chrome') != -1 ||
+      navigator.userAgent.indexOf('Firefox') != -1 ||
+      navigator.userAgent.indexOf('Safari') != -1 ||
+      navigator.userAgent.indexOf('Edge') != -1
+    ) {
+      return true;
+    }
+
+    return false;
+  }
+
   return (
     <Box height="100vh" display="flex" flexDirection="column">
       <CssBaseline />
       <DialogContextProvider>
+        {!isSupportedBrowser() && showUnsupportedBrowserAlert && (
+          <Alert onClose={() => setShowUnsupportedBrowserAlert(false)} severity="error">
+            This is an unsupported browser. Some functionality may not work as expected.
+          </Alert>
+        )}
+
         <Header />
 
         <Box component="main" flex="1 1 auto">
