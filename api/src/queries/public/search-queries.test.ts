ba485acf4ffe1d2d37dc4df498ac97ceec8d9d3d
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-10-08 11:01:40 -0700

    tests

--- api/src/queries/public/search-queries.test.ts
+++ api/src/queries/public/search-queries.test.ts
@@ -3,7 +3,7 @@ import { describe } from 'mocha';
 import { getPublicSpatialSearchResultsSQL } from './search-queries';
 
 describe('getPublicSpatialSearchResultsSQL', () => {
-  it('returns a SQLStatement when valid filter fields provided (project)', () => {
+  it('returns a non null result', () => {
     const response = getPublicSpatialSearchResultsSQL();
 
     expect(response).to.not.be.null;
