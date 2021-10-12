7a7f67af8937b25d45a19ae43dac4796ba060310
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-10-12 11:47:36 -0700

    more

--- api/src/paths/access-request.test.ts
+++ api/src/paths/access-request.test.ts
@@ -3,7 +3,9 @@ import { describe } from 'mocha';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import * as access_request from './access-request';
-// import * as db from '../database/db';
+import * as user_queries from '../queries/users/user-queries';
+import * as db from '../database/db';
+import SQL from 'sql-template-strings';
 
 chai.use(sinonChai);
 
@@ -12,33 +14,34 @@ describe('updateAccessRequest', () => {
     sinon.restore();
   });
 
-  // const dbConnectionObj = {
-  //   systemUserId: () => {
-  //     return null;
-  //   },
-  //   open: async () => {
-  //     // do nothing
-  //   },
-  //   release: () => {
-  //     // do nothing
-  //   },
-  //   commit: async () => {
-  //     // do nothing
-  //   },
-  //   rollback: async () => {
-  //     // do nothing
-  //   },
-  //   query: async () => {
-  //     // do nothing
-  //   }
-  // };
+  const dbConnectionObj = {
+    systemUserId: () => {
+      return null;
+    },
+    open: async () => {
+      // do nothing
+    },
+    release: () => {
+      // do nothing
+    },
+    commit: async () => {
+      // do nothing
+    },
+    rollback: async () => {
+      // do nothing
+    },
+    query: async () => {
+      // do nothing
+    }
+  };
 
   const sampleReq = {
     keycloak_token: {},
     body: {
       userIdentifier: 1,
       identitySource: 'identitySource',
-      requestId: 1
+      requestId: 1,
+      requestStatusTypeId: 1
     }
   } as any;
 
@@ -115,6 +118,54 @@ describe('updateAccessRequest', () => {
     } catch (actualError) {
       expect(actualError.status).to.equal(400);
       expect(actualError.message).to.equal('Missing required body param: requestStatusTypeId');
+    }
+  });
+
+  it('should throw a 400 error when fails to get getUserByUserIdentifierSQL statement', async () => {
+    sinon.stub(db, 'getDBConnection').returns({
+      ...dbConnectionObj,
+      systemUserId: () => {
+        return 20;
+      }
+    });
+    sinon.stub(user_queries, 'getUserByUserIdentifierSQL').returns(null);
+
+    try {
+      const result = access_request.updateAccessRequest();
+
+      await result(sampleReq, (null as unknown) as any, (null as unknown) as any);
+      expect.fail();
+    } catch (actualError) {
+      expect(actualError.status).to.equal(400);
+      expect(actualError.message).to.equal('Failed to build SQL get statement');
+    }
+  });
+
+  it('should throw a 400 error when no userId and no systemUserId', async () => {
+    const mockQuery = sinon.stub();
+
+    mockQuery.resolves({
+      rows: [null],
+      rowCount: 1
+    });
+
+    sinon.stub(db, 'getDBConnection').returns({
+      ...dbConnectionObj,
+      systemUserId: () => {
+        return null;
+      },
+      query: mockQuery
+    });
+    sinon.stub(user_queries, 'getUserByUserIdentifierSQL').returns(SQL`something`);
+
+    try {
+      const result = access_request.updateAccessRequest();
+
+      await result(sampleReq, (null as unknown) as any, (null as unknown) as any);
+      expect.fail();
+    } catch (actualError) {
+      expect(actualError.status).to.equal(400);
+      expect(actualError.message).to.equal('Failed to identify system user ID');
     }
   });
 });
