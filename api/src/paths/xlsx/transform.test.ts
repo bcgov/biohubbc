c6fc8f4d845c767a8480c3b127ab2cf1bf8c77b3
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-10-13 15:43:47 -0700

    fix

--- api/src/paths/xlsx/transform.test.ts
+++ api/src/paths/xlsx/transform.test.ts
@@ -2,7 +2,10 @@ import chai, { expect } from 'chai';
 import { describe } from 'mocha';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
+import { getMockDBConnection } from '../../__mocks__/db';
 import * as transform from './transform';
+import * as validate from './validate';
+import * as db from '../../database/db';
 
 chai.use(sinonChai);
 
@@ -42,5 +45,62 @@ describe('persistParseErrors', () => {
     await result({ ...sampleReq, parseError: 'some error exists' }, sampleRes as any, (null as unknown) as any);
 
     expect(actualResult).to.eql({ status: 'failed', reason: 'Unable to parse submission' });
+  });
+});
+
+describe('getTransformationSchema', () => {
+  const sampleReq = {
+    keycloak_token: {},
+    body: {
+      occurrence_submission_id: 1
+    }
+  } as any;
+
+  let actualResult: any = null;
+
+  const sampleRes = {
+    status: () => {
+      return {
+        json: (result: any) => {
+          actualResult = result;
+        }
+      };
+    }
+  };
+
+  const dbConnectionObj = getMockDBConnection();
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('should return with a failed status if no transformationSchema', async () => {
+    sinon.stub(db, 'getDBConnection').returns({ ...dbConnectionObj, systemUserId: () => 20 });
+    sinon.stub(validate, 'getTemplateMethodologySpecies').resolves({
+      transform: null
+    });
+
+    const result = transform.getTransformationSchema();
+    await result(sampleReq, sampleRes as any, (null as unknown) as any);
+
+    expect(actualResult).to.eql({
+      status: 'failed',
+      reason: 'Unable to fetch an appropriate transformation schema for your submission'
+    });
+  });
+
+  it('should set the transformationSchema in the request and call next on success', async () => {
+    const nextSpy = sinon.spy();
+
+    sinon.stub(db, 'getDBConnection').returns({ ...dbConnectionObj, systemUserId: () => 20 });
+    sinon.stub(validate, 'getTemplateMethodologySpecies').resolves({
+      transform: 'transform'
+    });
+
+    const result = transform.getTransformationSchema();
+    await result(sampleReq, (null as unknown) as any, (nextSpy) as any);
+
+    expect(sampleReq.transformationSchema).to.eql('transform');
+    expect(nextSpy).to.have.been.called;
   });
 });
