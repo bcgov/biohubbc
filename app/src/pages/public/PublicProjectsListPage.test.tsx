3d6e1428c1bb7d5f73e2fa1f91c6b558dcffef2b
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-10-13 16:55:21 -0700

    tsts

---
+++ app/src/pages/public/PublicProjectsListPage.test.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { cleanup, render, waitFor } from '@testing-library/react';
+import PublicProjectsListPage from './PublicProjectsListPage';
+import { useBiohubApi } from 'hooks/useBioHubApi';
+
+jest.mock('../../hooks/useBioHubApi');
+const mockUseBiohubApi = {
+  public: {
+    project: {
+      getProjectsList: jest.fn()
+    }
+  }
+};
+
+const mockBiohubApi = ((useBiohubApi as unknown) as jest.Mock<typeof mockUseBiohubApi>).mockReturnValue(
+  mockUseBiohubApi
+);
+
+describe('PublicProjectsListPage', () => {
+  beforeEach(() => {
+    mockBiohubApi().public.project.getProjectsList.mockClear();
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
+
+  test('renders with a proper list of projects when published', async () => {
+    mockBiohubApi().public.project.getProjectsList.mockResolvedValue([
+      {
+        id: 1,
+        name: 'Project 1',
+        start_date: null,
+        end_date: null,
+        coordinator_agency: 'coordinator agency',
+        project_type: 'project type',
+        permits_list: '1, 2, 3',
+        publish_status: 'Published'
+      }
+    ]);
+
+    const { asFragment, getByTestId } = render(
+      <MemoryRouter>
+        <PublicProjectsListPage />
+      </MemoryRouter>
+    );
+
+    await waitFor(() => {
+      expect(getByTestId('project-table')).toBeInTheDocument();
+      expect(asFragment()).toMatchSnapshot();
+    });
+  });
+
+  test('renders with a proper list of projects when unpublished', async () => {
+    mockBiohubApi().public.project.getProjectsList.mockResolvedValue([
+      {
+        id: 1,
+        name: 'Project 1',
+        start_date: null,
+        end_date: null,
+        coordinator_agency: 'coordinator agency',
+        project_type: 'project type',
+        permits_list: '1, 2, 3',
+        publish_status: 'Unpublished'
+      }
+    ]);
+
+    const { asFragment, getByTestId } = render(
+      <MemoryRouter>
+        <PublicProjectsListPage />
+      </MemoryRouter>
+    );
+
+    await waitFor(() => {
+      expect(getByTestId('project-table')).toBeInTheDocument();
+      expect(asFragment()).toMatchSnapshot();
+    });
+  });
+});
