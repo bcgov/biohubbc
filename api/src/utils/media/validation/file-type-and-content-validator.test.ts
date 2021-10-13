0771f7e7d43cec9ed17eedfca31ef93101a265eb
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2021-10-13 08:20:08 -0700

    test

--- api/src/utils/media/validation/file-type-and-content-validator.test.ts
+++ api/src/utils/media/validation/file-type-and-content-validator.test.ts
@@ -1,9 +1,15 @@
 import { expect } from 'chai';
 import { describe } from 'mocha';
-import { DWCArchive, DWCArchiveValidator } from '../dwc/dwc-archive-file';
+//import { CSVWorkBook, CSVWorksheet } from '../csv/csv-file';
+import xlsx from 'xlsx';
+import { DEFAULT_XLSX_SHEET, DWCArchive, DWCArchiveValidator } from '../dwc/dwc-archive-file';
 import { ArchiveFile, IMediaFile, MediaFile, MediaValidation } from '../media-file';
 import { XLSXCSV, XLSXCSVValidator } from '../xlsx/xlsx-file';
-import { getFileEmptyValidator, getFileMimeTypeValidator } from './file-type-and-content-validator';
+import {
+  getFileEmptyValidator,
+  getFileMimeTypeValidator,
+  getRequiredFilesValidator
+} from './file-type-and-content-validator';
 
 describe('getFileEmptyValidator', () => {
   it('adds an error when the buffer is empty', () => {
@@ -92,5 +98,99 @@ describe('getFileMimeTypeValidator', () => {
     validator(xlsxCSV);
 
     expect(xlsxCSV.mediaValidation.fileErrors).to.eql([]);
+  });
+
+  it('adds no errors when config it not found', () => {
+    const validMimetypes = undefined;
+
+    const validator = getFileMimeTypeValidator(validMimetypes) as DWCArchiveValidator;
+
+    const mediaFile = new MediaFile('otherName', 'otherMime', Buffer.from(''));
+
+    const archiveFile = new ArchiveFile('testName', 'validMime', Buffer.from(''), [mediaFile]);
+
+    const dwcArchive = new DWCArchive(archiveFile);
+
+    validator(dwcArchive);
+
+    expect(dwcArchive.mediaValidation.fileErrors).to.eql([]);
+  });
+});
+
+describe('getRequiredFilesValidator', () => {
+  it('adds no error an when the submitted file list is undefined', () => {
+    const submissionRequiredFilesValidatorConfig = undefined;
+
+    const validator = getRequiredFilesValidator(submissionRequiredFilesValidatorConfig) as XLSXCSVValidator;
+
+    const mediaFile = new MediaFile('testName', 'validMime', Buffer.from(''));
+
+    const xlsxCSV = new XLSXCSV(mediaFile);
+
+    validator(xlsxCSV);
+
+    expect(xlsxCSV.mediaValidation.fileErrors).to.eql([]);
+  });
+
+  it('adds no error an when the required file list is empty', () => {
+    const submissionRequiredFilesValidatorConfig = {
+      submission_required_files_validator: {
+        required_files: []
+      }
+    };
+
+    const validator = getRequiredFilesValidator(submissionRequiredFilesValidatorConfig) as XLSXCSVValidator;
+
+    const mediaFile = new MediaFile('testName', 'validMime', Buffer.from(''));
+
+    const xlsxCSV = new XLSXCSV(mediaFile);
+
+    validator(xlsxCSV);
+
+    expect(xlsxCSV.mediaValidation.fileErrors).to.eql([]);
+  });
+
+  it('checks that a submission is a valid DWCArchive, and adds an error if a required file is missing', () => {
+    const submissionRequiredFilesValidatorConfig = {
+      submission_required_files_validator: {
+        required_files: ['event', 'occurrence']
+      }
+    };
+
+    const validator = getRequiredFilesValidator(submissionRequiredFilesValidatorConfig) as DWCArchiveValidator;
+
+    const archiveFile = new ArchiveFile('testName', 'validMime', Buffer.from(''), [
+      new MediaFile('occurrence', 'b', Buffer.from(''))
+    ]);
+
+    const xlsxCSV = new DWCArchive(archiveFile);
+
+    validator(xlsxCSV);
+
+    expect(xlsxCSV.mediaValidation.fileErrors).to.eql(['Missing required file: event']);
+  });
+
+  it('checks that a submission is a valid XLSXCSV, and adds an error if a required file is missing', () => {
+    const submissionRequiredFilesValidatorConfig = {
+      submission_required_files_validator: {
+        required_files: ['sheet2']
+      }
+    };
+
+    const validator = getRequiredFilesValidator(submissionRequiredFilesValidatorConfig) as XLSXCSVValidator;
+
+    const newWorkbook = xlsx.utils.book_new();
+
+    const worksheet = xlsx.utils.aoa_to_sheet([[]]);
+
+    xlsx.utils.book_append_sheet(newWorkbook, worksheet, DEFAULT_XLSX_SHEET);
+
+    const mediaFile = new MediaFile('worksheet', 'validMime', Buffer.from(''));
+
+    const xlsxCSV = new XLSXCSV(mediaFile);
+
+    validator(xlsxCSV);
+
+    expect(xlsxCSV.mediaValidation.fileErrors).to.eql(['Missing required sheet: sheet2']);
   });
 });
