395ac9d18771dcc608b2adaf4ad1956532e30787
Nick Phura <nickphura@gmail.com>
2021-10-15 15:56:33 -0700

    Update utm regex

--- api/src/utils/spatial-utils.ts
+++ api/src/utils/spatial-utils.ts
@@ -12,7 +12,7 @@ const SOPUTH_UTM_BASE_ZONE_NUMBER = 32700;
 const NORTH_UTM_ZONE_LETTERS = ['N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X'];
 const SOUTH_UTM_ZONE_LETTERS = ['C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M'];
 
-const UTM_STRING_FORMAT = RegExp(/^[1-9]{0,1}[0-9][NPQRSTUVWXCDEFGHJKLM]{0,1} [0-9]+ [0-9]+$/i);
+const UTM_STRING_FORMAT = RegExp(/^[1-9]?[0-9][NPQRSTUVWXCDEFGHJKLM]? [0-9]+ [0-9]+$/i);
 
 /**
  * Parses a UTM string of the form: `9N 573674 6114170`
