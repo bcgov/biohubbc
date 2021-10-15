08c6d156f74e96f2901189b3f59d79e9545342aa
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-10-15 11:12:24 -0700

    tests

--- api/src/paths/project/{projectId}/view.test.ts
+++ api/src/paths/project/{projectId}/view.test.ts
@@ -90,4 +90,134 @@ describe('getProjectForView', () => {
       expect(actualError.message).to.equal('Failed to build SQL get statement');
     }
   });
+
+  it('should throw a 400 error when no sql statement returned for getActivitiesByProjectSQL', async () => {
+    sinon.stub(db, 'getDBConnection').returns({
+      ...dbConnectionObj,
+      systemUserId: () => {
+        return 20;
+      }
+    });
+
+    sinon.stub(project_view_queries, 'getProjectSQL').returns(SQL`some`);
+    sinon.stub(project_view_queries, 'getProjectPermitsSQL').returns(SQL`some`);
+    sinon.stub(project_view_update_queries, 'getLocationByProjectSQL').returns(SQL`some`);
+    sinon.stub(project_view_update_queries, 'getActivitiesByProjectSQL').returns(null);
+
+    try {
+      const result = view.getProjectForView();
+
+      await result(sampleReq, (null as unknown) as any, (null as unknown) as any);
+      expect.fail();
+    } catch (actualError) {
+      expect(actualError.status).to.equal(400);
+      expect(actualError.message).to.equal('Failed to build SQL get statement');
+    }
+  });
+
+  it('should throw a 400 error when no sql statement returned for getIUCNActionClassificationByProjectSQL', async () => {
+    sinon.stub(db, 'getDBConnection').returns({
+      ...dbConnectionObj,
+      systemUserId: () => {
+        return 20;
+      }
+    });
+
+    sinon.stub(project_view_queries, 'getProjectSQL').returns(SQL`some`);
+    sinon.stub(project_view_queries, 'getProjectPermitsSQL').returns(SQL`some`);
+    sinon.stub(project_view_update_queries, 'getLocationByProjectSQL').returns(SQL`some`);
+    sinon.stub(project_view_update_queries, 'getActivitiesByProjectSQL').returns(SQL`some`);
+    sinon.stub(project_view_queries, 'getIUCNActionClassificationByProjectSQL').returns(null);
+
+    try {
+      const result = view.getProjectForView();
+
+      await result(sampleReq, (null as unknown) as any, (null as unknown) as any);
+      expect.fail();
+    } catch (actualError) {
+      expect(actualError.status).to.equal(400);
+      expect(actualError.message).to.equal('Failed to build SQL get statement');
+    }
+  });
+
+  it('should throw a 400 error when no sql statement returned for getFundingSourceByProjectSQL', async () => {
+    sinon.stub(db, 'getDBConnection').returns({
+      ...dbConnectionObj,
+      systemUserId: () => {
+        return 20;
+      }
+    });
+
+    sinon.stub(project_view_queries, 'getProjectSQL').returns(SQL`some`);
+    sinon.stub(project_view_queries, 'getProjectPermitsSQL').returns(SQL`some`);
+    sinon.stub(project_view_update_queries, 'getLocationByProjectSQL').returns(SQL`some`);
+    sinon.stub(project_view_update_queries, 'getActivitiesByProjectSQL').returns(SQL`some`);
+    sinon.stub(project_view_queries, 'getIUCNActionClassificationByProjectSQL').returns(SQL`some`);
+    sinon.stub(project_view_update_queries, 'getFundingSourceByProjectSQL').returns(null);
+
+    try {
+      const result = view.getProjectForView();
+
+      await result(sampleReq, (null as unknown) as any, (null as unknown) as any);
+      expect.fail();
+    } catch (actualError) {
+      expect(actualError.status).to.equal(400);
+      expect(actualError.message).to.equal('Failed to build SQL get statement');
+    }
+  });
+
+  it('should throw a 400 error when no sql statement returned for getIndigenousPartnershipsByProjectSQL', async () => {
+    sinon.stub(db, 'getDBConnection').returns({
+      ...dbConnectionObj,
+      systemUserId: () => {
+        return 20;
+      }
+    });
+
+    sinon.stub(project_view_queries, 'getProjectSQL').returns(SQL`some`);
+    sinon.stub(project_view_queries, 'getProjectPermitsSQL').returns(SQL`some`);
+    sinon.stub(project_view_update_queries, 'getLocationByProjectSQL').returns(SQL`some`);
+    sinon.stub(project_view_update_queries, 'getActivitiesByProjectSQL').returns(SQL`some`);
+    sinon.stub(project_view_queries, 'getIUCNActionClassificationByProjectSQL').returns(SQL`some`);
+    sinon.stub(project_view_update_queries, 'getFundingSourceByProjectSQL').returns(SQL`some`);
+    sinon.stub(project_view_queries, 'getIndigenousPartnershipsByProjectSQL').returns(null);
+
+    try {
+      const result = view.getProjectForView();
+
+      await result(sampleReq, (null as unknown) as any, (null as unknown) as any);
+      expect.fail();
+    } catch (actualError) {
+      expect(actualError.status).to.equal(400);
+      expect(actualError.message).to.equal('Failed to build SQL get statement');
+    }
+  });
+
+  it('should throw a 400 error when no sql statement returned for getStakeholderPartnershipsByProjectSQL', async () => {
+    sinon.stub(db, 'getDBConnection').returns({
+      ...dbConnectionObj,
+      systemUserId: () => {
+        return 20;
+      }
+    });
+
+    sinon.stub(project_view_queries, 'getProjectSQL').returns(SQL`some`);
+    sinon.stub(project_view_queries, 'getProjectPermitsSQL').returns(SQL`some`);
+    sinon.stub(project_view_update_queries, 'getLocationByProjectSQL').returns(SQL`some`);
+    sinon.stub(project_view_update_queries, 'getActivitiesByProjectSQL').returns(SQL`some`);
+    sinon.stub(project_view_queries, 'getIUCNActionClassificationByProjectSQL').returns(SQL`some`);
+    sinon.stub(project_view_update_queries, 'getFundingSourceByProjectSQL').returns(SQL`some`);
+    sinon.stub(project_view_queries, 'getIndigenousPartnershipsByProjectSQL').returns(SQL`some`);
+    sinon.stub(project_view_update_queries, 'getStakeholderPartnershipsByProjectSQL').returns(null);
+
+    try {
+      const result = view.getProjectForView();
+
+      await result(sampleReq, (null as unknown) as any, (null as unknown) as any);
+      expect.fail();
+    } catch (actualError) {
+      expect(actualError.status).to.equal(400);
+      expect(actualError.message).to.equal('Failed to build SQL get statement');
+    }
+  });
 });
