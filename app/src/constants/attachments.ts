928c13582247b6b1810a0afcbbe5693e6f8e1cae
Nick Phura <nickphura@gmail.com>
2021-10-19 09:33:13 -0700

    Remove png from report type

--- app/src/constants/attachments.ts
+++ app/src/constants/attachments.ts
@@ -11,7 +11,7 @@ export enum ProjectSurveyAttachmentValidExtensions {
   AUDIO = '.wav, .mp3, .mp4, .wma',
   DATA = '.txt, .xls, .xlsx, .xlsm, .xlsb, .accdb, .mdb, .ods, .csv',
   IMAGE = '.gif, .png, .jpg, .jpeg, .svg, .tiff, .bmp, .tif',
-  REPORT = '.doc, .docx, .png, .pdf',
+  REPORT = '.doc, .docx, .pdf',
   SPATIAL = '.kml, .gpx, .zip',
   VIDEO = '.mp4, .mov, .wmv, .ave',
   OTHER = '.wav, .mp3, .mp4, .wma, .txt, .xls, .xlsx, .xlsm, .xlsb, .accdb, .mdb, .ods, .csv, .gif, .png, .jpg, .jpeg, .svg, .tiff, .bmp, .tif, .doc, .docx, .kml, .gdb, .lyr, .shp, .las, .laz, .zlas, .gml, .gpx, .json, .mp4, .mov, .wmv, .ave'
