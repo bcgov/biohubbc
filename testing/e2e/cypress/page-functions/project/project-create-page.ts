db15ba7aeff3cb9780d13483e626b4039d85bfbf
Roland Stens <roland.stens@gmail.com>
2021-10-28 13:30:44 -0700

    Adding Test scripts (#617)
    * Adding Test scripts
    
    * Create project and Survey
    
    * Remove second test file

--- testing/e2e/cypress/page-functions/project/project-create-page.ts
+++ testing/e2e/cypress/page-functions/project/project-create-page.ts
@@ -57,7 +57,11 @@ export function add_permits(navloc, permit_nr, permit_type, sampling) {
     permit_nr || faker.random.number()
   );
   cy.get("#permits\\.\\[0\\]\\.permit_type").focus().type("{enter}");
-  cy.get('div.MuiPaper-root > ul.MuiList-root > li.MuiButtonBase-root',{ includeShadowDom: true }).eq(faker.random.number({ min: 0, max: 2 })).click({ force: true });
+  cy.get("div.MuiPaper-root > ul.MuiList-root > li.MuiButtonBase-root", {
+    includeShadowDom: true,
+  })
+    .eq(faker.random.number({ min: 0, max: 2 }))
+    .click({ force: true });
 }
 
 export function add_locations(description, kml_file) {
@@ -70,7 +74,7 @@ export function add_locations(description, kml_file) {
     "shapes/" + (kml_file || faker.random.number({ min: 1, max: 9 })) + ".kml"
   );
   cy.wait(5000);
-  cy.get('button').contains('Close').click();
+  cy.get("button").contains("Close").click();
 }
 
 export function add_gpx(gpx_file) {
@@ -80,7 +84,7 @@ export function add_gpx(gpx_file) {
     "shapes/" + (gpx_file || faker.random.number({ min: 1, max: 8 })) + ".gpx"
   );
   cy.wait(5000);
-  cy.get('button').contains('Close').click();
+  cy.get("button").contains("Close").click();
 }
 
 export function add_zip(zip_file) {
@@ -90,7 +94,7 @@ export function add_zip(zip_file) {
     "shapes/" + (zip_file || faker.random.number({ min: 1, max: 1 })) + ".zip"
   );
   cy.wait(5000);
-  cy.get('button').contains('Close').click();
+  cy.get("button").contains("Close").click();
 }
 
 export function add_project_info(
@@ -118,10 +122,12 @@ export function add_project_info(
   }
   cy.get("#project_activities").click();
   var i = 0;
-    while (i < faker.random.number({ min: 1, max: 4 })) {
-        cy.get("#project_activities-option-" + faker.random.number({ min: 1, max: 7 })).click();
-        i++;
-    }
+  while (i < faker.random.number({ min: 1, max: 4 })) {
+    cy.get(
+      "#project_activities-option-" + faker.random.number({ min: 1, max: 7 })
+    ).click();
+    i++;
+  }
   cy.get("#start_date").type(
     start_date ||
       "20" +
@@ -151,8 +157,11 @@ export function add_objectives(objectives, caveats) {
   cy.get("#caveats").type(caveats || faker.lorem.paragraph());
 }
 
-export function add_classification(classification, sub_classification1, sub_classification2) {
-
+export function add_classification(
+  classification,
+  sub_classification1,
+  sub_classification2
+) {
   var subclass1_count, subclass2_count;
   cy.get("span.MuiStepLabel-iconContainer").eq(5).click(); // Click on the Navigation bar
   cy.contains("IUCN Conservation Actions Classification").should("be.visible");
@@ -169,9 +178,13 @@ export function add_classification(classification, sub_classification1, sub_clas
     ).click();
   }
 
-  cy.get('#classificationDetails\\.\\[0\\]\\.subClassification1').focus().type("{downarrow}{enter}"); // Select the first Entry
+  cy.get("#classificationDetails\\.\\[0\\]\\.subClassification1")
+    .focus()
+    .type("{downarrow}{enter}"); // Select the first Entry
 
-  cy.get("#classificationDetails\\.\\[0\\]\\.subClassification2").focus().type("{downarrow}{enter}"); // Select the first Entry
+  cy.get("#classificationDetails\\.\\[0\\]\\.subClassification2")
+    .focus()
+    .type("{downarrow}{enter}"); // Select the first Entry
 
   cy.wait(5000);
 }
@@ -179,10 +192,16 @@ export function add_classification(classification, sub_classification1, sub_clas
 export function add_funding(start_date, end_date) {
   cy.get("span.MuiStepLabel-iconContainer").eq(6).click(); // Click on the Navigation bar
   cy.contains("Funding").should("be.visible");
-  cy.get('button[data-testid="add-button"]').contains("Add Funding Source").click();
-  cy.get('#agency_id').focus().type("{downarrow}{enter}");
-  cy.get('#agency_project_id').type(faker.random.number({ min: 1000, max: 9999999 }));
-  cy.get('#funding_amount').type(faker.random.number({ min: 100, max: 100000 }));
+  cy.get('button[data-testid="add-button"]')
+    .contains("Add Funding Source")
+    .click();
+  cy.get("#agency_id").focus().type("{downarrow}{enter}");
+  cy.get("#agency_project_id").type(
+    faker.random.number({ min: 1000, max: 9999999 })
+  );
+  cy.get("#funding_amount").type(
+    faker.random.number({ min: 100, max: 100000 })
+  );
   cy.get("#start_date").type(
     start_date ||
       "20" +
@@ -201,7 +220,7 @@ export function add_funding(start_date, end_date) {
         "-" +
         faker.random.number({ min: 10, max: 28 })
   );
-  cy.get('button').contains("Save Changes").click();
+  cy.get("button").contains("Save Changes").click();
   cy.wait(5000);
 }
 
@@ -209,8 +228,96 @@ export function add_partnerships() {
   cy.get("span.MuiStepLabel-iconContainer").eq(7).click(); // Click on the Navigation bar
   cy.contains("Partnerships").should("be.visible");
 
-  cy.get('#indigenous_partnerships').focus().type("{downarrow}{enter}");
-  cy.get('#stakeholder_partnerships').focus().type("{downarrow}{enter}");
+  cy.get("#indigenous_partnerships").focus().type("{downarrow}{enter}");
+  cy.get("#stakeholder_partnerships").focus().type("{downarrow}{enter}");
+
+  cy.wait(5000);
+}
+
+export function publish_project() {
+  cy.get('button[data-testid="publish-project-button"]')
+    .contains("Publish Project")
+    .should("be.visible");
+  cy.get('button[data-testid="publish-project-button"]').click();
+  cy.get('button[data-testid="publish-project-button"]')
+    .contains("Unpublish Project")
+    .should("be.visible");
+  cy.get('button[data-testid="publish-project-button"]').click();
+  cy.get('button[data-testid="publish-project-button"]')
+    .contains("Publish Project")
+    .should("be.visible");
+  cy.wait(2000);
+}
+
+export function attach_file() {
+  cy.get("a").contains("Attachments").should("be.visible");
+  cy.get("a").contains("Attachments").click();
+  cy.get("h2").contains("Project Attachments").should("be.visible");
+  cy.get("button").contains("Upload").click();
+  cy.wait(1000);
+  cy.get("li").contains("Upload Attachments").click({ force: true });
+  cy.get('input[data-testid="drop-zone-input"]').attachFile("1.xlsx");
+  cy.wait(2000);
+  cy.get("button").contains("Close").click();
+}
+
+export function add_survey() {
+  cy.get("a").contains("Project Details").click();
+  cy.get("h2").contains("Project Details").should("be.visible");
+  cy.get('button[title="Edit General Information"]').click();
+
+  cy.get("input#start_date").then(($input) => {
+    const sdate = $input.val().toString();
+    cy.log("sdate", sdate);
+
+    cy.get("input#end_date").then(($input) => {
+      const edate = $input.val().toString();
+      cy.log("edate", edate);
+
+      cy.get("button").contains("Cancel").click();
+      cy.get("a").contains("Surveys").click();
+
+      cy.get("h2").contains("Surveys").should("be.visible");
+
+      cy.get("button").contains("Create Survey").click();
+      cy.get("h1").contains("Create Survey").should("be.visible");
+
+      cy.get("#survey_name").clear().type(faker.lorem.words());
+
+      cy.log("sdate", sdate);
+      cy.get("#start_date").type(sdate);
+      cy.log("edate", edate);
+      cy.get("#end_date").type(edate);
+    });
+  });
+
+  cy.get("#focal_species").focus().click().type("{downArrow}{enter}");
+
+  cy.get("#ancillary_species")
+    .focus()
+    .click()
+    .type("{downArrow}{downArrow}{enter}");
+  cy.get("#common_survey_methodology_id")
+    .focus()
+    .type("{enter}{downArrow}{enter}");
+  cy.get("#survey_purpose").type(faker.lorem.text());
+  cy.get("#permit_number").click().type("{downArrow}{enter}");
+
+  cy.get("#funding_sources").click().type("{enter}");
+  cy.get("#biologist_first_name").type(faker.name.firstName());
+  cy.get("#biologist_last_name").type(faker.name.lastName());
+
+  cy.get("#survey_area_name").clear().type(faker.lorem.words());
+  cy.get('[data-testid="boundary_file-upload"]').click();
+  cy.get('[data-testid="drop-zone-input"]').attachFile(
+    "shapes/" + faker.random.number({ min: 1, max: 9 }) + ".kml"
+  );
+  cy.wait(5000);
+  cy.get("button").contains("Close").click();
+
+  cy.get('input[name="sedis_procedures_accepted"]').click();
+  cy.get('input[name="foippa_requirements_accepted"]').click();
+  cy.get("button").contains("Save and Exit").click();
 
   cy.wait(5000);
 }
