c022ced66075f43adc1a1173f586aa6286f922ea
Nick Phura <nickphura@gmail.com>
2021-11-08 12:05:10 -0800

    Add migration for missed lookup table populate sql

---
+++ database/src/migrations/20211108115529_populate_summary_message_type.ts
@@ -0,0 +1,31 @@
+import * as fs from 'fs';
+import Knex from 'knex';
+import path from 'path';
+
+const DB_SCHEMA = process.env.DB_SCHEMA;
+
+const DB_RELEASE = 'release.0.34';
+
+export async function up(knex: Knex): Promise<void> {
+  const populate_summary_submission_message_type = fs.readFileSync(
+    path.join(__dirname, DB_RELEASE, 'populate_summary_submission_message_type.sql')
+  );
+
+  await knex.raw(`
+    set schema '${DB_SCHEMA}';
+    set search_path = ${DB_SCHEMA},public;
+
+    ${populate_summary_submission_message_type}
+
+    set search_path = biohub_dapi_v1;
+    set role biohub_api;
+
+    create or replace view summary_submission_message_type as select * from ${DB_SCHEMA}.summary_submission_message_type;
+
+    set role postgres;
+  `);
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.raw('');
+}
