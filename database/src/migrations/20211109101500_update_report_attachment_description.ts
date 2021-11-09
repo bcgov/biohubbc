f952e925fd5b5c9f87b701160f8c5ec10e8d8167
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2021-11-09 10:41:52 -0800

    migration (#637)

---
+++ database/src/migrations/20211109101500_update_report_attachment_description.ts
@@ -0,0 +1,37 @@
+import * as Knex from 'knex';
+
+const DB_SCHEMA = process.env.DB_SCHEMA;
+
+/**
+ * Add a new record to the `proprietor_type` lookup table.
+ *
+ * @export
+ * @param {Knex} knex
+ * @return {*}  {Promise<void>}
+ */
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw(`
+    SET schema '${DB_SCHEMA}';
+    SET search_path = public, biohub_dapi_v1;
+
+    DROP VIEW if exists project_report_attachment;
+    DROP VIEW if exists survey_report_attachment;
+
+    SET search_path = ${DB_SCHEMA};
+
+    ALTER TABLE project_report_attachment ALTER COLUMN description TYPE varchar(3000);
+    ALTER TABLE survey_report_attachment ALTER COLUMN description TYPE varchar(3000);
+
+    SET search_path = biohub_dapi_v1;
+    SET ROLE biohub_api;
+
+    CREATE OR REPLACE VIEW project_report_attachment as SELECT * FROM ${DB_SCHEMA}.project_report_attachment;
+    CREATE OR REPLACE VIEW survey_report_attachment as SELECT * FROM ${DB_SCHEMA}.survey_report_attachment;
+
+    SET ROLE postgres;
+  `);
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.raw('');
+}
