949b5849a43df636fae729b5c624acb5b90fa92e
Charlie Garrett-Jones <charlie.garrettjones@bayseum.com>
2021-11-13 17:10:24 -0800

    BHBC-1457
    - initial commit

--- api/src/queries/dwc/dwc-queries.ts
+++ api/src/queries/dwc/dwc-queries.ts
@@ -3,24 +3,52 @@ import { getLogger } from '../../utils/logger';
 
 const defaultLog = getLogger('queries/dwc/dwc-queries');
 
-// Nick: Run the formatter/linter
-// Nick: See `make format[-fix]` and `make lint[-fix]`.
-
+// TODO: remove
 /**
- * SQL query to get EML for a particular data pacakge.
+ * SQL query to get EML for a particular data package.
  *
  * @param {number} dataPackageId
  * @param {string} suppliedTitle
  * @returns {SQLStatement} sql query object
  */
-export const getDataPackageEMLSQL = (dataPackageId: number, suppliedTitle?: string): SQLStatement | null => {
-  defaultLog.debug({ label: 'getDataPackageEMLSQL', message: 'params', dataPackageId, suppliedTitle });
+// export const getDataPackageEMLSQL = (dataPackageId: number, suppliedTitle?: string): SQLStatement | null => {
+//   defaultLog.debug({ label: 'getDataPackageEMLSQL', message: 'params', dataPackageId, suppliedTitle });
 
-  if (!dataPackageId) {
-    return null;
-  }
+//   if (!dataPackageId) {
+//     return null;
+//   }
 
-  const sqlStatement: SQLStatement = SQL`SELECT api_get_eml_data_package(${dataPackageId}, ${suppliedTitle});`;
+//   const sqlStatement: SQLStatement = SQL`SELECT api_get_eml_data_package(${dataPackageId}, ${suppliedTitle});`;
+
+//   defaultLog.debug({
+//     label: 'getDataPackageEMLSQL',
+//     message: 'sql',
+//     'sqlStatement.text': sqlStatement.text,
+//     'sqlStatement.values': sqlStatement.values
+//   });
+
+//   return sqlStatement;
+// };
+
+/**
+ * SQL query to get submission occurrence record given package ID for a particular survey.
+ *
+ * @param {number} dataPackageId
+ * @returns {SQLStatement} sql query object
+ */
+export const getSurveyOccurrenceSubmissionSQL = (dataPackageId: number): SQLStatement | null => {
+  defaultLog.debug({ label: 'getSurveyOccurrenceSubmissionSQL', message: 'params', dataPackageId });
+
+  const sqlStatement: SQLStatement = SQL`
+    SELECT 
+      os.* 
+    from 
+      occurrence_submission os
+      , occurrence_submission_data_package osdp 
+    where 
+      osdp.data_package_id  = ${dataPackageId}
+      and os.occurrence_submission_id = osdp.occurrence_submission_id;
+  `;
 
   defaultLog.debug({
     label: 'getSurveyOccurrenceSubmissionSQL',
@@ -33,31 +61,54 @@ export const getDataPackageEMLSQL = (dataPackageId: number, suppliedTitle?: stri
 };
 
 /**
- * SQL query to get sumbission occurrence record given package ID for a particular survey.
+ * SQL query to get data package record given package ID.
  *
  * @param {number} dataPackageId
  * @returns {SQLStatement} sql query object
  */
-export const getSurveyOccurrenceSubmissionSQL = (dataPackageId: number): SQLStatement | null => {
-  defaultLog.debug({ label: 'getDataPackageEMLSQL', message: 'params', dataPackageId });
-
-  if (!dataPackageId) {
-    return null;
-  }
+export const getDataPackageSQL = (dataPackageId: number): SQLStatement | null => {
+  defaultLog.debug({ label: 'getDataPackageSQL', message: 'params', dataPackageId });
 
   const sqlStatement: SQLStatement = SQL`
     SELECT 
-      os.* 
+      * 
     from 
-      occurrence_submission os
-      , occurrence_submission_data_package osdp 
+      data_package
     where 
-      osdp.data_package_id  = ${dataPackageId}
-      and os.occurrence_submission_id = osdp.occurrence_submission_id ;
+      data_package_id  = ${dataPackageId};
   `;
 
   defaultLog.debug({
-    label: 'getSurveyOccurrenceSubmissionSQL',
+    label: 'getDataPackageSQL',
+    message: 'sql',
+    'sqlStatement.text': sqlStatement.text,
+    'sqlStatement.values': sqlStatement.values
+  });
+
+  return sqlStatement;
+};
+
+/**
+ * SQL query to get occurrence submission publish date.
+ *
+ * @param {number} occurrenceSubmissionId
+ * @returns {SQLStatement} sql query object
+ */
+export const getPublishedSurveyStatusSQL = (occurrenceSubmissionId: number): SQLStatement | null => {
+  defaultLog.debug({ label: 'getPublishedSurveyStatusSQL', message: 'params', occurrenceSubmissionId });
+
+  const sqlStatement: SQLStatement = SQL`
+    SELECT 
+      *
+    from 
+      survey_status
+    where
+      survey_status = api_get_character_system_constant('OCCURRENCE_SUBMISSION_STATE_PUBLISHED')
+      and occurrence_submission_id  = ${occurrenceSubmissionId};
+  `;
+
+  defaultLog.debug({
+    label: 'getPublishedSurveyStatusSQL',
     message: 'sql',
     'sqlStatement.text': sqlStatement.text,
     'sqlStatement.values': sqlStatement.values
