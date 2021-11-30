2efd367783256f1a35760718ef1589b7c6cce826
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2021-11-30 11:22:07 -0800

    clean up

--- database/src/migrations/20211015105029_validation_schemas_2.ts
+++ database/src/migrations/20211015105029_validation_schemas_2.ts
@@ -62,7 +62,7 @@ const validationSchemas = [
 export async function up(knex: Knex): Promise<void> {
   await knex.raw(`
     set schema '${DB_SCHEMA}';
-    set search_path = ${DB_SCHEMA},public, biohub_dapi_v1;
+    set search_path = ${DB_SCHEMA},public;
   `);
 
   for (const validationSchema of validationSchemas) {
