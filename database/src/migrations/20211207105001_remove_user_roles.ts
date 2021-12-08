a1afa4d174a4ec46fb59d6abeef6b8dfd94bdd18
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2021-12-08 14:30:42 -0800

    update system user roles

--- database/src/migrations/20211207105001_remove_user_roles.ts
+++ database/src/migrations/20211207105001_remove_user_roles.ts
@@ -8,15 +8,15 @@ export async function up(knex: Knex): Promise<void> {
   set schema '${DB_SCHEMA}';
   set search_path = ${DB_SCHEMA},public;
 
-
-  --DELETE FROM system_user_role where system_role_id in (select id from system_role where name in (3,4,5));
+  UPDATE system_user_role SET system_role_id = 5 WHERE system_role_id = 3;
+  UPDATE system_user_role SET system_role_id = 6 WHERE system_role_id = 4;
+  UPDATE system_user_role SET system_role_id = 6 WHERE system_role_id = 5;
 
   --Delete from system_user
 
   DELETE FROM system_role WHERE name = 'Government User';
   DELETE FROM system_role WHERE name = 'External User';
   DELETE FROM system_role WHERE name = 'Public User';
-
 
 
   `);
