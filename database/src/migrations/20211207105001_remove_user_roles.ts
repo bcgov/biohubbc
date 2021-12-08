e080d43bce2b2d3056c62b4df9ba06f894721104
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2021-12-08 13:40:33 -0800

    wip

--- database/src/migrations/20211207105001_remove_user_roles.ts
+++ database/src/migrations/20211207105001_remove_user_roles.ts
@@ -8,9 +8,16 @@ export async function up(knex: Knex): Promise<void> {
   set schema '${DB_SCHEMA}';
   set search_path = ${DB_SCHEMA},public;
 
+
+  --DELETE FROM system_user_role where system_role_id in (select id from system_role where name in (3,4,5));
+
+  --Delete from system_user
+
   DELETE FROM system_role WHERE name = 'Government User';
   DELETE FROM system_role WHERE name = 'External User';
   DELETE FROM system_role WHERE name = 'Public User';
+
+
 
   `);
 }
