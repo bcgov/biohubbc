934a5b9a9ec1ea6e927ffd21db1605e7bd00613c
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2021-12-08 16:17:07 -0800

    fix

--- database/src/migrations/20211207105001_remove_user_roles.ts
+++ database/src/migrations/20211207105001_remove_user_roles.ts
@@ -23,33 +23,31 @@ export async function up(knex: Knex): Promise<void> {
   UPDATE
     system_user_role
   SET
-    system_role_id = (select system_role_id from system_role where name = ${NEW_SYSTEM_ROLE.DATA_ADMINISTRATOR})
+    system_role_id = (select system_role_id from system_role where name = '${NEW_SYSTEM_ROLE.DATA_ADMINISTRATOR}')
   WHERE
-    system_role_id = (select system_role_id from system_role where name = ${OLD_SYSTEM_ROLE.GOVERNMENT_USER});
+    system_role_id = (select system_role_id from system_role where name = '${OLD_SYSTEM_ROLE.GOVERNMENT_USER}');
 
 
   UPDATE
     system_user_role
   SET
-    system_role_id = (select system_role_id from system_role where name = ${NEW_SYSTEM_ROLE.PROJECT_CREATOR})
+    system_role_id = (select system_role_id from system_role where name = '${NEW_SYSTEM_ROLE.PROJECT_CREATOR}')
   WHERE
-    system_role_id = (select system_role_id from system_role where name = ${OLD_SYSTEM_ROLE.EXTERNAL_USER});
-
+    system_role_id = (select system_role_id from system_role where name = '${OLD_SYSTEM_ROLE.EXTERNAL_USER}');
 
   UPDATE
     system_user_role
   SET
-    system_role_id = (select system_role_id from system_role where name = ${NEW_SYSTEM_ROLE.PROJECT_CREATOR})
+    system_role_id = (select system_role_id from system_role where name = '${NEW_SYSTEM_ROLE.PROJECT_CREATOR}')
   WHERE
-    system_role_id = (select system_role_id from system_role where name = ${OLD_SYSTEM_ROLE.PUBLIC_USER});
+    system_role_id = (select system_role_id from system_role where name = '${OLD_SYSTEM_ROLE.PUBLIC_USER}');
 
 
   --Delete from system_user
 
-  DELETE FROM system_role WHERE name = 'Government User';
-  DELETE FROM system_role WHERE name = 'External User';
-  DELETE FROM system_role WHERE name = 'Public User';
-
+  DELETE FROM system_role WHERE name = '${OLD_SYSTEM_ROLE.GOVERNMENT_USER}';
+  DELETE FROM system_role WHERE name = '${OLD_SYSTEM_ROLE.EXTERNAL_USER}';
+  DELETE FROM system_role WHERE name = '${OLD_SYSTEM_ROLE.PUBLIC_USER}';
 
   `);
 }
