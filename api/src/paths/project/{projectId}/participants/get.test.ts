00ec427a9481c60d5c8d606f720e66b19c6b3d53
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2021-12-09 11:34:21 -0800

    Endpoint Tests (#661)

---
+++ api/src/paths/project/{projectId}/participants/get.test.ts
@@ -0,0 +1,128 @@
+import chai, { expect } from 'chai';
+import { describe } from 'mocha';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import * as get_project_participants from './get';
+import * as db from '../../../../database/db';
+import * as project_participation_queries from '../../../../queries/project-participation/project-participation-queries';
+import SQL from 'sql-template-strings';
+import { getMockDBConnection } from '../../../../__mocks__/db';
+import { CustomError } from '../../../../errors/CustomError';
+
+chai.use(sinonChai);
+
+describe('gets a list of project participants', () => {
+  const dbConnectionObj = getMockDBConnection();
+
+  const sampleReq = {
+    keycloak_token: {},
+    body: {},
+    params: {
+      projectId: 1
+    }
+  } as any;
+
+  let actualResult: any = null;
+
+  const sampleRes = {
+    status: () => {
+      return {
+        json: (result: any) => {
+          actualResult = result;
+        }
+      };
+    }
+  };
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('should throw a 400 error when no projectId is provided', async () => {
+    sinon.stub(db, 'getDBConnection').returns(dbConnectionObj);
+
+    try {
+      const result = get_project_participants.getParticipants();
+      await result(
+        { ...sampleReq, params: { ...sampleReq.params, projectId: null } },
+        (null as unknown) as any,
+        (null as unknown) as any
+      );
+      expect.fail();
+    } catch (actualError) {
+      expect((actualError as CustomError).status).to.equal(400);
+      expect((actualError as CustomError).message).to.equal('Missing required param `projectId`');
+    }
+  });
+
+  it('should throw a 400 error when no sql statement returned for getAllProjectParticipants', async () => {
+    sinon.stub(db, 'getDBConnection').returns({
+      ...dbConnectionObj,
+      systemUserId: () => {
+        return 20;
+      }
+    });
+
+    sinon.stub(project_participation_queries, 'getAllProjectParticipants').returns(null);
+
+    try {
+      const result = get_project_participants.getParticipants();
+
+      await result(sampleReq, (null as unknown) as any, (null as unknown) as any);
+      expect.fail();
+    } catch (actualError) {
+      expect((actualError as CustomError).status).to.equal(400);
+      expect((actualError as CustomError).message).to.equal('Failed to build SQL get statement');
+    }
+  });
+
+  it('should throw a 400 error when getAllProjectParticipants query fails', async () => {
+    const mockQuery = sinon.stub();
+
+    mockQuery.resolves({
+      rows: null
+    });
+
+    sinon.stub(db, 'getDBConnection').returns({
+      ...dbConnectionObj,
+      systemUserId: () => {
+        return 20;
+      },
+      query: mockQuery
+    });
+
+    sinon.stub(project_participation_queries, 'getAllProjectParticipants').returns(SQL`something`);
+
+    try {
+      const result = get_project_participants.getParticipants();
+
+      await result(sampleReq, (null as unknown) as any, (null as unknown) as any);
+      expect.fail();
+    } catch (actualError) {
+      expect((actualError as CustomError).status).to.equal(400);
+      expect((actualError as CustomError).message).to.equal('Failed to get project participants');
+    }
+  });
+
+  it('should return participants on success', async () => {
+    const mockQuery = sinon.stub();
+
+    mockQuery.resolves({ rows: [{ id: 1 }] });
+
+    sinon.stub(db, 'getDBConnection').returns({
+      ...dbConnectionObj,
+      systemUserId: () => {
+        return 20;
+      },
+      query: mockQuery
+    });
+
+    sinon.stub(project_participation_queries, 'getAllProjectParticipants').returns(SQL`something`);
+
+    const result = get_project_participants.getParticipants();
+
+    await result(sampleReq, sampleRes as any, (null as unknown) as any);
+
+    expect(actualResult).to.eql({ participants: [{ id: 1 }] });
+  });
+});
