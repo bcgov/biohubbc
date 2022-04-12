55fe6f0092f5aaaeea22bb3c7b2cd40efe0030c8
Nick Phura <nickphura@gmail.com>
2022-04-12 11:11:28 -0700

    Update siteminder url

--- app/src/contexts/configContext.tsx
+++ app/src/contexts/configContext.tsx
@@ -60,7 +60,7 @@ const getLocalConfig = (): IConfig => {
       clientId: process.env.SSO_CLIENT_ID || 'biohubbc'
     },
     SITEMINDER_LOGOUT_URL:
-      process.env.REACT_APP_SITEMINDER_LOGOUT_URL || 'https://logontest.gov.bc.ca/clp-cgi/logoff.cgi',
+      process.env.REACT_APP_SITEMINDER_LOGOUT_URL || 'https://logontest7.gov.bc.ca/clp-cgi/logoff.cgi',
     MAX_UPLOAD_NUM_FILES: Number(process.env.REACT_APP_MAX_UPLOAD_NUM_FILES) || 10,
     MAX_UPLOAD_FILE_SIZE: Number(process.env.REACT_APP_MAX_UPLOAD_FILE_SIZE) || 52428800
   };
