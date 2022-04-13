6cc476b100de29aeebc9d7dea3aa142b92ad0c52
Curtis Upshall <curtisupshall@gmail.com>
2022-04-12 17:17:39 -0700

    BHBC-1658: Merge parent theme correctly.

--- app/src/components/fields/SelectWithSubtext.tsx
+++ app/src/components/fields/SelectWithSubtext.tsx
@@ -4,9 +4,12 @@ import InputLabel from '@material-ui/core/InputLabel';
 import ListItemText from '@material-ui/core/ListItemText';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
+import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core';
 import { useFormikContext } from 'formik';
 import get from 'lodash-es/get';
 import React from 'react';
+import appTheme from 'themes/appTheme';
 
 export interface ISelectWithSubtextFieldOption {
   value: string | number;
@@ -22,36 +25,69 @@ export interface ISelectWithSubtextField {
   required?: boolean;
 }
 
+const selectWithSubtextTheme = createMuiTheme({
+  ...appTheme,
+  overrides: {
+    ...appTheme?.overrides || {},
+    MuiMenu: {
+      paper: {
+        maxWidth: 500,
+        maxHeight: 500
+      }
+    },
+    MuiMenuItem: {
+      root: {
+        whiteSpace: 'break-spaces',
+        borderBottom: '1px solid rgba(0, 0, 0, 0.12)'
+      },
+    },
+    MuiListItemText: {
+      primary: {
+        fontWeight: 700
+      }
+    },
+    MuiTypography: {
+      body1: {
+        '& + p': {
+          marginTop: 0
+        }
+      }
+    }
+  }
+})
+
 const SelectWithSubtextField: React.FC<ISelectWithSubtextField> = (props) => {
   const { values, touched, errors, handleChange } = useFormikContext<ISelectWithSubtextFieldOption>();
 
   return (
-    <FormControl fullWidth variant="outlined" required={props.required} style={{ width: '100%' }}>
-      <InputLabel id={`${props.name}-label`}>{props.label}</InputLabel>
-      <Select
-        id={props.id}
-        name={props.name}
-        labelId={`${props.name}-label`}
-        label={props.label}
-        value={get(values, props.name)}
-        labelWidth={300}
-        onChange={handleChange}
-        error={get(touched, props.name) && Boolean(get(errors, props.name))}
-        displayEmpty
-        inputProps={{ 'aria-label': props.label }}
-        renderValue={(value) => {
-          // convert the selected `value` back into its matching `label`
-          const code = props.options.find((item) => item.value === value);
-          return <>{code?.label}</>;
-        }}>
-        {props.options.map((item) => (
-          <MenuItem key={item.value} value={item.value}>
-            <ListItemText primary={item.label} secondary={item.subText} />
-          </MenuItem>
-        ))}
-      </Select>
-      <FormHelperText>{get(touched, props.name) && get(errors, props.name)}</FormHelperText>
-    </FormControl>
+    <ThemeProvider theme={selectWithSubtextTheme}>
+      <FormControl fullWidth variant="outlined" required={props.required} style={{ width: '100%' }}>
+        <InputLabel id={`${props.name}-label`}>{props.label}</InputLabel>
+        <Select
+          id={props.id}
+          name={props.name}
+          labelId={`${props.name}-label`}
+          label={props.label}
+          value={get(values, props.name)}
+          labelWidth={300}
+          onChange={handleChange}
+          error={get(touched, props.name) && Boolean(get(errors, props.name))}
+          displayEmpty
+          inputProps={{ 'aria-label': props.label }}
+          renderValue={(value) => {
+            // convert the selected `value` back into its matching `label`
+            const code = props.options.find((item) => item.value === value);
+            return <>{code?.label}</>;
+          }}>
+          {props.options.map((item) => (
+            <MenuItem key={item.value} value={item.value}>
+              <ListItemText primary={item.label} secondary={item.subText} />
+            </MenuItem>
+          ))}
+        </Select>
+        <FormHelperText>{get(touched, props.name) && get(errors, props.name)}</FormHelperText>
+      </FormControl>
+    </ThemeProvider>
   );
 };
 
--- app/src/features/surveys/components/PurposeAndMethodologyForm.tsx
+++ app/src/features/surveys/components/PurposeAndMethodologyForm.tsx
@@ -8,9 +8,6 @@ import MultiAutocompleteFieldVariableSize, {
 import SelectWithSubtextField, { ISelectWithSubtextFieldOption } from 'components/fields/SelectWithSubtext';
 import React from 'react';
 import yup from 'utils/YupSchema';
-import { ThemeProvider } from '@material-ui/styles';
-import appTheme from 'themes/appTheme';
-import { createMuiTheme } from '@material-ui/core';
 
 export interface IPurposeAndMethodologyForm {
   intended_outcome_id: number;
@@ -43,29 +40,6 @@ export interface IPurposeAndMethodologyFormProps {
   vantage_codes: IMultiAutocompleteFieldOption[];
 }
 
-const selectWithSubheadersTheme = createMuiTheme({
-  typography: appTheme.typography,
-  overrides: {
-    MuiMenu: {
-      paper: {
-        maxWidth: 500,
-        maxHeight: 500
-      }
-    },
-    MuiMenuItem: {
-      root: {
-        whiteSpace: 'break-spaces',
-        borderBottom: '1px solid rgba(0, 0, 0, 0.12)'
-      },
-    },
-    MuiListItemText: {
-      primary: {
-        fontWeight: 700
-      }
-    }
-  }
-})
-
 /**
  * Create survey - general information fields
  *
@@ -73,56 +47,54 @@ const selectWithSubheadersTheme = createMuiTheme({
  */
 const PurposeAndMethodologyForm: React.FC<IPurposeAndMethodologyFormProps> = (props) => {
   return (
-    <ThemeProvider theme={selectWithSubheadersTheme}>
-      <form>
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <Box component="fieldset" mt={4}>
-              <Typography component="legend">Purpose of Survey</Typography>
-            </Box>
-            <SelectWithSubtextField
-              id="intended_outcome_id"
-              name="intended_outcome_id"
-              label="Intended Outcomes"
-              options={props.intended_outcomes}
-              required={true}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <CustomTextField name="additional_details" label="Additional Details" other={{ multiline: true, rows: 2 }} />
-          </Grid>
-          <Grid item xs={12}>
-            <Box component="fieldset" mt={4}>
-              <Typography component="legend">Survey Methodology</Typography>
-            </Box>
-            <SelectWithSubtextField
-              id="field_method_id"
-              name="field_method_id"
-              label="Field Method"
-              options={props.field_methods}
-              required={true}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <SelectWithSubtextField
-              id="ecological_season_id"
-              name="ecological_season_id"
-              label="Ecological Season"
-              options={props.ecological_seasons}
-              required={true}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <MultiAutocompleteFieldVariableSize
-              id="vantage_code_ids"
-              label="Vantage Code"
-              options={props.vantage_codes}
-              required={true}
-            />
-          </Grid>
+    <form>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Box component="fieldset" mt={4}>
+            <Typography component="legend">Purpose of Survey</Typography>
+          </Box>
+          <SelectWithSubtextField
+            id="intended_outcome_id"
+            name="intended_outcome_id"
+            label="Intended Outcomes"
+            options={props.intended_outcomes}
+            required={true}
+          />
         </Grid>
-      </form>
-    </ThemeProvider>
+        <Grid item xs={12}>
+          <CustomTextField name="additional_details" label="Additional Details" other={{ multiline: true, rows: 2 }} />
+        </Grid>
+        <Grid item xs={12}>
+          <Box component="fieldset" mt={4}>
+            <Typography component="legend">Survey Methodology</Typography>
+          </Box>
+          <SelectWithSubtextField
+            id="field_method_id"
+            name="field_method_id"
+            label="Field Method"
+            options={props.field_methods}
+            required={true}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <SelectWithSubtextField
+            id="ecological_season_id"
+            name="ecological_season_id"
+            label="Ecological Season"
+            options={props.ecological_seasons}
+            required={true}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <MultiAutocompleteFieldVariableSize
+            id="vantage_code_ids"
+            label="Vantage Code"
+            options={props.vantage_codes}
+            required={true}
+          />
+        </Grid>
+      </Grid>
+    </form>
   );
 };
 
