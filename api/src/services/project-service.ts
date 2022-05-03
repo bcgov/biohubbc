9981e686a941088c5c4afc7cbadf8cf7ed278c79
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2022-05-03 15:30:15 -0700

    public attachments received attachment/reports && secured/unsecured correctly

--- api/src/services/project-service.ts
+++ api/src/services/project-service.ts
@@ -256,6 +256,18 @@ export class ProjectService extends DBService {
       this.getPartnershipsData(projectId)
     ]);
 
+    console.log(
+      'object returned is: ',
+      projectId,
+      projectData,
+      objectiveData,
+      coordinatorData,
+      permitData,
+      iucnData,
+      fundingData,
+      partnershipsData
+    );
+
     return {
       id: projectId,
       project: projectData,
