af1c7ee5fd7c5a7cb7b728e89bf902d341701825
AlfredRosenthal <alfred.rosenthal@quartech.com>
2022-09-02 16:32:40 -0700

    added migration

---
+++ database/src/migrations/20220902155400_moose_summary_results_schema_and_validation.ts
@@ -0,0 +1,66 @@
+import { Knex } from 'knex';
+import { COMMON_SURVEY_METHODOLOGY, TEMPLATE_NAME } from './common/common';
+import { mooseSummary } from './template_methodology_species_validations/moose_summary_results_1';
+
+const DB_SCHEMA = process.env.DB_SCHEMA;
+const moose_summary_results = mooseSummary
+
+const validationSchema = [
+  // Common Survey Methodology: Composition, SRB
+  {
+    v_schema: JSON.stringify(moose_summary_results),
+    field_method: COMMON_SURVEY_METHODOLOGY.COMPOSITION,
+    template: TEMPLATE_NAME.MOOSE_SUMMARY_RESULTS
+  }
+];
+
+/**
+ * Populate template validations.
+ *
+ * @export
+ * @param {Knex} knex
+ * @return {*}  {Promise<void>}
+ */
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw(`
+    SET schema '${DB_SCHEMA}';
+    set search_path = ${DB_SCHEMA},public;
+
+    insert into
+      ${DB_SCHEMA}.template (name, version, record_effective_date, description)
+    values
+      ('${TEMPLATE_NAME.MOOSE_SUMMARY_RESULTS}', '1.0', now(), '${TEMPLATE_NAME.MOOSE_SUMMARY_RESULTS}');
+  `);
+
+  for (const v_schema of validationSchema) {
+    await knex.raw(`
+      ${insertValidation(v_schema.v_schema, v_schema.field_method, v_schema.template)}
+    `);
+  }
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.raw(``);
+}
+
+/**
+ * SQL to update the template_methodology_species validation row.
+ * The select statement returns the template_methodology_species_id
+ * The validation field is filled with a JSON schema based on the combination of:
+ * 1) field_method
+ * 2) template_name
+ *
+ * @param {string} validationSchema
+ * @param {string} field_method
+ * @param {string} template_name
+ */
+const insertValidation = (validationSchema: string, field_method: string, template_name: string) => `
+  INSERT INTO
+    ${DB_SCHEMA}.template_methodology_species (field_method_id, template_id, validation)
+  VALUES
+    (
+      (select field_method_id from field_method where name = '${field_method}'),
+      (select template_id from template where name = '${template_name}'),
+      '${validationSchema}'
+    );
+`;
