9884f73687f6323bae36dd27b2c44389f55ac61c
Kjartan <Kjartan.Einarsson@quartech.com>
2022-09-02 13:47:07 -0700

    add transect validation

--- database/src/migrations/20220809133100_update_moose_survey_validation_schemas.ts
+++ database/src/migrations/20220809133100_update_moose_survey_validation_schemas.ts
@@ -1,24 +1,28 @@
 import { Knex } from 'knex';
 import { mooseCompositionJSON } from './template_methodology_species_validations/moose_composition_survey_1';
 import { mooseSrbJSON } from './template_methodology_species_validations/moose_srb_survey_1';
+import { mooseTransectDistanceJSON } from './template_methodology_species_validations/moose_transect_distance_survey_1';
 
 const DB_SCHEMA = process.env.DB_SCHEMA;
 
 const moose_composition_validation = mooseCompositionJSON;
 const moose_srb_validation = mooseSrbJSON;
+const moose_transect_distance = mooseTransectDistanceJSON;
 
 enum COMMON_SURVEY_METHODOLOGY {
   COMPOSITION = 'Composition',
-  STRATIFIED_RANDOM_BLOCK = 'Stratified Random Block'
+  STRATIFIED_RANDOM_BLOCK = 'Stratified Random Block',
+  TRANSECT = 'Encounter Transects'
 }
 
 enum TEMPLATE_NAME {
   MOOSE_COMPOSITION_SURVEY = 'Moose Composition Survey',
-  MOOSE_SRB_SURVEY = 'Moose SRB Survey'
+  MOOSE_SRB_SURVEY = 'Moose SRB Survey',
+  MOOSE_TRANSECT_DISTANCE_SURVEY = 'Moose Transect Distance Survey'
 }
 
 const validationSchema = [
-  // Common Survey Methodology: Composition, SRB
+  // Common Survey Methodology: Composition, SRB, Transect
   {
     v_schema: JSON.stringify(moose_composition_validation),
     field_method: COMMON_SURVEY_METHODOLOGY.COMPOSITION,
@@ -28,8 +32,16 @@ const validationSchema = [
     v_schema: JSON.stringify(moose_srb_validation),
     field_method: COMMON_SURVEY_METHODOLOGY.STRATIFIED_RANDOM_BLOCK,
     template: TEMPLATE_NAME.MOOSE_SRB_SURVEY
+  },
+  {
+    v_schema: JSON.stringify(moose_transect_distance),
+    field_method: COMMON_SURVEY_METHODOLOGY.TRANSECT,
+    template: TEMPLATE_NAME.MOOSE_TRANSECT_DISTANCE_SURVEY
   }
 ];
+
+console.log('validationSchema', validationSchema);
+console.log('v_schema: JSON.stringify(moose_transect_distance)', JSON.stringify(moose_transect_distance));
 
 /**
  * Populate template validations.
@@ -52,6 +64,11 @@ export async function up(knex: Knex): Promise<void> {
       ${DB_SCHEMA}.template (name, version, record_effective_date, description)
     values
       ('${TEMPLATE_NAME.MOOSE_SRB_SURVEY}', '1.0', now(), 'Moose SRB Survey');
+
+    insert into
+      ${DB_SCHEMA}.template (name, version, record_effective_date, description)
+    values
+      ('${TEMPLATE_NAME.MOOSE_TRANSECT_DISTANCE_SURVEY}', '1.0', now(), 'Moose Transect Distance Survey');
   `);
 
   for (const v_schema of validationSchema) {
