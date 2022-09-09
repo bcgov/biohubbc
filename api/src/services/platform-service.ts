a9ed3b7622e993674f3e098ebae03d9ce5deeaa1
Kjartan <Kjartan.Einarsson@quartech.com>
2022-09-09 14:47:26 -0700

    remove incorrect edit

--- api/src/services/platform-service.ts
+++ api/src/services/platform-service.ts
@@ -157,10 +157,10 @@ export class PlatformService extends DBService {
     const surveyService = new SurveyService(this.connection);
     const surveyData = await surveyService.getLatestSurveyOccurrenceSubmission(surveyId);
 
-    if (!surveyData.input_key) {
+    if (!surveyData.output_key) {
       throw new HTTP400('no s3Key found');
     }
-    const s3File = await getFileFromS3(surveyData.input_key);
+    const s3File = await getFileFromS3(surveyData.output_key);
 
     if (!s3File) {
       throw new HTTP400('no s3File found');
