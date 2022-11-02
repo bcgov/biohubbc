3c0d660f29b60a1dba0732b4c6fade945f087d20
Kjartan <Kjartan.Einarsson@quartech.com>
2022-11-02 15:06:48 -0700

    permutation test working

--- database/src/migrations/20221014123456_update_templates_ids.ts
+++ database/src/migrations/20221014123456_update_templates_ids.ts
@@ -1,31 +1,25 @@
 import { Knex } from 'knex';
+import { COMMON_SURVEY_METHODOLOGY } from './common/template.enum';
+import { deerAerialNonSRBRecruitCompJSON } from './template_methodology_species_validations/20221021/deer_aerial_non_srb_recruit_comp_survey';
+import { deerGroundTransectRecruitCompJSON } from './template_methodology_species_validations/20221021/deer_ground_transect_recruit_comp_survey';
+import { elkAerialTransectDistanceJSON } from './template_methodology_species_validations/20221021/elk_aerial_transect_distance_1';
+import { elkNonSRBJSON } from './template_methodology_species_validations/20221021/elk_non_srb_survey';
+import { elkSRBJSON } from './template_methodology_species_validations/20221021/elk_srb_survey';
+import { goatRecruitmentCompositionJSON } from './template_methodology_species_validations/20221021/goat_population_recruitment_composition_survey_1';
+import { mooseCompositionJSON } from './template_methodology_species_validations/20221021/moose_composition_survey_1';
+import { mooseSrbJSON } from './template_methodology_species_validations/20221021/moose_srb_survey_1';
+import { mooseTransectDistanceJSON } from './template_methodology_species_validations/20221021/moose_transect_distance_survey_1';
+import { sheepRecruitmentCompositionJSON } from './template_methodology_species_validations/20221021/sheep_population_recruitment_composition_survey_1';
 
 const DB_SCHEMA = process.env.DB_SCHEMA;
 
-enum INTENDED_OUTCOME {
-  HABITAT_ASSESSMENT = '1',
-  RECONNAISSANCE = '2',
-  RECRUITMENT = '3',
-  POPULATION_COMPOSITION = '4',
-  COMMUNITY_COMPOSITION = '5',
-  POPULATION_COUNT = '6',
-  POPULATION_COUNT_RECRUITMENT = '7',
-  POPULATION_COUNT_COMPOSITION = '8',
-  POPULATION_INDEX = '9',
-  MORTALITY = '10',
-  SURVIVAL = '11',
-  SPECIMEN_COLLECTION = '12',
-  TRANSLOCATION = '13',
-  DISTRIBUTION_RANGE_MAP = '14'
-}
-
-enum WILD_TAXON_IDS {
-  MOOSE = '4146', //M_ALAM
-  GOAT = '4164', //M-ORAM
-  SHEEP = '8618', //M-OVDA
-  ELK = '4148', //M-CECA
-  DEER = '4149' //M-ODHE
-}
+const taxonIdLists = {
+  moose: ['4146', '6896', '6897', '6898'],
+  goat: ['4164'],
+  sheep: ['473', '474', '475', '4165', '6904', '8618'],
+  elk: ['2226', '2227', '4148', '6901'],
+  deer: ['477', '6902', '4147', '4149', '4689', '8619']
+};
 
 /**
  * Populate template validations.
@@ -40,30 +34,85 @@ export async function up(knex: Knex): Promise<void> {
     set search_path = ${DB_SCHEMA},public;
   `);
 
-  updateTemplateIds(knex, 3, INTENDED_OUTCOME.RECRUITMENT, WILD_TAXON_IDS.MOOSE); //Moose Recruitment Survey
-  updateTemplateIds(knex, 2, null, WILD_TAXON_IDS.MOOSE); //Moose SRB or Composition Survey
-  updateTemplateIds(knex, 1, null, WILD_TAXON_IDS.MOOSE); //Moose SRB or Composition Survey
+  permutateTemplates(
+    knex,
+    'Moose Composition Survey',
+    [COMMON_SURVEY_METHODOLOGY.COMPOSITION],
+    taxonIdLists.moose,
+    JSON.stringify(mooseCompositionJSON)
+  );
 
-  updateTemplateIds(knex, 4, null, WILD_TAXON_IDS.GOAT); //Goat Composition or Recruitment Survey
-  updateTemplateIds(knex, 5, null, WILD_TAXON_IDS.GOAT); //Goat Composition or Recruitment Survey
+  permutateTemplates(
+    knex,
+    'Moose SRB Survey',
+    [COMMON_SURVEY_METHODOLOGY.STRATIFIED_RANDOM_BLOCK],
+    taxonIdLists.moose,
+    JSON.stringify(mooseSrbJSON)
+  );
 
-  updateTemplateIds(knex, 6, null, WILD_TAXON_IDS.SHEEP); //Sheep Composition or Recruitment Survey
-  updateTemplateIds(knex, 7, null, WILD_TAXON_IDS.SHEEP); //Sheep Composition or Recruitment Survey
+  permutateTemplates(
+    knex,
+    'Moose Transect Distance Survey',
+    [COMMON_SURVEY_METHODOLOGY.ENCOUNTER_TRANSECTS],
+    taxonIdLists.moose,
+    JSON.stringify(mooseTransectDistanceJSON)
+  );
 
-  updateTemplateIds(knex, 8, null, WILD_TAXON_IDS.MOOSE); //Moose Composition Survey
-  updateTemplateIds(knex, 9, null, WILD_TAXON_IDS.MOOSE); //Moose SRB Survey
+  permutateTemplates(
+    knex,
+    'Sheep Population Total Count Recruitment Composition Survey',
+    [COMMON_SURVEY_METHODOLOGY.TOTAL_COUNT],
+    taxonIdLists.sheep,
+    JSON.stringify(sheepRecruitmentCompositionJSON)
+  );
 
-  updateTemplateIds(knex, 10, null, WILD_TAXON_IDS.MOOSE); //Moose Transect Distance Survey
+  permutateTemplates(
+    knex,
+    'Goat Population Total Count Recruitment Composition Survey',
+    [COMMON_SURVEY_METHODOLOGY.TOTAL_COUNT],
+    taxonIdLists.goat,
+    JSON.stringify(goatRecruitmentCompositionJSON)
+  );
 
-  updateTemplateIds(knex, 11, INTENDED_OUTCOME.POPULATION_COUNT_RECRUITMENT, WILD_TAXON_IDS.SHEEP); //Sheep Population Total Count Recruitment Composition Survey
-  updateTemplateIds(knex, 12, INTENDED_OUTCOME.POPULATION_COUNT_RECRUITMENT, WILD_TAXON_IDS.GOAT); //Goat Population Total Count Recruitment Composition Survey
+  permutateTemplates(
+    knex,
+    'Elk Aerial Transect Distance Sampling Recruitment Composition Survey',
+    [COMMON_SURVEY_METHODOLOGY.ENCOUNTER_TRANSECTS],
+    taxonIdLists.goat,
+    JSON.stringify(elkAerialTransectDistanceJSON)
+  );
 
-  updateTemplateIds(knex, 13, INTENDED_OUTCOME.RECRUITMENT, WILD_TAXON_IDS.ELK); //Elk Aerial Transect Distance Sampling Recruitment Composition Survey
-  updateTemplateIds(knex, 14, INTENDED_OUTCOME.RECRUITMENT, WILD_TAXON_IDS.ELK); //Elk Aerial SRB Recruit Composition Survey
-  updateTemplateIds(knex, 15, INTENDED_OUTCOME.RECRUITMENT, WILD_TAXON_IDS.ELK); //Elk Aerial Non SRB Recruit Composition Survey
+  permutateTemplates(
+    knex,
+    'Elk Aerial SRB Recruit Composition Survey',
+    [COMMON_SURVEY_METHODOLOGY.STRATIFIED_RANDOM_BLOCK],
+    taxonIdLists.elk,
+    JSON.stringify(elkSRBJSON)
+  );
 
-  updateTemplateIds(knex, 16, INTENDED_OUTCOME.RECRUITMENT, WILD_TAXON_IDS.DEER); //Deer Aerial Non SRB Recruit Composition Survey
-  updateTemplateIds(knex, 17, INTENDED_OUTCOME.RECRUITMENT, WILD_TAXON_IDS.DEER); //Deer Ground Transect Recruit Composition Survey
+  permutateTemplates(
+    knex,
+    'Elk Aerial Non SRB Recruit Composition Survey',
+    [COMMON_SURVEY_METHODOLOGY.COMPOSITION, COMMON_SURVEY_METHODOLOGY.RECRUITMENT],
+    taxonIdLists.elk,
+    JSON.stringify(elkNonSRBJSON)
+  );
+
+  permutateTemplates(
+    knex,
+    'Deer Aerial Non SRB Recruit Composition Survey',
+    [COMMON_SURVEY_METHODOLOGY.COMPOSITION, COMMON_SURVEY_METHODOLOGY.RECRUITMENT],
+    taxonIdLists.deer,
+    JSON.stringify(deerAerialNonSRBRecruitCompJSON)
+  );
+
+  permutateTemplates(
+    knex,
+    'Deer Ground Transect Recruit Composition Survey',
+    [COMMON_SURVEY_METHODOLOGY.FIXED_WIDTH_TRANSECTS],
+    taxonIdLists.deer,
+    JSON.stringify(deerGroundTransectRecruitCompJSON)
+  );
 }
 
 /*
@@ -87,15 +136,57 @@ export async function down(knex: Knex): Promise<void> {
   await knex.raw(``);
 }
 
-const updateTemplateIds = async (knex: Knex, templateId: number, intendedOutcome: string | null, wldTaxon: string) => {
+const permutateTemplates = async (
+  knex: Knex,
+  templateName: string,
+  fieldMethod: any[],
+  wldTaxon: string[],
+  validationSchema: string
+) => {
+  wldTaxon.forEach((taxon) => {
+    fieldMethod.forEach((field) => {
+      CreateTemplateRows(knex, templateName, field, taxon, validationSchema);
+    });
+  });
+};
+
+const CreateTemplateRows = async (
+  knex: Knex,
+  templateName: string,
+  fieldMethod: string | null,
+  wldTaxon: string,
+  validationSchema: string
+) => {
   await knex.raw(`
-    UPDATE
-      ${DB_SCHEMA}.template_methodology_species tms
-    SET
-      intended_outcome_id = ${intendedOutcome},
-      wldtaxonomic_units_id = ${wldTaxon}
-    WHERE
-      template_methodology_species_id = ${templateId}
+    INSERT INTO
+      ${DB_SCHEMA}.template_methodology_species (field_method_id, template_id, wldtaxonomic_units_id, validation)
+    VALUES
+      (
+        (select field_method_id from field_method where name = '${fieldMethod}'),
+        (select template_id from template where name = '${templateName}'),
+        '${wldTaxon}',
+        '${validationSchema}'
+      )
     ;
   `);
 };
+
+// const updateTemplateIds = async (
+//   knex: Knex,
+//   templateId: number,
+//   fieldMethod: string | null,
+//   intendedOutcome: string | null,
+//   wldTaxon: string
+// ) => {
+//   await knex.raw(`
+//     UPDATE
+//       ${DB_SCHEMA}.template_methodology_species tms
+//     SET
+//       intended_outcome_id = ${intendedOutcome},
+//       field_method_id = (select field_method_id from field_method where name = '${fieldMethod}'),
+//       wldtaxonomic_units_id = ${wldTaxon}
+//     WHERE
+//       template_methodology_species_id = ${templateId}
+//     ;
+//   `);
+// };
