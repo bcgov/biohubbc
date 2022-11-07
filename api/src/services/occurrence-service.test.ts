83e6884a4418310cf71f301498e4789624db4a44
AlfredRosenthal <alfred.rosenthal@quartech.com>
2022-11-07 13:39:04 -0800

    lint-format fix

--- api/src/services/occurrence-service.test.ts
+++ api/src/services/occurrence-service.test.ts
@@ -27,116 +27,115 @@ describe('OccurrenceService', () => {
   describe.only('Get Headers and Rows From DWC Archive', () => {
     it('should return valid information', () => {
       const service = mockService();
-      const dwc = new DWCArchive(new ArchiveFile("test", "zip", Buffer.from([]), []));
+      const dwc = new DWCArchive(new ArchiveFile('test', 'zip', Buffer.from([]), []));
       sinon.stub(dwc, 'worksheets').value({
         event: {
           getHeaders: () => {
-          return ["id",
-          "verbatimCoordinates",
-          "eventDate"]
+            return ['id', 'verbatimCoordinates', 'eventDate'];
           },
           getRows: () => []
         },
         occurrence: {
           getHeaders: () => {
-            return ["id",
-            "associatedTaxa",
-            "lifeStage",
-            "sex",
-            "individualCount",
-            "organismQuantity",
-            "organismQuantityType"]
+            return [
+              'id',
+              'associatedTaxa',
+              'lifeStage',
+              'sex',
+              'individualCount',
+              'organismQuantity',
+              'organismQuantityType'
+            ];
           },
           getRows: () => []
         },
         taxon: {
           getHeaders: () => {
-            return ["id",
-            "vernacularName"]
+            return ['id', 'vernacularName'];
           },
           getRows: () => []
         }
       });
       const data = service.getHeadersAndRowsFromDWCArchive(dwc);
       expect(data).to.have.keys([
-        "occurrenceRows",
-        "occurrenceIdHeader",
-        "associatedTaxaHeader",
-        "eventRows",
-        "lifeStageHeader",
-        "sexHeader",
-        "individualCountHeader",
-        "organismQuantityHeader",
-        "organismQuantityTypeHeader",
-        "occurrenceHeaders",
-        "eventIdHeader",
-        "eventDateHeader",
-        "eventVerbatimCoordinatesHeader",
-        "taxonRows",
-        "taxonIdHeader",
-        "vernacularNameHeader"
+        'occurrenceRows',
+        'occurrenceIdHeader',
+        'associatedTaxaHeader',
+        'eventRows',
+        'lifeStageHeader',
+        'sexHeader',
+        'individualCountHeader',
+        'organismQuantityHeader',
+        'organismQuantityTypeHeader',
+        'occurrenceHeaders',
+        'eventIdHeader',
+        'eventDateHeader',
+        'eventVerbatimCoordinatesHeader',
+        'taxonRows',
+        'taxonIdHeader',
+        'vernacularNameHeader'
       ]);
 
       // -1 implies that the header doesn't exist in the file
-      expect(data["occurrenceRows"]).not.to.be.eql(-1);
-      expect(data["occurrenceIdHeader"]).not.to.be.eql(-1);
-      expect(data["associatedTaxaHeader"]).not.to.be.eql(-1);
-      expect(data["eventRows"]).not.to.be.eql(-1);
-      expect(data["lifeStageHeader"]).not.to.be.eql(-1);
-      expect(data["sexHeader"]).not.to.be.eql(-1);
-      expect(data["individualCountHeader"]).not.to.be.eql(-1);
-      expect(data["organismQuantityHeader"]).not.to.be.eql(-1);
-      expect(data["organismQuantityTypeHeader"]).not.to.be.eql(-1);
-      expect(data["occurrenceHeaders"]).not.to.be.eql(-1);
-      expect(data["eventIdHeader"]).not.to.be.eql(-1);
-      expect(data["eventDateHeader"]).not.to.be.eql(-1);
-      expect(data["eventVerbatimCoordinatesHeader"]).not.to.be.eql(-1);
-      expect(data["taxonRows"]).not.to.be.eql(-1);
-      expect(data["taxonIdHeader"]).not.to.be.eql(-1);
-      expect(data["vernacularNameHeader"]).not.to.be.eql(-1);
+      expect(data['occurrenceRows']).not.to.be.eql(-1);
+      expect(data['occurrenceIdHeader']).not.to.be.eql(-1);
+      expect(data['associatedTaxaHeader']).not.to.be.eql(-1);
+      expect(data['eventRows']).not.to.be.eql(-1);
+      expect(data['lifeStageHeader']).not.to.be.eql(-1);
+      expect(data['sexHeader']).not.to.be.eql(-1);
+      expect(data['individualCountHeader']).not.to.be.eql(-1);
+      expect(data['organismQuantityHeader']).not.to.be.eql(-1);
+      expect(data['organismQuantityTypeHeader']).not.to.be.eql(-1);
+      expect(data['occurrenceHeaders']).not.to.be.eql(-1);
+      expect(data['eventIdHeader']).not.to.be.eql(-1);
+      expect(data['eventDateHeader']).not.to.be.eql(-1);
+      expect(data['eventVerbatimCoordinatesHeader']).not.to.be.eql(-1);
+      expect(data['taxonRows']).not.to.be.eql(-1);
+      expect(data['taxonIdHeader']).not.to.be.eql(-1);
+      expect(data['vernacularNameHeader']).not.to.be.eql(-1);
     });
 
     it('should return object with undefined values', () => {
       const service = mockService();
-      const dwc = new DWCArchive(new ArchiveFile("test", "zip", Buffer.from([]), []));
+      const dwc = new DWCArchive(new ArchiveFile('test', 'zip', Buffer.from([]), []));
       const data = service.getHeadersAndRowsFromDWCArchive(dwc);
       expect(data).to.have.keys([
-        "occurrenceRows",
-        "occurrenceIdHeader",
-        "associatedTaxaHeader",
-        "eventRows",
-        "lifeStageHeader",
-        "sexHeader",
-        "individualCountHeader",
-        "organismQuantityHeader",
-        "organismQuantityTypeHeader",
-        "occurrenceHeaders",
-        "eventIdHeader",
-        "eventDateHeader",
-        "eventVerbatimCoordinatesHeader",
-        "taxonRows",
-        "taxonIdHeader",
-        "vernacularNameHeader"
+        'occurrenceRows',
+        'occurrenceIdHeader',
+        'associatedTaxaHeader',
+        'eventRows',
+        'lifeStageHeader',
+        'sexHeader',
+        'individualCountHeader',
+        'organismQuantityHeader',
+        'organismQuantityTypeHeader',
+        'occurrenceHeaders',
+        'eventIdHeader',
+        'eventDateHeader',
+        'eventVerbatimCoordinatesHeader',
+        'taxonRows',
+        'taxonIdHeader',
+        'vernacularNameHeader'
       ]);
-      expect(data["occurrenceRows"]).to.be.undefined;
-      expect(data["occurrenceIdHeader"]).to.be.undefined;
-      expect(data["associatedTaxaHeader"]).to.be.undefined;
-      expect(data["eventRows"]).to.be.undefined;
-      expect(data["lifeStageHeader"]).to.be.undefined;
-      expect(data["sexHeader"]).to.be.undefined;
-      expect(data["individualCountHeader"]).to.be.undefined;
-      expect(data["organismQuantityHeader"]).to.be.undefined;
-      expect(data["organismQuantityTypeHeader"]).to.be.undefined;
-      expect(data["occurrenceHeaders"]).to.be.undefined;
-      expect(data["eventIdHeader"]).to.be.undefined;
-      expect(data["eventDateHeader"]).to.be.undefined;
-      expect(data["eventVerbatimCoordinatesHeader"]).to.be.undefined;
-      expect(data["taxonRows"]).to.be.undefined;
-      expect(data["taxonIdHeader"]).to.be.undefined;
-      expect(data["vernacularNameHeader"]).to.be.undefined;
-    })
+      expect(data['occurrenceRows']).to.be.undefined;
+      expect(data['occurrenceIdHeader']).to.be.undefined;
+      expect(data['associatedTaxaHeader']).to.be.undefined;
+      expect(data['eventRows']).to.be.undefined;
+      expect(data['lifeStageHeader']).to.be.undefined;
+      expect(data['sexHeader']).to.be.undefined;
+      expect(data['individualCountHeader']).to.be.undefined;
+      expect(data['organismQuantityHeader']).to.be.undefined;
+      expect(data['organismQuantityTypeHeader']).to.be.undefined;
+      expect(data['occurrenceHeaders']).to.be.undefined;
+      expect(data['eventIdHeader']).to.be.undefined;
+      expect(data['eventDateHeader']).to.be.undefined;
+      expect(data['eventVerbatimCoordinatesHeader']).to.be.undefined;
+      expect(data['taxonRows']).to.be.undefined;
+      expect(data['taxonIdHeader']).to.be.undefined;
+      expect(data['vernacularNameHeader']).to.be.undefined;
+    });
   });
-  
+
   it('should return a post occurrence', async () => {
     const submissionId = 1;
     const repo = sinon.stub(OccurrenceRepository.prototype, 'getOccurrenceSubmission').resolves({
