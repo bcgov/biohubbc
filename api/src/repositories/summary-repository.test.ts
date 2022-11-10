888fb4fe335ca94d27862af14d0057eb29609fa0
Curtis <curtisupshall@gmail.com>
2022-11-09 16:49:33 -0800

    BHBC-1976: Summary repo test boilerplate

--- api/src/repositories/summary-repository.test.ts
+++ api/src/repositories/summary-repository.test.ts
@@ -1,8 +1,11 @@
-import chai from 'chai';
+import chai, { expect } from 'chai';
 import { describe } from 'mocha';
-
+import { QueryResult } from 'pg';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
+import { HTTP400 } from '../errors/http-error';
+import { getMockDBConnection } from '../__mocks__/db';
+import { SummaryRepository } from './summary-repository';
 
 chai.use(sinonChai);
 
@@ -10,4 +13,12 @@ describe('SummaryRepository', () => {
   afterEach(() => {
     sinon.restore();
   });
+
+  describe('findSummarySubmissionById', () => {
+    afterEach(() => {
+      sinon.restore();
+    });
+
+  });
+
 });
