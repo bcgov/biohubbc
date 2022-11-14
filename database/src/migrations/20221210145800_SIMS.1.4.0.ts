3a203b124f3fea4f89d962f597701599ee68c050
cgarrettjones <charlie.garrettjones@bayseum.com>
2022-11-14 14:41:05 -0800

    - addition of security review timestamp attributes to survey report, project and project report artifacts

--- database/src/migrations/20221210145800_SIMS.1.4.0.ts
+++ database/src/migrations/20221210145800_SIMS.1.4.0.ts
@@ -15,10 +15,22 @@ export async function up(knex: Knex): Promise<void> {
     set search_path=${DB_SCHEMA};
 
     alter table survey_attachment add column security_review_timestamp timestamptz(6);
-    comment on column survey_attachment.security_review_timestamp is 'The datetime that the security review for the survey attachment was completed.';
+    comment on column survey_attachment.security_review_timestamp is 'The datetime that the security review for the artifact was completed.';
+
+    alter table survey_report_attachment add column security_review_timestamp timestamptz(6);
+    comment on column survey_report_attachment.security_review_timestamp is 'The datetime that the security review for the artifact was completed.';
+
+    alter table project_attachment add column security_review_timestamp timestamptz(6);
+    comment on column project_attachment.security_review_timestamp is 'The datetime that the security review for the artifact was completed.';
+
+    alter table project_report_attachment add column security_review_timestamp timestamptz(6);
+    comment on column project_report_attachment.security_review_timestamp is 'The datetime that the security review for the artifact was completed.';
 
     SET SEARCH_PATH = ${DB_SCHEMA_DAPI_V1};
     create or replace view survey_attachment as select * from ${DB_SCHEMA}.survey_attachment;
+    create or replace view survey_report_attachment as select * from ${DB_SCHEMA}.survey_report_attachment;
+    create or replace view project_attachment as select * from ${DB_SCHEMA}.project_attachment;
+    create or replace view project_report_attachment as select * from ${DB_SCHEMA}.project_report_attachment;
   `);
 }
 
