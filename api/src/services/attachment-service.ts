c46a7040dc882a0a14e8ff957066370c63146a74
Curtis Upshall <curtisupshall@gmail.com>
2022-11-21 09:43:56 -0800

    BHBC-1097: PR attachment service and repo

--- api/src/services/attachment-service.ts
+++ api/src/services/attachment-service.ts
@@ -1,5 +1,9 @@
 import { IDBConnection } from '../database/db';
-import { AttachmentRepository } from '../repositories/attachment-repository';
+import {
+    AttachmentRepository,
+    IGetAttachmentsSource,
+    IGetReportAttachmentsSource
+} from '../repositories/attachment-repository';
 import { DBService } from './db-service';
 
 export class AttachmentService extends DBService {
@@ -11,27 +15,11 @@ export class AttachmentService extends DBService {
     this.attachmentRepository = new AttachmentRepository(connection);
   }
 
-  async addSecurityToAttachment(securityIds: number[], attachmentId: number): Promise<void> {
-    return this.attachmentRepository.addSecurityToAttachments(securityIds, attachmentId);
+  async getProjectAttachments(projectId: number): Promise<IGetAttachmentsSource[]> {
+    return this.attachmentRepository.getProjectAttachments(projectId);
   }
 
-  async addSecurityToReportAttachment(securityIds: number[], attachmentId: number): Promise<void> {
-    return this.attachmentRepository.addSecurityToReportAttachments(securityIds, attachmentId);
-  }
-
-  async addSecurityToAttachments(securityIds: number[], attachmentIds: number[]): Promise<void[]> {
-    const promises = attachmentIds.map((item) => this.addSecurityToAttachment(securityIds, item));
-
-    const results = await Promise.all(promises);
-
-    return results;
-  }
-
-  async addSecurityToReportAttachments(securityIds: number[], attachmentIds: number[]): Promise<void[]> {
-    const promises = attachmentIds.map((item) => this.addSecurityToReportAttachment(securityIds, item));
-
-    const results = await Promise.all(promises);
-
-    return results;
-  }
+  async getProjectReportAttachments(projectId: number): Promise<IGetReportAttachmentsSource[]> {
+    return this.attachmentRepository.getProjectReportAttachments(projectId);
+  } 
 }
