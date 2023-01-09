f1cba268bf2e6722f59e313c20227b24fee3c504
Nick Phura <nickphura@gmail.com>
2023-01-09 15:03:30 -0800

    Validation/Transformation Config BUilders

---
+++ utils/templates/transformation/transformation-config-builder.ts
@@ -0,0 +1,20 @@
+import Ajv from 'ajv';
+import * as fs from 'fs';
+import { transformationConfigJSONSchema } from '../../../api/src/utils/media/xlsx/transformation/xlsx-transform-schema';
+import { TransformSchema } from '../../../api/src/utils/media/xlsx/transformation/xlsx-transform-schema-parser';
+
+// Build your transform config here, using any helper typescript functions or variables to reduce the manual effort
+// needed to make the config.
+const TEMPLATE_SCHEMA: TransformSchema = { dwcMeta: [], map: [], templateMeta: [] };
+
+// Validates the config against the json-schema definition to catch errors early
+const ajv = new Ajv();
+ajv.validate(transformationConfigJSONSchema, TEMPLATE_SCHEMA);
+if (ajv.errors) {
+  throw new Error(JSON.stringify(ajv.errors));
+}
+
+// Write the raw transform JSON config.
+fs.writeFile('./transformation_config.json', TEMPLATE_SCHEMA, (error) => {
+  throw new Error(JSON.stringify(error));
+});
---
+++ utils/templates/validation/validation-config-builder.ts
@@ -0,0 +1,86 @@
+import * as fs from 'fs';
+
+// Helper functions for constructing validation configs
+const basicNumericValidator = () => {
+  return [
+    {
+      column_numeric_validator: {
+        name: '',
+        description: ''
+      }
+    }
+  ];
+};
+
+const basicDateValidator = () => {
+  return [
+    {
+      column_format_validator: {
+        reg_exp: '^d{4}-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])$',
+        reg_exp_flags: 'g',
+        expected_format: 'Dates need to be formatted in YYYY-MM-DD. For example: 2020-09-15.'
+      }
+    }
+  ];
+};
+
+const basicRequiredValidator = () => {
+  return [
+    {
+      column_format_validator: {
+        reg_exp: '^(?!s*$).+',
+        reg_exp_flags: 'g'
+      }
+    }
+  ];
+};
+
+const starterObject = {
+  name: '',
+  description: '',
+  files: [
+    {
+      name: '',
+      description: '',
+      validations: [],
+      columns: [
+        {
+          name: '',
+          description: '',
+          validations: []
+        }
+      ]
+    }
+  ],
+  validations: []
+};
+
+const templateValidationSchema = {
+  name: '',
+  description: '',
+  files: [
+    {
+      name: '',
+      description: '',
+      validations: [],
+      columns: [
+        {
+          name: '',
+          description: '',
+          validations: []
+        }
+      ]
+    }
+  ],
+  validations: []
+};
+
+fs.writeFile('./template_output.json', JSON.stringify(templateValidationSchema), (err) => {
+  if (err) {
+    console.error(err);
+  }
+  // file written successfully
+
+  console.log('All done!');
+});
+// console.log(JSON.stringify(mooseArialSRBRecruitmentCompositionSurvey));
