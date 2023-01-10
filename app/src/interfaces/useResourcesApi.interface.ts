5539edbcd3853d5814663fba4730b8d4ae355643
Curtis Upshall <curtisupshall@gmail.com>
2023-01-10 11:34:46 -0800

    BHBC-2076: Enhanced typedefs for resource files

--- app/src/interfaces/useResourcesApi.interface.ts
+++ app/src/interfaces/useResourcesApi.interface.ts
@@ -1,17 +1,68 @@
 /**
- * A single resource
+ * A single resource file.
  *
- * @TODO
  * @export
- * @interface IResource
+ * @interface IResourceFile
  */
-export interface IResource {
-  //
+export interface IResourceFile {
+  /**
+   * The URL (key) of the file
+   *
+   * @memberof IResourceFile
+   */
+  url: string;
+  /**
+   * The size of the file (in bytes)
+   * 
+   * @memberof IResourceFile
+   */
+  fileSize: number;
+  /**
+   * Resource metadata for the file
+   * 
+   * @memberof IResourceFile
+   */
+  metadata: IResourceMetadata
 }
 
 /**
- * @TODO jsdoc
+ * Resource file metadata
+ * 
+ * @export
+ * @interface IResourceMetadata
+ */
+export interface IResourceMetadata {
+  /**
+   * The name of the template
+   * 
+   * @memberof IResourceMetadata
+   */
+  templateName: string;
+  /**
+   * The type of resource template
+   * 
+   * @memberof IResourceMetadata
+   */
+  templateType: string;
+  /**
+   * (Optional) The species pertaining to this particular resource
+   * 
+   * @memberof IResourceMetadata
+   */
+  species?: string;
+}
+
+/**
+ * List resources endpoint response object.
+ *
+ * @export
+ * @interface IListResourcesResponse
  */
 export interface IListResourcesResponse {
-  files: any[]
+  /**
+   * An array of resource files
+   * 
+   * @memberof IListResourcesResponse
+   */
+  files: IResourceFile[]
 }
