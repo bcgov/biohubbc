1d8cd062f52fb9c3abdb333029b22ace97bec415
Curtis Upshall <curtisupshall@gmail.com>
2023-01-17 14:39:04 -0800

    BHBC-2145: usePolling hook

---
+++ app/src/hooks/usePolling.tsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+
+interface IUsePollingConfig {
+    startEnabled: boolean;
+    initialDelayMs: number;
+    maxDelayMs: number;
+    lockoutDurationMs: number;
+    lockoutPolls: number;
+    growthRate: (prevDelay: number) => number
+}
+
+const defaultConfig: IUsePollingConfig = {
+    startEnabled: true,
+    initialDelayMs: 5000,
+    maxDelayMs: 60000,
+    lockoutDurationMs: 120000,
+    lockoutPolls: 0,
+    growthRate: (prevDelay: number) => 2 * prevDelay
+}
+
+export const usePolling = (callback: () => void, config: IUsePollingConfig) => {
+    const pollingConfig: IUsePollingConfig = { ...defaultConfig, ...config }
+    const [isPolling, setIsPolling] = useState<boolean>();
+
+    const { initialDelayMs, maxDelayMs, lockoutDurationMs, lockoutPolls, growthRate } = pollingConfig
+
+    const nextDelay = (delayMs: number) => {
+        return Math.min(growthRate(delayMs), maxDelayMs);
+    }
+
+    if (lockoutDurationMs > 0 || lockoutPolls > 0) {
+        let lockoutPollDurationMs = initialDelayMs;
+        let currentDelay = initialDelayMs;
+        for (let i = 0; i < lockoutPolls - 1; i ++) {
+            currentDelay = nextDelay(currentDelay);
+            lockoutPollDurationMs += currentDelay;
+        }
+
+        setTimeout(() => {
+            disable();
+        }, Math.min(lockoutDurationMs, lockoutPollDurationMs));
+    }
+
+    const enable = () => {
+        setIsPolling(true);
+    }
+
+    const disable = () => {
+        setIsPolling(false);
+    }
+
+    return {
+        isPolling,
+        enable,
+        disable,
+        poll: () => callback()
+    }
+}
