1aa5004fea36abf2aa2a07c259df129561d8581f
Curtis Upshall <curtisupshall@gmail.com>
2023-02-10 15:18:46 -0800

    BHBC-2213: Refactor ReadMoreField component to fix bug

--- app/src/features/projects/view/components/ProjectObjectives.tsx
+++ app/src/features/projects/view/components/ProjectObjectives.tsx
@@ -15,11 +15,15 @@ export interface IProjectObjectivesProps {
  * @return {*}
  */
 const ProjectObjectives: React.FC<IProjectObjectivesProps> = (props) => {
-  const {
-    projectForViewData: { objectives }
-  } = props;
+  const { projectForViewData: { objectives } } = props;
 
-  return <ReadMoreField text={objectives.objectives} maxCharLength={200} />;
+  return (
+    <ReadMoreField
+      text={objectives.objectives}
+      maxCharLength={200}
+      TypographyProps={{ color: 'textSecondary' }}
+    />
+  );
 };
 
 export default ProjectObjectives;
