5a0798b80ccf58db30d7bf4e41c86f5e8cae534f
AlfredRosenthal <alfred.rosenthal@quartech.com>
2023-03-02 09:25:54 -0800

    updated insert template

--- utils/templates/sql/INSERT_TEMPLTE.sql
+++ utils/templates/sql/INSERT_TEMPLTE.sql
@@ -1,34 +1,36 @@
+set search_path = biohub;
+set schema 'biohub';
+
 do $$
 	declare
-		_template_id integer := 1;
-		_template_name varchar := 'Template Name';
+		_template_id integer; -- set after tempalte is created
+		_template_name varchar := 'Template Name 1';
 		_template_version varchar := '2.0';
 		_tempalte_description varchar := '';
-		_field_method_name varchar := 'Stratified Random Block';
-		_field_method_id integer := 0;
-		_taxonomy_id integer := 1;
-    
+		
+		_taxonomy_ids integer[] := array[1,2,3]; -- wild taxonomic IDs from elastic search
+		_taxonomy_id integer; -- used as an index in the loop, do not set
+
 		_validation_schema varchar := $v_s${}$v_s$;
 		_transformation_schema varchar := $t_s${}$t_s$;
 begin
-	insert into
-	  biohub.template (name, version, record_effective_date, description)
-	values
-	  (_template_name, _template_version, now(), _tempalte_description)
-	 returning template_id into _template_id;
-
-	select field_method_id into _field_method_id from field_method  where name = _field_method_name and record_end_date is null;
-
 	INSERT INTO
-		biohub.template_methodology_species (field_method_id, wldtaxonomic_units_id, template_id, validation, transform)
+	  template (name, version, record_effective_date, description)
 	VALUES
-    (
-      _field_method_id,
-      _taxonomy_id,
-      _template_id,
-      _validation_schema::json,
-      _transformation_schema::json
-    );
+	  (_template_name, _template_version, now(), _tempalte_description)
+	RETURNING template_id into _template_id;
 
+	FOREACH _taxonomy_id IN ARRAY _taxonomy_ids LOOP 
+		INSERT INTO
+			template_methodology_species (field_method_id, wldtaxonomic_units_id, template_id, validation, transform)
+		VALUES
+			(
+				null,
+				_taxonomy_id,
+				_template_id,
+				_validation_schema::json,
+				_transformation_schema::json
+			);
+  END LOOP;
 	RAISE NOTICE 'All done!';
 end $$
