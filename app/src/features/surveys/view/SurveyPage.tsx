280fd4d1c510bfa8dd1785ddcd9a287521d3714b
Kjartan <Kjartan.Einarsson@quartech.com>
2023-03-21 10:23:52 -0700

    Refactor SurveyPage with dataloaders

--- app/src/features/surveys/view/SurveyPage.tsx
+++ app/src/features/surveys/view/SurveyPage.tsx
@@ -5,14 +5,12 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import { IMarkerLayer } from 'components/map/components/MarkerCluster';
 import { IStaticLayer } from 'components/map/components/StaticLayers';
+import SubmissionAlertBar from 'components/publish/SubmissionAlertBar';
 import SurveyDetails from 'features/surveys/view/SurveyDetails';
 import { useBiohubApi } from 'hooks/useBioHubApi';
 import useDataLoader from 'hooks/useDataLoader';
 import useDataLoaderError from 'hooks/useDataLoaderError';
-import { IGetAllCodeSetsResponse } from 'interfaces/useCodesApi.interface';
-import { IGetProjectForViewResponse } from 'interfaces/useProjectApi.interface';
-import { IGetSurveyForViewResponse } from 'interfaces/useSurveyApi.interface';
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { parseSpatialDataByType } from 'utils/spatial-utils';
 import SurveyStudyArea from './components/SurveyStudyArea';
@@ -31,17 +29,33 @@ const SurveyPage: React.FC = () => {
 
   const biohubApi = useBiohubApi();
 
-  const [isLoadingProject, setIsLoadingProject] = useState(true);
-  const [projectWithDetails, setProjectWithDetails] = useState<IGetProjectForViewResponse | null>(null);
-
-  const [isLoadingSurvey, setIsLoadingSurvey] = useState(true);
-  const [surveyWithDetails, setSurveyWithDetails] = useState<IGetSurveyForViewResponse | null>(null);
-
-  const [isLoadingCodes, setIsLoadingCodes] = useState(true);
-  const [codes, setCodes] = useState<IGetAllCodeSetsResponse>();
-
   const [markerLayers, setMarkerLayers] = useState<IMarkerLayer[]>([]);
   const [staticLayers, setStaticLayers] = useState<IStaticLayer[]>([]);
+
+  const codesDataLoader = useDataLoader(() => biohubApi.codes.getAllCodeSets());
+  codesDataLoader.load();
+
+  const projectDataLoader = useDataLoader(() => biohubApi.project.getProjectForView(urlParams['id']));
+  useDataLoaderError(projectDataLoader, () => {
+    return {
+      dialogTitle: 'Error Loading Project Details',
+      dialogText:
+        'An error has occurred while attempting to load project details, please try again. If the error persists, please contact your system administrator.'
+    };
+  });
+  projectDataLoader.load();
+
+  const surveyDataLoader = useDataLoader(() =>
+    biohubApi.survey.getSurveyForView(urlParams['id'], urlParams['survey_id'])
+  );
+  useDataLoaderError(surveyDataLoader, () => {
+    return {
+      dialogTitle: 'Error Loading Survey Details',
+      dialogText:
+        'An error has occurred while attempting to load survey details, please try again. If the error persists, please contact your system administrator.'
+    };
+  });
+  surveyDataLoader.load();
 
   const mapDataLoader = useDataLoader((datasetID: number) => biohubApi.observation.getOccurrencesForView(datasetID));
   useDataLoaderError(mapDataLoader, () => {
@@ -53,50 +67,6 @@ const SurveyPage: React.FC = () => {
   });
 
   useEffect(() => {
-    const getCodes = async () => {
-      const codesResponse = await biohubApi.codes.getAllCodeSets();
-
-      if (!codesResponse) {
-        return;
-      }
-
-      setCodes(codesResponse);
-    };
-
-    if (isLoadingCodes && !codes) {
-      getCodes();
-      setIsLoadingCodes(false);
-    }
-  }, [urlParams, biohubApi.codes, isLoadingCodes, codes]);
-
-  const getProject = useCallback(async () => {
-    const projectWithDetailsResponse = await biohubApi.project.getProjectForView(urlParams['id']);
-
-    if (!projectWithDetailsResponse) {
-      return;
-    }
-
-    setProjectWithDetails(projectWithDetailsResponse);
-  }, [biohubApi.project, urlParams]);
-
-  const getSurvey = useCallback(async () => {
-    const surveyWithDetailsResponse = await biohubApi.survey.getSurveyForView(urlParams['id'], urlParams['survey_id']);
-
-    if (!surveyWithDetailsResponse) {
-      return;
-    }
-    setSurveyWithDetails(surveyWithDetailsResponse);
-
-    const getOccurrences = (occurrenceSubmissionId: number) => {
-      mapDataLoader.refresh(occurrenceSubmissionId);
-    };
-
-    if (surveyWithDetailsResponse.surveySupplementaryData.occurrence_submission.id) {
-      getOccurrences(surveyWithDetailsResponse.surveySupplementaryData.occurrence_submission.id);
-    }
-  }, [biohubApi.survey, urlParams, mapDataLoader]);
-
-  useEffect(() => {
     if (mapDataLoader.data) {
       const result = parseSpatialDataByType(mapDataLoader.data);
 
@@ -105,44 +75,37 @@ const SurveyPage: React.FC = () => {
     }
   }, [mapDataLoader.data]);
 
-  useEffect(() => {
-    if (isLoadingProject && !projectWithDetails) {
-      getProject();
-      setIsLoadingProject(false);
-    }
-  }, [isLoadingProject, projectWithDetails, getProject]);
-
-  useEffect(() => {
-    if (isLoadingSurvey && !surveyWithDetails) {
-      getSurvey();
-      setIsLoadingSurvey(false);
-    }
-  }, [isLoadingSurvey, surveyWithDetails, getSurvey]);
-
-  if (!projectWithDetails || !surveyWithDetails || !codes) {
+  if (!projectDataLoader.data || !surveyDataLoader.data || !codesDataLoader.data) {
     return <CircularProgress className="pageProgress" size={40} />;
   }
 
   return (
     <>
-      <SurveyHeader projectWithDetails={projectWithDetails} surveyWithDetails={surveyWithDetails} refresh={getSurvey} />
+      <SurveyHeader
+        projectWithDetails={projectDataLoader.data}
+        surveyWithDetails={surveyDataLoader.data}
+        refresh={surveyDataLoader.refresh}
+      />
       <Container maxWidth="xl">
         <Box my={3}>
           <Grid container spacing={3}>
+            <Grid item md={12}>
+              <SubmissionAlertBar submitted={false} />
+            </Grid>
             <Grid item md={12} lg={4}>
               <Paper elevation={0}>
                 <SurveyDetails
-                  projectForViewData={projectWithDetails}
-                  surveyForViewData={surveyWithDetails}
-                  codes={codes}
-                  refresh={getSurvey}
+                  projectForViewData={projectDataLoader.data}
+                  surveyForViewData={surveyDataLoader.data}
+                  codes={codesDataLoader.data}
+                  refresh={surveyDataLoader.refresh}
                 />
               </Paper>
             </Grid>
             <Grid item md={12} lg={8}>
               <Box mb={3}>
                 <Paper elevation={0}>
-                  <SurveyObservations refresh={getSurvey} />
+                  <SurveyObservations refresh={surveyDataLoader.refresh} />
                 </Paper>
               </Box>
               <Box mb={3}>
@@ -152,16 +115,19 @@ const SurveyPage: React.FC = () => {
               </Box>
               <Box mb={3}>
                 <Paper elevation={0}>
-                  <SurveyAttachments projectForViewData={projectWithDetails} surveyForViewData={surveyWithDetails} />
+                  <SurveyAttachments
+                    projectForViewData={projectDataLoader.data}
+                    surveyForViewData={surveyDataLoader.data}
+                  />
                 </Paper>
               </Box>
               <Box mb={3}>
                 <Paper elevation={0}>
                   <SurveyStudyArea
-                    surveyForViewData={surveyWithDetails}
-                    projectForViewData={projectWithDetails}
+                    surveyForViewData={surveyDataLoader.data}
+                    projectForViewData={projectDataLoader.data}
                     mapLayersForView={{ markerLayers: markerLayers, staticLayers: staticLayers }}
-                    refresh={getSurvey}
+                    refresh={surveyDataLoader.refresh}
                   />
                 </Paper>
               </Box>
