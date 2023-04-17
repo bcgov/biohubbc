0850ab6825ec4ad6de12c770fc8efa9cc97a3097
AlfredRosenthal <110939485+AlfredRosenthal@users.noreply.github.com>
2023-04-17 16:29:53 -0700

    BHBC-2215: Update Project Role permissions for API (#1003)
    * updated view-occurrences endpoint to properly check authorization rules
    
    * updated various endpoints to match current role rules (see Confluence -> SIMS -> Roles & Permissions)

--- app/src/features/surveys/view/components/SurveyStudyArea.tsx
+++ app/src/features/surveys/view/components/SurveyStudyArea.tsx
@@ -95,8 +95,8 @@ const SurveyStudyArea = () => {
     wmu: []
   });
 
-  const mapDataLoader = useDataLoader((occurrenceSubmissionId: number) =>
-    biohubApi.observation.getOccurrencesForView(occurrenceSubmissionId)
+  const mapDataLoader = useDataLoader((projectId: number, occurrenceSubmissionId: number) =>
+    biohubApi.observation.getOccurrencesForView(projectId, occurrenceSubmissionId)
   );
   useDataLoaderError(mapDataLoader, () => {
     return {
@@ -117,9 +117,9 @@ const SurveyStudyArea = () => {
 
   useEffect(() => {
     if (occurrence_submission_id) {
-      mapDataLoader.load(occurrence_submission_id);
+      mapDataLoader.load(surveyContext.projectId, occurrence_submission_id);
     }
-  }, [mapDataLoader, occurrence_submission_id]);
+  }, [mapDataLoader, occurrence_submission_id, surveyContext.projectId]);
 
   const zoomToBoundaryExtent = useCallback(() => {
     setBounds(calculateUpdatedMapBounds(surveyGeometry));
