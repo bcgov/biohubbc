8e8ffdb44f4c4287605d589bdba6bdfaa34c818d
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2023-04-25 14:55:30 -0700

    tweak

--- api/src/paths/draft/create.test.ts
+++ api/src/paths/draft/create.test.ts
@@ -11,7 +11,7 @@ import * as create from './create';
 
 chai.use(sinonChai);
 
-describe.only('draft (create)', () => {
+describe('draft (create)', () => {
   describe('openapi schema', () => {
     const ajv = new Ajv();
 
