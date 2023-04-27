f6f6029861e502e902fe9492735114f228f3d5d1
Curtis <curtisupshall@gmail.com>
2023-04-27 11:59:45 -0700

    SIMSBIOHUB-30: Add browser history

--- app/src/pages/landing/LandingPage.tsx
+++ app/src/pages/landing/LandingPage.tsx
@@ -1,26 +1,26 @@
-import React, { useContext } from 'react';
+import React, { useContext, useMemo } from 'react';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
-//import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-
-import { AuthStateContext } from 'contexts/authStateContext';
 import { makeStyles, Theme } from '@material-ui/core/styles';
+import { AuthStateContext } from 'contexts/authStateContext';
 import BaseLayout from 'layouts/BaseLayout';
+import { ensureProtocol } from 'utils/Utils';
+
 interface ILandingPageProps {
   originalPath: string;
 }
 
 const useStyles = makeStyles((theme: Theme) => ({
   baseLayoutContainer: {
+    backgroundColor: '#00438A', // Contingency background-color pending hero image load
     color: 'white',
-    backgroundColor: 'blue',
     minHeight: '100vh',
     background: `url('/assets/hero.jpg')`,
     backgroundPosition: 'center',
     backgroundRepeat: 'no-repeat',
-    backgroundZize: 'cover',
+    backgroundSize: 'cover',
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
@@ -30,24 +30,36 @@ const useStyles = makeStyles((theme: Theme) => ({
     }
   },
   heroHeader: {
-    // mt: -4,
-    fontSize: '3rem',
-    letterSpacing: '-0.03rem'
+    maxWidth: '22ch',
+    fontSize: '3.5rem',
+    letterSpacing: '-0.03rem',
+    textShadow: '0px 0px 15px rgba(0,13,26,0.5)'
   },
   heroSubheader: {
-    marginTop: '3rem',
-    marginBottom: '6rem',
     maxWidth: '45ch',
-    fontSize: '1.25rem',
-    lineHeight: '1.5'
+    marginTop: '3rem',
+    marginBottom: '3rem',
+    fontSize: '1.325rem',
+    lineHeight: '1.5',
+    textShadow: '0px 0px 10px rgba(0,13,26,1)'
   },
   heroButton: {
+    textTransform: 'uppercase',
     color: '#003366',
     fontWeight: 700,
-    backgroundColor: '#fcba19'
+    backgroundColor: '#fcba19',
+    padding: '12px 44px'
   },
   actionsContainer: {
     maxWidth: '45ch',
+    '& p': {
+      margin: '1.5rem 0'
+    }
+  },
+  heroLink: {
+    color: '#fcba19',
+    fontWeight: 700,
+    textDecoration: 'none'
   }
 }));
 
@@ -55,14 +67,11 @@ export const LandingPage: React.FC<ILandingPageProps> = ({ originalPath }) => {
   const { keycloakWrapper } = useContext(AuthStateContext);
   const classes = useStyles();
 
-  const handleLogin = () => {
-    keycloakWrapper?.keycloak?.login();
-  }
-  
+  const loginUrl = useMemo(() => ensureProtocol(keycloakWrapper?.keycloak?.createLoginUrl() || ''), []);
 
   return (
     <BaseLayout className={classes.baseLayoutContainer}>
-      <Container maxWidth="sm" className={''}>
+      <Container maxWidth="md" className={''}>
         <Typography variant="h1" className={classes.heroHeader}>
           Species Inventory Management System
         </Typography>
@@ -71,10 +80,18 @@ export const LandingPage: React.FC<ILandingPageProps> = ({ originalPath }) => {
         </Typography>
         <Box className={classes.actionsContainer}>
           <p>
-            To access this application, you must use a valid BC government-issued IDIR or BCeID account credentia
+            To access this application, you must use a valid BC government-issued IDIR or BCeID account credential.
           </p>
-          <Button onClick={() => handleLogin()} variant='contained' className={classes.heroButton} size='large'>Log In</Button>
-          <p>Don't have an account? <a href='/'>Register here.</a></p>
+          <Button
+            component='a'
+            href={loginUrl}
+            variant='contained'
+            className={classes.heroButton}
+            size='large'
+          >
+            Log In
+          </Button>
+          <p>Don't have an account? <a className={classes.heroLink} href='/'>Register here.</a></p>
         </Box>
       </Container>
     </BaseLayout>
