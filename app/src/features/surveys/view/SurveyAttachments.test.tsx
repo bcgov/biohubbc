c66bdbda258b90844930a759f45ddf60841e24e0
Kjartan <Kjartan.Einarsson@quartech.com>
2023-05-01 12:43:04 -0700

    front end test fix

--- app/src/features/surveys/view/SurveyAttachments.test.tsx
+++ app/src/features/surveys/view/SurveyAttachments.test.tsx
@@ -7,11 +7,13 @@ import {
   waitFor
 } from '@testing-library/react';
 import { AttachmentType } from 'constants/attachments';
+import { AuthStateContext, IAuthState } from 'contexts/authStateContext';
 import { DialogContextProvider } from 'contexts/dialogContext';
 import { IProjectAuthStateContext, ProjectAuthStateContext } from 'contexts/projectAuthStateContext';
 import { ISurveyContext, SurveyContext } from 'contexts/surveyContext';
 import { useBiohubApi } from 'hooks/useBioHubApi';
 import { DataLoader } from 'hooks/useDataLoader';
+import { IKeycloakWrapper } from 'hooks/useKeycloakWrapper';
 import React from 'react';
 import SurveyAttachments from './SurveyAttachments';
 
@@ -195,14 +197,20 @@ describe('SurveyAttachments', () => {
       hasLoadedParticipantInfo: true
     };
 
+    const mockAuthStateContext: IAuthState = {
+      keycloakWrapper: ({ hasSystemRole: () => true } as unknown) as IKeycloakWrapper
+    };
+
     const { baseElement, queryByText, getByTestId, getAllByTestId, queryByTestId } = render(
-      <ProjectAuthStateContext.Provider value={mockProjectAuthStateContext}>
-        <DialogContextProvider>
-          <SurveyContext.Provider value={mockSurveyContext}>
-            <SurveyAttachments />
-          </SurveyContext.Provider>
-        </DialogContextProvider>
-      </ProjectAuthStateContext.Provider>
+      <AuthStateContext.Provider value={mockAuthStateContext}>
+        <ProjectAuthStateContext.Provider value={mockProjectAuthStateContext}>
+          <DialogContextProvider>
+            <SurveyContext.Provider value={mockSurveyContext}>
+              <SurveyAttachments />
+            </SurveyContext.Provider>
+          </DialogContextProvider>
+        </ProjectAuthStateContext.Provider>
+      </AuthStateContext.Provider>
     );
 
     await waitFor(() => {
@@ -262,14 +270,20 @@ describe('SurveyAttachments', () => {
       hasLoadedParticipantInfo: true
     };
 
-    const { baseElement, queryByText, getByTestId, queryByTestId, getAllByTestId } = render(
-      <ProjectAuthStateContext.Provider value={mockProjectAuthStateContext}>
-        <DialogContextProvider>
-          <SurveyContext.Provider value={mockSurveyContext}>
-            <SurveyAttachments />
-          </SurveyContext.Provider>
-        </DialogContextProvider>
-      </ProjectAuthStateContext.Provider>
+    const mockAuthStateContext: IAuthState = {
+      keycloakWrapper: ({ hasSystemRole: () => true } as unknown) as IKeycloakWrapper
+    };
+
+    const { baseElement, queryByText, getByTestId, getAllByTestId, queryByTestId } = render(
+      <AuthStateContext.Provider value={mockAuthStateContext}>
+        <ProjectAuthStateContext.Provider value={mockProjectAuthStateContext}>
+          <DialogContextProvider>
+            <SurveyContext.Provider value={mockSurveyContext}>
+              <SurveyAttachments />
+            </SurveyContext.Provider>
+          </DialogContextProvider>
+        </ProjectAuthStateContext.Provider>
+      </AuthStateContext.Provider>
     );
 
     await waitFor(() => {
@@ -328,14 +342,20 @@ describe('SurveyAttachments', () => {
       hasLoadedParticipantInfo: true
     };
 
-    const { baseElement, queryByText, getAllByRole, queryByTestId, getAllByTestId } = render(
-      <ProjectAuthStateContext.Provider value={mockProjectAuthStateContext}>
-        <DialogContextProvider>
-          <SurveyContext.Provider value={mockSurveyContext}>
-            <SurveyAttachments />
-          </SurveyContext.Provider>
-        </DialogContextProvider>
-      </ProjectAuthStateContext.Provider>
+    const mockAuthStateContext: IAuthState = {
+      keycloakWrapper: ({ hasSystemRole: () => true } as unknown) as IKeycloakWrapper
+    };
+
+    const { baseElement, queryByText, getAllByTestId, queryByTestId, getAllByRole } = render(
+      <AuthStateContext.Provider value={mockAuthStateContext}>
+        <ProjectAuthStateContext.Provider value={mockProjectAuthStateContext}>
+          <DialogContextProvider>
+            <SurveyContext.Provider value={mockSurveyContext}>
+              <SurveyAttachments />
+            </SurveyContext.Provider>
+          </DialogContextProvider>
+        </ProjectAuthStateContext.Provider>
+      </AuthStateContext.Provider>
     );
 
     await waitFor(() => {
