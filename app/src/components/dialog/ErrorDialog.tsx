9bb61b844053cb4a46df7b6dd74258bdf96b45e7
Curtis Upshall <curtisupshall@gmail.com>
2023-05-18 11:12:35 -0700

    SIMSBIOHUB-61: Added dialog styling for dialog visual

--- app/src/components/dialog/ErrorDialog.tsx
+++ app/src/components/dialog/ErrorDialog.tsx
@@ -7,6 +7,21 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import React from 'react';
+import DialogVisual from './DialogVisual';
+import { Theme } from '@material-ui/core/styles/createMuiTheme';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import clsx from 'clsx';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  dialogTitle: {
+    textAlign: 'center',
+  },
+  visualHidden: {
+    textAlign: 'unset'
+  }
+}));
+
+const DEFAULT_ERROR_DIALOG_TITLE = 'Error!'
 
 export interface IErrorDialogProps {
   /**
@@ -38,6 +53,13 @@ export interface IErrorDialogProps {
    */
   dialogErrorDetails?: (string | object)[];
   /**
+   * If set, hides the Dialog Visual shown by the error dialog.
+   *
+   * @type {boolean}
+   * @memberof IErrorDialogProps
+   */
+  hideVisual?: boolean;
+  /**
    * Set to `true` to open the dialog, `false` to close the dialog.
    *
    * @type {boolean}
@@ -66,6 +88,7 @@ export interface IErrorDialogProps {
  * @return {*}
  */
 export const ErrorDialog: React.FC<IErrorDialogProps> = (props) => {
+  const classes = useStyles();
   const [isExpanded, setIsExpanded] = React.useState(false);
 
   const ErrorDetailsList = (errorProps: { errors: (string | object)[] }) => {
@@ -80,21 +103,26 @@ export const ErrorDialog: React.FC<IErrorDialogProps> = (props) => {
     return <ul>{items}</ul>;
   };
 
-  if (!props.open) {
-    return <></>;
-  }
-
   return (
     <Box>
       <Dialog
         open={props.open}
         onClose={props.onClose}
         aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description">
-        <DialogTitle id="alert-dialog-title">{props.dialogTitle}</DialogTitle>
+        aria-describedby="alert-dialog-description"
+        keepMounted={false}>
+        {!props.hideVisual && (
+          <DialogVisual severity='error' />
+        )}
+
+        <DialogTitle id="alert-dialog-title" className={clsx(classes.dialogTitle, { [classes.visualHidden]: props.hideVisual })}>
+          {props.dialogTitle || DEFAULT_ERROR_DIALOG_TITLE}
+        </DialogTitle>
+
         <DialogContent>
           <DialogContentText id="alert-dialog-description">{props.dialogText}</DialogContentText>
         </DialogContent>
+
         {props.dialogError && (
           <DialogContent>
             <DialogContentText id="alert-dialog-description">{props.dialogError}</DialogContentText>
