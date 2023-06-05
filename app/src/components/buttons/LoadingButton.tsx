83a6431d115a01d855af3cbbe426034625688f26
Al Rosenthal <alfred.k.rosenthal@gmail.com>
2023-06-05 11:51:45 -0700

    Bugfix: Make Styles Issue (#1041)
    fixed issue with import pathing

--- app/src/components/buttons/LoadingButton.tsx
+++ app/src/components/buttons/LoadingButton.tsx
@@ -1,8 +1,8 @@
 import Box from '@material-ui/core/Box';
 import Button, { ButtonProps } from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import { makeStyles } from '@material-ui/core/styles';
 import { Theme } from '@material-ui/core/styles/createMuiTheme';
-import { makeStyles } from '@material-ui/styles';
 import React from 'react';
 
 const useStyles = makeStyles((theme: Theme) => ({
