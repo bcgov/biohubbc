fd9463ce130bf8077cdef35e2115d3116e671198
Curtis Upshall <curtisupshall@gmail.com>
2023-06-30 12:23:57 -0700

    SIMSBIOHUB-127: Added tests WIP

--- api/src/paths/spatial/regions.test.ts
+++ api/src/paths/spatial/regions.test.ts
@@ -1,11 +1,121 @@
-import chai from 'chai';
+import chai, { expect } from 'chai';
 import { describe } from 'mocha';
+import * as db from '../../database/db';
 import sinonChai from 'sinon-chai';
+import * as regions from './regions'
+import sinon from 'sinon';
+//import { HTTPError } from '../../errors/http-error';
+import { getMockDBConnection } from '../../__mocks__/db';
+import { ZodError } from 'zod';
+import { BcgwLayerService } from '../../services/bcgw-layer-service';
+import { Feature } from 'geojson';
 
 chai.use(sinonChai);
 
-describe('getRegions', () => {
-  it('', async () => {
-    // TODO
+describe.only('getRegions', () => {
+  const dbConnectionObj = getMockDBConnection();
+
+  it('throws a ZodError for invalid geoJSON features', async () => {
+    sinon.stub(db, 'getDBConnection').returns(dbConnectionObj);
+
+    const sampleReq = {
+      body: {
+        features: [
+          { __invalidGeoJsonFeature: true }
+        ]
+      }
+    } as any;
+  
+    /*
+    const actualResult: any = {
+      id: null,
+      date: null
+    };
+
+    const sampleRes = {
+      status: () => {
+        return {
+          json: (result: any) => {
+            actualResult = result;
+          }
+        };
+      }
+    };
+    */
+    
+    try {
+      const result = regions.getRegions();
+
+      await result(sampleReq, (null as unknown) as any, (null as unknown) as any);
+      expect.fail();
+    } catch (actualError: any) {
+      expect((actualError)).to.be.instanceOf(ZodError);
+    }
+  });
+
+  it.only('sorts features into known and unknown arrays', async () => {
+    sinon.stub(db, 'getDBConnection').returns(dbConnectionObj);
+
+    sinon.stub(BcgwLayerService.prototype, 'findRegionName').callsFake((feature: Feature) => {
+      return feature.id && {
+        'testid1': null,
+        'testid2': { regionName: 'region2', sourceLayer: 'source2' },
+        'testid3': null,
+        'testid4': { regionName: 'region4', sourceLayer: 'source4' },
+      }[feature.id] || null;
+    });
+
+    const sampleReq = {
+      body: {
+        features: [
+          {
+            type: 'Feature',
+            geometry: { type: 'Point', coordinates: [0, 0] },
+            properties: {},
+            id: 'testid1'
+          },
+          {
+            type: 'Feature',
+            geometry: { type: 'Point', coordinates: [0, 0] },
+            properties: {},
+            id: 'testid2'
+          },
+          {
+            type: 'Feature',
+            geometry: { type: 'Point', coordinates: [0, 0] },
+            properties: {},
+            id: 'testid3'
+          },
+          {
+            type: 'Feature',
+            geometry: { type: 'Point', coordinates: [0, 0] },
+            properties: {},
+            id: 'testid4'
+          }
+        ]
+      }
+    } as any;
+  
+    /*
+    const actualResult: any = {
+      id: null,
+      date: null
+    };
+
+    const sampleRes = {
+      status: () => {
+        return {
+          json: (result: any) => {
+            actualResult = result;
+          }
+        };
+      }
+    };
+    */
+    
+    const result = regions.getRegions();
+
+    await result(sampleReq, (null as unknown) as any, (null as unknown) as any);
+
   });
 });
