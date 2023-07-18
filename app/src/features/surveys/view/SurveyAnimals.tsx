b5e0bb811189bade2d05bb95b2a35ab5a1d51743
Mac Deluca <Mac.Deluca@quartech.com>
2023-07-18 10:27:34 -0700

    Created section for individual animals in survey

---
+++ app/src/features/surveys/view/SurveyAnimals.tsx
@@ -0,0 +1,55 @@
+import { Box, Button, Divider, Paper } from '@material-ui/core';
+import { mdiAttachment, mdiFilePdfBox, mdiTrayArrowUp } from '@mdi/js';
+import Icon from '@mdi/react';
+import { ProjectRoleGuard } from 'components/security/Guards';
+import { H2MenuToolbar } from 'components/toolbar/ActionToolbars';
+import { PROJECT_ROLE, SYSTEM_ROLE } from 'constants/roles';
+import React from 'react';
+import NoSummarySectionData from '../components/NoSummarySectionData';
+
+const SurveyAnimals: React.FC = () => {
+  const handleUploadReportClick = () => {
+    //;
+  };
+  const handleUploadAttachmentClick = () => {
+    //;
+  };
+
+  return (
+    <Box>
+      <H2MenuToolbar
+        label="Individual Animals"
+        buttonLabel="Upload"
+        buttonTitle="Upload Animals"
+        buttonProps={{ variant: 'contained', disableElevation: true }}
+        buttonStartIcon={<Icon path={mdiTrayArrowUp} size={1} />}
+        menuItems={[
+          {
+            menuLabel: 'Upload Individual',
+            menuIcon: <Icon path={mdiFilePdfBox} size={1} />,
+            menuOnClick: handleUploadReportClick
+          },
+          {
+            menuLabel: 'Upload Bulk',
+            menuIcon: <Icon path={mdiAttachment} size={1} />,
+            menuOnClick: handleUploadAttachmentClick
+          }
+        ]}
+        renderButton={(buttonProps) => (
+          <ProjectRoleGuard
+            validProjectRoles={[PROJECT_ROLE.PROJECT_LEAD, PROJECT_ROLE.PROJECT_EDITOR]}
+            validSystemRoles={[SYSTEM_ROLE.SYSTEM_ADMIN, SYSTEM_ROLE.DATA_ADMINISTRATOR]}>
+            <Button {...buttonProps} />
+          </ProjectRoleGuard>
+        )}
+      />
+      <Divider></Divider>
+      <Box p={3}>
+        <Paper variant="outlined">
+          <NoSummarySectionData text={'No Individual Animals'} />
+        </Paper>
+      </Box>
+    </Box>
+  );
+};
+export default SurveyAnimals;
