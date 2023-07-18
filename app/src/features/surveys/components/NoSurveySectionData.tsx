da7190279194e0426bea0a4c6e136b442b81471b
Mac Deluca <Mac.Deluca@quartech.com>
2023-07-18 11:51:19 -0700

    renamed file to NoSurveySectionData

--- app/src/features/surveys/components/NoSurveySectionData.tsx
+++ app/src/features/surveys/components/NoSurveySectionData.tsx
@@ -1,5 +1,5 @@
 import Box from '@material-ui/core/Box';
-import Paper from '@material-ui/core/Paper';
+import Paper, { PaperProps } from '@material-ui/core/Paper';
 import { makeStyles, Theme } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme: Theme) => ({
@@ -11,17 +11,22 @@ const useStyles = makeStyles((theme: Theme) => ({
   }
 }));
 
-const NoSummaryResults = () => {
+interface NoSurveySectionDataProps {
+  text: string;
+  paperVariant?: PaperProps['variant'];
+}
+
+const NoSurveySectionData = ({ text, paperVariant }: NoSurveySectionDataProps) => {
   const classes = useStyles();
   return (
     <>
-      <Paper variant="outlined" className={classes.importFile}>
+      <Paper variant={paperVariant} elevation={0} className={classes.importFile}>
         <Box display="flex" flex="1 1 auto" alignItems="center" justifyContent="center" p={2}>
-          <span data-testid="summaries-nodata">No Summary Results</span>
+          <span data-testid="no-summary-section-data">{text}</span>
         </Box>
       </Paper>
     </>
   );
 };
 
-export default NoSummaryResults;
+export default NoSurveySectionData;
