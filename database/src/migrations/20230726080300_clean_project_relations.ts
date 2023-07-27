b51c65a675bfe2ffda5d2f5be9a160bb63908aea
Alfred Rosenthal <alfred.k.rosenthal@gmail.com>
2023-07-27 13:01:16 -0700

    wip

--- database/src/migrations/20230726080300_clean_project_relations.ts
+++ database/src/migrations/20230726080300_clean_project_relations.ts
@@ -12,15 +12,30 @@ const DB_SCHEMA_DAPI_V1 = process.env.DB_SCHEMA_DAPI_V1;
  */
 export async function up(knex: Knex): Promise<void> {
   await knex.raw(`
+    -------------------------------------------------------------------------
+    -- Remove old views
+    -------------------------------------------------------------------------
     SET search_path = ${DB_SCHEMA_DAPI_V1};
     DROP VIEW project;
     DROP VIEW climate_change_initiative;
     DROP VIEW project_climate_initiative;
+    DROP VIEW survey;
 
+    -------------------------------------------------------------------------
+    -- Remove Climate initiative tables
+    -------------------------------------------------------------------------
     SET search_path = ${DB_SCHEMA};
     DROP TABLE project_climate_initiative;
     DROP TABLE climate_change_initiative;
     
+
+    -------------------------------------------------------------------------
+    -- Add comments column to survey
+    -------------------------------------------------------------------------
+    ALTER TABLE survey ADD COLUMN 
+    -------------------------------------------------------------------------
+    -- Remove caveats table
+    -------------------------------------------------------------------------
     ALTER TABLE project DROP COLUMN caveats;
 
     SET search_path = ${DB_SCHEMA_DAPI_V1};
