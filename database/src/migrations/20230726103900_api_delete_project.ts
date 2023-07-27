63a26c86a277bca42b74c55609ac1d58962a77e7
Alfred Rosenthal <alfred.k.rosenthal@gmail.com>
2023-07-27 14:03:54 -0700

    updated delete for project

--- database/src/migrations/20230726103900_api_delete_project.ts
+++ database/src/migrations/20230726103900_api_delete_project.ts
@@ -41,7 +41,7 @@ export async function up(knex: Knex): Promise<void> {
  
      delete from survey where project_id = p_project_id;
      delete from stakeholder_partnership where project_id = p_project_id;
-     delete from project_activity where project_id = p_project_id;
+     delete from project_type where project_id = p_project_id;
      delete from project_management_actions where project_id = p_project_id;
      delete from project_funding_source where project_id = p_project_id;
      delete from project_iucn_action_classification where project_id = p_project_id;
