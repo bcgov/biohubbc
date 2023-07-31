61927116718fa50f9f1c2a1d5697e5cf933919b7
Alfred Rosenthal <alfred.k.rosenthal@gmail.com>
2023-07-31 16:24:50 -0700

    renamed project role permission table

--- database/src/migrations/20230728122200_new_project_roles.ts
+++ database/src/migrations/20230728122200_new_project_roles.ts
@@ -63,8 +63,8 @@ export async function up(knex: Knex): Promise<void> {
     COMMENT ON COLUMN project_permission.revision_count           IS 'Revision count used for concurrency control.';
     COMMENT ON TABLE  project_permission                          IS 'Project permissions.';
 
-    CREATE TABLE project_role_project_permission(
-      project_role_project_permission_id    integer            GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
+    CREATE TABLE project_role_permission(
+      project_role_permission_id    integer            GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
       project_permission_id                 integer           NOT NULL,
       project_role_id                       integer           NOT NULL,
       create_date                           timestamptz(6)    DEFAULT now() NOT NULL,
@@ -72,18 +72,18 @@ export async function up(knex: Knex): Promise<void> {
       update_date                           timestamptz(6),
       update_user                           integer,
       revision_count                        integer           DEFAULT 0 NOT NULL,
-      CONSTRAINT project_role_project_permission_pk PRIMARY KEY (project_role_project_permission_id)
+      CONSTRAINT project_role_permission_pk PRIMARY KEY (project_role_permission_id)
     );
 
-    COMMENT ON COLUMN project_role_project_permission.project_role_project_permission_id  IS 'System generated surrogate primary key identifier.';
-    COMMENT ON COLUMN project_role_project_permission.project_permission_id               IS 'The id of the project permission.';
-    COMMENT ON COLUMN project_role_project_permission.project_role_id                     IS 'The id of the project role.';
-    COMMENT ON COLUMN project_role_project_permission.create_date                         IS 'the datetime the record was created';
-    COMMENT ON COLUMN project_role_project_permission.create_user                         IS 'The id of the user who created the record as identified in the system user table.';
-    COMMENT ON COLUMN project_role_project_permission.update_date                         IS 'The datetime the record was updated.';
-    COMMENT ON COLUMN project_role_project_permission.update_user                         IS 'The id of the user who updated the record as identified in the system user table.';
-    COMMENT ON COLUMN project_role_project_permission.revision_count                      IS 'Revision count used for concurrency control.';
-    COMMENT ON TABLE  project_role_project_permission                                     IS 'A join table for project roles and their permissions.';
+    COMMENT ON COLUMN project_role_permission.project_role_permission_id  IS 'System generated surrogate primary key identifier.';
+    COMMENT ON COLUMN project_role_permission.project_permission_id               IS 'The id of the project permission.';
+    COMMENT ON COLUMN project_role_permission.project_role_id                     IS 'The id of the project role.';
+    COMMENT ON COLUMN project_role_permission.create_date                         IS 'the datetime the record was created';
+    COMMENT ON COLUMN project_role_permission.create_user                         IS 'The id of the user who created the record as identified in the system user table.';
+    COMMENT ON COLUMN project_role_permission.update_date                         IS 'The datetime the record was updated.';
+    COMMENT ON COLUMN project_role_permission.update_user                         IS 'The id of the user who updated the record as identified in the system user table.';
+    COMMENT ON COLUMN project_role_permission.revision_count                      IS 'Revision count used for concurrency control.';
+    COMMENT ON TABLE  project_role_permission                                     IS 'A join table for project roles and their permissions.';
 
     -------------------------------------------------------------------------
     -- Create audit and journal triggers for new tables
@@ -91,18 +91,18 @@ export async function up(knex: Knex): Promise<void> {
     CREATE TRIGGER audit_project_permission BEFORE INSERT OR UPDATE OR DELETE ON project_permission for each ROW EXECUTE PROCEDURE tr_audit_trigger();
     CREATE TRIGGER journal_project_permission AFTER INSERT OR UPDATE OR DELETE ON project_permission for each ROW EXECUTE PROCEDURE tr_journal_trigger();
     
-    CREATE TRIGGER audit_project_role_project_permission BEFORE INSERT OR UPDATE OR DELETE ON project_role_project_permission for each ROW EXECUTE PROCEDURE tr_audit_trigger();
-    CREATE TRIGGER journal_project_role_project_permission AFTER INSERT OR UPDATE OR DELETE ON project_role_project_permission for each ROW EXECUTE PROCEDURE tr_journal_trigger();
+    CREATE TRIGGER audit_project_role_permission BEFORE INSERT OR UPDATE OR DELETE ON project_role_permission for each ROW EXECUTE PROCEDURE tr_audit_trigger();
+    CREATE TRIGGER journal_project_role_permission AFTER INSERT OR UPDATE OR DELETE ON project_role_permission for each ROW EXECUTE PROCEDURE tr_journal_trigger();
 
     ----------------------------------------------------------------------------------------
     -- Create Indexes and Constraints for new tables
     ----------------------------------------------------------------------------------------
 
-    ALTER TABLE project_role_project_permission ADD CONSTRAINT project_role_project_permission_fk1 
+    ALTER TABLE project_role_permission ADD CONSTRAINT project_role_permission_fk1 
       FOREIGN KEY (project_permission_id)
       REFERENCES project_permission(project_permission_id);
 
-    ALTER TABLE project_role_project_permission ADD CONSTRAINT project_role_project_permission_fk2
+    ALTER TABLE project_role_permission ADD CONSTRAINT project_role_permission_fk2
       FOREIGN KEY (project_role_id)
       REFERENCES project_role(project_role_id);
 
@@ -110,8 +110,8 @@ export async function up(knex: Knex): Promise<void> {
     CREATE UNIQUE INDEX project_permission_nuk1 ON project_permission(name, (record_end_date is NULL)) where record_end_date is null;
 
     -- Add indexes on foreign key columns
-    CREATE UNIQUE INDEX project_role_project_permission_idx1 ON project_role_project_permission(project_permission_id);
-    CREATE UNIQUE INDEX project_role_project_permission_idx2 ON project_role_project_permission(project_role_id);
+    CREATE UNIQUE INDEX project_role_permission_idx1 ON project_role_permission(project_permission_id);
+    CREATE UNIQUE INDEX project_role_permission_idx2 ON project_role_permission(project_role_id);
 
     -------------------------------------------------------------------------
     -- End date old roles
@@ -125,7 +125,7 @@ export async function up(knex: Knex): Promise<void> {
     VALUES 
       ('Coordinator', NOW(), 'The administrative lead of the project.'),
       ('Collaborator', NOW(), 'A participant team member of the project.'),
-      ('Observer', NOW(), 'A reviewer of the project.');
+      ('Observer', NOW(), 'Read only permissions for a project.');
 
     -------------------------------------------------------------------------
     -- Update existing project_participation with new project roles
@@ -134,44 +134,21 @@ export async function up(knex: Knex): Promise<void> {
     -- select the role id for the old role
     -- select the role id for the new role
     -- update the existing project_participation row for the old role id with the new role id
-    UPDATE project_participation SET project_role_id = (
-      SELECT project_role_id 
-      FROM project_role 
-      WHERE name = 'Coordinator')
-    WHERE project_participation.project_participation_id = (
-      SELECT pp.project_participation_id 
-      FROM project_participation pp, (
-        SELECT project_role_id 
-        FROM project_role 
-        WHERE name = 'Project Lead') as pr
-      WHERE pp.project_role_id = pr.project_role_id
+    UPDATE project_participation as pp SET project_role_id = (
+      SELECT pr1.project_role_id 
+      FROM project_role pr1
+      WHERE pr1.name = 'Coordinator') 
+    WHERE pp.project_participation_id IN (
+      SELECT project_participation_id  
+      FROM project_participation pp 
+      WHERE pp.project_role_id = (
+        SELECT pr2.project_role_id 
+        FROM project_role pr2
+        WHERE pr2.name = 'Editor'
+      )
     );
 
-    UPDATE project_participation SET project_role_id = (
-      SELECT project_role_id 
-      FROM project_role 
-      WHERE name = 'Collaborator')
-    WHERE project_participation.project_participation_id = (
-      SELECT pp.project_participation_id 
-      FROM project_participation pp, (
-        SELECT project_role_id 
-        FROM project_role 
-        WHERE name = 'Editor') as pr
-      WHERE pp.project_role_id = pr.project_role_id
-    );
 
-    UPDATE project_participation SET project_role_id = (
-      SELECT project_role_id 
-      FROM project_role 
-      WHERE name = 'Observer')
-    WHERE project_participation.project_participation_id = (
-      SELECT pp.project_participation_id 
-      FROM project_participation pp, (
-        SELECT project_role_id 
-        FROM project_role 
-        WHERE name = 'Viewer') as pr
-      WHERE pp.project_role_id = pr.project_role_id
-    );
 
     -------------------------------------------------------------------------
     -- Create views
@@ -179,7 +156,7 @@ export async function up(knex: Knex): Promise<void> {
     SET SEARCH_PATH=biohub_dapi_v1;
     
     CREATE OR REPLACE VIEW project_permission AS SELECT * FROM biohub.project_permission;
-    CREATE OR REPLACE VIEW project_role_project_permission AS SELECT * FROM biohub.project_role_project_permission;
+    CREATE OR REPLACE VIEW project_role_permission AS SELECT * FROM biohub.project_role_permission;
 
   `);
 }
