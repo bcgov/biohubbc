28ebba2c82ca2d4974ff57af779df41e340e4c86
Mac Deluca <Mac.Deluca@quartech.com>
2023-08-16 09:19:37 -0700

    updated animal captured sub header

--- app/src/features/surveys/view/survey-animals/IndividualAnimalForm.tsx
+++ app/src/features/surveys/view/survey-animals/IndividualAnimalForm.tsx
@@ -1,6 +1,6 @@
 import { Typography } from '@mui/material';
 import { Form, useFormikContext } from 'formik';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Critter, IAnimal } from './animal';
 import CaptureAnimalForm from './form-sections/CaptureAnimalForm';
 import FamilyAnimalForm from './form-sections/FamilyAnimalForm';
@@ -9,15 +9,26 @@ import MarkingAnimalForm from './form-sections/MarkingAnimalForm';
 import MeasurementAnimalForm from './form-sections/MeasurementAnimalForm';
 import MortalityAnimalForm from './form-sections/MortalityAnimalForm';
 
+interface IndividualAnimalFormProps {
+  getAnimalCount: (num: number) => void;
+}
+
 /**
  * Renders The 'Individual Animals' Form displayed in Survey view
  * Note: Lots of conditionally rendered sections.
  *
+ * params {IndividualAnimalFormProps}
  * returns {*}
  */
 
-const IndividualAnimalForm = () => {
+const IndividualAnimalForm = ({ getAnimalCount }: IndividualAnimalFormProps) => {
   const { values } = useFormikContext<IAnimal>();
+
+  useEffect(() => {
+    //placeholder for when the form handles multiple animals.
+    getAnimalCount(values.general.taxon_id ? 1 : 0);
+  }, [values.general.taxon_id]);
+
   return (
     <Form>
       <Typography variant="h4">Add New Individual</Typography>
