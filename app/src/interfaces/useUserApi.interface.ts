05c8af2faec0265cc8fe17aae2d304d8e24894dd
Alfred Rosenthal <alfred.k.rosenthal@gmail.com>
2023-08-17 15:46:56 -0700

    added new interface for user search

--- app/src/interfaces/useUserApi.interface.ts
+++ app/src/interfaces/useUserApi.interface.ts
@@ -6,3 +6,16 @@ export interface IGetUserResponse {
   identity_source: string;
   role_names: string[];
 }
+
+export interface ISearchUserResponse {
+  system_user_id: number;
+  user_identifier: string;
+  user_guid: string;
+  identity_source: string;
+  record_end_date: string;
+  role_ids: number[];
+  role_names: string[];
+  email: string;
+  display_name: string;
+  agency: string;
+}
