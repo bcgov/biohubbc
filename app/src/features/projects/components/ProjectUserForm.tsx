ba35b75d5b95cb8a3a1e3d6c52757bd2b39d14bb
Alfred Rosenthal <alfred.k.rosenthal@gmail.com>
2023-08-17 16:54:32 -0700

    filtered out selected users from dropdown

--- app/src/features/projects/components/ProjectUserForm.tsx
+++ app/src/features/projects/components/ProjectUserForm.tsx
@@ -6,7 +6,7 @@ import { ICode } from 'interfaces/useCodesApi.interface';
 import { ICreateProjectRequest } from 'interfaces/useProjectApi.interface';
 import { ISearchUserResponse } from 'interfaces/useUserApi.interface';
 import { debounce } from 'lodash';
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import yup from 'utils/YupSchema';
 
 export const AddProjectUser = yup.object().shape({
@@ -51,18 +51,25 @@ const ProjectUserForm: React.FC<IProjectUser> = (props) => {
 
   const [searchUsers, setSearchUsers] = useState<ISearchUserResponse[]>([]);
   const [isSearching, setIsSearching] = useState(false);
-  // const [selectedUsers, setSelectedUsers] = useState<{ system_user_id: number; role_id: number }[]>([]);
+  const [selectedUsers] = useState<ISearchUserResponse[]>([]);
+
+  useEffect(() => {
+    // currently logged in user is assumed to be the 'creator'
+    // this will need to move out specifically to the create project step because it will be assumed they are a coordinator on the project
+  }, []);
 
   const handleSearch = useMemo(
     () =>
-      debounce(async (inputValue: string, existingValues: number[]) => {
+      debounce(async (inputValue: string, existingValues: ISearchUserResponse[]) => {
         setIsSearching(true);
         const response = await biohubApi.user.searchSystemUser(inputValue.toLowerCase());
-        // const newOptions = convertOptions(response.searchResponse).filter(
-        //   (item) => !existingValues?.includes(item.value)
-        // );
+
+        // filter out any selected values from dropdown
+        const filteredList = response.filter(
+          (item) => !existingValues.some((existing) => existing.system_user_id === item.system_user_id)
+        );
         setIsSearching(false);
-        setSearchUsers(response);
+        setSearchUsers(filteredList);
       }, 500),
     [biohubApi.user]
   );
@@ -84,17 +91,19 @@ const ProjectUserForm: React.FC<IProjectUser> = (props) => {
       </Box>
       <Box mt={3}>
         <Autocomplete
-          autoSelect
           clearOnBlur
-          blurOnSelect
           handleHomeEndKeys
           id=""
           options={searchUsers}
-          onInputChange={(event, value) => {
-            handleSearch(value, []);
+          getOptionLabel={(option) => option.display_name || ''}
+          onInputChange={(_, value) => {
+            handleSearch(value, selectedUsers);
           }}
-          onChange={(event, option) => {
-            console.log('IS THIS THE SELECT OR THE SEARCH?');
+          onChange={(_, option) => {
+            if (option) {
+              selectedUsers.push(option);
+            }
+            console.log(selectedUsers);
           }}
           renderInput={(params) => (
             <TextField
