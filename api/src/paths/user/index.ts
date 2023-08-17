13591f170b5cc334132eac1e009e538f9ee831ca
Alfred Rosenthal <alfred.k.rosenthal@gmail.com>
2023-08-17 15:46:38 -0700

    updated logger path

--- api/src/paths/user/index.ts
+++ api/src/paths/user/index.ts
@@ -5,7 +5,7 @@ import { authorizeRequestHandler } from '../../request-handlers/security/authori
 import { UserService } from '../../services/user-service';
 import { getLogger } from '../../utils/logger';
 
-const defaultLog = getLogger('paths/funding-sources/index');
+const defaultLog = getLogger('paths/user/index');
 
 export const GET: Operation = [
   authorizeRequestHandler(() => {
