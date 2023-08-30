2e5409111bcc1532926ec88bde38d942e5033300
Curtis Upshall <curtisupshall@gmail.com>
2023-08-30 15:49:14 -0700

    SIMSBIOHUB-262: Address PR feedback

--- database/src/migrations/20230830143800_create_survey_site_strategies.ts
+++ database/src/migrations/20230830143800_create_survey_site_strategies.ts
@@ -1,8 +1,10 @@
 import { Knex } from 'knex';
 
 /**
- * Adds new survey partnerships tables
- *
+ * 1. Adds two new tables:
+ *  - Codes table for site selection strategy 
+ *  - Xref table for survey site selection strategies
+ * 2. Updates the survey delete procedure to account for blocks, stratums and survey site selection strategies
  * @export
  * @param {Knex} knex
  * @return {*}  {Promise<void>}
@@ -19,8 +21,8 @@ export async function up(knex: Knex): Promise<void> {
   CREATE TABLE site_strategy(
     site_strategy_id                        integer           GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
     name                                    varchar(50)       NOT NULL,
-    record_effective_date                   date              NOT NULL,
     description                             varchar(250),
+    record_effective_date                   date              NOT NULL,
     record_end_date                         date,
     create_date                             timestamptz(6)    DEFAULT now() NOT NULL,
     create_user                             integer           NOT NULL,
@@ -29,7 +31,6 @@ export async function up(knex: Knex): Promise<void> {
     revision_count                          integer           DEFAULT 0 NOT NULL,
     CONSTRAINT site_strategy_pk PRIMARY KEY (site_strategy_id)
   );
-
 
 
   COMMENT ON COLUMN site_strategy.site_strategy_id IS 'System generated surrogate primary key identifier.'
@@ -61,11 +62,11 @@ export async function up(knex: Knex): Promise<void> {
   ----------------------------------------------------------------------------------------
 
   -- Add unique constraint
-  CREATE UNIQUE INDEX site_strategy_uk1 ON site_strategy(name);
+  CREATE UNIQUE INDEX site_strategy_uk1 ON site_strategy(name, record_end_date);
 
   -- Create audit and journal triggers
-  create trigger audit_observation before insert or update or delete on site_strategy for each row execute procedure tr_audit_trigger();
-  create trigger journal_observation after insert or update or delete on site_strategy for each row execute procedure tr_journal_trigger();
+  create trigger audit_site_strategy before insert or update or delete on site_strategy for each row execute procedure tr_audit_trigger();
+  create trigger journal_site_strategy after insert or update or delete on site_strategy for each row execute procedure tr_journal_trigger();
 
   
   ----------------------------------------------------------------------------------------
@@ -85,7 +86,7 @@ export async function up(knex: Knex): Promise<void> {
   CREATE TABLE survey_site_strategy(
     survey_site_strategy_id           integer           GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
     survey_id                         integer           NOT NULL,
-    type_id                           integer           NOT NULL,
+    site_strategy_id                  integer           NOT NULL,
     create_date                       timestamptz(6)    DEFAULT now() NOT NULL,
     create_user                       integer           NOT NULL,
     update_date                       timestamptz(6),
@@ -132,13 +133,108 @@ export async function up(knex: Knex): Promise<void> {
   CREATE TRIGGER journal_survey_site_strategy AFTER INSERT OR UPDATE OR DELETE ON survey_site_strategy for each ROW EXECUTE PROCEDURE tr_journal_trigger();
 
   ----------------------------------------------------------------------------------------
-  -- Create views
+  -- Create new views for both new tables
   ----------------------------------------------------------------------------------------
 
   set search_path=biohub_dapi_v1;
 
   create or replace view site_strategy as select * from biohub.site_strategy;
   create or replace view survey_site_strategy as select * from biohub.survey_site_strategy;
+
+  ----------------------------------------------------------------------------------------
+  -- Update api_delete_survey procedure
+  ----------------------------------------------------------------------------------------
+
+  set search_path=biohub;
+
+  CREATE OR REPLACE PROCEDURE api_delete_survey(p_survey_id integer)
+  LANGUAGE plpgsql
+  SECURITY DEFINER
+  AS $procedure$
+   -- *******************************************************************
+     -- Procedure: api_delete_survey
+     -- Purpose: deletes a survey and dependencies
+     --
+     -- MODIFICATION HISTORY
+     -- Person           Date        Comments
+     -- ---------------- ----------- --------------------------------------
+     -- shreyas.devalapurkar@quartech.com
+     --                  2021-06-18  initial release
+     -- charlie.garrettjones@quartech.com
+     --                  2021-06-21  added occurrence submission delete
+     -- charlie.garrettjones@quartech.com
+     --                  2021-09-21  added survey summary submission delete
+     -- kjartan.einarsson@quartech.com
+     --                  2022-08-28  added survey_vantage, survey_spatial_component, survey delete
+     -- charlie.garrettjones@quartech.com
+     --                  2022-09-07  changes to permit model
+     -- charlie.garrettjones@quartech.com
+     --                  2022-10-05  1.3.0 model changes
+     -- charlie.garrettjones@quartech.com
+     --                  2022-10-05  1.5.0 model changes, drop concept of occurrence deletion for published data
+     -- charlie.garrettjones@quartech.com
+     --                  2023-03-14  1.7.0 model changes
+     -- alfred.rosenthal@quartech.com
+     --                  2023-03-15  added missing publish tables to survey delete
+     -- curtis.upshall@quartech.com
+     --                  2023-04-28  change order of survey delete procedure
+     -- alfred.rosenthal@quartech.com
+     --                  2023-07-26  delete regions
+     -- curtis.upshall@quartech.com
+     --                  2023-08-24  delete partnerships
+     -- curtis.upshall@quartech.com
+     --                  2023-08-24  delete survey blocks and stratums and participation
+     -- *******************************************************************
+     declare
+
+     begin
+      with occurrence_submissions as (select occurrence_submission_id from occurrence_submission where survey_id = p_survey_id), submission_spatial_components as (select submission_spatial_component_id from submission_spatial_component
+      where occurrence_submission_id in (select occurrence_submission_id from occurrence_submissions))
+      delete from spatial_transform_submission where submission_spatial_component_id in (select submission_spatial_component_id from submission_spatial_components);
+      delete from submission_spatial_component where occurrence_submission_id in (select occurrence_submission_id from occurrence_submission where survey_id = p_survey_id);
+
+      with occurrence_submissions as (select occurrence_submission_id from occurrence_submission where survey_id = p_survey_id)
+      , submission_statuses as (select submission_status_id from submission_status
+      where occurrence_submission_id in (select occurrence_submission_id from occurrence_submissions))
+      delete from submission_message where submission_status_id in (select submission_status_id from submission_statuses);
+      delete from submission_status where occurrence_submission_id in (select occurrence_submission_id from occurrence_submission where survey_id = p_survey_id);
+
+      delete from occurrence_submission_publish where occurrence_submission_id in (select occurrence_submission_id from occurrence_submission where survey_id = p_survey_id);
+
+      delete from occurrence_submission where survey_id = p_survey_id;
+      
+      delete from survey_summary_submission_publish where survey_summary_submission_id in (select survey_summary_submission_id from survey_summary_submission where survey_id = p_survey_id);
+      delete from survey_summary_submission_message where survey_summary_submission_id in (select survey_summary_submission_id from survey_summary_submission where survey_id = p_survey_id);
+      delete from survey_summary_submission where survey_id = p_survey_id;
+      delete from survey_proprietor where survey_id = p_survey_id;
+      delete from survey_attachment_publish where survey_attachment_id in (select survey_attachment_id from survey_attachment where survey_id = p_survey_id);
+      delete from survey_attachment where survey_id = p_survey_id;
+      delete from survey_report_author where survey_report_attachment_id in (select survey_report_attachment_id from survey_report_attachment where survey_id = p_survey_id);
+      delete from survey_report_publish where survey_report_attachment_id in (select survey_report_attachment_id from survey_report_attachment where survey_id = p_survey_id);
+      delete from survey_report_attachment where survey_id = p_survey_id;
+      delete from study_species where survey_id = p_survey_id;
+      delete from survey_funding_source where survey_id = p_survey_id;
+      delete from survey_vantage where survey_id = p_survey_id;
+      delete from survey_spatial_component where survey_id = p_survey_id;
+      delete from survey_metadata_publish where survey_id = p_survey_id;
+      delete from survey_region where survey_id = p_survey_id;
+      delete from survey_first_nation_partnership where survey_id = p_survey_id;
+      delete from survey_block where survey_id = p_survey_id;
+      delete from permit where survey_id = p_survey_id;
+      delete from survey_type where survey_id = p_survey_id;
+      delete from survey_first_nation_partnership where survey_id = p_survey_id;
+      delete from survey_stakeholder_partnership where survey_id = p_survey_id;
+      delete from survey_participation where survey_id = p_survey_id;
+      delete from survey_stratum where survey_id = p_survey_id;
+      delete from survey_block where survey_id = p_survey_id;
+      delete from survey_site_strategy where survey_id = p_survey_id; 
+      delete from survey where survey_id = p_survey_id;
+
+      exception
+        when others THEN
+          raise;
+      end;
+   $procedure$;
 
   `);
 }
