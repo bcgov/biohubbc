817b47a11a1b2ea7df2cda638b544e9e3426b6aa
JeremyQuartech <jeremy.kissack@quartech.com>
2023-08-31 13:32:18 -0700

    Added empty telemetry form

---
+++ app/src/features/surveys/view/survey-animals/TelemetryDeviceForm.tsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material"
+import { Form } from "formik"
+
+const IndividualAnimalForm = () => {
+
+    return (
+        <Form>
+            <Typography variant="h4">Add Telemetry Device</Typography>
+        </Form>
+    )
+}
+
+export default IndividualAnimalForm;
