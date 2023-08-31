9f5d0b4db247ae240ce0fe8c5585aa929c82f7f2
Curtis Upshall <curtisupshall@gmail.com>
2023-08-31 14:12:35 -0700

    SIMSBIOHUB-192: Added stratum form yup schema

--- app/src/features/surveys/components/SurveyStratumForm.tsx
+++ app/src/features/surveys/components/SurveyStratumForm.tsx
@@ -4,6 +4,7 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import Paper from '@mui/material/Paper';
+import DialogActions from '@mui/materia/DialogActions';
 import {  useFormikContext } from 'formik';
 import { IEditSurveyRequest } from 'interfaces/useSurveyApi.interface';
 import yup from 'utils/YupSchema';
@@ -19,14 +20,49 @@ interface IStratum {
   description: string;
 }
 
+interface IStratumForm {
+  index: number | null;
+  stratum: IStratum;
+}
+
+const StratumFormInitialValues: IStratumForm = {
+  index: null,
+  stratum: {
+    survey_stratum_id: undefined,
+    name: '',
+    description: ''
+  }
+}
+
+const StratumFormYupSchema = yup.object().shape({
+  index: yup.number().nullable(true),
+  stratum: yup.object().shape({
+    survey_stratum_id: yup.number(),
+    name: yup
+      .string()
+      .required('Must provide a stratum name')
+      .max(300, 'Name cannot exceed 300 characters'),
+    description: yup
+      .string()
+      .max(3000, 'Description cannot exceed 3000 characters'),
+  })
+})
+
 interface IStratumDialogProps {
   open: boolean;
   editing: boolean;
-  stratumIndex: number | null;
-  onClose: () => void;
+  stratumForm: IStratumForm
+  onCancel: () => void;
+  onSave: (stratumForm: IStratumForm) => void;
 }
 
 const StratumDialog = (props: IStratumDialogProps) => {
+  const [name, setName] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
+
+  const formikProps = useFormikContext();
+  const { handleChange } = formikProps;
+
   const handleChangeName = () => {
     //
   }
@@ -35,15 +71,19 @@ const StratumDialog = (props: IStratumDialogProps) => {
     //
   }
 
+  const handleSave = () => {
+    props.onSave(null)
+  }
+
   const formikContext = useFormikContext<IEditSurveyRequest>();
-  const { values } = formikContext
+  const { values, handleSubmit } = formikContext
 
   return (
     <ComponentDialog
       open={props.open}
       dialogTitle={props.editing ? 'Edit Stratum Details' : 'Add Stratum'}
-      onClose={props.onClose}>
-      <>
+      onClose={props.onCancel}>
+      <form onSubmit={handleSubmit}>
         <DialogContentText>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. Aliquam erat volutpat. Donec placerat nisl magna, et faucibus arcu condimentum sed.</DialogContentText>
         <TextField
           fullWidth
@@ -63,7 +103,10 @@ const StratumDialog = (props: IStratumDialogProps) => {
           onChange={handleChangeDescription}
           label='Description'
         />
-
+        <DialogActions>
+          <Button onClick={() => handleSave()}>Save</Button>
+          <Button onClick={() => handleCancel()}>Cancel</Button>
+        </DialogActions>
       </>
     </ComponentDialog>
   )
@@ -76,7 +119,10 @@ const StratumDialog = (props: IStratumDialogProps) => {
  */
 const SurveyStratumForm = () => {
   const [stratumIndex, setStratumIndex] =  useState<number | null>(null);
+  const [initialStratum, setInitialStratum] = useState<IStratum | null>(null);
+
   const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [dialogOpen, setDialogOpen] = useState<boolean>(false);
 
   const formikProps = useFormikContext<IEditSurveyRequest>();
   const { values, handleSubmit, setFieldValue } = formikProps;
@@ -102,82 +148,92 @@ const SurveyStratumForm = () => {
   */
 
   return (
-    <form onSubmit={handleSubmit}>
+    <>
       <StratumDialog
-        open={stratumIndex !== null}
-        onClose={() => {}}
+        open={dialogOpen}
+        onCancel={() => {}}
+
+        onSave={(stratum) => {
+          if (isEditing) {
+            // 
+          } else {
+
+          }
+        }}
         stratumIndex={stratumIndex}
         editing={false}
       />
-      <Box my={2}>
-        {values.site_selection_strategies.stratums.map((stratum, index: number) => {
-          return (
-            <Box mt={1} className="userRoleItemContainer">
-              <Paper
-                variant="outlined"
-                // className={error ? 'userRoleItemError' : 'userRoleItem'}
-                sx={{
-                  '&.userRoleItem': {
-                    borderColor: 'grey.400'
-                  },
-                  '&.userRoleItemError': {
-                    borderColor: 'error.main',
-                    '& .MuiOutlinedInput-notchedOutline': {
-                      borderColor: 'error.main'
+      <form onSubmit={handleSubmit}>
+        <Box my={2}>
+          {values.site_selection_strategies.stratums.map((stratum, index: number) => {
+            return (
+              <Box mt={1} className="userRoleItemContainer">
+                <Paper
+                  variant="outlined"
+                  // className={error ? 'userRoleItemError' : 'userRoleItem'}
+                  sx={{
+                    '&.userRoleItem': {
+                      borderColor: 'grey.400'
                     },
-                    '& + p': {
-                      pt: 0.75,
-                      pb: 0.75,
-                      pl: 2
+                    '&.userRoleItemError': {
+                      borderColor: 'error.main',
+                      '& .MuiOutlinedInput-notchedOutline': {
+                        borderColor: 'error.main'
+                      },
+                      '& + p': {
+                        pt: 0.75,
+                        pb: 0.75,
+                        pl: 2
+                      }
                     }
-                  }
-                }}>
-                <Box display="flex" alignItems="center" px={2} py={1.5}>
-                  <Box flex="1 1 auto">
-                  
-                    <Typography variant="subtitle1" fontWeight="bold">
-                      {stratum.name}
-                    </Typography>
-                    <Typography variant="subtitle2" color="textSecondary">
-                      {stratum.description}
-                    </Typography>
+                  }}>
+                  <Box display="flex" alignItems="center" px={2} py={1.5}>
+                    <Box flex="1 1 auto">
+                    
+                      <Typography variant="subtitle1" fontWeight="bold">
+                        {stratum.name}
+                      </Typography>
+                      <Typography variant="subtitle2" color="textSecondary">
+                        {stratum.description}
+                      </Typography>
+                    </Box>
+                    <Box flex="0 0 auto">
+                      <IconButton
+                        sx={{
+                          ml: 2
+                        }}
+                        aria-label="remove user from project team"
+                        onClick={() => {
+                          
+                        }}>
+                        <Icon path={mdiDotsVertical} size={1}></Icon>
+                      </IconButton>
+                    </Box>
                   </Box>
-                  <Box flex="0 0 auto">
-                    <IconButton
-                      sx={{
-                        ml: 2
-                      }}
-                      aria-label="remove user from project team"
-                      onClick={() => {
-                        
-                      }}>
-                      <Icon path={mdiDotsVertical} size={1}></Icon>
-                    </IconButton>
-                  </Box>
-                </Box>
-              </Paper>
-              {
-              //error
-              }
-            </Box>
-          );
-        })}
+                </Paper>
+                {
+                //error
+                }
+              </Box>
+            );
+          })}
 
-        <Box mt={2}>
-          <Button
-            data-testid="stratum-add-button"
-            variant="outlined"
-            color="primary"
-            title="Create Stratum"
-            aria-label="Create Stratum"
-            startIcon={<Icon path={mdiPlus} size={1} />}
-            onClick={() => handleCreateStratum()}
-            >
-            Add Stratum
-          </Button>
+          <Box mt={2}>
+            <Button
+              data-testid="stratum-add-button"
+              variant="outlined"
+              color="primary"
+              title="Create Stratum"
+              aria-label="Create Stratum"
+              startIcon={<Icon path={mdiPlus} size={1} />}
+              onClick={() => handleCreateStratum()}
+              >
+              Add Stratum
+            </Button>
+          </Box>
         </Box>
-      </Box>
-    </form>
+      </form>
+    </>
   );
 };
 
