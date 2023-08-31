cbee3d5227e78b023576ed86d2c96ce380be93d6
JeremyQuartech <jeremy.kissack@quartech.com>
2023-08-31 15:17:15 -0700

    WIP: telemetry form

--- app/src/features/surveys/view/survey-animals/TelemetryDeviceForm.tsx
+++ app/src/features/surveys/view/survey-animals/TelemetryDeviceForm.tsx
@@ -1,13 +1,17 @@
 import { Typography } from "@mui/material"
+import Grid from '@mui/material/Grid';
 import { Form } from "formik"
 
-const IndividualAnimalForm = () => {
+const TelemetryDeviceForm = () => {
 
     return (
         <Form>
             <Typography variant="h4">Add Telemetry Device</Typography>
+            <Grid item xs={6} key={'telemetry-device-section'}>
+
+            </Grid>
         </Form>
     )
 }
 
-export default IndividualAnimalForm;
+export default TelemetryDeviceForm;
