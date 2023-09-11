53cc39b5ee88144950803840a0fb699e7a4c4579
jeznorth <twenty15creative@gmail.com>
2023-09-11 13:50:00 -0700

    SIMSBIOHUB-259: General UI Fixes / Clean-up (#1088)
    Front-end polish and content updates
    ---------
    
    Co-authored-by: Alfred Rosenthal <alfred.k.rosenthal@gmail.com>

--- app/src/features/surveys/components/CreateSurveyBlockDialog.tsx
+++ app/src/features/surveys/components/CreateSurveyBlockDialog.tsx
@@ -1,9 +1,4 @@
-import CloseIcon from '@mui/icons-material/Close';
-import { Typography } from '@mui/material';
-import IconButton from '@mui/material/IconButton';
-import Snackbar from '@mui/material/Snackbar';
 import EditDialog from 'components/dialog/EditDialog';
-import { useState } from 'react';
 import BlockForm from './BlockForm';
 import { BlockYupSchema } from './SurveyBlockSection';
 interface ICreateBlockProps {
@@ -14,15 +9,10 @@ interface ICreateBlockProps {
 
 const CreateSurveyBlockDialog: React.FC<ICreateBlockProps> = (props) => {
   const { open, onSave, onClose } = props;
-  const [isSnackBarOpen, setIsSnackBarOpen] = useState(false);
-  const [blockName, setBlockName] = useState('');
   return (
     <>
       <EditDialog
         dialogTitle={'Add Block'}
-        dialogText={
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. Aliquam erat volutpat. Donec placerat nisl magna, et faucibus arcu condimentum sed.'
-        }
         open={open}
         dialogLoading={false}
         component={{
@@ -37,35 +27,8 @@ const CreateSurveyBlockDialog: React.FC<ICreateBlockProps> = (props) => {
         dialogSaveButtonLabel="Add Block"
         onCancel={() => onClose()}
         onSave={(formValues) => {
-          setBlockName(formValues.name);
-          setIsSnackBarOpen(true);
           onSave(formValues);
         }}
-      />
-
-      <Snackbar
-        open={isSnackBarOpen}
-        autoHideDuration={6000}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'center'
-        }}
-        onClose={() => {
-          setIsSnackBarOpen(false);
-          setBlockName('');
-        }}
-        message={
-          <>
-            <Typography variant="body2" component="div">
-              Block <strong>{blockName}</strong> has been added.
-            </Typography>
-          </>
-        }
-        action={
-          <IconButton size="small" aria-label="close" color="inherit" onClick={() => setIsSnackBarOpen(false)}>
-            <CloseIcon fontSize="small" />
-          </IconButton>
-        }
       />
     </>
   );
