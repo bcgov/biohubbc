88558b65c26687fdbeaad5ef3d61d489449039ef
Kjartan <Kjartan.Einarsson@quartech.com>
2023-09-15 15:00:59 -0700

    db migration, initial repo files

---
+++ api/src/repositories/sample-location-repository.ts
@@ -0,0 +1,178 @@
+import { Feature } from 'geojson';
+import SQL from 'sql-template-strings';
+import { z } from 'zod';
+import { ApiExecuteSQLError } from '../errors/api-error';
+import { generateGeometryCollectionSQL } from '../utils/spatial-utils';
+import { BaseRepository } from './base-repository';
+
+export interface PostSampleLocation {
+  survey_sample_site_id: number | null;
+  survey_id: number;
+  name: string;
+  description: string;
+  survey_sample_site: Feature;
+}
+
+// This describes the a row in the database for Survey Block
+export const SampleLocationRecord = z.object({
+  survey_sample_site_id: z.number(),
+  survey_id: z.number(),
+  geojson: z.any(),
+  geography: z.any(),
+  description: z.string(),
+  create_date: z.string(),
+  create_user: z.number(),
+  update_date: z.string().nullable(),
+  update_user: z.number().nullable(),
+  revision_count: z.number()
+});
+export type SampleLocationRecord = z.infer<typeof SampleLocationRecord>;
+
+/**
+ * Sample Repository
+ *
+ * @export
+ * @class SampleLocationRepository
+ * @extends {BaseRepository}
+ */
+export class SampleLocationRepository extends BaseRepository {
+  /**
+   * Gets all survey Sample Locations.
+   *
+   * @param {number} surveyId
+   * @return {*}  {Promise<SampleLocationRecord[]>}
+   * @memberof SampleLocationRepository
+   */
+  async getSampleLocationsForSurveyId(surveyId: number): Promise<SampleLocationRecord[]> {
+    const sql = SQL`
+      SELECT *
+      FROM survey_sample_site
+      WHERE survey_id = ${surveyId};
+    `;
+
+    const response = await this.connection.sql(sql, SampleLocationRecord);
+    return response.rows || [];
+  }
+
+  /**
+   * updates a survey Sample Location.
+   *
+   * @param {PostSampleLocation} sample
+   * @return {*}  {Promise<SampleLocationRecord>}
+   * @memberof SampleLocationRepository
+   */
+  async updateSampleLocation(sample: PostSampleLocation): Promise<SampleLocationRecord> {
+    const sql = SQL`
+      UPDATE survey_sample_site
+      SET
+        survey_id=${sample.survey_id},
+        name=${sample.name},
+        description=${sample.description},
+        geojson=${sample.survey_sample_site},
+        geography=public.geography(
+          public.ST_Force2D(
+            public.ST_SetSRID(
+    `;
+    const geometryCollectionSQL = generateGeometryCollectionSQL(sample.survey_sample_site);
+    sql.append(geometryCollectionSQL);
+    sql.append(SQL`, 4326)))`);
+    sql.append(SQL`
+        )
+        WHERE
+        survey_sample_site_id = ${sample.survey_sample_site_id}
+      RETURNING
+        *;`);
+
+    const response = await this.connection.sql(sql, SampleLocationRecord);
+
+    if (!response.rowCount) {
+      throw new ApiExecuteSQLError('Failed to update survey block', [
+        'SampleLocationRepository->updateSampleLocation',
+        'rows was null or undefined, expected rows != null'
+      ]);
+    }
+
+    return response.rows[0];
+  }
+
+  /**
+   * Inserts a new survey Sample Location.
+   *
+   * @param {PostSampleLocation} sample
+   * @return {*}  {Promise<SampleLocationRecord>}
+   * @memberof SampleLocationRepository
+   */
+  async insertSampleLocation(sample: PostSampleLocation): Promise<SampleLocationRecord> {
+    const sqlStatement = SQL`
+    INSERT INTO survey_sample_site (
+      survey_id,
+      name,
+      description,
+      geojson,
+      geography,
+    ) VALUES (
+      ${sample.survey_id},
+      ${sample.name},
+      ${sample.description},
+      ${sample.survey_sample_site},
+      `;
+    const geometryCollectionSQL = generateGeometryCollectionSQL(sample.survey_sample_site);
+
+    sqlStatement.append(SQL`
+        ,public.geography(
+          public.ST_Force2D(
+            public.ST_SetSRID(
+      `);
+
+    sqlStatement.append(geometryCollectionSQL);
+
+    sqlStatement.append(SQL`
+        , 4326)))
+      `);
+    sqlStatement.append(SQL`
+      )
+      RETURNING
+        *;
+    `);
+
+    const response = await this.connection.sql(sqlStatement, SampleLocationRecord);
+
+    if (!response.rowCount) {
+      throw new ApiExecuteSQLError('Failed to insert sample location', [
+        'SampleLocationRepository->insertSampleLocation',
+        'rows was null or undefined, expected rows != null'
+      ]);
+    }
+
+    return response.rows[0];
+  }
+
+  /**
+   * Deletes a survey Sample Location.
+   *
+   * @param {number} sampleLocationId
+   * @return {*}  {Promise<SampleLocationRecord>}
+   * @memberof SampleLocationRepository
+   */
+  async deleteSampleLocationRecord(sampleLocationId: number): Promise<SampleLocationRecord> {
+    const sqlStatement = SQL`
+      DELETE FROM
+        survey_sample_site
+      WHERE
+        survey_sample_site_id = ${sampleLocationId}
+      RETURNING
+        *;
+    `;
+
+    const response = await this.connection.sql(sqlStatement, SampleLocationRecord);
+
+    if (!response?.rowCount) {
+      throw new ApiExecuteSQLError('Failed to delete survey block record', [
+        'SampleLocationRepository->deleteSampleLocationRecord',
+        'rows was null or undefined, expected rows != null'
+      ]);
+    }
+
+    return response.rows[0];
+  }
+}
--- database/src/migrations/20230915000000_create_survey_observation_tables.ts
+++ database/src/migrations/20230915000000_create_survey_observation_tables.ts
@@ -11,14 +11,16 @@ export async function up(knex: Knex): Promise<void> {
   await knex.raw(`--sql
 
   ----------------------------------------------------------------------------------------
-  -- Create new sample_location table
+  -- Create new survey_sample_site table
   ----------------------------------------------------------------------------------------
 
   SET SEARCH_PATH=biohub,public;
 
-  CREATE TABLE sample_location(
-      sample_location_id                  integer                     GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
+  CREATE TABLE survey_sample_site(
+      survey_sample_site_id               integer                     GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
       survey_id                           integer                     NOT NULL,
+      name                                varchar(50)                 NOT NULL,
+      description                         varchar(250),
       geometry                            geometry(geometry, 3005),
       geography                           geography(geometry)         NOT NULL,
       geojson                             jsonb                       NOT NULL,
@@ -27,20 +29,22 @@ export async function up(knex: Knex): Promise<void> {
       update_date                         timestamptz(6),
       update_user                         integer,
       revision_count                      integer                     DEFAULT 0 NOT NULL,
-      CONSTRAINT sample_location_pk PRIMARY KEY (sample_location_id)
+      CONSTRAINT survey_sample_site_pk PRIMARY KEY (survey_sample_site_id)
   );
 
-  COMMENT ON COLUMN sample_location.sample_location_id IS 'System generated surrogate primary key identifier.';
-  COMMENT ON COLUMN sample_location.survey_id IS 'System generated surrogate primary key identifier.';
-  COMMENT ON COLUMN sample_location.geometry IS 'The containing geometry of the record.';
-  COMMENT ON COLUMN sample_location.geography IS 'The containing geography of the record.';
-  COMMENT ON COLUMN sample_location.geojson IS 'A GeoJSON representation of the geometry which may contain additional metadata.';
-  COMMENT ON COLUMN sample_location.create_date IS 'The datetime the record was created.';
-  COMMENT ON COLUMN sample_location.create_user IS 'The id of the user who created the record as identified in the system user table.';
-  COMMENT ON COLUMN sample_location.update_date IS 'The datetime the record was updated.';
-  COMMENT ON COLUMN sample_location.update_user IS 'The id of the user who updated the record as identified in the system user table.';
-  COMMENT ON COLUMN sample_location.revision_count IS 'Revision count used for concurrency control.';
-  COMMENT ON TABLE sample_location IS 'sample location associated with a given survey.';
+  COMMENT ON COLUMN survey_sample_site.survey_sample_site_id IS 'System generated surrogate primary key identifier.';
+  COMMENT ON COLUMN survey_sample_site.survey_id IS 'System generated surrogate primary key identifier.';
+  COMMENT ON COLUMN survey_sample_site.name IS 'The name of the sample site.';
+  COMMENT ON COLUMN survey_sample_site.description IS 'The description of the sample site.';
+  COMMENT ON COLUMN survey_sample_site.geometry IS 'The containing geometry of the record.';
+  COMMENT ON COLUMN survey_sample_site.geography IS 'The containing geography of the record.';
+  COMMENT ON COLUMN survey_sample_site.geojson IS 'A GeoJSON representation of the geometry which may contain additional metadata.';
+  COMMENT ON COLUMN survey_sample_site.create_date IS 'The datetime the record was created.';
+  COMMENT ON COLUMN survey_sample_site.create_user IS 'The id of the user who created the record as identified in the system user table.';
+  COMMENT ON COLUMN survey_sample_site.update_date IS 'The datetime the record was updated.';
+  COMMENT ON COLUMN survey_sample_site.update_user IS 'The id of the user who updated the record as identified in the system user table.';
+  COMMENT ON COLUMN survey_sample_site.revision_count IS 'Revision count used for concurrency control.';
+  COMMENT ON TABLE survey_sample_site IS 'sample location associated with a given survey.';
 
 
   ----------------------------------------------------------------------------------------
@@ -48,20 +52,20 @@ export async function up(knex: Knex): Promise<void> {
   ----------------------------------------------------------------------------------------
 
   -- Add foreign key constraint from child table to parent table on survey_id
-  ALTER TABLE sample_location ADD CONSTRAINT sample_location_fk1
+  ALTER TABLE survey_sample_site ADD CONSTRAINT survey_sample_site_fk1
     FOREIGN KEY (survey_id)
     REFERENCES survey(survey_id);
 
   -- Add foreign key index
-  CREATE INDEX sample_location_idx1 ON sample_location(survey_id);
+  CREATE INDEX survey_sample_site_idx1 ON survey_sample_site(survey_id);
 
   -- Add unique constraint (TODO: is this needed?)
 
-  --CREATE UNIQUE INDEX sample_location_uk1 ON sample_location(name, survey_id);
+  --CREATE UNIQUE INDEX survey_sample_site_uk1 ON survey_sample_site(name, survey_id);
 
   -- Create audit and journal triggers
-  create trigger audit_sample_location before insert or update or delete on sample_location for each row execute procedure tr_audit_trigger();
-  create trigger journal_sample_location after insert or update or delete on sample_location for each row execute procedure tr_journal_trigger();
+  create trigger audit_survey_sample_site before insert or update or delete on survey_sample_site for each row execute procedure tr_audit_trigger();
+  create trigger journal_survey_sample_site after insert or update or delete on survey_sample_site for each row execute procedure tr_journal_trigger();
 
   ----------------------------------------------------------------------------------------
   -- Create new methods lookup table
@@ -102,72 +106,63 @@ export async function up(knex: Knex): Promise<void> {
 
 
   ----------------------------------------------------------------------------------------
-  -- Create new sample_method table
+  -- Create new survey_sample_method table
   ----------------------------------------------------------------------------------------
 
-  CREATE TABLE sample_method(
-      sample_method_id                    integer                     GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
-      sample_location_id                  integer                     NOT NULL,
+  CREATE TABLE survey_sample_method(
+      survey_sample_method_id                    integer                     GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
+      survey_sample_site_id                  integer                     NOT NULL,
       method_lookup_id                    integer                     NOT NULL,
       description                         varchar(250),
-      start_date                          date,
-      end_date                            date,
       create_date                         timestamptz(6)              DEFAULT now() NOT NULL,
       create_user                         integer                     NOT NULL,
       update_date                         timestamptz(6),
       update_user                         integer,
       revision_count                      integer                     DEFAULT 0 NOT NULL,
-      CONSTRAINT sample_method_pk PRIMARY KEY (sample_method_id)
+      CONSTRAINT survey_sample_method_pk PRIMARY KEY (survey_sample_method_id)
   );
 
-  COMMENT ON COLUMN sample_method.sample_method_id IS 'System generated surrogate primary key identifier.';
-  COMMENT ON COLUMN sample_method.sample_location_id IS 'System generated surrogate primary key identifier.';
-  COMMENT ON COLUMN sample_method.method_lookup_id IS 'System generated surrogate primary key identifier.';
-  COMMENT ON COLUMN sample_method.description IS 'The description of the sample method.';
-  COMMENT ON COLUMN sample_method.start_date IS 'Funding start date.';
-  COMMENT ON COLUMN sample_method.end_date IS 'Funding end date.';
-  COMMENT ON COLUMN sample_method.create_date IS 'The datetime the record was created.';
-  COMMENT ON COLUMN sample_method.create_user IS 'The id of the user who created the record as identified in the system user table.';
-  COMMENT ON COLUMN sample_method.update_date IS 'The datetime the record was updated.';
-  COMMENT ON COLUMN sample_method.update_user IS 'The id of the user who updated the record as identified in the system user table.';
-  COMMENT ON COLUMN sample_method.revision_count IS 'Revision count used for concurrency control.';
-  COMMENT ON TABLE sample_method IS 'blocks associated with a given survey.';
+  COMMENT ON COLUMN survey_sample_method.survey_sample_method_id IS 'System generated surrogate primary key identifier.';
+  COMMENT ON COLUMN survey_sample_method.survey_sample_site_id IS 'System generated surrogate primary key identifier.';
+  COMMENT ON COLUMN survey_sample_method.method_lookup_id IS 'System generated surrogate primary key identifier.';
+  COMMENT ON COLUMN survey_sample_method.description IS 'The description of the sample method.';
+  COMMENT ON COLUMN survey_sample_method.create_date IS 'The datetime the record was created.';
+  COMMENT ON COLUMN survey_sample_method.create_user IS 'The id of the user who created the record as identified in the system user table.';
+  COMMENT ON COLUMN survey_sample_method.update_date IS 'The datetime the record was updated.';
+  COMMENT ON COLUMN survey_sample_method.update_user IS 'The id of the user who updated the record as identified in the system user table.';
+  COMMENT ON COLUMN survey_sample_method.revision_count IS 'Revision count used for concurrency control.';
+  COMMENT ON TABLE survey_sample_method IS 'blocks associated with a given survey.';
 
 
   ----------------------------------------------------------------------------------------
   -- Create new keys and indices
   ----------------------------------------------------------------------------------------
 
-
-
-  -- Add foreign key constraint from child table to parent table on sample_location_id
-  ALTER TABLE sample_method ADD CONSTRAINT sample_method_fk2
-    FOREIGN KEY (sample_location_id)
-    REFERENCES sample_location(sample_location_id);
+  -- Add foreign key constraint from child table to parent table on survey_sample_site_id
+  ALTER TABLE survey_sample_method ADD CONSTRAINT survey_sample_method_fk1
+    FOREIGN KEY (survey_sample_site_id)
+    REFERENCES survey_sample_site(survey_sample_site_id);
 
   -- Add foreign key constraint from child table to parent table on method_lookup_id
-  ALTER TABLE sample_method ADD CONSTRAINT sample_method_fk3
+  ALTER TABLE survey_sample_method ADD CONSTRAINT survey_sample_method_fk2
     FOREIGN KEY (method_lookup_id)
     REFERENCES method_lookup(method_lookup_id);
 
   -- Add foreign key index
-  CREATE INDEX sample_method_idx1 ON sample_method(survey_id);
+  CREATE INDEX survey_sample_method_idx1 ON survey_sample_method(survey_sample_site_id);
 
-  -- Add unique constraint (TODO: is this needed?)
-
-  --CREATE UNIQUE INDEX sample_method_uk1 ON sample_method(name, survey_id);
 
   -- Create audit and journal triggers
-  create trigger audit_sample_method before insert or update or delete on sample_method for each row execute procedure tr_audit_trigger();
-  create trigger journal_sample_method after insert or update or delete on sample_method for each row execute procedure tr_journal_trigger();
+  create trigger survey_audit_sample_method before insert or update or delete on survey_sample_method for each row execute procedure tr_audit_trigger();
+  create trigger survey_journal_sample_method after insert or update or delete on survey_sample_method for each row execute procedure tr_journal_trigger();
 
    ----------------------------------------------------------------------------------------
-  -- Create new sample_method table
+  -- Create new survey_sample_method table
   ----------------------------------------------------------------------------------------
 
-  CREATE TABLE sample_period(
-      sample_period_id                    integer                     GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
-      sample_method_id                    integer                     NOT NULL,
+  CREATE TABLE survey_sample_period(
+      survey_sample_period_id                    integer                     GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
+      survey_sample_method_id                    integer                     NOT NULL,
       start_date                          date,
       end_date                            date,
       create_date                         timestamptz(6)              DEFAULT now() NOT NULL,
@@ -175,33 +170,33 @@ export async function up(knex: Knex): Promise<void> {
       update_date                         timestamptz(6),
       update_user                         integer,
       revision_count                      integer                     DEFAULT 0 NOT NULL,
-      CONSTRAINT sample_period_pk PRIMARY KEY (sample_period_id)
+      CONSTRAINT survey_sample_period_pk PRIMARY KEY (survey_sample_period_id)
   );
 
-  COMMENT ON COLUMN sample_period.sample_period_id IS 'System generated surrogate primary key identifier.';
-  COMMENT ON COLUMN sample_period.sample_method_id IS 'System generated surrogate primary key identifier.';
-  COMMENT ON COLUMN sample_period.start_date IS 'Funding start date.';
-  COMMENT ON COLUMN sample_period.end_date IS 'Funding end date.';
-  COMMENT ON COLUMN sample_period.create_date IS 'The datetime the record was created.';
-  COMMENT ON COLUMN sample_period.create_user IS 'The id of the user who created the record as identified in the system user table.';
-  COMMENT ON COLUMN sample_period.update_date IS 'The datetime the record was updated.';
-  COMMENT ON COLUMN sample_period.update_user IS 'The id of the user who updated the record as identified in the system user table.';
-  COMMENT ON COLUMN sample_period.revision_count IS 'Revision count used for concurrency control.';
-  COMMENT ON TABLE sample_period IS 'blocks associated with a given survey.';
+  COMMENT ON COLUMN survey_sample_period.survey_sample_period_id IS 'System generated surrogate primary key identifier.';
+  COMMENT ON COLUMN survey_sample_period.survey_sample_method_id IS 'System generated surrogate primary key identifier.';
+  COMMENT ON COLUMN survey_sample_period.start_date IS 'Funding start date.';
+  COMMENT ON COLUMN survey_sample_period.end_date IS 'Funding end date.';
+  COMMENT ON COLUMN survey_sample_period.create_date IS 'The datetime the record was created.';
+  COMMENT ON COLUMN survey_sample_period.create_user IS 'The id of the user who created the record as identified in the system user table.';
+  COMMENT ON COLUMN survey_sample_period.update_date IS 'The datetime the record was updated.';
+  COMMENT ON COLUMN survey_sample_period.update_user IS 'The id of the user who updated the record as identified in the system user table.';
+  COMMENT ON COLUMN survey_sample_period.revision_count IS 'Revision count used for concurrency control.';
+  COMMENT ON TABLE survey_sample_period IS 'blocks associated with a given survey.';
 
 
   ----------------------------------------------------------------------------------------
   -- Create new keys and indices
   ----------------------------------------------------------------------------------------
 
-  -- Add foreign key constraint from child table to parent table on sample_method_id
-  ALTER TABLE sample_period ADD CONSTRAINT sample_period_fk1
-    FOREIGN KEY (sample_method_id)
-    REFERENCES sample_method(sample_method_id);
+  -- Add foreign key constraint from child table to parent table on survey_sample_method_id
+  ALTER TABLE survey_sample_period ADD CONSTRAINT survey_sample_period_fk1
+    FOREIGN KEY (survey_sample_method_id)
+    REFERENCES survey_sample_method(survey_sample_method_id);
 
   -- Create audit and journal triggers
-  create trigger audit_sample_period before insert or update or delete on sample_period for each row execute procedure tr_audit_trigger();
-  create trigger journal_sample_period after insert or update or delete on sample_period for each row execute procedure tr_journal_trigger();
+  create trigger audit_survey_sample_period before insert or update or delete on survey_sample_period for each row execute procedure tr_audit_trigger();
+  create trigger journal_survey_sample_period after insert or update or delete on survey_sample_period for each row execute procedure tr_journal_trigger();
 
 
   ----------------------------------------------------------------------------------------
@@ -220,11 +215,11 @@ export async function up(knex: Knex): Promise<void> {
 
   set search_path=biohub_dapi_v1;
 
-  create or replace view sample_location as select * from biohub.sample_location;
+  create or replace view survey_sample_site as select * from biohub.survey_sample_site;
   create or replace view method_lookup as select * from biohub.method_lookup;
-  create or replace view sample_method as select * from biohub.sample_method;
-  create or replace view sample_period as select * from biohub.sample_period;
-n
+  create or replace view survey_sample_method as select * from biohub.survey_sample_method;
+  create or replace view survey_sample_period as select * from biohub.survey_sample_period;
+
   `);
 }
 
