260fdfa27148452f0bc369a007404e8ae90fc5e0
Alfred Rosenthal <alfred.k.rosenthal@gmail.com>
2023-09-22 15:53:14 -0700

    added error dialog for API call

--- app/src/features/surveys/observations/sampling-sites/SamplingSitePage.tsx
+++ app/src/features/surveys/observations/sampling-sites/SamplingSitePage.tsx
@@ -5,7 +5,9 @@ import Divider from '@mui/material/Divider';
 import Paper from '@mui/material/Paper';
 import { makeStyles } from '@mui/styles';
 import { Container } from '@mui/system';
+import { IErrorDialogProps } from 'components/dialog/ErrorDialog';
 import HorizontalSplitFormComponent from 'components/fields/HorizontalSplitFormComponent';
+import { DialogContext } from 'contexts/dialogContext';
 import { SurveyContext } from 'contexts/surveyContext';
 import { SamplingSiteMethodYupSchema } from 'features/surveys/components/CreateSamplingMethod';
 import { ISurveySampleMethodData } from 'features/surveys/components/MethodForm';
@@ -13,6 +15,7 @@ import SamplingSiteMethodForm from 'features/surveys/components/SamplingMethodFo
 import SamplingSiteImportForm from 'features/surveys/components/SurveySamplingSiteImportForm';
 import { Formik, FormikProps } from 'formik';
 import { Feature } from 'geojson';
+import { APIError } from 'hooks/api/useAxios';
 import { useBiohubApi } from 'hooks/useBioHubApi';
 import { useContext, useRef, useState } from 'react';
 import yup from 'utils/YupSchema';
@@ -44,6 +47,7 @@ const SamplingSitePage = () => {
   const classes = useStyles();
   const biohubApi = useBiohubApi();
   const surveyContext = useContext(SurveyContext);
+  const dialogContext = useContext(DialogContext);
   const [formikRef] = useState(useRef<FormikProps<any>>(null));
 
   if (!surveyContext.surveyDataLoader.data) {
@@ -59,15 +63,34 @@ const SamplingSitePage = () => {
       .min(1, 'At least 1 Sampling Method is required')
   });
 
+  const showCreateErrorDialog = (textDialogProps?: Partial<IErrorDialogProps>) => {
+    dialogContext.setErrorDialog({
+      dialogTitle: 'Error Creating Sampling Site(s)',
+      dialogText:
+        'An error has occurred while attempting to create your sampling site, please try again. If the error persists, please contact your system administrator.',
+      onClose: () => {
+        dialogContext.setErrorDialog({ open: false });
+      },
+      onOk: () => {
+        dialogContext.setErrorDialog({ open: false });
+      },
+      ...textDialogProps,
+      open: true
+    });
+  };
+
   const handleSubmit = async (values: ICreateSamplingSiteRequest) => {
     try {
       await biohubApi.samplingSite.createSamplingSites(surveyContext.projectId, surveyContext.surveyId, values);
     } catch (error) {
-      console.log('________API ERROR____________');
-      console.log(error);
+      showCreateErrorDialog({
+        dialogTitle: 'Error Creating Sampling Site(s)',
+        dialogError: (error as APIError).message,
+        dialogErrorDetails: (error as APIError)?.errors
+      });
     }
   };
-  console.log(formikRef.current?.errors);
+
   return (
     <Box display="flex" flexDirection="column" sx={{ height: '100%' }}>
       <SamplingSiteHeader />
@@ -115,7 +138,10 @@ const SamplingSitePage = () => {
                 <Button
                   variant="outlined"
                   color="primary"
-                  onClick={() => console.log('Cancel Button')}
+                  onClick={() => {
+                    // TODO: Go back to Manage Observations page
+                    console.log('Cancel Button');
+                  }}
                   className={classes.actionButton}>
                   Cancel
                 </Button>
