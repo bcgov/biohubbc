48de997d88bc018309776e828a557c7e17dbd814
jeznorth <twenty15creative@gmail.com>
2023-09-27 13:51:09 -0700

    Telemetry UI Updates

--- app/src/features/surveys/view/survey-animals/TelemetryDeviceForm.tsx
+++ app/src/features/surveys/view/survey-animals/TelemetryDeviceForm.tsx
@@ -1,4 +1,4 @@
-import { Box, Divider, FormHelperText, Paper, Typography } from '@mui/material';
+import { Box, FormHelperText, Typography } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import CustomTextField from 'components/fields/CustomTextField';
 import SingleDateField from 'components/fields/SingleDateField';
@@ -9,6 +9,10 @@ import { useTelemetryApi } from 'hooks/useTelemetryApi';
 import moment from 'moment';
 import { Fragment, useEffect, useState } from 'react';
 import { IAnimalTelemetryDevice, IDeploymentTimespan } from './device';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+// import { grey } from '@mui/material/colors';
 
 export enum TELEMETRY_DEVICE_FORM_MODE {
   ADD = 'add',
@@ -93,41 +97,45 @@ const DeviceFormSection = ({ values, index, mode }: IDeviceFormSectionProps): JS
 
   return (
     <>
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <CustomTextField label="Device ID" name={`${index}.device_id`} other={{ disabled: mode === 'edit' }} />
+      <Box component="fieldset">
+        <Typography component="legend">Device Metadata</Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <CustomTextField label="Device ID" name={`${index}.device_id`} other={{ disabled: mode === 'edit' }} />
+          </Grid>
+          <Grid item xs={4}>
+            <CustomTextField label="Device Frequency" name={`${index}.frequency`} />
+          </Grid>
+          <Grid item xs={2}>
+            <TelemetrySelectField
+              label="Unit"
+              name={`${index}.frequency_unit`}
+              id="frequency_unit"
+              fetchData={async () => {
+                const codeVals = await api.devices.getCodeValues('frequency_unit');
+                return codeVals.map((a) => a.description);
+              }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TelemetrySelectField
+              label="Device Manufacturer"
+              name={`${index}.device_make`}
+              id="manufacturer"
+              fetchData={api.devices.getCollarVendors}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <CustomTextField label="Device Model" name={`${index}.device_model`} />
+          </Grid>
         </Grid>
-        <Grid item xs={4}>
-          <CustomTextField label="Device Frequency" name={`${index}.frequency`} />
-        </Grid>
-        <Grid item xs={2}>
-          <TelemetrySelectField
-            label="Unit"
-            name={`${index}.frequency_unit`}
-            id="frequency_unit"
-            fetchData={async () => {
-              const codeVals = await api.devices.getCodeValues('frequency_unit');
-              return codeVals.map((a) => a.description);
-            }}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <TelemetrySelectField
-            label="Device Manufacturer"
-            name={`${index}.device_make`}
-            id="manufacturer"
-            fetchData={api.devices.getCollarVendors}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <CustomTextField label="Device Model" name={`${index}.device_model`} />
-        </Grid>
-      </Grid>
-      <Box sx={{ mt: 3 }}>
-        <Paper sx={{ padding: 3 }}>
-          <Typography sx={{ ml: 1, mb: 3 }}>Deployments</Typography>
-          {<DeploymentFormSection index={index} deployments={values[index].deployments ?? []} />}
-        </Paper>
+      </Box>
+      <Box component="fieldset" sx={{ mt: 3 }}>
+        
+        <Typography component="legend">Deployment Dates</Typography>
+        
+        {<DeploymentFormSection index={index} deployments={values[index].deployments ?? []} />}
+
         {Object.entries(bctwErrors).length > 0 && (
           <Grid item xs={12}>
             {Object.entries(bctwErrors).map((bctwError) => (
@@ -151,19 +159,39 @@ const TelemetryDeviceForm = ({ mode }: ITelemetryDeviceFormProps) => {
 
   return (
     <Form>
-      <>
+      <> 
         {values.map((device, idx) => (
-          <Box key={`device-form-section-${mode === TELEMETRY_DEVICE_FORM_MODE.ADD ? 'add' : device.device_id}`}>
-            <Typography sx={{ mt: 2, mb: 2 }}>Device Metadata</Typography>
-            <DeviceFormSection
-              mode={mode}
-              values={values}
-              key={`device-form-section-${mode === TELEMETRY_DEVICE_FORM_MODE.ADD ? 'add' : device.device_id}`}
-              index={idx}
-            />
-            <Divider sx={{ mt: 3 }} />
-          </Box>
-        ))}
+          <Card 
+            variant="outlined"
+            sx={{
+              '& + div': {
+                mt: 2
+              }
+            }}
+          >
+            <CardHeader 
+              title={'Device ID:' + ' ' + device.device_id}
+              sx={{
+                fontSize: '18px'
+              }}  
+            >
+            </CardHeader>
+            <CardContent
+              sx={{
+                pt: 0
+              }}
+            >
+              <Box key={`device-form-section-${mode === TELEMETRY_DEVICE_FORM_MODE.ADD ? 'add' : device.device_id}`}>
+                <DeviceFormSection
+                  mode={mode}
+                  values={values}
+                  key={`device-form-section-${mode === TELEMETRY_DEVICE_FORM_MODE.ADD ? 'add' : device.device_id}`}
+                  index={idx}
+                />
+              </Box>
+            </CardContent>
+          </Card>
+        ))} 
       </>
     </Form>
   );
