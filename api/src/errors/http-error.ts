b67d561dc4565ee205a07a494f896db1445412a0
Curtis Upshall <curtisupshall@gmail.com>
2023-09-27 15:41:31 -0700

    SISMBIOHUB-223: Removed http-error change; added test boilerplate

--- api/src/errors/http-error.ts
+++ api/src/errors/http-error.ts
@@ -129,5 +129,5 @@ export const ensureHTTPError = (error: HTTPError | ApiError | Error | any): HTTP
     return new HTTP500('Unexpected Error', [error.name, error.message]);
   }
 
-  return new HTTP500('Unexpected Error', [error]);
+  return new HTTP500('Unexpected Error');
 };
---
+++ api/src/paths/project/{projectId}/survey/{surveyId}/observation/index.test.ts
@@ -0,0 +1,81 @@
+import chai, { expect } from 'chai';
+import { describe } from 'mocha';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import * as db from '../../../../../../database/db';
+import { HTTPError } from '../../../../../../errors/http-error';
+import { getMockDBConnection, getRequestHandlerMocks } from '../../../../../../__mocks__/db';
+import { ObservationService } from '../../../../../../services/observation-service';
+import { insertUpdateSurveyObservations } from '.';
+
+chai.use(sinonChai);
+
+// TODO remove skip
+describe.skip('insertUpdateSurveyObservations', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  describe('openAPI schema', () => {
+    // TODO
+  })
+
+  it('inserts and updates survey observations', async () => {
+    const dbConnectionObj = getMockDBConnection();
+
+    sinon.stub(db, 'getDBConnection').returns(dbConnectionObj);
+
+    const insertUpdateSurveyObservationsStub = sinon
+      .stub(ObservationService.prototype, 'insertUpdateSurveyObservations')
+      .resolves();
+
+    const { mockReq, mockRes, mockNext } = getRequestHandlerMocks();
+
+    mockReq.params = {
+      projectId: '1',
+      surveyId: '2'
+    };
+
+    mockReq.body = {
+      // TODO
+    };
+
+    try {
+      const requestHandler = insertUpdateSurveyObservations();
+
+      await requestHandler(mockReq, mockRes, mockNext);
+    } catch (actualError) {
+      expect.fail();
+    }
+
+    expect(insertUpdateSurveyObservationsStub).to.have.been.calledOnceWith(
+      2,
+      {
+        // TODO
+      }
+    );
+    expect(mockRes.statusValue).to.equal(200);
+    expect(mockRes.jsonValue).to.eql({ id: 2 });
+  });
+
+  it('catches and re-throws error', async () => {
+    const dbConnectionObj = getMockDBConnection({ release: sinon.stub() });
+
+    sinon.stub(db, 'getDBConnection').returns(dbConnectionObj);
+
+    sinon.stub(ObservationService.prototype, 'insertUpdateSurveyObservations').rejects(new Error('a test error'));
+
+    const { mockReq, mockRes, mockNext } = getRequestHandlerMocks();
+
+    try {
+      const requestHandler = insertUpdateSurveyObservations();
+
+      await requestHandler(mockReq, mockRes, mockNext);
+      expect.fail();
+    } catch (actualError) {
+      expect(dbConnectionObj.release).to.have.been.called;
+
+      expect((actualError as HTTPError).message).to.equal('a test error');
+    }
+  });
+});
