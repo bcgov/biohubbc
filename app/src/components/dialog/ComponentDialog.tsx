9de6940ec078a821440f491735a722d7d3c16019
Alfred Rosenthal <alfred.k.rosenthal@gmail.com>
2023-10-13 14:18:03 -0700

    adding loading button to dialog

--- app/src/components/dialog/ComponentDialog.tsx
+++ app/src/components/dialog/ComponentDialog.tsx
@@ -1,4 +1,4 @@
-import Button from '@mui/material/Button';
+import { LoadingButton } from '@mui/lab';
 import Dialog, { DialogProps } from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
@@ -43,6 +43,11 @@ export interface IComponentDialogProps {
    * @memberof IComponentDialogProps
    */
   dialogProps?: Partial<DialogProps>;
+
+  /**
+   *
+   */
+  isLoading?: boolean;
 }
 
 /**
@@ -73,9 +78,9 @@ const ComponentDialog: React.FC<PropsWithChildren<IComponentDialogProps>> = (pro
       <DialogTitle id="component-dialog-title">{props.dialogTitle}</DialogTitle>
       <DialogContent>{props.children}</DialogContent>
       <DialogActions>
-        <Button onClick={props.onClose} color="primary" variant="contained" autoFocus>
+        <LoadingButton loading={props.isLoading} onClick={props.onClose} color="primary" variant="contained" autoFocus>
           {props.closeButtonLabel ? props.closeButtonLabel : 'Ok'}
-        </Button>
+        </LoadingButton>
       </DialogActions>
     </Dialog>
   );
