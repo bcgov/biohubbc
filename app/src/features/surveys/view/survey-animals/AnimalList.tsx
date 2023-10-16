18367a10d4ce3517da9f4b8f5c8ae1d2c6720ccb
Mac Deluca <Mac.Deluca@quartech.com>
2023-10-16 11:47:53 -0700

    rendering the animal data inside the sidebar

--- app/src/features/surveys/view/survey-animals/AnimalList.tsx
+++ app/src/features/surveys/view/survey-animals/AnimalList.tsx
@@ -1,11 +1,27 @@
 import { Button, Toolbar, Typography } from "@mui/material"
 import Icon from '@mdi/react';
 import { Box } from "@mui/system"
-import React from "react"
+import React, { useContext } from "react"
 import { mdiPlus } from "@mdi/js";
 import { Link as RouterLink } from 'react-router-dom';
+import { useBiohubApi } from "hooks/useBioHubApi";
+import useDataLoader from "hooks/useDataLoader";
+import { SurveyContext } from "contexts/surveyContext";
 
 const AnimalList = () => {
+  const bhApi = useBiohubApi();
+  const surveyContext = useContext(SurveyContext);
+
+  const {
+    //refresh: refreshCritters,
+    load: loadCritters,
+    data: critterData
+  } = useDataLoader(() => bhApi.survey.getSurveyCritters(surveyContext.projectId, surveyContext.surveyId));
+
+  if (!critterData) {
+    loadCritters();
+  }
+
   return (
     <Box display="flex" flexDirection="column" height="100%">
       <Toolbar
@@ -33,6 +49,8 @@ const AnimalList = () => {
           Add
           </Button>
       </Toolbar>
+      {/*temp critter data*/}
+      {critterData?.map(critter => <div>{critter.animal_id}</div>)}
       <Box display="flex" flex="1 1 auto" height="100%" alignItems="center" justifyContent="center">
         <Typography variant="body2">No Animals</Typography>
       </Box>
