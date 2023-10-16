023f26145df998dc197d4aabf4d0aac444eedb2a
Nick Phura <nickphura@gmail.com>
2023-10-16 11:58:53 -0700

    Add comments

--- api/src/paths/funding-sources/index.ts
+++ api/src/paths/funding-sources/index.ts
@@ -137,6 +137,12 @@ export function getFundingSources(): RequestHandler {
   };
 }
 
+/**
+ * Checks if the system user is an admin (has an admin level system role).
+ *
+ * @param {SystemUser} systemUserObject
+ * @return {*}  {boolean} `true` if the user is an admin, `false` otherwise.
+ */
 function isAdmin(systemUserObject: SystemUser): boolean {
   return (
     systemUserObject.role_names.includes(SYSTEM_ROLE.SYSTEM_ADMIN) ||
@@ -144,6 +150,13 @@ function isAdmin(systemUserObject: SystemUser): boolean {
   );
 }
 
+/**
+ * Removes sensitive (admin-only) fields from the funding sources response, returning a new sanitized array.
+ *
+ * @param {((FundingSource & FundingSourceSupplementaryData)[])} fundingSources
+ * @return {*}  {((FundingSource & FundingSourceSupplementaryData)[])} array of funding sources with admin-only fields
+ * removed.
+ */
 function removeNonAdminFieldsFromFundingSourcesResponse(
   fundingSources: (FundingSource & FundingSourceSupplementaryData)[]
 ): (FundingSource & FundingSourceSupplementaryData)[] {
