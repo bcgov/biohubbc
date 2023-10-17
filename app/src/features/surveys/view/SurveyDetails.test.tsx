3b13565606a52478104c38fc870d6cf3360a055f
Curtis Upshall <curtisupshall@gmail.com>
2023-10-17 09:46:20 -0700

    Lint fix

--- app/src/features/surveys/view/SurveyDetails.test.tsx
+++ app/src/features/surveys/view/SurveyDetails.test.tsx
@@ -1,15 +1,15 @@
 import { CodesContext, ICodesContext } from 'contexts/codesContext';
 import { SurveyContext } from 'contexts/surveyContext';
 import { createMemoryHistory } from 'history';
+import { GetRegionsResponse } from 'hooks/api/useSpatialApi';
 import { DataLoader } from 'hooks/useDataLoader';
 import { IGetSurveyForViewResponse } from 'interfaces/useSurveyApi.interface';
 import { Router } from 'react-router';
 import { codes } from 'test-helpers/code-helpers';
 import { getSurveyForViewResponse } from 'test-helpers/survey-helpers';
 import { render, waitFor } from 'test-helpers/test-utils';
-import SurveyDetails from './SurveyDetails';
 import { useBiohubApi } from '../../../hooks/useBioHubApi';
-import { GetRegionsResponse } from 'hooks/api/useSpatialApi';
+import SurveyDetails from './SurveyDetails';
 
 const history = createMemoryHistory({ initialEntries: ['/admin/projects/1/surveys/2'] });
 
