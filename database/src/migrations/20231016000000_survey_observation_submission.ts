306f88ba46b2f162001d9691b2534fb700fdc776
JeremyQuartech <jeremy.kissack@quartech.com>
2023-10-17 10:16:04 -0700

    Created migration for new table, survey_observation_submission

---
+++ database/src/migrations/20231016000000_survey_observation_submission.ts
@@ -0,0 +1,63 @@
+import { Knex } from 'knex';
+
+/**
+ * Added survey_observation_submission table.
+ *
+ * @export
+ * @param {Knex} knex
+ * @return {*}  {Promise<void>}
+ */
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw(`--sql
+  ----------------------------------------------------------------------------------------
+  -- Create survey_observation_submission table
+  ----------------------------------------------------------------------------------------
+  set search_path=biohub;
+  CREATE TABLE survey_observation_submission(
+    submission_id                           integer           GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
+    key                                     varchar(255)      NOT NULL,
+    survey_id                               integer           NOT NULL,
+    original_filename                       varchar(255)      NOT NULL,
+    create_date                             timestamptz(6)    DEFAULT now() NOT NULL,
+    create_user                             integer           NOT NULL,
+    update_date                             timestamptz(6),
+    update_user                             integer,
+    CONSTRAINT survey_observation_submission_pk PRIMARY KEY (submission_id)
+  );
+  
+  COMMENT ON COLUMN survey_observation_submission.submission_id                   IS 'System generated surrogate primary key identifier.';
+  COMMENT ON COLUMN survey_observation_submission.key                             IS 'The key of the survey observation submission.';
+  COMMENT ON COLUMN survey_observation_submission.survey_id                       IS 'The survey id of the survey observation submission.';
+  COMMENT ON COLUMN survey_observation_submission.original_filename               IS 'The original filename of the survey observation submission.';
+  COMMENT ON COLUMN survey_observation_submission.create_date                     IS 'The date the survey observation submission was created.';
+  COMMENT ON COLUMN survey_observation_submission.create_user                     IS 'The user id of the survey observation submission creator.';
+  COMMENT ON COLUMN survey_observation_submission.update_date                     IS 'The date the survey observation submission was updated.';
+  COMMENT ON COLUMN survey_observation_submission.update_user                     IS 'The user id of the survey observation submission updater.';
+  COMMENT ON TABLE  survey_observation_submission                                 IS 'Survey observation submission table';
+
+  ----------------------------------------------------------------------------------------
+  -- Create new keys and indices
+  ----------------------------------------------------------------------------------------
+
+  -- Add foreign key constraint from child table to parent table on survey_attachment_id
+  ALTER TABLE survey_observation_submission ADD CONSTRAINT survey_observation_submission_fk1
+    FOREIGN KEY (survey_id)
+    REFERENCES survey(survey_id);
+
+  -- Add foreign key index
+  CREATE INDEX survey_observation_submission_idx1 ON survey_observation_submission(survey_id);
+
+  -- Add unique constraint
+  CREATE UNIQUE INDEX survey_observation_submission_uk1 ON survey_observation_submission(key);
+  
+  ----------------------------------------------------------------------------------------
+  -- Create View
+  ----------------------------------------------------------------------------------------
+  set search_path=biohub_dapi_v1;
+  create or replace view survey_observation_submission as select * from biohub.survey_observation_submission;
+  `);
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.raw(``);
+}
