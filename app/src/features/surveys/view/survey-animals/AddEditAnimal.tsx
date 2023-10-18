5842608cd64c38eee6835a18415c9b218d8fff9f
Mac Deluca <Mac.Deluca@quartech.com>
2023-10-18 14:19:35 -0700

    added critter ID with copy feature

--- app/src/features/surveys/view/survey-animals/AddEditAnimal.tsx
+++ app/src/features/surveys/view/survey-animals/AddEditAnimal.tsx
@@ -5,12 +5,13 @@ import { Box, Button, Collapse, Grid, IconButton, Toolbar, Typography } from '@m
 import CircularProgress from '@mui/material/CircularProgress';
 import CustomTextField from 'components/fields/CustomTextField';
 import { SurveyAnimalsI18N } from 'constants/i18n';
+import { DialogContext } from 'contexts/dialogContext';
 import { SurveyContext } from 'contexts/surveyContext';
 import { Form, useFormikContext } from 'formik';
 import { isEqual } from 'lodash-es';
 import React, { useContext, useMemo } from 'react';
 import { useParams } from 'react-router';
-import { IAnimal, IAnimalSubSections } from './animal';
+import { getAnimalFieldName, IAnimal, IAnimalGeneral, IAnimalSubSections } from './animal';
 import CaptureAnimalForm from './form-sections/CaptureAnimalForm';
 import CollectionUnitAnimalForm from './form-sections/CollectionUnitAnimalForm';
 import FamilyAnimalForm from './form-sections/FamilyAnimalForm';
@@ -30,6 +31,7 @@ export const AddEditAnimal = (props: AddEditAnimalProps) => {
   const surveyContext = useContext(SurveyContext);
   const { survey_critter_id } = useParams<{ survey_critter_id: string }>();
   const { submitForm, initialValues, values, resetForm } = useFormikContext<IAnimal>();
+  const dialogContext = useContext(DialogContext);
 
   const renderFormContent = useMemo(() => {
     const sectionMap: Partial<Record<IAnimalSubSections, JSX.Element>> = {
@@ -50,6 +52,18 @@ export const AddEditAnimal = (props: AddEditAnimalProps) => {
   if (!surveyContext.surveyDataLoader.data) {
     return <CircularProgress className="pageProgress" size={40} />;
   }
+
+  const setPopup = (message: string) => {
+    dialogContext.setSnackbar({
+      open: true,
+      snackbarAutoCloseMs: 1500,
+      snackbarMessage: (
+        <Typography variant="body2" component="div">
+          {message}
+        </Typography>
+      )
+    });
+  };
 
   return (
     <>
@@ -98,24 +112,30 @@ export const AddEditAnimal = (props: AddEditAnimalProps) => {
         </Box>
       </Toolbar>
       <Box p={2}>
-        <Grid item mb={3}>
-          <CustomTextField
-            label="Critter ID"
-            name="test"
-            other={{
-              InputProps: {
-                endAdornment: (
-                  <IconButton aria-label={`Copy Critter ID`}>
-                    <Icon path={mdiContentCopy} size={0.8} />
-                  </IconButton>
-                )
-              },
-              disabled: true,
-              variant: 'filled',
-              defaultValue: initialValues.general.critter_id,
-              sx: { maxWidth: '30%' }
-            }}
-          />
+        <Grid container>
+          <Grid item mb={2} lg={4}>
+            <CustomTextField
+              label="Critter ID"
+              name={getAnimalFieldName<IAnimalGeneral>('general', 'critter_id')}
+              other={{
+                InputProps: {
+                  endAdornment: (
+                    <IconButton
+                      aria-label={`Copy Critter ID`}
+                      onClick={() => {
+                        navigator.clipboard.writeText(initialValues.general?.critter_id ?? '');
+                        setPopup('Copied Critter ID');
+                      }}>
+                      <Icon path={mdiContentCopy} size={0.8} />
+                    </IconButton>
+                  )
+                },
+                disabled: true,
+                variant: 'filled',
+                defaultValue: initialValues.general.critter_id
+              }}
+            />
+          </Grid>
         </Grid>
         <Form>{parseInt(survey_critter_id) ? renderFormContent : null}</Form>
       </Box>
