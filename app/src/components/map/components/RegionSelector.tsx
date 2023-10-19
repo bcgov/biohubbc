a003a1e5ca18a347e563ba4282c6773212762256
Alfred Rosenthal <alfred.k.rosenthal@gmail.com>
2023-10-19 11:17:50 -0700

    added new region auto complete

--- app/src/components/map/components/RegionSelector.tsx
+++ app/src/components/map/components/RegionSelector.tsx
@@ -1,3 +1,62 @@
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import { useState } from 'react';
+
+interface IRegionOption {
+  key: string;
+  name: string;
+}
+
+export interface IRegionSelectorProps {
+  onRegionSelected: () => void;
+}
+
 export const RegionSelector = () => {
-  return <></>;
+  const regions: IRegionOption[] = [
+    {
+      key: 'pub:WHSE_WILDLIFE_MANAGEMENT.WAA_WILDLIFE_MGMT_UNITS_SVW',
+      name: 'Wildlife Management Units'
+    },
+    {
+      key: 'pub:WHSE_TANTALIS.TA_PARK_ECORES_PA_SVW',
+      name: 'Parks and EcoRegions'
+    },
+    {
+      key: 'pub:WHSE_ADMIN_BOUNDARIES.ADM_NR_REGIONS_SPG',
+      name: 'NRM Regional Boundaries'
+    }
+  ];
+
+  const [selectedRegion, setSelectedRegion] = useState<IRegionOption | undefined>(undefined);
+
+  const handleOnChange = (selected: { key: string; name: string }) => {
+    setSelectedRegion(selected);
+  };
+
+  const handleCheckOptionEquality = (option: IRegionOption, value: IRegionOption) => {
+    return option.key === value.key;
+  };
+
+  return (
+    <>
+      <Autocomplete
+        id="layer"
+        size="medium"
+        sx={{ width: 250, fontSize: 14 }}
+        options={regions}
+        value={selectedRegion}
+        getOptionLabel={(option) => option.name}
+        isOptionEqualToValue={handleCheckOptionEquality}
+        renderInput={(params) => (
+          <TextField {...params} variant="standard" label="Region Select" placeholder="Regions" />
+        )}
+        onChange={(_, option) => {
+          console.log(option);
+          if (option) {
+            handleOnChange(option);
+          }
+        }}
+      />
+    </>
+  );
 };
