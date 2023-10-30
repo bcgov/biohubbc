079d67c6dc31ddddb538924c870a313f388cb2f0
Alfred Rosenthal <alfred.k.rosenthal@gmail.com>
2023-10-30 14:07:07 -0700

    added depreciation tags to table and column

--- database/src/migrations/20231030120000_depreciate_field_method.ts
+++ database/src/migrations/20231030120000_depreciate_field_method.ts
@@ -10,8 +10,8 @@ export async function up(knex: Knex): Promise<void> {
   await knex.raw(`--sql
     SET search_path = 'biohub';
 
-    COMMENT ON TABLE field_method IS '';
-    COMMENT ON COLUMN survey.field_method_id IS '';
+    COMMENT ON TABLE field_method IS '(Depreciated) A common methodology is a specific species inventory protocol that may be implemented across various species and strata. Examples include Stratified Random Block.';
+    COMMENT ON COLUMN survey.field_method_id IS '(Depreciated) System generated surrogate primary key identifier.';
   `);
 }
 
