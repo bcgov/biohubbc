a5db372b1a09cd4206c227bc19ea2bfffbe3e88a
Kjartan <Kjartan.Einarsson@quartech.com>
2023-11-16 13:38:22 -0800

    remove duplicate file, clean fragment

--- app/src/features/surveys/view/components/SurveyBaseHeader.tsx
+++ app/src/features/surveys/view/components/SurveyBaseHeader.tsx
@@ -19,54 +19,52 @@ const SurveyBaseHeader = (props: ISurveyHeader) => {
   const { title, subTitle, breadCrumb, buttonJSX } = props;
 
   return (
-    <>
-      <Paper
-        elevation={0}
-        square={true}
-        id="pageTitle"
-        sx={{
-          position: 'sticky',
-          top: 0,
-          zIndex: 1002,
-          pt: 3,
-          pb: 3.75,
-          borderBottomStyle: 'solid',
-          borderBottomWidth: '1px',
-          borderBottomColor: grey[300]
-        }}>
-        <Container maxWidth="xl">
-          {breadCrumb}
-          <Box
-            display="flex"
-            justifyContent="space-between"
-            gap="1.5rem"
-            sx={{
-              flexDirection: { xs: 'column', lg: 'row' }
-            }}>
-            <Box>
-              <Typography
-                component="h1"
-                variant="h2"
-                sx={{
-                  display: 'block',
-                  overflow: 'hidden',
-                  whiteSpace: 'nowrap',
-                  textOverflow: 'ellipsis',
-                  ml: '-2px'
-                }}>
-                {title}
-              </Typography>
-              <Box display="flex" alignItems="center" flexWrap="wrap" mt={1}>
-                {subTitle}
-              </Box>
-            </Box>
-            <Box display="flex" alignItems="flex-start" flex="0 0 auto">
-              {buttonJSX}
+    <Paper
+      elevation={0}
+      square={true}
+      id="pageTitle"
+      sx={{
+        position: 'sticky',
+        top: 0,
+        zIndex: 1002,
+        pt: 3,
+        pb: 3.75,
+        borderBottomStyle: 'solid',
+        borderBottomWidth: '1px',
+        borderBottomColor: grey[300]
+      }}>
+      <Container maxWidth="xl">
+        {breadCrumb}
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          gap="1.5rem"
+          sx={{
+            flexDirection: { xs: 'column', lg: 'row' }
+          }}>
+          <Box>
+            <Typography
+              component="h1"
+              variant="h2"
+              sx={{
+                display: 'block',
+                overflow: 'hidden',
+                whiteSpace: 'nowrap',
+                textOverflow: 'ellipsis',
+                ml: '-2px'
+              }}>
+              {title}
+            </Typography>
+            <Box display="flex" alignItems="center" flexWrap="wrap" mt={1}>
+              {subTitle}
             </Box>
           </Box>
-        </Container>
-      </Paper>
-    </>
+          <Box display="flex" alignItems="flex-start" flex="0 0 auto">
+            {buttonJSX}
+          </Box>
+        </Box>
+      </Container>
+    </Paper>
   );
 };
 
