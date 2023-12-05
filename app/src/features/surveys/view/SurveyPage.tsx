8830c47a38d093ae6849fecc8f315ab9a6ca6b46
Kjartan <Kjartan.Einarsson@quartech.com>
2023-12-05 12:45:12 -0800

    merge conflicts fixed

--- app/src/features/surveys/view/SurveyPage.tsx
+++ app/src/features/surveys/view/SurveyPage.tsx
@@ -1,4 +1,4 @@
-import { mdiMapSearchOutline } from '@mdi/js';
+import { mdiCog, mdiMapSearchOutline } from '@mdi/js';
 import Icon from '@mdi/react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
