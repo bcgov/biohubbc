adc86e7a36b1036d1af907add2f4a5838d30c7cd
Kjartan <Kjartan.Einarsson@quartech.com>
2024-01-04 16:59:16 -0800

    added checkbox and delete endpoint

---
+++ api/src/paths/project/{projectId}/survey/{surveyId}/sample-site/delete.ts
@@ -0,0 +1,154 @@
+import { RequestHandler } from 'express';
+import { Operation } from 'express-openapi';
+import { PROJECT_PERMISSION, SYSTEM_ROLE } from '../../../../../../constants/roles';
+import { getDBConnection } from '../../../../../../database/db';
+import { HTTP400, HTTP500 } from '../../../../../../errors/http-error';
+import { authorizeRequestHandler } from '../../../../../../request-handlers/security/authorization';
+import { ObservationService } from '../../../../../../services/observation-service';
+import { SampleLocationService } from '../../../../../../services/sample-location-service';
+import { getLogger } from '../../../../../../utils/logger';
+
+const defaultLog = getLogger('paths/project/{projectId}/survey/{surveyId}/sample-site/delete');
+
+export const POST: Operation = [
+  authorizeRequestHandler((req) => {
+    return {
+      or: [
+        {
+          validProjectPermissions: [PROJECT_PERMISSION.COORDINATOR],
+          projectId: Number(req.params.projectId),
+          discriminator: 'ProjectPermission'
+        },
+        {
+          validSystemRoles: [SYSTEM_ROLE.DATA_ADMINISTRATOR],
+          discriminator: 'SystemRole'
+        }
+      ]
+    };
+  }),
+  deleteSurveySampleSiteRecords()
+];
+
+POST.apiDoc = {
+  description: 'Delete survey sample sites.',
+  tags: ['survey'],
+  security: [
+    {
+      Bearer: []
+    }
+  ],
+  parameters: [
+    {
+      in: 'path',
+      name: 'projectId',
+      schema: {
+        type: 'integer',
+        minimum: 1
+      },
+      required: true
+    },
+    {
+      in: 'path',
+      name: 'surveyId',
+      schema: {
+        type: 'integer',
+        minimum: 1
+      },
+      required: true
+    }
+  ],
+  requestBody: {
+    description: 'Survey sample site delete request object.',
+    content: {
+      'application/json': {
+        schema: {
+          type: 'object',
+          required: ['surveySampleSiteIds'],
+          properties: {
+            surveySampleSiteIds: {
+              items: {
+                type: 'integer',
+                minimum: 1
+              }
+            }
+          }
+        }
+      }
+    }
+  },
+  responses: {
+    204: {
+      description: 'Delete survey sample site OK'
+    },
+    400: {
+      $ref: '#/components/responses/400'
+    },
+    401: {
+      $ref: '#/components/responses/401'
+    },
+    403: {
+      $ref: '#/components/responses/403'
+    },
+    500: {
+      $ref: '#/components/responses/500'
+    },
+    default: {
+      $ref: '#/components/responses/default'
+    }
+  }
+};
+
+export function deleteSurveySampleSiteRecords(): RequestHandler {
+  return async (req, res) => {
+    const surveyId = Number(req.params.surveyId);
+    const surveySampleSiteIds = req.body.surveySampleSiteIds as number[];
+
+    if (!surveySampleSiteIds) {
+      throw new HTTP400('Missing required body `surveySampleSiteIds`');
+    }
+
+    const connection = getDBConnection(req['keycloak_token']);
+
+    try {
+      await connection.open();
+
+      const observationService = new ObservationService(connection);
+      const sampleLocationService = new SampleLocationService(connection);
+
+      const sampleLocations = await sampleLocationService.getSampleLocationsForSurveyId(surveyId);
+
+      const unableToDelete: number[] = [];
+
+      const deleteSampleSites = surveySampleSiteIds.map(async (surveySampleSiteId) => {
+        if (
+          (await observationService.getObservationsCountBySampleSiteId(surveyId, surveySampleSiteId)).observationCount >
+          0
+        ) {
+          unableToDelete.push(surveySampleSiteId);
+        } else {
+          await sampleLocationService.deleteSampleLocationRecord(surveySampleSiteId);
+        }
+      });
+
+      await Promise.all(deleteSampleSites);
+
+      const notDeleted = sampleLocations.filter((item) => unableToDelete.includes(item.survey_sample_site_id));
+
+      if (notDeleted.length > 0) {
+        throw new HTTP500(
+          `Cannot delete a sample sites [${notDeleted.map((item) => item.name)}] that is associated with an observation`
+        );
+      }
+
+      await connection.commit();
+
+      return res.status(204).send();
+    } catch (error) {
+      defaultLog.error({ label: 'deleteSurveySampleSiteRecords', message: 'error', error });
+      await connection.rollback();
+      throw error;
+    } finally {
+      connection.release();
+    }
+  };
+}
