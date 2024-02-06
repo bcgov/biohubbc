f7a3e6233c14e400fc02d586af8a2b6d0fc47f32
Curtis Upshall <curtisupshall@gmail.com>
2024-02-06 15:22:19 -0800

    SIMSBIOHUB-459: Fix database migration by recreating view; deprecated wldtaxonomic column.

--- database/src/migrations/20240124141800_observation_scientific_name.ts
+++ database/src/migrations/20240124141800_observation_scientific_name.ts
@@ -11,8 +11,21 @@ export async function up(knex: Knex): Promise<void> {
   await knex.raw(`--sql 
     SET search_path = 'biohub';
 
-    ALTER TABLE survey_observation ADD itis_tsn integer;
-    ALTER TABLE survey_observation ADD itis_scientific_name varchar(300);
+    ALTER TABLE survey_observation ADD COLUMN itis_tsn INTEGER;
+    ALTER TABLE survey_observation ADD COLUMN itis_scientific_name VARCHAR(300);
+
+    COMMENT ON COLUMN survey_observation.wldtaxonomic_units_id IS '(Deprecated) 'The species associated with the observation.';
+    COMMENT ON COLUMN survey_observation.itis_tsn IS 'The ITIS TSN identifier for the species associated with the observation.';
+    COMMENT ON COLUMN survey_observation.itis_scientific_name IS 'The scientific name for the species associated with the observation.';
+
+    ----------------------------------------------------------------------------------------
+    -- Create new views
+    ----------------------------------------------------------------------------------------
+  
+    set search_path=biohub_dapi_v1;
+  
+    create or replace view survey_observation as select * from biohub.survey_observation;
+
   `);
 }
 
