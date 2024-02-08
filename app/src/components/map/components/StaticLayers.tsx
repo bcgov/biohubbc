c80bf992efba6e6fab52e5ff6b13726ba2a7f1d8
Curtis Upshall <curtisupshall@gmail.com>
2024-02-07 18:45:53 -0800

    SIMSBIOHUB-460, 475 - Study Area + Observations Import bugs (#1209)
    * Fixed a bug where newly added Study Areas were not shown until page reload
    * Fixed a bug where users could not import survey observations

--- app/src/components/map/components/StaticLayers.tsx
+++ app/src/components/map/components/StaticLayers.tsx
@@ -1,6 +1,6 @@
 import { Feature } from 'geojson';
 import L from 'leaflet';
-import React, { ReactElement } from 'react';
+import { PropsWithChildren, ReactElement, useMemo } from 'react';
 import {
   FeatureGroup,
   GeoJSON,
@@ -15,7 +15,7 @@ import { coloredPoint } from 'utils/mapUtils';
 
 export interface IStaticLayerFeature {
   geoJSON: Feature;
-  key?: string | number;
+  key: string | number;
   GeoJSONProps?: Partial<GeoJSONProps>;
   popup?: ReactElement;
   PopupProps?: Partial<PopupProps>;
@@ -33,63 +33,68 @@ export interface IStaticLayer {
 }
 
 export interface IStaticLayersProps {
-  layers?: IStaticLayer[];
+  layers: IStaticLayer[];
 }
 
-const StaticLayers: React.FC<React.PropsWithChildren<IStaticLayersProps>> = (props) => {
-  if (!props.layers?.length) {
+const StaticLayers = (props: PropsWithChildren<IStaticLayersProps>) => {
+  const layerControls: ReactElement[] = useMemo(
+    () =>
+      props.layers
+        .filter((layer) => Boolean(layer.features?.length))
+        .map((layer) => {
+          const layerColors = layer.layerColors || { color: '#1f7dff', fillColor: '#1f7dff' };
+
+          return (
+            <LayersControl.Overlay checked={true} name={layer.layerName} key={`static-layer-${layer.layerName}`}>
+              <FeatureGroup key={`static-feature-group-${layer.layerName}`}>
+                {layer.features.map((item, index) => {
+                  const id = item.key || item.geoJSON.id || index;
+
+                  return (
+                    <GeoJSON
+                      key={`static-feature-${id}`}
+                      style={{ ...layerColors }}
+                      pointToLayer={(feature, latlng) => {
+                        if (feature.properties?.radius) {
+                          return new L.Circle([latlng.lat, latlng.lng], feature.properties.radius);
+                        }
+
+                        return coloredPoint({ latlng });
+                      }}
+                      data={item.geoJSON}
+                      {...item.GeoJSONProps}>
+                      {item.tooltip && (
+                        <Tooltip
+                          key={`static-feature-tooltip-${id}`}
+                          direction="top"
+                          sticky={true}
+                          {...item.TooltipProps}>
+                          {item.tooltip}
+                        </Tooltip>
+                      )}
+                      {item.popup && (
+                        <Popup
+                          key={`static-feature-popup-${id}`}
+                          keepInView={false}
+                          closeButton={false}
+                          autoPan={false}
+                          {...item.PopupProps}>
+                          {item.popup}
+                        </Popup>
+                      )}
+                    </GeoJSON>
+                  );
+                })}
+              </FeatureGroup>
+            </LayersControl.Overlay>
+          );
+        }),
+    [props.layers]
+  );
+
+  if (!layerControls.length) {
     return null;
   }
-
-  const layerControls: ReactElement[] = [];
-
-  props.layers.forEach((layer) => {
-    if (!layer.features?.length) {
-      return;
-    }
-    const layerColors = layer.layerColors || { color: '#1f7dff', fillColor: '#1f7dff' };
-
-    layerControls.push(
-      <LayersControl.Overlay checked={true} name={layer.layerName} key={`static-layer-${layer.layerName}`}>
-        <FeatureGroup key={`static-feature-group-${layer.layerName}`}>
-          {layer.features.map((item, index) => {
-            const id = item.key || item.geoJSON.id || index;
-
-            return (
-              <GeoJSON
-                key={`static-feature-${id}`}
-                style={{ ...layerColors }}
-                pointToLayer={(feature, latlng) => {
-                  if (feature.properties?.radius) {
-                    return new L.Circle([latlng.lat, latlng.lng], feature.properties.radius);
-                  }
-
-                  return coloredPoint({ latlng });
-                }}
-                data={item.geoJSON}
-                {...item.GeoJSONProps}>
-                {item.tooltip && (
-                  <Tooltip key={`static-feature-tooltip-${id}`} direction="top" sticky={true} {...item.TooltipProps}>
-                    {item.tooltip}
-                  </Tooltip>
-                )}
-                {item.popup && (
-                  <Popup
-                    key={`static-feature-popup-${id}`}
-                    keepInView={false}
-                    closeButton={false}
-                    autoPan={false}
-                    {...item.PopupProps}>
-                    {item.popup}
-                  </Popup>
-                )}
-              </GeoJSON>
-            );
-          })}
-        </FeatureGroup>
-      </LayersControl.Overlay>
-    );
-  });
 
   return <>{layerControls}</>;
 };
