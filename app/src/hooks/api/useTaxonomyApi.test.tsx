6c00fbcd3d4a7a04292651a154e4cb45a3de2b42
Curtis Upshall <curtisupshall@gmail.com>
2024-02-12 12:32:20 -0800

    SIMSBIOHUB-459: App test coverage

--- app/src/hooks/api/useTaxonomyApi.test.tsx
+++ app/src/hooks/api/useTaxonomyApi.test.tsx
@@ -1,11 +1,11 @@
+import { act } from '@testing-library/react';
+import { renderHook } from '@testing-library/react-hooks';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
-import useTaxonomyApi from './useTaxonomyApi';
 import { ConfigContext, IConfig } from 'contexts/configContext';
 import { PropsWithChildren } from 'react';
-import { renderHook } from '@testing-library/react-hooks';
 import { AuthProvider, AuthProviderProps } from 'react-oidc-context';
-import { act } from '@testing-library/react';
+import useTaxonomyApi from './useTaxonomyApi';
 
 describe('useTaxonomyApi', () => {
   let mock: any;
@@ -18,11 +18,14 @@ describe('useTaxonomyApi', () => {
 
   const wrapper = ({ children }: PropsWithChildren) => (
     <AuthProvider {...authConfig}>
-      <ConfigContext.Provider value={{
-        BACKBONE_PUBLIC_API_HOST: 'http://example.com',
-        BIOHUB_TAXON_PATH: '/api/taxonomy/species',
-        BIOHUB_TAXON_TSN_PATH: '/api/taxonomy/species/list',
-      } as IConfig}>
+      <ConfigContext.Provider
+        value={
+          {
+            BACKBONE_PUBLIC_API_HOST: 'http://example.com',
+            BIOHUB_TAXON_PATH: '/api/taxonomy/species',
+            BIOHUB_TAXON_TSN_PATH: '/api/taxonomy/species/list'
+          } as IConfig
+        }>
         {children}
       </ConfigContext.Provider>
     </AuthProvider>
@@ -36,34 +39,71 @@ describe('useTaxonomyApi', () => {
     mock.restore();
   });
 
-  it('getSpeciesFromIds works as expected', async () => {
-    const mockResponse = {
-      searchResponse: [
-        {
-          tsn: '1',
-          commonName: 'something',
-          scientificName: 'something'
-        },
-        {
-          tsn: '2',
-          commonName: 'anything',
-          scientificName: 'anything'
-        }
-      ]
-    }
+  describe('getSpeciesFromIds', () => {
+    it('getSpeciesFromIds works as expected', async () => {
+      const mockResponse = {
+        searchResponse: [
+          {
+            tsn: '1',
+            commonName: 'something',
+            scientificName: 'something'
+          },
+          {
+            tsn: '2',
+            commonName: 'anything',
+            scientificName: 'anything'
+          }
+        ]
+      };
 
-    mock.onGet('/api/taxonomy/species/list').reply(200, mockResponse);
+      mock.onGet('/api/taxonomy/species/list').reply(200, mockResponse);
 
-    const { result, waitFor } = renderHook(() => useTaxonomyApi(), { wrapper });
+      const { result, waitFor } = renderHook(() => useTaxonomyApi(), { wrapper });
 
-    await act(async () => {
-      result.current.getSpeciesFromIds([1, 2]);
+      await act(async () => {
+        await waitFor(
+          () => {
+            result.current.getSpeciesFromIds([1, 2]).then((data) => {
+              expect(data).toEqual(mockResponse.searchResponse);
+            });
+          },
+          { timeout: 2000 }
+        );
+      });
+    });
+  });
 
-      await waitFor(() => {
-        result.current.getSpeciesFromIds([1, 2]).then((data) => {
-          expect(data).toEqual(mockResponse.searchResponse);
-        });
-      }, { timeout: 2000 });
+  describe('searchSpeciesByTerms', () => {
+    it('searchSpeciesByTerms works as expected', async () => {
+      const mockSearchTermResponse = {
+        searchResponse: [
+          {
+            tsn: '3',
+            commonName: 'something',
+            scientificName: 'something'
+          },
+          {
+            tsn: '4',
+            commonName: 'anything',
+            scientificName: 'anything'
+          }
+        ]
+      };
+
+      mock.onGet('/api/taxonomy/species').reply(200, mockSearchTermResponse);
+
+      const { result, waitFor } = renderHook(() => useTaxonomyApi(), { wrapper });
+
+      await act(async () => {
+        await waitFor(
+          () => {
+            result.current.searchSpeciesByTerms(['aaaa', 'bbbb']).then((data) => {
+              expect(data).toEqual(mockSearchTermResponse.searchResponse);
+            });
+          },
+          { timeout: 2000 }
+        );
+      });
     });
   });
 });
