b7be5f7f18756ff33080d5e2789aa8100c0d8a19
jeznorth <twenty15creative@gmail.com>
2024-02-26 13:23:28 -0800

    Update Funding Source List and Table views

--- app/src/features/funding-sources/list/FundingSourcesListPage.tsx
+++ app/src/features/funding-sources/list/FundingSourcesListPage.tsx
@@ -127,7 +127,7 @@ const FundingSourcesListPage: React.FC = () => {
               </Typography>
             </Toolbar>
             <Divider></Divider>
-            <Box pt={0} pb={1} px={3}>
+            <Box py={1} pb={3} px={3}>
               <FundingSourcesTable
                 fundingSources={fundingSourceDataLoader.data ?? []}
                 onView={(fundingSourceId) => {
