67a381ac2c22a60ab9df8ad2442b6648de9df155
Macgregor Aubertin-Young <macgregor.aubertin-young@gov.bc.ca>
2024-02-29 15:21:03 -0800

    text

--- app/src/pages/landing/LandingIntro.tsx
+++ app/src/pages/landing/LandingIntro.tsx
@@ -5,7 +5,7 @@ export const LandingIntro = () => {
   return (
     <>
       <Grid container spacing={5} justifyContent="space-between">
-        <Grid item xs={10} md={6} xl={4}>
+        <Grid item xs={10} md={6}>
           <QuestionAnswer
             title="What is the Species Inventory Management System?"
             subtext="A space to manage fish and wildlife data with collaborators from multiple organizations.
@@ -13,26 +13,26 @@ export const LandingIntro = () => {
             and invite collaborators to do the same in a shared workspace."
           />
         </Grid>
-        <Grid item xs={10} md={6} xl={4}>
+        <Grid item xs={10} md={6}>
           <QuestionAnswer
             title="Who can gain access?"
             subtext="Staff, contractors, and partners of the Province who collect 
             and manage fish and wildlife data. You must have an IDIR or BCeID to log in."
           />
         </Grid>
-        <Grid item xs={10} md={6} xl={4}>
+        <Grid item xs={10} md={6}>
           <QuestionAnswer
             title="What's the benefit?"
             subtext={
               <>
-                Simple screens let teams organize ecological survey information in an intuitive way. Keep track of when,
-                where, and how you surveyed, and your findings, in a workspace that your whole team&mdash;and only your
+                Simple screens let teams organize ecological survey information in an intuitive way. Keep track of where,
+                when, and how you surveyed, and your findings, in a workspace that your whole team&mdash;and only your
                 team&mdash;can access.
               </>
             }
           />
         </Grid>
-        <Grid item xs={10} md={6} xl={4}>
+        <Grid item xs={10} md={6}>
           <QuestionAnswer
             title="How can I get started?"
             subtext={
@@ -44,18 +44,20 @@ export const LandingIntro = () => {
             }
           />
         </Grid>
-        <Grid item xs={12} my={5}>
+        <Grid item xs={12} mt={5}>
           <Box maxWidth="100%" component="img" src="/assets/manage-observations.jpg" />
         </Grid>
       </Grid>
+      <Box mt={5}>
       <QuestionAnswer
         title="Looking for more information?"
         subtext={
           <>
-            Contact <a href="mailto:biohub@gov.bc.ca">biohub@gov.bc.ca</a> to request a demo, ask questions, and learn more.
+            Contact <a href="mailto:biohub@gov.bc.ca">biohub@gov.bc.ca</a> to request a demo, ask questions, and learn
+            more.
           </>
         }
-      />
+      /></Box>
     </>
   );
 };
