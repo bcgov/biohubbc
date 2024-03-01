c8930bf90a91044bac0fa71e550c1ac112c4751e
Mac Deluca <Mac.Deluca@quartech.com>
2024-03-01 10:38:09 -0800

    Added some comments for new frontend requests

--- app/src/hooks/cb_api/useCritterApi.tsx
+++ app/src/hooks/cb_api/useCritterApi.tsx
@@ -2,17 +2,25 @@ import { AxiosInstance } from 'axios';
 
 const useCritterApi = (axios: AxiosInstance) => {
   /**
-   * Get a critter with detailed response
+   * Get a critter with detailed response.
+   * Includes all markings, captures, mortalities etc.
    *
    * @async
-   * @param {string} critter_id - critter identifier.
-   * @returns {Promise<[TODO:type]>} [TODO:description]
+   * @param {string} critter_id - Critter identifier.
+   * @returns {Promise<[TODO:type]>}
    */
   const getDetailedCritter = async (critter_id: string) => {
     const { data } = await axios.get(`/api/critterbase/critters/${critter_id}?format=detailed`);
     return data;
   };
 
+  /**
+   * Get multiple critters by ids.
+   *
+   * @async
+   * @param {string[]} critter_ids - Critter identifiers.
+   * @returns {Promise<[TODO:type]>}
+   */
   const getMultipleCrittersByIds = async (critter_ids: string[]) => {
     const { data } = await axios.post(`/api/critterbase/critters/`, { critter_ids });
     return data;
