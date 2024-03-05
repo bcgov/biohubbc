d03d1f80298cf5bcd6def4a8fd1c41606f0269bb
Macgregor Aubertin-Young <macgregor.aubertin-young@gov.bc.ca>
2024-03-04 17:37:11 -0800

    styling

--- app/src/components/species/components/SpeciesCard.tsx
+++ app/src/components/species/components/SpeciesCard.tsx
@@ -1,7 +1,8 @@
 import { mdiCircle } from '@mdi/js';
 import Icon from '@mdi/react';
-import { Box, Chip, colors, Stack, Typography } from '@mui/material';
+import { Chip, colors, Stack, Typography } from '@mui/material';
 import { grey } from '@mui/material/colors';
+import React from 'react';
 
 interface ISpeciesCard {
   commonNames: string[];
@@ -13,14 +14,14 @@ interface ISpeciesCard {
 
 const SpeciesCard = (props: ISpeciesCard) => {
   const rankColors = [
-    { color: colors.blue[400], ranks: ['Subspecies', 'Variety'] },
-    { color: colors.purple[400], ranks: ['Species'] },
-    { color: colors.green[400], ranks: ['Genus'] },
-    { color: colors.blue[400], ranks: ['Family'] },
-    { color: colors.red[400], ranks: ['Order'] },
-    { color: colors.deepOrange[400], ranks: ['Class'] },
-    { color: colors.pink[400], ranks: ['Phylum'] },
-    { color: colors.grey[400], ranks: ['Kingdom'] }
+    { color: colors.blue[600], ranks: ['Subspecies', 'Variety'] },
+    { color: colors.purple[600], ranks: ['Species'] },
+    { color: colors.teal[600], ranks: ['Genus'] },
+    { color: colors.blue[600], ranks: ['Family'] },
+    { color: colors.indigo[600], ranks: ['Order'] },
+    { color: colors.deepOrange[600], ranks: ['Class'] },
+    { color: colors.pink[600], ranks: ['Phylum'] },
+    { color: colors.grey[600], ranks: ['Kingdom'] }
   ];
 
   return (
@@ -44,26 +45,24 @@ const SpeciesCard = (props: ISpeciesCard) => {
               size="small"
               sx={{
                 minWidth: '0.4rem',
-                borderRadius: '4px',
-                padding: '1px 1px',
+                padding: '0.8px 0.8px',
                 margin: '0 10px 3px 10px',
-                opacity: 0.4,
+                opacity: 0.6,
                 backgroundColor: rankColors.find((color) => color.ranks.includes(props.rank))?.color || grey[800],
                 '& .MuiChip-label': {
-                  pt: '2px',
                   letterSpacing: '0.03rem',
                   color: '#fff',
                   fontWeight: 100,
-                  fontSize: '0.7rem'
+                  fontSize: '0.6rem'
                 }
               }}
             />
           )}
         </Typography>
-        <Box display="flex" alignItems="center">
+        <Stack direction="row" alignItems='center'>
           {props.commonNames?.length > 0 &&
             props.commonNames.map((name, index) => (
-              <>
+              <React.Fragment key={`${name}-${index}`}>
                 {index > 0 && <Icon path={mdiCircle} size={0.15} color={grey[500]} style={{ margin: '0 5px' }} />}
                 <Typography
                   variant="body2"
@@ -73,33 +72,17 @@ const SpeciesCard = (props: ISpeciesCard) => {
                   }}>
                   {name}
                 </Typography>
-              </>
+              </React.Fragment>
             ))}
-        </Box>
+        </Stack>
       </Typography>
       <Stack spacing={1} direction="row">
-        {props.kingdom && (
-          <Chip
-            title="Taxonomic kingdom"
-            label={props.kingdom}
-            size="small"
-            sx={{
-              // backgroundColor: grey[700],
-              '& .MuiChip-label': {
-                // color: '#fff',
-                letterSpacing: '0.03rem'
-              }
-            }}
-          />
-        )}
         <Chip
           title="Taxonomic serial number (ID)"
           label={'ID: ' + props.tsn}
           size="small"
           sx={{
-            // backgroundColor: grey[700],
             '& .MuiChip-label': {
-              // color: '#fff',
               letterSpacing: '0.03rem'
             }
           }}
