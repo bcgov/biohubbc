7d52781a1a9453a5708bea14637313679db009d4
Nick Phura <nickphura@gmail.com>
2024-03-08 14:34:47 -0800

    Move dialog context to wrap individual pages, to improve observations page performance

--- app/src/features/surveys/observations/SurveyObservationPage.tsx
+++ app/src/features/surveys/observations/SurveyObservationPage.tsx
@@ -1,6 +1,7 @@
 import Box from '@mui/material/Box';
 import CircularProgress from '@mui/material/CircularProgress';
 import Stack from '@mui/material/Stack';
+import { DialogContextProvider } from 'contexts/dialogContext';
 import { ObservationsTableContext, ObservationsTableContextProvider } from 'contexts/observationsTableContext';
 import { ProjectContext } from 'contexts/projectContext';
 import { SurveyContext } from 'contexts/surveyContext';
@@ -44,24 +45,28 @@ export const SurveyObservationPage = () => {
         }}>
         {/* Sampling Site List */}
         <Box flex="0 0 auto" width="400px">
-          <SamplingSiteList />
+          <DialogContextProvider>
+            <SamplingSiteList />
+          </DialogContextProvider>
         </Box>
 
         {/* Observations Component */}
         <Box flex="1 1 auto">
-          <TaxonomyContextProvider>
-            <ObservationsTableContextProvider>
-              <ObservationsTableContext.Consumer>
-                {(context) => {
-                  if (!context?._muiDataGridApiRef.current) {
-                    return <CircularProgress className="pageProgress" size={40} />;
-                  }
+          <DialogContextProvider>
+            <TaxonomyContextProvider>
+              <ObservationsTableContextProvider>
+                <ObservationsTableContext.Consumer>
+                  {(context) => {
+                    if (!context?._muiDataGridApiRef.current) {
+                      return <CircularProgress className="pageProgress" size={40} />;
+                    }
 
-                  return <ObservationsTableContainer />;
-                }}
-              </ObservationsTableContext.Consumer>
-            </ObservationsTableContextProvider>
-          </TaxonomyContextProvider>
+                    return <ObservationsTableContainer />;
+                  }}
+                </ObservationsTableContext.Consumer>
+              </ObservationsTableContextProvider>
+            </TaxonomyContextProvider>
+          </DialogContextProvider>
         </Box>
       </Stack>
     </Stack>
