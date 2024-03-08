76c8f57fe9cd7ac8d07b158d4dab7b3d86e36fec
Nick Phura <nickphura@gmail.com>
2024-03-08 13:21:47 -0800

    Tweak openapi spec

--- api/src/paths/project/{projectId}/survey/{surveyId}/observations/measurements/delete.ts
+++ api/src/paths/project/{projectId}/survey/{surveyId}/observations/measurements/delete.ts
@@ -6,7 +6,7 @@ import { authorizeRequestHandler } from '../../../../../../../request-handlers/s
 import { ObservationSubCountMeasurementService } from '../../../../../../../services/observation-subcount-measurement-service';
 import { getLogger } from '../../../../../../../utils/logger';
 
-const defaultLog = getLogger('/api/project/{projectId}/survey/{surveyId}/observation/measurements');
+const defaultLog = getLogger('/api/project/{projectId}/survey/{surveyId}/observations/measurements');
 
 export const POST: Operation = [
   authorizeRequestHandler((req) => {
@@ -28,7 +28,7 @@ export const POST: Operation = [
 ];
 
 POST.apiDoc = {
-  description: 'Delete survey observations.',
+  description: 'Delete survey observation measurements.',
   tags: ['observation'],
   security: [
     {
@@ -39,26 +39,36 @@ POST.apiDoc = {
     {
       in: 'path',
       name: 'projectId',
+      schema: {
+        type: 'integer',
+        minimum: 1
+      },
       required: true
     },
     {
       in: 'path',
       name: 'surveyId',
+      schema: {
+        type: 'integer',
+        minimum: 1
+      },
       required: true
     }
   ],
   requestBody: {
-    description: 'Survey observation record data',
+    description: 'Survey observation measurement delete request body.',
     content: {
       'application/json': {
         schema: {
           type: 'object',
           properties: {
             measurement_ids: {
+              description: 'An array of measurement ids (critterbase taxon measurement ids) to delete',
               type: 'array',
               minItems: 1,
               items: {
-                type: 'string'
+                type: 'string',
+                format: 'uuid'
               }
             }
           }
@@ -99,7 +109,7 @@ POST.apiDoc = {
 };
 
 /**
- * Deletes survey observations.
+ * Deletes survey observation measurements.
  *
  * @export
  * @return {*}  {RequestHandler}
@@ -108,21 +118,21 @@ export function deleteObservationMeasurements(): RequestHandler {
   return async (req, res) => {
     const surveyId = Number(req.params.surveyId);
 
-    defaultLog.debug({ label: 'deleteSurveyObservations', surveyId });
+    defaultLog.debug({ label: 'deleteObservationMeasurements', surveyId });
 
     const connection = getDBConnection(req['keycloak_token']);
 
     try {
       await connection.open();
-      const service = new ObservationSubCountMeasurementService(connection);
 
+      const service = new ObservationSubCountMeasurementService(connection);
       await service.deleteMeasurementsForTaxonMeasurementIds(surveyId, req.body.measurement_ids);
 
       await connection.commit();
 
       return res.status(204);
     } catch (error) {
-      defaultLog.error({ label: 'deleteSurveyObservations', message: 'error', error });
+      defaultLog.error({ label: 'deleteObservationMeasurements', message: 'error', error });
       await connection.rollback();
       throw error;
     } finally {
