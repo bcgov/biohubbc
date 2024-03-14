eb1657100bcb61ffc8bbf9def16c56bb9b53877b
Nick Phura <nickphura@gmail.com>
2024-03-14 16:04:25 -0700

    Tweaks

--- app/.pipeline/scripts/app.build.js
+++ app/.pipeline/scripts/app.build.js
@@ -3,10 +3,10 @@
 const { appBuild } = require('../lib/app.build.js');
 const config = require('../config.js');
 
-if (config.options.phase === 'pr') {
-  console.log(JSON.stringify(options));
-  console.log(JSON.stringify(phases));
-}
+// if (config.options.phase === 'pr') {
+console.log(JSON.stringify(options));
+console.log(JSON.stringify(phases));
+// }
 
 // Builds the app image
 appBuild(config);
--- database/.pipeline/scripts/db.setup.build.js
+++ database/.pipeline/scripts/db.setup.build.js
@@ -3,10 +3,10 @@
 const { dbSetupBuild } = require('../lib/db.setup.build.js');
 const config = require('../config.js');
 
-if (config.options.phase === 'pr') {
-  console.log(JSON.stringify(options));
-  console.log(JSON.stringify(phases));
-}
+// if (config.options.phase === 'pr') {
+console.log(JSON.stringify(options));
+console.log(JSON.stringify(phases));
+// }
 
 // build database Setup (migrations, seeding, etc) image
 dbSetupBuild(config);
