bcgov/biohubbc

commit dfbe6e45d60e4922e216d392ed3f7188b54bf47c

SIMSBIOHUB-388, 481: Add loading buttons to edit/create survey + project forms; Access request bug (#1221)
* SIMSBIOHUB-481: Fixed an error handling bug when creating a SIMS Access Request.
* SIMSBIOHUB-388: Uses the LoadingButton to show loading state when creating/editing a survey or project; Consolidates the edit and create forms for projects and surveys into a single component (EditProjectForm and EditSurveyForm, respectively).

app/src/features/surveys/edit/EditSurveyPage.tsx
@@ -1,9 +1,12 @@
+import { LoadingButton } from '@mui/lab';
 import Box from '@mui/material/Box';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Button from '@mui/material/Button';
 import CircularProgress from '@mui/material/CircularProgress';
 import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import { IErrorDialogProps } from 'components/dialog/ErrorDialog';
 import PageHeader from 'components/layout/PageHeader';
@@ -39,6 +42,7 @@ const EditSurveyPage = () => {
 
   // Ability to bypass showing the 'Are you sure you want to cancel' dialog
   const [enableCancelCheck, setEnableCancelCheck] = useState(true);
+  const [isSaving, setIsSaving] = useState(false);
 
   const dialogContext = useContext(DialogContext);
 
@@ -46,13 +50,12 @@ const EditSurveyPage = () => {
   useEffect(() => {
     codesContext.codesDataLoader.load();
   }, [codesContext.codesDataLoader]);
-  const codes = codesContext.codesDataLoader.data;
 
   const projectContext = useContext(ProjectContext);
+
   useEffect(() => {
     projectContext.projectDataLoader.load(projectContext.projectId);
   }, [projectContext.projectDataLoader, projectContext.projectId]);
-  const projectData = projectContext.projectDataLoader.data?.projectData;
 
   const surveyContext = useContext(SurveyContext);
 
@@ -118,6 +121,7 @@ const EditSurveyPage = () => {
    * @return {*}
    */
   const handleSubmit = async (values: IEditSurveyRequest) => {
+    setIsSaving(true);
     try {
       const response = await biohubApi.survey.updateSurvey(
         projectContext.projectId,
@@ -136,7 +140,7 @@ const EditSurveyPage = () => {
 
       surveyContext.surveyDataLoader.refresh(projectContext.projectId, surveyContext.surveyId);
 
-      history.push(`/admin/projects/${projectData?.project.project_id}/surveys/${response.id}/details`);
+      history.push(`/admin/projects/${projectContext.projectId}/surveys/${response.id}/details`);
     } catch (error) {
       const apiError = error as APIError;
       showEditErrorDialog({
@@ -144,6 +148,8 @@ const EditSurveyPage = () => {
         dialogError: apiError?.message,
         dialogErrorDetails: apiError?.errors
       });
+    } finally {
+      setIsSaving(false);
     }
   };
 
@@ -173,7 +179,7 @@ const EditSurveyPage = () => {
     return true;
   };
 
-  if (!codes || !projectData || !surveyData) {
+  if (!codesContext.codesDataLoader.data || !projectContext.projectDataLoader.data || !surveyData) {
     return <CircularProgress className="pageProgress" size={40} />;
   }
 
@@ -184,13 +190,13 @@ const EditSurveyPage = () => {
         title="Edit Survey Details"
         breadCrumbJSX={
           <Breadcrumbs aria-label="breadcrumb" separator={'>'}>
-            <Link component={RouterLink} underline="hover" to={`/admin/projects/${projectData.project.project_id}/`}>
-              {projectData.project.project_name}
+            <Link component={RouterLink} underline="hover" to={`/admin/projects/${projectContext.projectId}/`}>
+              {projectContext.projectDataLoader.data.projectData.project.project_name}
             </Link>
             <Link
               component={RouterLink}
               underline="hover"
-              to={`/admin/projects/${projectData.project.project_id}/surveys/${surveyId}/details`}>
+              to={`/admin/projects/${projectContext.projectId}/surveys/${surveyId}/details`}>
               {surveyData && surveyData.survey_details && surveyData.survey_details.survey_name}
             </Link>
             <Typography component="a" color="textSecondary" aria-current="page">
@@ -200,10 +206,14 @@ const EditSurveyPage = () => {
         }
         buttonJSX={
           <>
-            <Button color="primary" variant="contained" onClick={() => formikRef.current?.submitForm()}>
+            <LoadingButton
+              loading={isSaving}
+              color="primary"
+              variant="contained"
+              onClick={() => formikRef.current?.submitForm()}>
               Save and Exit
-            </Button>
-            <Button color="primary" variant="outlined" onClick={handleCancel}>
+            </LoadingButton>
+            <Button disabled={isSaving} color="primary" variant="outlined" onClick={handleCancel}>
               Cancel
             </Button>
           </>
@@ -212,14 +222,24 @@ const EditSurveyPage = () => {
 
       <Box my={3}>
         <Container maxWidth="xl">
-          <EditSurveyForm
-            codes={codes}
-            projectData={projectData}
-            surveyData={surveyData}
-            handleSubmit={handleSubmit}
-            handleCancel={handleCancel}
-            formikRef={formikRef}
-          />
+          <Box p={5} component={Paper} display="block">
+            <EditSurveyForm initialSurveyData={surveyData} handleSubmit={handleSubmit} formikRef={formikRef} />
+            <Stack mt={5} flexDirection="row" justifyContent="flex-end" gap={1}>
+              <LoadingButton
+                loading={isSaving}
+                type="submit"
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                  formikRef.current?.submitForm();
+                }}>
+                Save and Exit
+              </LoadingButton>
+              <Button disabled={isSaving} variant="outlined" color="primary" onClick={handleCancel}>
+                Cancel
+              </Button>
+            </Stack>
+          </Box>
         </Container>
       </Box>
     </>

database/src/seeds/03_basic_project_survey_setup.ts
@@ -5,8 +5,8 @@ const DB_SCHEMA = process.env.DB_SCHEMA;
 const DB_SCHEMA_DAPI_V1 = process.env.DB_SCHEMA_DAPI_V1;
 const PROJECT_SEEDER_USER_IDENTIFIER = process.env.PROJECT_SEEDER_USER_IDENTIFIER;
 
-const NUM_SEED_PROJECTS = Number(process.env.NUM_SEED_PROJECTS ?? 1);
-const NUM_SEED_SURVEYS_PER_PROJECT = Number(process.env.NUM_SEED_SURVEYS_PER_PROJECT ?? 1);
+const NUM_SEED_PROJECTS = Number(process.env.NUM_SEED_PROJECTS ?? 2);
+const NUM_SEED_SURVEYS_PER_PROJECT = Number(process.env.NUM_SEED_SURVEYS_PER_PROJECT ?? 2);
 
 const focalTaxonIdOptions = [
   { itis_tsn: 180703, itis_scientific_name: 'Alces alces' }, // Moose

api/src/paths/administrative-activity.ts
@@ -143,7 +143,12 @@ export function createAdministrativeActivity(): RequestHandler {
 
       await connection.commit();
 
-      await administrativeActivityService.sendAccessRequestNotificationEmailToAdmin();
+      try {
+        await administrativeActivityService.sendAccessRequestNotificationEmailToAdmin();
+      } catch (error) {
+        // If email notifiation fails to send, simply log the error and continue.
+        defaultLog.error({ label: 'administrativeActivity', message: 'createAdministrativeActivity', error });
+      }
 
       return res.status(200).json(response);
     } catch (error) {

app/src/components/fields/HorizontalSplitFormComponent.tsx
@@ -1,12 +1,30 @@
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import React from 'react';
+import { ReactElement } from 'react';
 
 export interface IHorizontalSplitFormComponentProps {
+  /**
+   * The title of the section
+   *
+   * @type {string}
+   * @memberof IHorizontalSplitFormComponentProps
+   */
   title: string;
-  summary: string;
-  component: any;
+  /**
+   * The description for the section (optional)
+   *
+   * @type {string}
+   * @memberof IHorizontalSplitFormComponentProps
+   */
+  summary?: string;
+  /**
+   * The form component to render
+   *
+   * @type {ReactElement}
+   * @memberof IHorizontalSplitFormComponentProps
+   */
+  component: ReactElement;
 }
 
 /**
@@ -14,20 +32,22 @@ export interface IHorizontalSplitFormComponentProps {
  *
  * @return {*}
  */
-const HorizontalSplitFormComponent: React.FC<IHorizontalSplitFormComponentProps> = (props) => {
+const HorizontalSplitFormComponent = (props: IHorizontalSplitFormComponentProps) => {
   const { title, summary, component } = props;
 
   return (
     <Grid container spacing={3}>
-      <Grid item xs={12} lg={4}>
+      <Grid item sx={{ pb: 3 }} xs={12} lg={4}>
         <Typography variant="h3" component="h2">
           {title}
         </Typography>
-        <Box pt={'10px'} pb={3} maxWidth="55ch">
-          <Typography variant="body1" color="textSecondary">
-            {summary}
-          </Typography>
-        </Box>
+        {summary && (
+          <Box pt={1.25} maxWidth="55ch">
+            <Typography variant="body1" color="textSecondary">
+              {summary}
+            </Typography>
+          </Box>
+        )}
       </Grid>
       <Grid item xs={12} lg={8}>
         {component}

app/src/features/projects/components/ProjectUserForm.test.tsx
@@ -6,9 +6,9 @@ import { ICode } from 'interfaces/useCodesApi.interface';
 import { ISystemUser } from 'interfaces/useUserApi.interface';
 import { getMockAuthState, SystemAdminAuthState } from 'test-helpers/auth-helpers';
 import { fireEvent, render, waitFor, within } from 'test-helpers/test-utils';
-import ProjectUserForm, { ProjectUserRoleFormInitialValues, ProjectUserRoleYupSchema } from './ProjectUserForm';
+import ProjectUserForm, { ProjectUserRoleYupSchema } from './ProjectUserForm';
 
-const roles: ICode[] = [
+const mockRoles: ICode[] = [
   {
     id: 1,
     name: PROJECT_ROLE.COLLABORATOR
@@ -67,32 +67,34 @@ describe('ProjectUserForm', () => {
 
   it('renders correctly with default values', async () => {
     const authState = getMockAuthState({ base: SystemAdminAuthState });
+
+    const formikInitialValues = {
+      participants: [
+        {
+          system_user_id: 1,
+          user_identifier: 'identifier',
+          user_guid: '',
+          identity_source: 'IDIR',
+          record_end_date: '',
+          role_ids: [],
+          role_names: [],
+          email: 'user@email.com',
+          display_name: 'Test User',
+          agency: 'Business',
+          project_role_names: [PROJECT_ROLE.COORDINATOR]
+        }
+      ]
+    };
+
     const { getByTestId, getByText } = render(
       <AuthStateContext.Provider value={authState}>
         <Formik
-          initialValues={ProjectUserRoleFormInitialValues}
+          initialValues={formikInitialValues}
           validationSchema={ProjectUserRoleYupSchema}
           validateOnBlur={true}
           validateOnChange={false}
           onSubmit={async () => {}}>
-          <ProjectUserForm
-            users={[
-              {
-                system_user_id: 1,
-                user_identifier: 'identifier',
-                user_guid: '',
-                identity_source: 'IDIR',
-                record_end_date: '',
-                role_ids: [],
-                role_names: [],
-                email: 'user@email.com',
-                display_name: 'Test User',
-                agency: 'Business',
-                project_role_names: [PROJECT_ROLE.COORDINATOR]
-              }
-            ]}
-            roles={roles}
-          />
+          <ProjectUserForm roles={mockRoles} />
         </Formik>
       </AuthStateContext.Provider>
     );
@@ -105,15 +107,20 @@ describe('ProjectUserForm', () => {
 
   it('renders newly added users properly', async () => {
     const authState = getMockAuthState({ base: SystemAdminAuthState });
+
+    const formikInitialValues = {
+      participants: []
+    };
+
     const { getByTestId, getByText } = render(
       <AuthStateContext.Provider value={authState}>
         <Formik
-          initialValues={ProjectUserRoleFormInitialValues}
+          initialValues={formikInitialValues}
           validationSchema={ProjectUserRoleYupSchema}
           validateOnBlur={true}
           validateOnChange={false}
           onSubmit={async () => {}}>
-          <ProjectUserForm users={[]} roles={roles} />
+          <ProjectUserForm roles={mockRoles} />
         </Formik>
       </AuthStateContext.Provider>
     );
@@ -136,32 +143,34 @@ describe('ProjectUserForm', () => {
 
   it('renders removing a user', async () => {
     const authState = getMockAuthState({ base: SystemAdminAuthState });
+
+    const formikInitialValues = {
+      participants: [
+        {
+          system_user_id: 1,
+          user_identifier: 'identifier',
+          user_guid: '',
+          identity_source: 'IDIR',
+          record_end_date: '',
+          role_ids: [],
+          role_names: [],
+          email: 'user@email.com',
+          display_name: 'Test User',
+          agency: 'Business',
+          project_role_names: [PROJECT_ROLE.COORDINATOR]
+        }
+      ]
+    };
+
     const { getByTestId } = render(
       <AuthStateContext.Provider value={authState}>
         <Formik
-          initialValues={ProjectUserRoleFormInitialValues}
+          initialValues={formikInitialValues}
           validationSchema={ProjectUserRoleYupSchema}
           validateOnBlur={true}
           validateOnChange={false}
           onSubmit={async () => {}}>
-          <ProjectUserForm
-            users={[
-              {
-                system_user_id: 1,
-                user_identifier: 'identifier',
-                user_guid: '',
-                identity_source: 'IDIR',
-                record_end_date: '',
-                role_ids: [],
-                role_names: [],
-                email: 'user@email.com',
-                display_name: 'Test User',
-                agency: 'Business',
-                project_role_names: [PROJECT_ROLE.COORDINATOR]
-              }
-            ]}
-            roles={roles}
-          />
+          <ProjectUserForm roles={mockRoles} />
         </Formik>
       </AuthStateContext.Provider>
     );

commit a25ea1e86cc7c85f1f92bf2445e3b6de7df686cc

SIMSBIOHUB-464: App effect dependancy warnings (#1256)
* Addresses linting/console errors related to `useEffect` dependencies.
* Fixes a bug introduced by #1221 in which saving edits to a survey causes the old data to be shown while saving

app/src/contexts/telemetryTableContext.tsx
@@ -462,7 +462,7 @@ export const TelemetryTableContextProvider: React.FC<ITelemetryTableContextProvi
     if (telemetryDataContext.telemetryDataLoader.isReady) {
       telemetryDataContext.telemetryDataLoader.refresh(deployment_ids);
     }
-  }, [telemetryDataContext.telemetryDataLoader]);
+  }, [deployment_ids, telemetryDataContext.telemetryDataLoader]);
 
   // True if the data grid contains at least 1 unsaved record
   const hasUnsavedChanges = modifiedRowIds.length > 0 || addedRowIds.length > 0;

app/src/features/projects/list/ProjectsListPage.tsx
@@ -82,20 +82,6 @@ const ProjectsListPage = () => {
     );
   };
 
-  const refreshProjectsList = () => {
-    const sort = firstOrNull(sortModel);
-    const pagination = {
-      limit: paginationModel.pageSize,
-      sort: sort?.field || undefined,
-      order: sort?.sort || undefined,
-
-      // API pagination pages begin at 1, but MUI DataGrid pagination begins at 0.
-      page: paginationModel.page + 1
-    };
-
-    return projectsDataLoader.refresh(pagination, advancedFiltersModel);
-  };
-
   const projectRows =
     projectsDataLoader.data?.projects.map((project) => {
       return {
@@ -150,7 +136,20 @@ const ProjectsListPage = () => {
 
   // Refresh projects when pagination or sort changes
   useEffect(() => {
-    refreshProjectsList();
+    const sort = firstOrNull(sortModel);
+    const pagination = {
+      limit: paginationModel.pageSize,
+      sort: sort?.field || undefined,
+      order: sort?.sort || undefined,
+
+      // API pagination pages begin at 1, but MUI DataGrid pagination begins at 0.
+      page: paginationModel.page + 1
+    };
+
+    projectsDataLoader.refresh(pagination, advancedFiltersModel);
+
+    // Adding a DataLoader as a dependency causes an infinite rerender loop if a useEffect calls `.refresh`
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [sortModel, paginationModel, advancedFiltersModel]);
 
   /**

app/src/features/projects/view/components/TeamMember.tsx
@@ -13,21 +13,21 @@ interface IProjectParticipantsRoles {
   avatarColor: string;
 }
 
+// Define a custom sorting order for roles
+const roleOrder: { [key: string]: number } = {
+  Coordinator: 1,
+  Collaborator: 2,
+  Observer: 3
+};
+
 const TeamMembers = () => {
   const projectContext = useContext(ProjectContext);
 
   // Project data must be loaded by a parent before this component is rendered
   assert(projectContext.projectDataLoader.data);
 
-  // Define a custom sorting order for roles
-  const roleOrder: { [key: string]: number } = {
-    Coordinator: 1,
-    Collaborator: 2,
-    Observer: 3
-  };
-
-  const projectTeamMembers: IProjectParticipantsRoles[] = useMemo(
-    () =>
+  const projectTeamMembers: IProjectParticipantsRoles[] = useMemo(() => {
+    return (
       projectContext.projectDataLoader.data?.projectData.participants
         .map((member) => {
           const initials = member.display_name
@@ -46,9 +46,9 @@ const TeamMembers = () => {
           const roleA = a.roles[0] || '';
           const roleB = b.roles[0] || '';
           return roleOrder[roleA] - roleOrder[roleB];
-        }) || [],
-    [projectContext.projectDataLoader.data.projectData.participants]
-  );
+        }) ?? []
+    );
+  }, [projectContext.projectDataLoader.data.projectData.participants]);
 
   return (
     <Stack spacing={1}>

app/src/features/surveys/edit/EditSurveyPage.tsx
@@ -44,8 +44,8 @@ const EditSurveyPage = () => {
   const [isSaving, setIsSaving] = useState(false);
 
   const dialogContext = useContext(DialogContext);
-
   const codesContext = useContext(CodesContext);
+
   useEffect(() => {
     codesContext.codesDataLoader.load();
   }, [codesContext.codesDataLoader]);
@@ -58,25 +58,15 @@ const EditSurveyPage = () => {
 
   const surveyContext = useContext(SurveyContext);
 
-  const editSurveyDL = useDataLoader((projectId: number, surveyId: number) =>
+  const editSurveyDataLoader = useDataLoader((projectId: number, surveyId: number) =>
     biohubApi.survey.getSurveyForUpdate(projectId, surveyId)
   );
 
-  if (!editSurveyDL.data && surveyId) {
-    editSurveyDL.load(projectContext.projectId, surveyId);
+  if (surveyId) {
+    editSurveyDataLoader.load(projectContext.projectId, surveyId);
   }
-  const surveyData = editSurveyDL.data?.surveyData;
 
-  useEffect(() => {
-    const setFormikValues = (data: IEditSurveyRequest) => {
-      formikRef.current?.setValues(data);
-    };
-
-    if (editSurveyDL.data) {
-      setFormikValues(editSurveyDL.data.surveyData as unknown as IEditSurveyRequest);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [editSurveyDL]);
+  const surveyData = editSurveyDataLoader.data?.surveyData;
 
   const defaultCancelDialogProps = {
     dialogTitle: EditSurveyI18N.cancelTitle,

app/src/features/surveys/list/SurveysListPage.tsx
@@ -31,7 +31,8 @@ const SurveysListPage = () => {
   });
   const [sortModel, setSortModel] = useState<GridSortModel>([]);
 
-  const refreshSurveyList = () => {
+  // Refresh survey list when pagination or sort changes
+  useEffect(() => {
     const sort = firstOrNull(sortModel);
     const pagination: ApiPaginationRequestOptions = {
       limit: paginationModel.pageSize,
@@ -42,12 +43,10 @@ const SurveysListPage = () => {
       page: paginationModel.page + 1
     };
 
-    return projectContext.surveysListDataLoader.refresh(pagination);
-  };
+    projectContext.surveysListDataLoader.refresh(pagination);
 
-  // Refresh survey list when pagination or sort changes
-  useEffect(() => {
-    refreshSurveyList();
+    // Adding a DataLoader as a dependency causes an infinite rerender loop if a useEffect calls `.refresh`
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [sortModel, paginationModel]);
 
   const columns: GridColDef<SurveyBasicFieldsObject>[] = [

app/src/features/surveys/observations/sampling-sites/edit/components/SamplingSiteEditMapControl.tsx
@@ -102,13 +102,17 @@ const SamplingSiteEditMapControl = (props: ISamplingSiteEditMapControlProps) => 
   const [editedGeometry, setEditedGeometry] = useState<Feature[] | undefined>(undefined);
 
   useEffect(() => {
-    if (editedGeometry) {
-      updateStaticLayers(editedGeometry);
+    if (!editedGeometry) {
+      return;
     }
-  }, [editedGeometry]);
+
+    updateStaticLayers(editedGeometry);
+  }, [editedGeometry, updateStaticLayers]);
 
   useEffect(() => {
     updateStaticLayers(samplingSiteGeoJsonFeatures);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [samplingSiteGeoJsonFeatures]);
 
   return (

app/src/features/surveys/telemetry/ManualTelemetryList.tsx
@@ -94,6 +94,9 @@ const ManualTelemetryList = () => {
   useEffect(() => {
     surveyContext.deploymentDataLoader.refresh(surveyContext.projectId, surveyContext.surveyId);
     surveyContext.critterDataLoader.refresh(surveyContext.projectId, surveyContext.surveyId);
+
+    // Adding a DataLoader as a dependency causes an infinite rerender loop if a useEffect calls `.refresh`
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const deployments = surveyContext.deploymentDataLoader.data;

app/src/features/surveys/view/components/Permits.tsx
@@ -25,12 +25,12 @@ const Permits = () => {
 
   return (
     <>
-      {permit.permits?.map((item) => {
+      {permit.permits?.map((permit, index) => {
         return (
-          <Box component="dl">
-            <Box className="row" key={item.permit_id}>
-              <Typography component="dt">{`#${item.permit_number}`}</Typography>
-              <Typography component="dd">{item.permit_type}</Typography>
+          <Box component="dl" key={`${permit.permit_id}-${index}`}>
+            <Box className="row" key={permit.permit_id}>
+              <Typography component="dt">{`#${permit.permit_number}`}</Typography>
+              <Typography component="dd">{permit.permit_type}</Typography>
             </Box>
           </Box>
         );

database/src/seeds/03_basic_project_survey_setup.ts
@@ -246,16 +246,21 @@ const insertSurveyFundingData = (surveyId: number) => `
  */
 const insertSurveyFocalSpeciesData = (surveyId: number) => {
   const focalSpecies = focalTaxonIdOptions[Math.floor(Math.random() * focalTaxonIdOptions.length)];
+  const testValue = [
+    2012, 2013, 828, 2019, 1594, 1718, 2037, 2062, 2068, 2065, 2070, 2069, 23918, 23922, 23920, 35369, 35370, 28516
+  ][Math.floor(Math.random() * 18)];
 
   return `
     INSERT into study_species
       (
         survey_id,
+        wldtaxonomic_units_id,
         itis_tsn,
         is_focal
       )
     VALUES (
       ${surveyId},
+      ${testValue},
       ${focalSpecies.itis_tsn},
       'Y'
     );
@@ -614,10 +619,16 @@ const insertObservationSubCount = (surveyObservationId: number) => `
  * SQL to insert survey observation data. Requires sampling site, method, period.
  *
  */
-const insertSurveyObservationData = (surveyId: number, count: number) => `
+const insertSurveyObservationData = (surveyId: number, count: number) => {
+  const testValue = [
+    2012, 2013, 828, 2019, 1594, 1718, 2037, 2062, 2068, 2065, 2070, 2069, 23918, 23922, 23920, 35369, 35370, 28516
+  ][Math.floor(Math.random() * 18)];
+
+  return `
   INSERT INTO survey_observation
   (
     survey_id,
+    wldtaxonomic_units_id,
     itis_tsn,
     itis_scientific_name,
     latitude,
@@ -632,6 +643,7 @@ const insertSurveyObservationData = (surveyId: number, count: number) => `
   VALUES
   (
     ${surveyId},
+    ${testValue},
     $$${focalTaxonIdOptions[0].itis_tsn}$$,
     $$${focalTaxonIdOptions[0].itis_scientific_name}$$,
     $$${faker.number.int({ min: 48, max: 60 })}$$,
@@ -658,6 +670,7 @@ const insertSurveyObservationData = (surveyId: number, count: number) => `
   )
   RETURNING survey_observation_id;
 `;
+};
 
 /**
  * SQL to insert Project data