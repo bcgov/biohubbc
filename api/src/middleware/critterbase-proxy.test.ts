b5391b03aff3d7cf68c2f8cb9bd6f2b84fbedf4a
Mac Deluca <Mac.Deluca@quartech.com>
2024-04-08 14:24:06 -0700

    fix: updated tests

--- api/src/middleware/critterbase-proxy.test.ts
+++ api/src/middleware/critterbase-proxy.test.ts
@@ -15,35 +15,35 @@ describe('CritterbaseProxy', () => {
     });
 
     it('should reject all requests not coming from SIMS APP', () => {
-      expect(proxyFilter('test', { headers: { host: 'NOT-SIMS' } } as Request)).to.be.false;
+      expect(proxyFilter('test', { headers: { origin: 'NOT-SIMS' } } as Request)).to.be.false;
     });
 
     it('should allow requests coming from SIMS APP', () => {
-      expect(proxyFilter('test', { headers: { host: 'SIMS' } } as Request)).to.be.false;
+      expect(proxyFilter('test', { headers: { origin: 'SIMS' } } as Request)).to.be.false;
     });
 
     it('should allow all GET/POST/PATCH requests', () => {
-      expect(proxyFilter('test', { method: 'GET', headers: { host: 'SIMS' } } as Request)).to.be.true;
-      expect(proxyFilter('test', { method: 'POST', headers: { host: 'SIMS' } } as Request)).to.be.true;
-      expect(proxyFilter('test', { method: 'PATCH', headers: { host: 'SIMS' } } as Request)).to.be.true;
+      expect(proxyFilter('test', { method: 'GET', headers: { origin: 'SIMS' } } as Request)).to.be.true;
+      expect(proxyFilter('test', { method: 'POST', headers: { origin: 'SIMS' } } as Request)).to.be.true;
+      expect(proxyFilter('test', { method: 'PATCH', headers: { origin: 'SIMS' } } as Request)).to.be.true;
     });
 
     it('should reject unknown request methods', () => {
-      expect(proxyFilter('test', { method: 'UNKNOWN', headers: { host: 'SIMS' } } as Request)).to.be.false;
+      expect(proxyFilter('test', { method: 'UNKNOWN', headers: { origin: 'SIMS' } } as Request)).to.be.false;
     });
 
     it('should allow DELETE requests to capture endpoint', () => {
       expect(
         proxyFilter('/api/critterbase/captures/id', {
           method: 'DELETE',
-          headers: { host: 'SIMS' }
+          headers: { origin: 'SIMS' }
         } as Request)
       ).to.be.true;
 
       expect(
         proxyFilter('/api/critterbase/captures/id/test', {
           method: 'DELETE',
-          headers: { host: 'SIMS' }
+          headers: { origin: 'SIMS' }
         } as Request)
       ).to.be.false;
     });
@@ -52,14 +52,14 @@ describe('CritterbaseProxy', () => {
       expect(
         proxyFilter('/api/critterbase/markings/id', {
           method: 'DELETE',
-          headers: { host: 'SIMS' }
+          headers: { origin: 'SIMS' }
         } as Request)
       ).to.be.true;
 
       expect(
         proxyFilter('/api/critterbase/markings/id/test', {
           method: 'DELETE',
-          headers: { host: 'SIMS' }
+          headers: { origin: 'SIMS' }
         } as Request)
       ).to.be.false;
     });
@@ -68,14 +68,14 @@ describe('CritterbaseProxy', () => {
       expect(
         proxyFilter('/api/critterbase/measurements/qualitative/id', {
           method: 'DELETE',
-          headers: { host: 'SIMS' }
+          headers: { origin: 'SIMS' }
         } as Request)
       ).to.be.true;
 
       expect(
         proxyFilter('/api/critterbase/measurements/qualitative/id/test', {
           method: 'DELETE',
-          headers: { host: 'SIMS' }
+          headers: { origin: 'SIMS' }
         } as Request)
       ).to.be.false;
     });
@@ -84,14 +84,14 @@ describe('CritterbaseProxy', () => {
       expect(
         proxyFilter('/api/critterbase/collection-units/id', {
           method: 'DELETE',
-          headers: { host: 'SIMS' }
+          headers: { origin: 'SIMS' }
         } as Request)
       ).to.be.true;
 
       expect(
         proxyFilter('/api/critterbase/collection-units/id/test', {
           method: 'DELETE',
-          headers: { host: 'SIMS' }
+          headers: { origin: 'SIMS' }
         } as Request)
       ).to.be.false;
     });
@@ -100,14 +100,14 @@ describe('CritterbaseProxy', () => {
       expect(
         proxyFilter('/api/critterbase/mortality/id', {
           method: 'DELETE',
-          headers: { host: 'SIMS' }
+          headers: { origin: 'SIMS' }
         } as Request)
       ).to.be.true;
 
       expect(
         proxyFilter('/api/critterbase/mortality/id/test', {
           method: 'DELETE',
-          headers: { host: 'SIMS' }
+          headers: { origin: 'SIMS' }
         } as Request)
       ).to.be.false;
     });
