1d3c735630480087f2099f46cb6aabc9dca441f4
Mac Deluca <Mac.Deluca@quartech.com>
2024-04-22 16:17:46 -0700

    fix: updated SQL template string to be properly formatted

--- scripts/bctw-deployments/main.js
+++ scripts/bctw-deployments/main.js
@@ -32,7 +32,7 @@ const CARIBOU_FEATURES_FILE = "files/features.json";
  * Wrapper for exec util, grants ability to call bin commands from js script.
  *
  * @async
- * @param {string} command - Command to execute. ie: `jq < ${fileName}`
+ * @param {string} command - Command to execute. ie: `jq < ${fileName}` | `echo 'Hello World'`
  * @throws {error} - stderr - Error from command.
  * @returns {Promise<string>} Standard output as string.
  */
@@ -143,12 +143,12 @@ async function main() {
 
     const data = await jqPreParseInputFile(file_name_argument);
 
-    let sql;
+    let sql = "";
 
     for (const project of data) {
       sql += `WITH p AS (INSERT INTO project (name, objectives, coordinator_first_name, coordinator_last_name, coordinator_email_address, start_date, end_date) VALUES ($$Caribou - ${project.herd} - BCTW Telemetry$$, $$BCTW telemetry deployments for ${project.herd} Caribou$$, $$${CONFIG.first_name}$$, $$${CONFIG.last_name}$$, $$${CONFIG.email}$$, $$${project.start_date}$$, $$${project.end_date}$$) RETURNING project_id
       ), ppp AS (INSERT INTO project_participation (project_id, system_user_id, project_role_id) SELECT project_id, (select system_user_id from system_user where user_identifier = $$mauberti$$), (select project_role_id from project_role where name = $$${CONFIG.project_role}$$) FROM p
-      ), pp AS (INSERT INTO project_program (project_id, program_id) SELECT project_id, (select program_id from program where name = $$${CONFIG.project_program}$$) FROM p)
+      ), pp AS (INSERT INTO project_program (project_id, program_id) SELECT project_id, (select program_id from program where name = $$${CONFIG.project_program}$$) FROM p
     `;
       for (let sIndex = 0; sIndex < project.surveys.length; sIndex++) {
         const survey = project.surveys[sIndex];
@@ -161,22 +161,27 @@ async function main() {
           throw `Error: Missing herd geometry for: ${project.herd}`;
         }
 
-        sql += `, s${sIndex} AS (INSERT INTO survey (project_id, name, lead_first_name, lead_last_name, start_date, end_date, progress_id) SELECT project_id, $$Caribou - ${survey.year} - ${project.herd} - BCTW Telemetry$$, $$${CONFIG.first_name}$$, $$${CONFIG.last_name}$$, $$${project.start_date}$$, $$${project.end_date}$$, (select survey_progress_id from survey_progress where name = $$${CONFIG.survey_status}$$) FROM p RETURNING survey_id
+        sql += `), s${sIndex} AS (INSERT INTO survey (project_id, name, lead_first_name, lead_last_name, start_date, end_date, progress_id) SELECT project_id, $$Caribou - ${survey.year} - ${project.herd} - BCTW Telemetry$$, $$${CONFIG.first_name}$$, $$${CONFIG.last_name}$$, $$${project.start_date}$$, $$${project.end_date}$$, (select survey_progress_id from survey_progress where name = $$${CONFIG.survey_status}$$) FROM p RETURNING survey_id
           ), st${sIndex} AS (INSERT INTO survey_type (survey_id, type_id) SELECT survey_id, (select type_id from type where name = $$${CONFIG.survey_type}$$) FROM s${sIndex}
           ), ss${sIndex} AS (INSERT INTO study_species (survey_id, is_focal, itis_tsn) SELECT survey_id, true, ${CONFIG.caribou_tsn} FROM s${sIndex}
           ), sio1${sIndex} AS (INSERT INTO survey_intended_outcome (survey_id, intended_outcome_id) SELECT survey_id, (select intended_outcome_id from intended_outcome where name = $$${CONFIG.survey_intended_outcome_1}$$) FROM s${sIndex}
           ), sio2${sIndex} AS (INSERT INTO survey_intended_outcome (survey_id, intended_outcome_id) SELECT survey_id, (select intended_outcome_id from intended_outcome where name = $$${CONFIG.survey_intended_outcome_2}$$) FROM s${sIndex}
-          ), s${sIndex} AS (INSERT INTO survey_location (survey_id, name, description, geojson, geography) SELECT survey_id, $$${project.herd}$$, $$${project.herd} herd region boundary$$, $$[${feature}]$$, public.geography(public.ST_GeomFromGeoJSON($$${geometry}$$)) FROM s${sIndex})
+          ), sl${sIndex} AS (INSERT INTO survey_location (survey_id, name, description, geojson, geography) SELECT survey_id, $$${project.herd}$$, $$${project.herd} herd region boundary$$, $$[${feature}]$$, public.geography(public.ST_GeomFromGeoJSON($$${geometry}$$)) FROM s${sIndex}
       `;
         for (let dIndex = 0; dIndex < survey.deployments.length; dIndex++) {
           const deployment = survey.deployments[dIndex];
-          sql += `, survey${sIndex}critter${dIndex} AS (INSERT INTO critter (survey_id, critterbase_critter_id) SELECT survey_id, $$${deployment.critter_id}$$ FROM s${sIndex}
+          if (sIndex == project.surveys.length - 1) {
+            sql += `), survey${sIndex}critter${dIndex} AS (INSERT INTO critter (survey_id, critterbase_critter_id) SELECT survey_id, $$${deployment.critter_id}$$ FROM s${sIndex} RETURNING critter_id
+          ) INSERT INTO deployment (critter_id, bctw_deployment_id) SELECT critter_id, $$${deployment.deployment_id}$$ FROM survey${sIndex}critter${dIndex};\n\n`;
+          } else {
+            sql += `), survey${sIndex}critter${dIndex} AS (INSERT INTO critter (survey_id, critterbase_critter_id) SELECT survey_id, $$${deployment.critter_id}$$ FROM s${sIndex} RETURNING critter_id
           ), survey${sIndex}deployment${dIndex} AS (INSERT INTO deployment (critter_id, bctw_deployment_id) SELECT critter_id, $$${deployment.deployment_id}$$ FROM survey${sIndex}critter${dIndex}`;
+          }
         }
       }
     }
 
-    console.log(`BEGIN; ${sql}; COMMIT;`);
+    console.log(`${sql}`);
   } catch (err) {
     console.error(err);
   }
