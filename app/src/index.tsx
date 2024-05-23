e3722404879ba54f537ef684dffeedfcc0173e25
Macgregor Aubertin-Young <macgregor.aubertin-young@gov.bc.ca>
2024-05-23 10:36:56 -0700

    fix app index file

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -2,9 +2,9 @@ import App from 'App';
 import { createRoot } from 'react-dom/client';
 import * as serviceWorker from './serviceWorker';
 
-const container = document.getElementById('root');
-const root = createRoot(container!);
-root.render(<App />);
+//https://react.dev/blog/2022/03/08/react-18-upgrade-guide#updates-to-client-rendering-apis
+
+createRoot(document.getElementById('root')!).render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
