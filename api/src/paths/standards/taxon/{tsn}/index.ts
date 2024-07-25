01232a468ccd454c8ca0e9a88cf1f7ef71cd19dc
Macgregor Aubertin-Young <macgregor.aubertin-young@gov.bc.ca>
2024-07-25 10:28:00 -0700

    replace getApiUserDBConnection with getDBConnection temporarily

--- api/src/paths/standards/taxon/{tsn}/index.ts
+++ api/src/paths/standards/taxon/{tsn}/index.ts
@@ -1,7 +1,7 @@
 import { RequestHandler } from 'express';
 import { Operation } from 'express-openapi';
 import { SYSTEM_ROLE } from '../../../../constants/roles';
-import { getAPIUserDBConnection } from '../../../../database/db';
+import { getDBConnection } from '../../../../database/db';
 import { authorizeRequestHandler } from '../../../../request-handlers/security/authorization';
 import { StandardsService } from '../../../../services/standards-service';
 import { getLogger } from '../../../../utils/logger';
@@ -199,7 +199,9 @@ GET.apiDoc = {
  */
 export function getSpeciesStandards(): RequestHandler {
   return async (req, res) => {
-    const connection = getAPIUserDBConnection();
+    // API user DB connection does not work, possible because user does not exist in Critterbase?
+    // const connection = getAPIUserDBConnection();
+    const connection = getDBConnection(req.keycloak_token);
 
     try {
       const tsn = Number(req.params.tsn);
@@ -215,7 +217,7 @@ export function getSpeciesStandards(): RequestHandler {
       return res.status(200).json(getSpeciesStandardsResponse);
     } catch (error) {
       defaultLog.error({ label: 'getSpeciesStandards', message: 'error', error });
-      connection.rollback()
+      connection.rollback();
       throw error;
     } finally {
       connection.release();
