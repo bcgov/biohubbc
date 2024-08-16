b7885476c47c8a3c47dcdc7db039cbdcb4d26d2c
Andrew <105487051+LouisThedroux@users.noreply.github.com>
2024-08-16 16:00:23 -0700

    initial commit for the set up of stratum.ts. Other errors still apparent from previous sql that need to be fixed first before I can se if this would even run. But I imagine its not even remotely functioning as intended yet so no need to test anyway

---
+++ spi/src/transformations/stratum.ts
@@ -0,0 +1,28 @@
+import SQL from 'sql-template-strings';
+import { IDBConnection } from '../db';
+
+export const transformUsersSql = SQL`
+-------------------------------------------------------------------------------------------------
+-- Moving stratum data from SPI to SIMS to show the stratum that a Design Component is in for a particular survey.
+
+-- This is all the columns in SPI right now. doubt we will need all of them(?) so Ive commented out update date, but this is just a start.
+-------------------------------------------------------------------------------------------------
+    INSERT INTO 
+        biohub.stratum (spi_project_id, survey_id, study_area, design_component_id, stratum_name, create_date, create_id)
+    SELECT 
+        st.spi_project_id,
+        st.survey_id,
+        st.study_area_id,
+        st.design_component_id,
+        st.stratum_name,
+        st.when_created,
+        st.who_created, 
+        -- st.who_updated,
+        -- st.when_updated
+    FROM 
+        public.spi_component_stratums st;
+
+  await connection.sql(sql);`
+
+  console.log('Successfully transformed stratums');
+};
