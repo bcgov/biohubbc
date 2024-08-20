7b3897bf600a4512da8c8efdbbc49b0c5070dd58
Andrew <105487051+LouisThedroux@users.noreply.github.com>
2024-08-20 15:52:38 -0700

    formatting sampling_method.ts

--- spi/src/transformations/sampling_methods.ts
+++ spi/src/transformations/sampling_methods.ts
@@ -6,22 +6,14 @@ export const transformSamplingMethods = async (connection: IDBConnection): Promi
   
     const sql = SQL`
 -------------------------------------------------------------------------------------------------
--- Moving stratum data from SPI to SIMS to show the stratum that a Design Component is in for a particular survey.
-
--- This is all the columns in SPI right now. doubt we will need all of them(?) so Ive commented out update date, but this is just a start.
-
--- Create stratum
+-- Methods is gonna be a tricky one
 -------------------------------------------------------------------------------------------------
     INSERT INTO 
-        biohub.survey_stratum (survey_id, name, description, create_date, update_date)
-    SELECT 
-        s.survey_id,
-        st.stratum_name,
-        st.stratum_description,
-        st.when_created,
-        st.when_updated
+        biohub.target_sampling_site
+    SELECT *
+
     FROM 
-        public.spi_survey_stratums st
+        public.destination_sampling_site
     JOIN 
         biohub.survey s 
     ON 
