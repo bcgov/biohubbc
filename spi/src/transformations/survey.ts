800361a356a2a43787ba01a39e30d615406fd7ab
Nick Phura <nickphura@gmail.com>
2024-08-26 17:18:34 -0700

    WIP - Fixing survey permit issues

--- spi/src/transformations/survey.ts
+++ spi/src/transformations/survey.ts
@@ -60,25 +60,37 @@ export const transformSurveys = async (connection: IDBConnection): Promise<void>
 
 
     -------------------------------------------------------------------------------------------------
-    -- Inserts permits for transformed surveys
+    -- Inserts permits for transformed surveys, if the permit value is a number or comma-separated list of numbers
     -------------------------------------------------------------------------------------------------
     WITH w_surveys AS (
         SELECT
-            s.survey_id,
-            unnest(string_to_array(ss.wildlife_permit_label, ', ')) AS permit_number
+            ss.survey_id,
+            trim(unnest(string_to_array(ss.wildlife_permit_label, ','))) AS permit_number
         FROM
             public.spi_surveys ss
-        JOIN 
+        INNER JOIN 
             biohub.survey s ON ss.survey_id = s.spi_survey_id
         WHERE
-            ss.wildlife_permit_label IS NOT NULL
-        )
+            ss.wildlife_permit_label IS NOT null
+    ), w_valid_permits AS (
+      SELECT 
+        survey_id, 
+        CASE
+            WHEN permit_number ~ '^[0-9]+$' THEN permit_number::integer
+            ELSE NULL
+        END AS permit_number
+      from 
+        w_surveys
+    )
     INSERT INTO biohub.permit (survey_id, number, type)
     SELECT DISTINCT
-        survey_id,
+        survey_id, 
         permit_number,
         'Wildlife Permit - General'
-    FROM w_surveys;
+    FROM 
+        w_valid_permits
+    WHERE 
+        permit_number is not null;
 
     
     -------------------------------------------------------------------------------------------------
