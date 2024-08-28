bfb3a05cd05cbcb512a06effc00e799c9a5ce62c
Meijer <Annika.Meijer@gov.bc.ca>
2024-08-28 16:54:04 -0700

    study areas troubleshooting

--- spi/src/transformations/study-area.ts
+++ spi/src/transformations/study-area.ts
@@ -8,6 +8,29 @@ export const transformStudyAreas = async (connection: IDBConnection): Promise<vo
     -------------------------------------------------------------------------------------------------
     -- Transforms SPI Study Areas into SIMS Survey Locations 
     -------------------------------------------------------------------------------------------------
+    INSERT INTO biohub.survey_location (survey_id, name, description, geography)
+    SELECT 
+        bs.survey_id, 
+        sa.study_area_name, 
+        sa.study_area_description, 
+        igd.geo
+    FROM biohub.survey bs
+    JOIN public.spi_survey_study_areas ssa
+        ON bs.spi_survey_id = ssa.survey_id
+    JOIN public.spi_study_areas sa
+        ON ssa.study_area_id = sa.study_area_id
+    LEFT JOIN public.spi_document_references sdr
+        ON sa.study_area_id = sdr.study_area_id
+    LEFT JOIN public.imported_geo_data igd
+        ON sdr.document_reference_id = igd.spi_document_reference
+    WHERE sa.study_area_id IS NOT NULL;
+-- ran into inflated datasets due to multiple surveys migration
+-- ran into inflated datasets due to multiple unique document references per study area -- and naming repeating identically across these
+-- to test modify these code setting where study area id is equal to 5264
+
+
+
+
 
 -- ITS BROKEN -- FOR SOME REASON IT IS MANY-TO-MANY FRAKING OUT AND IS GOING EXPONENTIAL. PUBLIC.SPI_SURVEY_STUDY_AREAS HAS 7217 RECORDS. THE BELOW CODE IS RTURNING 2 MILLION ROWS. HELP.
 
