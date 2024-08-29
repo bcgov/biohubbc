733be81a2eb363e257ae1ea974a8278a512b6e61
Meijer <Annika.Meijer@gov.bc.ca>
2024-08-29 09:22:55 -0700

    added method lookups used in spi mig, may need to add more for general sims use. TBD.

--- database/src/migrations/20240828000000_method_lookup_values.ts
+++ database/src/migrations/20240828000000_method_lookup_values.ts
@@ -11,8 +11,17 @@ export async function up(knex: Knex): Promise<void> {
   await knex.raw(`--sql
   ----------------------------------------------------------------------------------------
   ----- inserting method lookup names into method_lookup table ---- 
-
-
+    INSERT INTO biohub.method_lookup (create_date, name, description)
+    values (now(), 'Visual Encounter', 'This recording was made through direct visual observation'),
+    (now(), 'Angling','This recording was made through the use of Angling'),
+    (now(), 'Audio Encounter','This recording or observation was made through hearing'),
+    (now(), 'Drone', 'This recording was made through the use of a Drone or UAV'),
+    (now(), 'Gun Net', 'This recording method relied on the use of a Net Gun'), 
+    (now(), 'Hair Snag', 'This recording was facilitated by the implementation of a Hair Snag'), 
+    (now(), 'Radar', 'This recording was made using Radar technology'),
+    (now(), 'Radio', 'This recording was facilitated using Radio technology'), 
+    (now(), 'Trap Net','This recording method relied on the use of a Trap Net'),
+    (now(), 'Undetermined', 'The method used to make this recording has not been determined');
 `);
 }
 
