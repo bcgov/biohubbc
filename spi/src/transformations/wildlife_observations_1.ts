5593e5f0e0760f0fd9512f99c898abcb23a4eb67
Meijer <Annika.Meijer@gov.bc.ca>
2024-09-03 14:29:56 -0700

    ramble on

--- spi/src/transformations/wildlife_observations_1.ts
+++ spi/src/transformations/wildlife_observations_1.ts
@@ -10,9 +10,16 @@ export const transformWildlifeObservations = async (connection: IDBConnection): 
     -- Create Wildlife Observations into sims observations table 
     -------------------------------------------------------------------------------------------------
     INSERT INTO 
-        biohub.survey_observation (survey_id, latitude, longitude, count, cobservation_date, observation_time, create_date, survey_sample_site_id, survey_sample_method_id, survey_sample_period_id, itis_tsn, itis_scientific_name)
+        biohub.survey_observation (survey_id, latitude, longitude, count, observation_date, observation_time, create_date, survey_sample_site_id, survey_sample_method_id, survey_sample_period_id, itis_tsn, itis_scientific_name)
     SELECT
-        s.survey_id
+        s.survey_id,
+        swo.latitude,
+        swo.longitude, 
+        swo.wlo_coun, 
+        -- date/time may need to be retrieved from sampling site visit 
+        swo.when_created
+        -- join tables for ids for period, method, site
+        -- need to drop the not null constraint for itis tsn, then need to insert the swo.taxonimic_unit_id into biohub.study_species.spi_wldtaxonomic_units_id
     FROM 
         public.spi_wildlife_observations swo
     JOIN 
