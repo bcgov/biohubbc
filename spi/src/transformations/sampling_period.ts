420b58affc2ca5bd27483ea82b4016dbec911cbd
Meijer <Annika.Meijer@gov.bc.ca>
2024-09-03 14:00:48 -0700

    changes to sampling period

--- spi/src/transformations/sampling_period.ts
+++ spi/src/transformations/sampling_period.ts
@@ -9,16 +9,17 @@ export const transformSampleVisits = async (connection: IDBConnection): Promise<
     INSERT INTO biohub.survey_sample_period (create_date, survey_sample_method_id, start_date, end_date, start_time, end_time)
     SELECT 
         sdcv.when_created, 
-        ssm.survey_sample_method_id,  -- Retrieved from the joined tables
-        MIN(DATE(sdcv.visit_date)) AS start_date,  
-        MAX(DATE(sdcv.visit_date)) AS end_date,    
-        TO_TIMESTAMP(CASE WHEN LENGTH(sdcv.start_time) = 4 
-            THEN sdcv.start_time ELSE LPAD(sdcv.start_time, 4, '0') END, 'HH24MI')::time AS start_time, 
-        COALESCE(
-            TO_TIMESTAMP(CASE WHEN LENGTH(sdcv.end_time) = 4 
-                THEN sdcv.end_time ELSE LPAD(sdcv.end_time, 4, '0') END, 'HH24MI')::time,
-            MAX(sdcv.visit_date)::time 
-        ) AS end_time
+        ssm.survey_sample_method_id,
+        DATE(sdcv.visit_date) AS start_date,
+        DATE(COALESCE(sdcv.visit_end_date, sdcv.visit_date)) AS end_date,
+        CASE 
+            WHEN sdcv.start_time = '0000' THEN NULL
+            ELSE TO_TIMESTAMP(LPAD(sdcv.start_time, 4, '0'), 'HH24MI')::time
+        END AS start_time,
+        CASE 
+            WHEN sdcv.end_time = '0000' THEN NULL
+            ELSE TO_TIMESTAMP(LPAD(sdcv.end_time, 4, '0'), 'HH24MI')::time
+        END AS end_time
     FROM 
         public.spi_design_component_visits sdcv
     JOIN 
