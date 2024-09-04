e03cdb23b80056af995b679346df16a47fc1e86f
Meijer <Annika.Meijer@gov.bc.ca>
2024-09-04 16:14:34 -0700

    please review possibilty of creating new temp table to use for spi wlo, with period id and visit id

--- spi/src/transformations/sampling_period.ts
+++ spi/src/transformations/sampling_period.ts
@@ -34,6 +34,34 @@ export const transformSampleVisits = async (connection: IDBConnection): Promise<
         sdcv.start_time, 
         sdcv.end_time, 
         ssm.survey_sample_method_id;
+
+    -- possibility to create another temporary table that houses the design component visit id and the survey sample period id
+    -- ensures that we can properly attribute spi_wildlife_observations to the appropriate sampling period to get dates 
+    
+    -- setting up the table -- 
+    CREATE TABLE IF NOT EXISTS public.migrate_spi_sample_period_visit (
+    design_component_visit_id INT,
+    survey_sample_period_id INT,
+    PRIMARY KEY (design_component_visit_id, survey_sample_period_id));
+
+    -- inserting component visit id and survey sample period id into the table -- 
+    INSERT INTO public.migrate_spi_sample_period_visit (design_component_visit_id, survey_sample_period_id)
+    SELECT
+        sdcv.design_component_visit_id,
+        ssp.survey_sample_period_id
+    FROM 
+        public.spi_design_component_visits sdcv
+    JOIN 
+        public.migrate_spi_sample_design_component msd
+        ON sdcv.design_component_id = msd.design_component_id
+    JOIN 
+        biohub.survey_sample_method ssm
+        ON msd.survey_sample_site_id = ssm.survey_sample_site_id
+    JOIN
+        biohub.survey_sample_period ssp
+        ON ssp.survey_sample_method_id = ssm.survey_sample_method_id
+    GROUP BY 
+        sdcv.design_component_visit_id, ssp.survey_sample_period_id;
     `;
 
   await connection.sql(transformSampleVisitsSql);
