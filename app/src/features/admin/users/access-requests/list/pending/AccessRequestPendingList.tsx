e1a31b898055c6af255d3fbb2501fc596a62de8a
Macgregor Aubertin-Young <108430771+mauberti-bc@users.noreply.github.com>
2024-09-05 13:48:59 -0700

    View past access requests in Manage User table (#1346)
    * update access request table & view actioned requests
    
    * update frontend tests & add jsdocs
    
    * frontend test is failing
    
    * fix: dropped test from ActiveUserList - table is larger then testing container, columns invisible
    
    ---------
    
    Co-authored-by: Mac Deluca <99926243+MacQSL@users.noreply.github.com>
    Co-authored-by: Mac Deluca <Mac.Deluca@quartech.com>

--- app/src/features/admin/users/access-requests/list/pending/AccessRequestPendingList.tsx
+++ app/src/features/admin/users/access-requests/list/pending/AccessRequestPendingList.tsx
@@ -1,88 +1,60 @@
-import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Divider from '@mui/material/Divider';
-import Paper from '@mui/material/Paper';
-import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { GridColDef } from '@mui/x-data-grid';
-import { AccessStatusChip } from 'components/chips/AccessStatusChip';
+import ColouredRectangleChip from 'components/chips/ColouredRectangleChip';
 import { StyledDataGrid } from 'components/data-grid/StyledDataGrid';
 import RequestDialog from 'components/dialog/RequestDialog';
+import { getAccessRequestStatusColour } from 'constants/colours';
 import { DATE_FORMAT } from 'constants/dateTimeFormats';
-import { AccessApprovalDispatchI18N, AccessDenialDispatchI18N, ReviewAccessRequestI18N } from 'constants/i18n';
-import { AdministrativeActivityStatusType } from 'constants/misc';
-import { DialogContext } from 'contexts/dialogContext';
+import { ReviewAccessRequestI18N } from 'constants/i18n';
+import { DialogContext, ISnackbarProps } from 'contexts/dialogContext';
+import dayjs from 'dayjs';
 import { APIError } from 'hooks/api/useAxios';
 import { useBiohubApi } from 'hooks/useBioHubApi';
 import { IGetAccessRequestsListResponse } from 'interfaces/useAdminApi.interface';
 import { IGetAllCodeSetsResponse } from 'interfaces/useCodesApi.interface';
 import { useContext, useState } from 'react';
-import { getFormattedDate } from 'utils/Utils';
 import ReviewAccessRequestForm, {
   IReviewAccessRequestForm,
   ReviewAccessRequestFormInitialValues,
   ReviewAccessRequestFormYupSchema
-} from './ReviewAccessRequestForm';
+} from '../../components/ReviewAccessRequestForm';
 
-export interface IAccessRequestListProps {
+interface IAccessRequestPendingListProps {
   accessRequests: IGetAccessRequestsListResponse[];
   codes: IGetAllCodeSetsResponse;
   refresh: () => void;
 }
 
-const pageSizeOptions = [10, 25, 50];
-
 /**
- * Page to display a list of user access.
+ * Returns a data grid component displaying pending access requests
  *
+ * @param props {IAccessRequestPendingListProps}
+ * @returns
  */
-const AccessRequestList = (props: IAccessRequestListProps) => {
+const AccessRequestPendingList = (props: IAccessRequestPendingListProps) => {
   const { accessRequests, codes, refresh } = props;
 
   const biohubApi = useBiohubApi();
+  const dialogContext = useContext(DialogContext);
 
   const [showReviewDialog, setShowReviewDialog] = useState<boolean>(false);
   const [activeReview, setActiveReview] = useState<IGetAccessRequestsListResponse | null>(null);
 
-  const dialogContext = useContext(DialogContext);
-
-  const defaultErrorDialogProps = {
-    dialogTitle: ReviewAccessRequestI18N.reviewErrorTitle,
-    dialogText: ReviewAccessRequestI18N.reviewErrorText,
-    open: false,
-    onClose: () => {
-      dialogContext.setErrorDialog({ open: false });
-    },
-    onOk: () => {
-      dialogContext.setErrorDialog({ open: false });
-    }
-  };
-
-  const dispatchApprovalErrorDialogProps = {
-    dialogTitle: AccessApprovalDispatchI18N.reviewErrorTitle,
-    dialogText: AccessApprovalDispatchI18N.reviewErrorText,
-    open: false,
-    onClose: () => {
-      dialogContext.setErrorDialog({ open: false });
-    },
-    onOk: () => {
-      dialogContext.setErrorDialog({ open: false });
-    }
-  };
-
-  const dispatchDenialErrorDialogProps = {
-    dialogTitle: AccessDenialDispatchI18N.reviewErrorTitle,
-    dialogText: AccessDenialDispatchI18N.reviewErrorText,
-    open: false,
-    onClose: () => {
-      dialogContext.setErrorDialog({ open: false });
-    },
-    onOk: () => {
-      dialogContext.setErrorDialog({ open: false });
-    }
+  const showSnackBar = (textDialogProps?: Partial<ISnackbarProps>) => {
+    dialogContext.setSnackbar({ ...textDialogProps, open: true });
   };
 
   const accessRequestsColumnDefs: GridColDef<IGetAccessRequestsListResponse>[] = [
+    {
+      field: 'display_name',
+      headerName: 'Display Name',
+      flex: 1,
+      disableColumnMenu: true,
+      valueGetter: (params) => {
+        return params.row.data?.displayName;
+      }
+    },
     {
       field: 'username',
       headerName: 'Username',
@@ -98,7 +70,7 @@ const AccessRequestList = (props: IAccessRequestListProps) => {
       headerName: 'Date of Request',
       disableColumnMenu: true,
       valueFormatter: (params) => {
-        return getFormattedDate(DATE_FORMAT.ShortMediumDateFormat, params.value);
+        return dayjs(params.value).format(DATE_FORMAT.ShortMediumDateTimeFormat);
       }
     },
     {
@@ -107,7 +79,12 @@ const AccessRequestList = (props: IAccessRequestListProps) => {
       headerName: 'Status',
       disableColumnMenu: true,
       renderCell: (params) => {
-        return <AccessStatusChip status={params.row.status_name} />;
+        return (
+          <ColouredRectangleChip
+            label={params.row.status_name}
+            colour={getAccessRequestStatusColour(params.row.status_name as 'Pending')}
+          />
+        );
       }
     },
     {
@@ -119,25 +96,31 @@ const AccessRequestList = (props: IAccessRequestListProps) => {
       disableColumnMenu: true,
       resizable: false,
       align: 'right',
-      renderCell: (params) => {
-        if (params.row.status_name !== AdministrativeActivityStatusType.PENDING) {
-          return <></>;
-        }
-
-        return (
-          <Button
-            color="primary"
-            variant="contained"
-            onClick={() => {
-              setActiveReview(params.row);
-              setShowReviewDialog(true);
-            }}>
-            Review Request
-          </Button>
-        );
-      }
+      renderCell: (params) => (
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={() => {
+            setActiveReview(params.row);
+            setShowReviewDialog(true);
+          }}>
+          Review Request
+        </Button>
+      )
     }
   ];
+
+  const defaultErrorDialogProps = {
+    dialogTitle: ReviewAccessRequestI18N.reviewErrorTitle,
+    dialogText: ReviewAccessRequestI18N.reviewErrorText,
+    open: false,
+    onClose: () => {
+      dialogContext.setErrorDialog({ open: false });
+    },
+    onOk: () => {
+      dialogContext.setErrorDialog({ open: false });
+    }
+  };
 
   const handleReviewDialogApprove = async (values: IReviewAccessRequestForm) => {
     if (!activeReview) {
@@ -156,6 +139,14 @@ const AccessRequestList = (props: IAccessRequestListProps) => {
         roleIds: (values.system_role && [values.system_role]) || []
       });
 
+      showSnackBar({
+        snackbarMessage: (
+          <Typography variant="body2" component="div">
+            Approved access request
+          </Typography>
+        )
+      });
+
       try {
         await biohubApi.admin.sendGCNotification(
           {
@@ -166,16 +157,18 @@ const AccessRequestList = (props: IAccessRequestListProps) => {
           {
             subject: 'SIMS: Your request for access has been approved.',
             header: 'Your request for access to the Species Inventory Management System has been approved.',
-            main_body1: 'This is an automated message from the BioHub Species Inventory Management System',
+            main_body1: 'This is an automated message from the Species Inventory Management System',
             main_body2: '',
             footer: ''
           }
         );
       } catch (error) {
-        dialogContext.setErrorDialog({
-          ...dispatchApprovalErrorDialogProps,
-          open: true,
-          dialogErrorDetails: (error as APIError).errors
+        showSnackBar({
+          snackbarMessage: (
+            <Typography variant="body2" component="div">
+              Approved access request, but failed to send notification
+            </Typography>
+          )
         });
       } finally {
         refresh();
@@ -199,6 +192,14 @@ const AccessRequestList = (props: IAccessRequestListProps) => {
     try {
       await biohubApi.admin.denyAccessRequest(activeReview.id);
 
+      showSnackBar({
+        snackbarMessage: (
+          <Typography variant="body2" component="div">
+            Approved access request
+          </Typography>
+        )
+      });
+
       try {
         await biohubApi.admin.sendGCNotification(
           {
@@ -209,16 +210,18 @@ const AccessRequestList = (props: IAccessRequestListProps) => {
           {
             subject: 'SIMS: Your request for access has been denied.',
             header: 'Your request for access to the Species Inventory Management System has been denied.',
-            main_body1: 'This is an automated message from the BioHub Species Inventory Management System',
+            main_body1: 'This is an automated message from the Species Inventory Management System',
             main_body2: '',
             footer: ''
           }
         );
       } catch (error) {
-        dialogContext.setErrorDialog({
-          ...dispatchDenialErrorDialogProps,
-          open: true,
-          dialogErrorDetails: (error as APIError).errors
+        showSnackBar({
+          snackbarMessage: (
+            <Typography variant="body2" component="div">
+              Denied access request, but failed to send notification
+            </Typography>
+          )
         });
       } finally {
         refresh();
@@ -246,51 +249,28 @@ const AccessRequestList = (props: IAccessRequestListProps) => {
           element: activeReview ? (
             <ReviewAccessRequestForm
               request={activeReview}
-              system_roles={
-                codes?.system_roles?.map((item) => {
-                  return { value: item.id, label: item.name };
-                }) || []
-              }
+              system_roles={codes?.system_roles?.map((item: any) => ({ value: item.id, label: item.name })) || []}
             />
           ) : (
             <></>
           )
         }}
       />
-      <Paper>
-        <Toolbar>
-          <Typography variant="h4" component="h2">
-            Access Requests{' '}
-            <Typography
-              component="span"
-              variant="inherit"
-              color="textSecondary"
-              sx={{
-                fontWeight: 400
-              }}>
-              ({Number(accessRequests?.length ?? 0).toLocaleString()})
-            </Typography>
-          </Typography>
-        </Toolbar>
-        <Divider></Divider>
-        <Box p={2}>
-          <StyledDataGrid
-            columns={accessRequestsColumnDefs}
-            rows={accessRequests}
-            noRowsMessage="No Access Requests"
-            pageSizeOptions={pageSizeOptions}
-            initialState={{
-              pagination: {
-                paginationModel: {
-                  pageSize: 10
-                }
-              }
-            }}
-          />
-        </Box>
-      </Paper>
+      <StyledDataGrid
+        columns={accessRequestsColumnDefs}
+        rows={accessRequests}
+        noRowsMessage="No Pending Access Requests"
+        pageSizeOptions={[10, 25, 50]}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 10
+            }
+          }
+        }}
+      />
     </>
   );
 };
 
-export default AccessRequestList;
+export default AccessRequestPendingList;
