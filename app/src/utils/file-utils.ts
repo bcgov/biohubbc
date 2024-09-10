407efe1e1ea0dd17bb8a29aef3b8837ee02abd0f
Mac Deluca <99926243+MacQSL@users.noreply.github.com>
2024-09-09 22:49:55 -0700

    SIMSBIOHUB-587 UI: Multi CSV Upload Page (#1357)
    - UI components for multi CSV import (Captures / Measurements / Markings)

---
+++ app/src/utils/file-utils.ts
@@ -0,0 +1,24 @@
+/**
+ * Get CSV template from a list of column headers.
+ *
+ * @param {string[]} headers - CSV column headers
+ * @returns {string} Encoded CSV template
+ */
+export const getCSVTemplate = (headers: string[]) => {
+  return 'data:text/csv;charset=utf-8,' + headers.join(',') + '\n';
+};
+
+/**
+ * Download a file client side.
+ *
+ * @param {string} fileContents - String representing the file contents
+ * @param {string} fileName - The name of the file to download
+ */
+export const downloadFile = (fileContents: string, fileName: string) => {
+  const encodedUri = encodeURI(fileContents);
+  const link = document.createElement('a');
+  link.setAttribute('href', encodedUri);
+  link.setAttribute('download', fileName);
+  document.body.appendChild(link);
+  link.click();
+};
