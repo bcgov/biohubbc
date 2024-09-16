8699ec698cffb3a9832da67e53ad1c51c102f9e5
Meijer <Annika.Meijer@gov.bc.ca>
2024-09-16 14:17:12 -0700

    proj test

--- spi/src/transformations/project.ts
+++ spi/src/transformations/project.ts
@@ -95,28 +95,7 @@ export const transformProjects = async (connection: IDBConnection): Promise<void
                 )
             )   
             RETURNING project_id, system_user_id, project_role_id, create_user
-                ),
-         w_assign_coordinator AS
-        (
-                INSERT INTO biohub.project_participation
-                (project_id, system_user_id, project_role_id, create_user)
-            SELECT
-                b.project_id,
-                (SELECT system_user_id FROM biohub.system_user WHERE user_identifier = 'spi') AS system_user_id,
-                (SELECT project_role_id FROM biohub.project_role WHERE name = 'Coordinator') AS project_role_id,
-                (SELECT system_user_id FROM biohub.system_user WHERE user_identifier = 'spi') AS create_user
-            FROM 
-                biohub.project b
-            WHERE NOT EXISTS (
-                SELECT 1
-                FROM biohub.project_participation pp
-                JOIN biohub.project_role pr 
-                    ON pp.project_role_id = pr.project_role_id
-                WHERE b.project_id = pp.project_id
-                AND pr.name = 'Coordinator'
-            )
-            AND b.spi_project_id IS NOT NULL
-        )
+                )
     SELECT * FROM w_insert_participation;
     SELECT * FROM w_assign_coordinator;
   `;
