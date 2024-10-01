8ea5b0e3912e19eb604e34888d45a751ce5ce821
Nick Phura <nickphura@gmail.com>
2024-10-01 12:31:26 -0700

    Tweak logger settings

--- api/src/utils/logger.test.ts
+++ api/src/utils/logger.test.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 import { describe } from 'mocha';
-import { getLogger, setLogLevel, setLogLevelFile } from './logger';
+import { getLogger, setLogLevel } from './logger';
 
 describe('logger', () => {
   describe('getLogger', () => {
@@ -30,46 +30,16 @@ describe('logger', () => {
       //const myLogger1 = require('./logger').getLogger('myLoggerA');
       const myLogger1 = getLogger('myLoggerA');
 
-      expect(myLogger1.transports[1].level).to.equal('info');
+      expect(myLogger1.transports[0].level).to.equal('info');
 
       setLogLevel('debug');
 
       const myLogger2 = getLogger('myLoggerA');
-      expect(myLogger2.transports[1].level).to.equal('debug');
+      expect(myLogger2.transports[0].level).to.equal('debug');
 
       const myNewLogger3 = getLogger('myNewLoggerA');
 
-      expect(myNewLogger3.transports[1].level).to.equal('debug');
-    });
-  });
-
-  describe('setLogLevelFile', () => {
-    let currentLogLevelFile: string | undefined;
-
-    beforeEach(() => {
-      currentLogLevelFile = process.env.LOG_LEVEL_FILE;
-
-      // Set initial log level file value
-      process.env.LOG_LEVEL_FILE = 'warn';
-    });
-
-    afterEach(() => {
-      // Restore the original log level
-      process.env.LOG_LEVEL_FILE = currentLogLevelFile;
-    });
-
-    it('sets the log level for the file transport', () => {
-      const myLogger4 = getLogger('myLoggerB');
-      expect(myLogger4.transports[0].level).to.equal('warn');
-
-      setLogLevelFile('error');
-
-      const myLogger5 = getLogger('myLoggerB');
-      expect(myLogger5.transports[0].level).to.equal('error');
-
-      const myNewLogger6 = getLogger('myNewLoggerB');
-
-      expect(myNewLogger6.transports[0].level).to.equal('error');
+      expect(myNewLogger3.transports[0].level).to.equal('debug');
     });
   });
 });
