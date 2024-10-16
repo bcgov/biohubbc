395c8495090e7202a5b6bbad428e0068e09414e1
Meijer <Annika.Meijer@gov.bc.ca>
2024-10-16 10:59:18 -0700

    survey job updates

--- database/src/migrations/20241012021000_survey_jobs.ts
+++ database/src/migrations/20241012021000_survey_jobs.ts
@@ -9,13 +9,38 @@ import { Knex } from 'knex';
  */
 export async function up(knex: Knex): Promise<void> {
   await knex.raw(`--sql
+    
+    SET SEARCH_PATH=biohub
+    
     ----------------------------------------------------------------------------------------
     -- Adding survey participant roles
     ----------------------------------------------------------------------------------------
-        INSERT INTO survey_job (name, record_effective_date, description)
+        INSERT INTO biohub.survey_job (name, record_effective_date, description)
         VALUES
           ('Crew lead', NOW(), 'A participant of a survey in a crew lead role.'),
           ('Crew member', NOW(), 'A participant of a survey in a crew member role.');
+
+    ----------------------------------------------------------------------------------------
+    -- Reassigning sims biologist roles to crew member
+    ----------------------------------------------------------------------------------------
+      
+        UPDATE biohub.survey_participation sp
+        SET sp.survey_job_id = (
+            SELECT sj.survey_job_id 
+            FROM biohub.survey_job sj
+            WHERE sj.name = 'Crew Member'
+        )
+        WHERE sp.survey_job_id = (
+            SELECT sj.survey_job_id 
+            FROM biohub.survey_job sj
+            WHERE sj.name = 'Biologist'
+        );
+
+    ----------------------------------------------------------------------------------------
+    -- deleting sime biologist role 
+    ----------------------------------------------------------------------------------------
+          DELETE FROM biohub.survey_job
+          WHERE name = 'Biologist';
 
   `);
 }
