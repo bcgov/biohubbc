a0cb45826283de2410c304318ff7c5ec10a9ee28
Mac Deluca <Mac.Deluca@quartech.com>
2024-10-17 14:13:57 -0700

    feat: added repo methods

---
+++ api/src/repositories/telemetry-repositories/telemetry-manual-repository.interface.ts
@@ -0,0 +1,18 @@
+import { TelemetryManualRecord } from '../../database-models/telemetry_manual';
+
+/**
+ * Interface reflecting the telemetry manual data required to create a new manual telemetry record.
+ *
+ */
+export type CreateManualTelemetry = Pick<
+  TelemetryManualRecord,
+  'deployment2_id' | 'latitude' | 'longitude' | 'acquisition_date' | 'transmission_date'
+>;
+
+/**
+ * Interface reflecting the telemetry manual data required to update an existing manual telemetry record.
+ *
+ */
+export type UpdateManualTelemetry = Partial<
+  Pick<TelemetryManualRecord, 'latitude' | 'longitude' | 'acquisition_date' | 'transmission_date'>
+>;
--- api/src/repositories/telemetry-repositories/telemetry-manual-repository.ts
+++ api/src/repositories/telemetry-repositories/telemetry-manual-repository.ts
@@ -1,4 +1,8 @@
+import { z } from 'zod';
+import { TelemetryManualRecord } from '../../database-models/telemetry_manual';
+import { getKnex } from '../../database/db';
 import { BaseRepository } from '../base-repository';
+import { CreateManualTelemetry, UpdateManualTelemetry } from './telemetry-manual-repository.interface';
 
 /**
  * A repository class for working with Manual telemetry data.
@@ -8,7 +12,77 @@ import { BaseRepository } from '../base-repository';
  * @extends {BaseRepository}
  */
 export class TelemetryManualRepository extends BaseRepository {
-  async createManualTelemetry(surveyId: number) {}
-  async updateManualTelemetry(surveyId: number) {}
-  async deleteManualTelemetry(surveyId: number) {}
+  /**
+   * Bulk create manual telemetry records.
+   *
+   * @param {number} surveyId
+   * @param {CreateManualTelemetry[]} telemetry - List of manual telemetry data
+   * @returns {Promise<TelemetryManualRecord[]>}
+   */
+  async bulkCreateManualTelemetry(
+    surveyId: number,
+    telemetry: CreateManualTelemetry[]
+  ): Promise<TelemetryManualRecord[]> {
+    const knex = getKnex();
+
+    const queryBuilder = knex
+      .insert(telemetry)
+      .into('telemetry_manual')
+      .join('deployment2', 'telemetry_manual.deployment2_id', 'deployment2.deployment2_id')
+      .where('deployment2.survey_id', surveyId);
+
+    const response = await this.connection.knex(queryBuilder, TelemetryManualRecord);
+
+    return response.rows;
+  }
+
+  /**
+   * Bulk update manual telemetry records.
+   *
+   * @param {number} surveyId
+   * @param {CreateManualTelemetry[]} telemetry - List of Manual telemetry data
+   * @returns {Promise<TelemetryManualRecord[]>}
+   */
+  async bulkUpdateManualTelemetry(
+    surveyId: number,
+    telemetry: UpdateManualTelemetry[]
+  ): Promise<TelemetryManualRecord[]> {
+    const knex = getKnex();
+
+    const queryBuilder = knex
+      .insert(telemetry)
+      .update('telemetry_manual')
+      .join('deployment2', 'telemetry_manual.deployment2_id', 'deployment2.deployment2_id')
+      .where('deployment2.survey_id', surveyId);
+
+    const response = await this.connection.knex(queryBuilder, TelemetryManualRecord);
+
+    return response.rows;
+  }
+
+  /**
+   * Bulk update manual telemetry records.
+   *
+   * @param {number} surveyId
+   * @param {CreateManualTelemetry} telemetryManualIds - List of manual telemetry IDs
+   * @returns {Promise<TelemetryManualRecord[]>}
+   */
+  async bulkDeleteManualTelemetry(
+    surveyId: number,
+    telemetryManualIds: number
+  ): Promise<{ telemetry_manual_id: number }[]> {
+    const knex = getKnex();
+
+    const queryBuilder = knex
+      .delete()
+      .from('telemetry_manual')
+      .join('deployment2', 'telemetry_manual.deployment2_id', 'deployment2.deployment2_id')
+      .where('telemetry_manual_id', telemetryManualIds)
+      .andWhere('deployment2.survey_id', surveyId)
+      .returning('telemetry_manual_id');
+
+    const response = await this.connection.knex(queryBuilder, z.object({ telemetry_manual_id: z.number() }));
+
+    return response.rows;
+  }
 }
