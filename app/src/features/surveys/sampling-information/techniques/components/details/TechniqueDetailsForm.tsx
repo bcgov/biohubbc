cf7922d4694d96782c190ad148a9c54c981e7c2c
Andrew <105487051+LouisThedroux@users.noreply.github.com>
2024-11-06 15:22:42 -0800

    initial tooltip for setection distance

--- app/src/features/surveys/sampling-information/techniques/components/details/TechniqueDetailsForm.tsx
+++ app/src/features/surveys/sampling-information/techniques/components/details/TechniqueDetailsForm.tsx
@@ -1,5 +1,7 @@
+import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import HelpButtonTooltip from 'components/buttons/HelpButtonTooltip';
 import CustomTextField from 'components/fields/CustomTextField';
 
 /**
@@ -11,7 +13,15 @@ export const TechniqueDetailsForm = () => {
   return (
     <Grid container spacing={3}>
       <Grid item xs={12}>
-        <Typography component="legend">Detection distance (optional)</Typography>
+        <Box display="flex" alignItems="center">
+          <Typography component="legend" sx={{ mr: 1 }}>
+            Detection distance (optional)
+          </Typography>
+          <HelpButtonTooltip content="The predefined limit set by the observer within which data is collected, regardless of actual visibility or other external conditions. It represents a methodological boundary rather than a measure of physical sight range." />
+        </Box>
+      </Grid>
+
+      <Grid item xs={12}>
         <CustomTextField
           name="distance_threshold"
           label="Maximum detection distance (metres)"
