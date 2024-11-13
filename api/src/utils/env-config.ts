943add109fea05c1cc313715dab9b0fff2c112f0
Mac Deluca <Mac.Deluca@quartech.com>
2024-11-13 09:19:18 -0800

    feat: made feature flag var optional + updated tests

--- api/src/utils/env-config.ts
+++ api/src/utils/env-config.ts
@@ -3,46 +3,46 @@ import { getLogger } from './logger';
 
 const defaultLog = getLogger('src/utils/env-config.ts');
 
-// Custom Zod string type for environment variables ie: HOST=' ' || HOST='' === 'Required'
-const ZodENVString = z.string().trim().min(1, { message: 'Required' });
+// Custom Zod string type for environment variables ie: '' or ' ' are invalid
+const ZodEnvString = z.string().trim().min(1, { message: 'Required' });
 
 // Schema for environment configuration
-export const ENVSchema = z.object({
+export const EnvSchema = z.object({
   // Environment
   NODE_ENV: z.enum(['development', 'test', 'production']),
-  NODE_OPTIONS: ZodENVString,
+  NODE_OPTIONS: ZodEnvString,
   TZ: z.literal('America/Vancouver'),
 
   // API server
-  API_HOST: ZodENVString,
+  API_HOST: ZodEnvString,
   API_PORT: z.coerce.number(),
 
   // Database
-  DB_HOST: ZodENVString,
+  DB_HOST: ZodEnvString,
   DB_PORT: z.coerce.number(),
-  DB_USER_API: ZodENVString,
-  DB_USER_API_PASS: ZodENVString,
-  DB_DATABASE: ZodENVString,
+  DB_USER_API: ZodEnvString,
+  DB_USER_API_PASS: ZodEnvString,
+  DB_DATABASE: ZodEnvString,
 
   // Keycloak
-  KEYCLOAK_HOST: ZodENVString,
-  KEYCLOAK_REALM: ZodENVString,
-  KEYCLOAK_ADMIN_USERNAME: ZodENVString,
-  KEYCLOAK_ADMIN_PASSWORD: ZodENVString,
-  KEYCLOAK_API_TOKEN_URL: ZodENVString,
-  KEYCLOAK_API_CLIENT_ID: ZodENVString,
-  KEYCLOAK_API_CLIENT_SECRET: ZodENVString,
-  KEYCLOAK_API_HOST: ZodENVString,
-  KEYCLOAK_API_ENVIRONMENT: ZodENVString,
+  KEYCLOAK_HOST: ZodEnvString,
+  KEYCLOAK_REALM: ZodEnvString,
+  KEYCLOAK_ADMIN_USERNAME: ZodEnvString,
+  KEYCLOAK_ADMIN_PASSWORD: ZodEnvString,
+  KEYCLOAK_API_TOKEN_URL: ZodEnvString,
+  KEYCLOAK_API_CLIENT_ID: ZodEnvString,
+  KEYCLOAK_API_CLIENT_SECRET: ZodEnvString,
+  KEYCLOAK_API_HOST: ZodEnvString,
+  KEYCLOAK_API_ENVIRONMENT: ZodEnvString,
 
   // Logging
   LOG_LEVEL: z.enum(['silent', 'error', 'warn', 'info', 'debug', 'silly']),
   LOG_LEVEL_FILE: z.enum(['silent', 'error', 'warn', 'info', 'debug', 'silly']),
-  LOG_FILE_DIR: ZodENVString,
-  LOG_FILE_NAME: ZodENVString,
-  LOG_FILE_DATE_PATTERN: ZodENVString,
-  LOG_FILE_MAX_SIZE: ZodENVString,
-  LOG_FILE_MAX_FILES: ZodENVString,
+  LOG_FILE_DIR: ZodEnvString,
+  LOG_FILE_NAME: ZodEnvString,
+  LOG_FILE_DATE_PATTERN: ZodEnvString,
+  LOG_FILE_MAX_SIZE: ZodEnvString,
+  LOG_FILE_MAX_FILES: ZodEnvString,
 
   // Validation
   API_RESPONSE_VALIDATION_ENABLED: z.enum(['true', 'false']),
@@ -54,51 +54,51 @@ export const ENVSchema = z.object({
   MAX_UPLOAD_FILE_SIZE: z.coerce.number(),
 
   // External Services
-  CB_API_HOST: ZodENVString,
-  APP_HOST: ZodENVString,
+  CB_API_HOST: ZodEnvString,
+  APP_HOST: ZodEnvString,
 
   // Biohub
-  BACKBONE_INTERNAL_API_HOST: ZodENVString,
-  BACKBONE_PUBLIC_API_HOST: ZodENVString,
-  BACKBONE_INTAKE_PATH: ZodENVString,
-  BACKBONE_ARTIFACT_INTAKE_PATH: ZodENVString,
-  BIOHUB_TAXON_PATH: ZodENVString,
-  BIOHUB_TAXON_TSN_PATH: ZodENVString,
+  BACKBONE_INTERNAL_API_HOST: ZodEnvString,
+  BACKBONE_PUBLIC_API_HOST: ZodEnvString,
+  BACKBONE_INTAKE_PATH: ZodEnvString,
+  BACKBONE_ARTIFACT_INTAKE_PATH: ZodEnvString,
+  BIOHUB_TAXON_PATH: ZodEnvString,
+  BIOHUB_TAXON_TSN_PATH: ZodEnvString,
 
   // Object Store
-  OBJECT_STORE_URL: ZodENVString,
-  OBJECT_STORE_ACCESS_KEY_ID: ZodENVString,
-  OBJECT_STORE_SECRET_KEY_ID: ZodENVString,
-  OBJECT_STORE_BUCKET_NAME: ZodENVString,
-  S3_KEY_PREFIX: ZodENVString,
+  OBJECT_STORE_URL: ZodEnvString,
+  OBJECT_STORE_ACCESS_KEY_ID: ZodEnvString,
+  OBJECT_STORE_SECRET_KEY_ID: ZodEnvString,
+  OBJECT_STORE_BUCKET_NAME: ZodEnvString,
+  S3_KEY_PREFIX: ZodEnvString,
 
   // GCNotify
-  GCNOTIFY_SECRET_API_KEY: ZodENVString,
-  GCNOTIFY_ADMIN_EMAIL: ZodENVString,
+  GCNOTIFY_SECRET_API_KEY: ZodEnvString,
+  GCNOTIFY_ADMIN_EMAIL: ZodEnvString,
   GCNOTIFY_ONBOARDING_REQUEST_EMAIL_TEMPLATE: z.string().uuid(),
   GCNOTIFY_ONBOARDING_REQUEST_SMS_TEMPLATE: z.string().uuid(),
   GCNOTIFY_REQUEST_RESUBMIT_TEMPLATE: z.string().uuid(),
-  GCNOTIFY_EMAIL_URL: ZodENVString,
-  GCNOTIFY_SMS_URL: ZodENVString,
+  GCNOTIFY_EMAIL_URL: ZodEnvString,
+  GCNOTIFY_SMS_URL: ZodEnvString,
 
   // ClamAV
   CLAMAV_PORT: z.coerce.number(),
-  CLAMAV_HOST: ZodENVString,
+  CLAMAV_HOST: ZodEnvString,
   ENABLE_FILE_VIRUS_SCAN: z.enum(['true', 'false']),
 
   // Extra
-  FEATURE_FLAGS: ZodENVString // flagA,flagB,flagC
+  FEATURE_FLAGS: ZodEnvString.optional() // flagA,flagB,flagC
 });
 
-type ENVSchema = z.infer<typeof ENVSchema>;
+type Env = z.infer<typeof EnvSchema>;
 
 /**
  * Load Environment Variables and validate them against the Zod schema.
  *
- * @returns {*} {void}
+ * @returns {*} {Env} Validated environment variables
  */
-export const loadEvironmentVariables = (): void => {
-  const parsed = ENVSchema.safeParse(process.env);
+export const loadEvironmentVariables = (): Env => {
+  const parsed = EnvSchema.safeParse(process.env);
 
   if (!parsed.success) {
     defaultLog.error({
@@ -109,12 +109,14 @@ export const loadEvironmentVariables = (): void => {
 
     process.exit(1);
   }
+
+  return parsed.data;
 };
 
-// Extend NodeJS ProcessEnv to include the ENVSchema
+// Extend NodeJS ProcessEnv to include the EnvSchema
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace NodeJS {
-    interface ProcessEnv extends ENVSchema {}
+    interface ProcessEnv extends Env {}
   }
 }
