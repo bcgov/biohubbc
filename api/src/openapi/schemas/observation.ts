462b432ed2c0672abd66c9a8ca1feedeca594aa4
Macgregor Aubertin-Young <108430771+mauberti-bc@users.noreply.github.com>
2024-11-12 16:41:11 -0800

    SIMSBIOHUB-629: Paginate Sampling Sites & Separate Spatial Data in API Responses (#1393)
    * Separate spatial and non-spatial sampling site endpoints
    * Add new sampling data grid cells to observations table
    * Update sampling grid controls to reset one-another, and pre-populate dropdowns
    - Add placeholder text to taxon/site search cell
    - Fix loading animation not stopping when selecting or cancelling search
    - Fix taxon/site cell adding the selection to the list (and creating duplicate entries in the dropdown)
    
    ---------
    
    Co-authored-by: Nick Phura <nickphura@gmail.com>
    Co-authored-by: Mac Deluca <99926243+MacQSL@users.noreply.github.com>

--- api/src/openapi/schemas/observation.ts
+++ api/src/openapi/schemas/observation.ts
@@ -1,5 +1,6 @@
 import { OpenAPIV3 } from 'openapi-types';
 import { paginationResponseSchema } from './pagination';
+import { SampleLocationSchema } from './sample-site';
 
 export const observervationsWithSubcountDataSchema: OpenAPIV3.SchemaObject = {
   type: 'object',
@@ -225,7 +226,8 @@ export const observervationsWithSubcountDataSchema: OpenAPIV3.SchemaObject = {
         'qualitative_measurements',
         'quantitative_measurements',
         'qualitative_environments',
-        'quantitative_environments'
+        'quantitative_environments',
+        'sample_sites'
       ],
       properties: {
         observationCount: {
@@ -412,7 +414,8 @@ export const observervationsWithSubcountDataSchema: OpenAPIV3.SchemaObject = {
               }
             }
           }
-        }
+        },
+        sample_sites: SampleLocationSchema
       }
     },
     pagination: { ...paginationResponseSchema }
