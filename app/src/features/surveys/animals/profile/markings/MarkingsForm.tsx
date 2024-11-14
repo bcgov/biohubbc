315f36ddc06e6cc283ef585bfe39a6b051590767
Meijer <Annika.Meijer@gov.bc.ca>
2024-11-14 13:51:40 -0800

    captures tooltipping

--- app/src/features/surveys/animals/profile/markings/MarkingsForm.tsx
+++ app/src/features/surveys/animals/profile/markings/MarkingsForm.tsx
@@ -16,6 +16,7 @@ import { IMarkings } from 'interfaces/useCritterApi.interface';
 import { useEffect, useState } from 'react';
 import { TransitionGroup } from 'react-transition-group';
 import { MarkingCard } from './MarkingCard';
+import HelpButtonStack from 'components/buttons/HelpButtonStack';
 
 /**
  * Returns the control for applying markings to an animal on the animal form
@@ -162,6 +163,7 @@ export const MarkingsForm = <FormikValuesType extends IMarkings>() => {
               ))}
             </TransitionGroup>
           </Stack>
+          <HelpButtonStack helpText='Please note that markings in SIMS do not include telemetry collars. Use the "Telemetry" page of your survey to deploy your device on your animal and associate it to this capture event.'>
           <Button
             sx={{ mt: 2 }}
             color="primary"
@@ -171,6 +173,7 @@ export const MarkingsForm = <FormikValuesType extends IMarkings>() => {
             onClick={() => setIsDialogOpen(true)}>
             Add Marking
           </Button>
+          </HelpButtonStack>
         </>
       )}
     />
