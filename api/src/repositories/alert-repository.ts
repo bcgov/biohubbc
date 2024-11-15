3b86c705f417e7ec3f8bd0ab72af9798d09e6605
Macgregor Aubertin-Young <108430771+mauberti-bc@users.noreply.github.com>
2024-11-15 09:11:41 -0800

    SIMSBIOHUB-620: Manage System Alerts (#1401)
    * Initial migration to add alert table WIP)
    
    * add alert endpoints, services, repos and frontend components copying funding sources patterns
    
    * alerts working
    
    * style alert table
    
    * separate requests for active and expired alerts & rename manage users to admin page
    
    * fix tests and code smells
    
    * address code smell
    
    * add updateAlert
    
    * ignore-skip
    
    * CSS Tweaks
    
    * Remove commented code
    
    * address PR comments
    
    * Adjust alert spacing css.
    
    * Tweak error message
    
    ---------
    
    Co-authored-by: Nick Phura <nickphura@gmail.com>
    Co-authored-by: Mac Deluca <99926243+MacQSL@users.noreply.github.com>

---
+++ api/src/repositories/alert-repository.ts
@@ -0,0 +1,189 @@
+import { Knex } from 'knex';
+import SQL from 'sql-template-strings';
+import { getKnex } from '../database/db';
+import { ApiExecuteSQLError } from '../errors/api-error';
+import { IAlert, IAlertCreateObject, IAlertFilterObject, IAlertUpdateObject } from '../models/alert-view';
+import { BaseRepository } from './base-repository';
+
+/**
+ * A repository class for accessing alert data.
+ *
+ * @export
+ * @class AlertRepository
+ * @extends {BaseRepository}
+ */
+export class AlertRepository extends BaseRepository {
+  /**
+   * Builds query for all alert records without filtering any records, and adds a status field based on record_end_date
+   *
+   * @return {*}  {Knex.QueryBuilder}
+   * @memberof AlertRepository
+   */
+  _getAlertBaseQuery(): Knex.QueryBuilder {
+    const knex = getKnex();
+
+    return knex
+      .select(
+        'alert.alert_id',
+        'alert.name',
+        'alert.message',
+        'alert.alert_type_id',
+        'alert.data',
+        'alert.severity',
+        'alert.record_end_date',
+        knex.raw(`
+          CASE
+            WHEN alert.record_end_date < NOW() THEN 'expired'
+            ELSE 'active'
+          END AS status
+        `)
+      )
+      .from('alert')
+      .orderBy('alert.create_date', 'DESC');
+  }
+
+  /**
+   * Get alert records with optional filters applied
+   *
+   * @param {IAlertFilterObject} filterObject
+   * @return {*}  {Promise<IAlert[]>}
+   * @memberof AlertRepository
+   */
+  async getAlerts(filterObject: IAlertFilterObject): Promise<IAlert[]> {
+    const queryBuilder = this._getAlertBaseQuery();
+
+    if (filterObject.expiresAfter) {
+      queryBuilder.where((qb) => {
+        qb.whereRaw(`alert.record_end_date >= ?`, [filterObject.expiresAfter]).orWhereNull('alert.record_end_date');
+      });
+    }
+
+    if (filterObject.expiresBefore) {
+      queryBuilder.where((qb) => {
+        qb.whereRaw(`alert.record_end_date < ?`, [filterObject.expiresBefore]);
+      });
+    }
+
+    if (filterObject.types && filterObject.types.length > 0) {
+      queryBuilder
+        .join('alert_type as at', 'at.alert_type_id', 'alert.alert_type_id')
+        .whereRaw('lower(at.name) = ANY(?)', [filterObject.types.map((type) => type.toLowerCase())]);
+    }
+
+    const response = await this.connection.knex(queryBuilder, IAlert);
+
+    return response.rows;
+  }
+
+  /**
+   * Get a specific alert by its Id
+   *
+   * @param {number} alertId
+   * @return {*}  {Promise<IAlert>}
+   * @memberof AlertRepository
+   */
+  async getAlertById(alertId: number): Promise<IAlert> {
+    const queryBuilder = this._getAlertBaseQuery();
+
+    queryBuilder.where('alert_id', alertId);
+
+    const response = await this.connection.knex(queryBuilder, IAlert);
+
+    return response.rows[0];
+  }
+
+  /**
+   * Update system alert.
+   *
+   * @param {IAlertUpdateObject} alert
+   * @return {*} Promise<number>
+   * @memberof AlertRepository
+   */
+  async updateAlert(alert: IAlertUpdateObject): Promise<number> {
+    const sqlStatement = SQL`
+      UPDATE alert
+      SET
+        name = ${alert.name},
+        message = ${alert.message},
+        alert_type_id = ${alert.alert_type_id},
+        severity = ${alert.severity},
+        data = ${JSON.stringify(alert.data)}::json,
+        record_end_date = ${alert.record_end_date}
+      WHERE
+        alert_id = ${alert.alert_id}
+      RETURNING alert_id
+      ;
+      `;
+
+    const response = await this.connection.sql(sqlStatement);
+
+    if (response.rowCount !== 1) {
+      throw new ApiExecuteSQLError('Failed to update alert', [
+        'AlertRepository->updateAlert',
+        'rowCount was !== 1, expected rowCount === 1'
+      ]);
+    }
+
+    return response.rows[0].alert_id;
+  }
+
+  /**
+   * Create system alert.
+   *
+   * @param {IAlertCreateObject} alert
+   * @return {*}  Promise<number>
+   * @memberof AlertRepository
+   */
+  async createAlert(alert: IAlertCreateObject): Promise<number> {
+    const sqlStatement = SQL`
+      INSERT INTO
+        alert (name, message, alert_type_id, data, severity, record_end_date)
+      VALUES
+        (${alert.name}, ${alert.message}, ${alert.alert_type_id}, ${JSON.stringify(alert.data)}, ${alert.severity}, ${
+      alert.record_end_date
+    })
+      RETURNING alert_id
+      ;
+      `;
+
+    const response = await this.connection.sql(sqlStatement);
+
+    if (response.rowCount !== 1) {
+      throw new ApiExecuteSQLError('Failed to create alert', [
+        'AlertRepository->createAlert',
+        'rowCount was !== 1, expected rowCount === 1'
+      ]);
+    }
+
+    return response.rows[0].alert_id;
+  }
+
+  /**
+   * Delete system alert.
+   *
+   * @param {number} alertId
+   * @return {*}  Promise<number>
+   * @memberof AlertRepository
+   */
+  async deleteAlert(alertId: number): Promise<number> {
+    const sqlStatement = SQL`
+      DELETE FROM
+        alert
+      WHERE
+        alert_id = ${alertId}
+      RETURNING alert_id
+      ;
+      `;
+
+    const response = await this.connection.sql(sqlStatement);
+
+    if (response.rowCount !== 1) {
+      throw new ApiExecuteSQLError('Failed to delete alert', [
+        'AlertRepository->deleteAlert',
+        'rowCount was !== 1, expected rowCount === 1'
+      ]);
+    }
+
+    return response.rows[0].alert_id;
+  }
+}
