0a9a57f79183558ce18c2d4cee9ac8964747083e
Macgregor Aubertin-Young <108430771+mauberti-bc@users.noreply.github.com>
2024-11-25 15:02:06 -0800

    SIMSBIOHUB-632: Display Markdown in Vote Dialogs (#1418)
    * Add markdown database table.
    * Add help buttons and ability for users to vote them up or down.
    
    ---------
    
    Co-authored-by: Nick Phura <nickphura@gmail.com>

--- app/src/features/surveys/view/components/sampling-data/components/SurveySamplingHeader.tsx
+++ app/src/features/surveys/view/components/sampling-data/components/SurveySamplingHeader.tsx
@@ -1,10 +1,13 @@
 import { mdiCog } from '@mdi/js';
 import Icon from '@mdi/react';
 import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import HelpButtonDialog from 'components/buttons/HelpButtonDialog';
 import { ProjectRoleGuard } from 'components/security/Guards';
 import { PROJECT_PERMISSION, SYSTEM_ROLE } from 'constants/roles';
+import { MarkdownTypeNameEnum } from 'interfaces/useMarkdownApi.interface';
 import { Link as RouterLink } from 'react-router-dom';
 
 export const SurveySamplingHeader = () => {
@@ -13,22 +16,22 @@ export const SurveySamplingHeader = () => {
       <Typography variant="h2" flex="1 1 auto">
         Sampling Information
       </Typography>
-      <ProjectRoleGuard
-        validProjectPermissions={[PROJECT_PERMISSION.COORDINATOR, PROJECT_PERMISSION.COLLABORATOR]}
-        validSystemRoles={[SYSTEM_ROLE.SYSTEM_ADMIN, SYSTEM_ROLE.DATA_ADMINISTRATOR]}>
-        <Button
-          component={RouterLink}
-          variant="contained"
-          color="primary"
-          aria-label="Manage Sampling Information"
-          to="sampling"
-          startIcon={<Icon path={mdiCog} size={0.75}></Icon>}
-          sx={{
-            my: -1
-          }}>
-          Manage
-        </Button>
-      </ProjectRoleGuard>
+      <Stack gap={1} direction="row">
+        <HelpButtonDialog markdownType={MarkdownTypeNameEnum.SAMPLING_INFORMATION} />
+        <ProjectRoleGuard
+          validProjectPermissions={[PROJECT_PERMISSION.COORDINATOR, PROJECT_PERMISSION.COLLABORATOR]}
+          validSystemRoles={[SYSTEM_ROLE.SYSTEM_ADMIN, SYSTEM_ROLE.DATA_ADMINISTRATOR]}>
+          <Button
+            component={RouterLink}
+            variant="contained"
+            color="primary"
+            aria-label="Manage Sampling Information"
+            to="sampling"
+            startIcon={<Icon path={mdiCog} size={0.75}></Icon>}>
+            Manage
+          </Button>
+        </ProjectRoleGuard>
+      </Stack>
     </Toolbar>
   );
 };
