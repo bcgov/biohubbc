c5cc6c6fce21a6499e0bbe32c4eff6a7762d4442
Andrew Thompson <andrew.thompson@gov.bc.ca>
2024-12-02 12:16:54 -0800

    initial page route and placeholder created

---
+++ app/src/features/support/SupportPage.tsx
@@ -0,0 +1,11 @@
+const PlaceholderPage = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Placeholder Page</h1>
+      <p>This is a simple placeholder for your new route.</p>
+      <p>Start coding here when you're ready!</p>
+    </div>
+  );
+};
+
+export default PlaceholderPage;
