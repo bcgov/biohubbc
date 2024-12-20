7f614d80c835035e61676ea21a57512329d92e50
Mac Deluca <Mac.Deluca@quartech.com>
2024-12-19 19:44:13 -0800

    chore: partially done initial CSVConfig setup for capture

---
+++ api/src/services/import-services/capture/import-captures-service.ts
@@ -0,0 +1,137 @@
+import { WorkSheet } from 'xlsx';
+import { IDBConnection } from '../../../database/db';
+import { CSVConfigUtils } from '../../../utils/csv-utils/csv-config-utils';
+import { validateCSVWorksheet } from '../../../utils/csv-utils/csv-config-validation';
+import { CSVConfig, CSVError } from '../../../utils/csv-utils/csv-config-validation.interface';
+import { getDescriptionCellValidator } from '../../../utils/csv-utils/csv-header-configs';
+import { getLogger } from '../../../utils/logger';
+import { DBService } from '../../db-service';
+import { SurveyCritterService } from '../../survey-critter-service';
+
+const defaultLog = getLogger('services/import/import-captures-service');
+
+const CAPTURE_LONGITUDE_ALIASES: Uppercase<string>[] = [
+  'CAPTURE LONGITUDE',
+  'CAPTURE_LONG',
+  'CAPTURE LONG',
+  'CAPTURE_LON',
+  'CAPTURE LON',
+  'CAPTURE_LNG',
+  'CAPTURE LNG'
+];
+
+const RELEASE_LONGITUDE_ALIASES: Uppercase<string>[] = [
+  'RELEASE LONGITUDE',
+  'RELEASE_LONG',
+  'RELEASE LONG',
+  'RELEASE_LON',
+  'RELEASE LON',
+  'RELEASE_LNG',
+  'RELEASE LNG'
+];
+
+// Capture CSV static headers
+export type CaptureCSVStaticHeader =
+  | 'ALIAS'
+  | 'CAPTURE_DATE'
+  | 'CAPTURE_TIME'
+  | 'CAPTURE_LATITUDE'
+  | 'CAPTURE_LONGITUDE'
+  | 'RELEASE_DATE'
+  | 'RELEASE_TIME'
+  | 'RELEASE_LATITUDE'
+  | 'RELEASE_LONGITUDE'
+  | 'CAPTURE_COMMENT'
+  | 'RELEASE_COMMENT';
+
+/**
+ * ImportCapturesService - A service for importing Captures from a CSV into Critterbase.
+ *
+ * @class ImportCapturesService
+ * @extends DBService
+ */
+export class ImportCapturesService extends DBService {
+  worksheet: WorkSheet;
+  surveyId: number;
+
+  surveyCritterService: SurveyCritterService;
+  utils: CSVConfigUtils<CaptureCSVStaticHeader>;
+
+  /**
+   * Construct an instance of ImportCapturesService.
+   *
+   * @param {IDBConnection} connection - DB connection
+   * @param {string} surveyId
+   */
+  constructor(connection: IDBConnection, worksheet: WorkSheet, surveyId: number) {
+    super(connection);
+
+    const initialConfig: CSVConfig<CaptureCSVStaticHeader> = {
+      staticHeadersConfig: {
+        ALIAS: { aliases: ['NICKNAME', 'ANIMAL'] },
+        CAPTURE_DATE: { aliases: ['CAPTURE_DATE'] },
+        CAPTURE_TIME: { aliases: ['CAPTURE_TIME'], optional: true },
+        CAPTURE_LATITUDE: { aliases: ['CAPTURE LATITUDE', 'CAPTURE_LAT', 'CAPTURE LAT'] },
+        CAPTURE_LONGITUDE: { aliases: CAPTURE_LONGITUDE_ALIASES },
+        RELEASE_DATE: { aliases: ['RELEASE DATE'], optional: true },
+        RELEASE_TIME: { aliases: ['RELEASE TIME'], optional: true },
+        RELEASE_LATITUDE: { aliases: ['RELEASE LATITUDE', 'RELEASE_LAT', 'RELEASE LAT'], optional: true },
+        RELEASE_LONGITUDE: { aliases: RELEASE_LONGITUDE_ALIASES, optional: true },
+        CAPTURE_COMMENT: { aliases: ['CAPTURE COMMENT', 'CAPTURE_NOTES', 'CAPTURE NOTES'], optional: true },
+        RELEASE_COMMENT: { aliases: ['RELEASE COMMENT', 'RELEASE_NOTES', 'RELEASE NOTES'], optional: true }
+      },
+      ignoreDynamicHeaders: false
+    };
+
+    this.worksheet = worksheet;
+    this.surveyId = surveyId;
+
+    this.surveyCritterService = new SurveyCritterService(connection);
+    this.utils = new CSVConfigUtils(this.worksheet, initialConfig);
+  }
+
+  /**
+   * Import a Capture CSV worksheet into Critterbase.
+   *
+   * @async
+   * @throws {ApiGeneralError} - If unable to fully insert records into Critterbase
+   * @returns {*} {Promise<CSVError[]>} List of CSV errors encountered during import
+   */
+  async importCSVWorksheet(): Promise<CSVError[]> {
+    const config = await this.getCSVConfig();
+
+    const { errors, rows } = validateCSVWorksheet(this.worksheet, config);
+
+    if (errors.length) {
+      return errors;
+    }
+
+    defaultLog.debug({ label: 'import captures', captures });
+
+    await this.surveyCritterService.critterbaseService.bulkCreate({ captures });
+
+    return [];
+  }
+
+  /**
+   * Get the CSV configuration for Captures.
+   *
+   * @returns {Promise<CSVConfig<CaptureCSVStaticHeader>>} The CSV configuration
+   */
+  async getCSVConfig(): Promise<CSVConfig<CaptureCSVStaticHeader>> {
+    this.utils.setStaticHeaderConfig('ALIAS', { validateCell: undefined });
+    this.utils.setStaticHeaderConfig('CAPTURE_DATE', { validateCell: undefined });
+    this.utils.setStaticHeaderConfig('CAPTURE_TIME', { validateCell: undefined });
+    this.utils.setStaticHeaderConfig('CAPTURE_LATITUDE', { validateCell: undefined });
+    this.utils.setStaticHeaderConfig('CAPTURE_LONGITUDE', { validateCell: undefined });
+    this.utils.setStaticHeaderConfig('RELEASE_DATE', { validateCell: undefined });
+    this.utils.setStaticHeaderConfig('RELEASE_TIME', { validateCell: undefined });
+    this.utils.setStaticHeaderConfig('RELEASE_LATITUDE', { validateCell: undefined });
+    this.utils.setStaticHeaderConfig('RELEASE_LONGITUDE', { validateCell: undefined });
+    this.utils.setStaticHeaderConfig('CAPTURE_COMMENT', { validateCell: getDescriptionCellValidator() });
+    this.utils.setStaticHeaderConfig('RELEASE_COMMENT', { validateCell: getDescriptionCellValidator() });
+
+    // Return the final CSV config
+    return this.utils.getConfig();
+  }
+}
