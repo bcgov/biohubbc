ed0ad02b93a95bfd5a8281a9127865c49b93201b
Meijer <Annika.Meijer@gov.bc.ca>
2025-01-06 16:58:47 -0800

    blocks,strata,techniques support content

--- app/src/features/support/data/foundation.tsx
+++ app/src/features/support/data/foundation.tsx
@@ -3,7 +3,7 @@ import { EnumMarkdownTypes, SupportPageView } from './types';
 
 const foundation = [
   {
-    label: 'Blocks',
+    label: 'Managing Blocks',
     description: [
       <Typography variant="body1" gutterBottom>
         This foundational data section will detail the elements of your survey that are required to establish the
@@ -13,43 +13,61 @@ const foundation = [
       </Typography>,
       <Typography variant="body1" gutterBottom>
         Establishing foundational data in your survey ensures that sampling objectives are clearly defined and that
-        metadata for sampling efforts are well-documented, accessible and interpretable.
-      </Typography>
+        metadata for sampling efforts are well-documented, accessible and interpretable. Foundational data are defined within a survey by navigating to the Manage Sampling Information page.
+      </Typography>,
+      <Typography variant="body1" gutterBottom>
+      <strong>Blocks</strong>
+    </Typography>,
+    <Typography variant="body1" gutterBottom>
+        Blocks are optional defined areas within a study area that enhance structured sampling and reduce variability. As a higher-level organizational unit, blocks contain sampling sites and help distribute sampling effort systematically across a large area, supporting randomization and reducing the potential for bias.
+    </Typography>, 
+    <Typography variant="body1" gutterBottom>
+      Blocks can also be thought of a strategic clustering of sampling sites. For example, a study could be designed along a series of transects, with distinct sampling sites. Biologists could be travelling along 5 transect lines and stopping at 6 distinct locations on each transect to conduct sampling. In this case, the survey would have a sum total of 30 sampling sites, organized into 6 sampling sites per block, 5 blocks (the transects).
+    </Typography>, 
+    <Typography variant="body1" gutterBottom>
+       A different application of blocks may be executed by biologists who sample by conducting aerial surveys. They may have a large study area (for example, a delineated Caribou Herd) within which they would like to sample 3 distinct regions in this year of their study. The biologist may choose to define these regions as block A, B, and C, respectively. If the biologist is flying for 6 days, they may be able to fly over each block twice, and conduct a count of observation continuously from the air. In this scenario, block A, B, and C would each have two sample sites: the flightline from each day the block was visited. In this case, the block is used to relate the two flightlines (sampling sites) that were conducted in the same general spatial region within a study area.
+    </Typography>
     ],
     markdownType: EnumMarkdownTypes[SupportPageView.FOUNDATION]?.[0]
   },
   {
-    label: 'Strata',
+    label: 'Managing Strata',
     description: [
       <Typography variant="body1" gutterBottom>
         <strong>Strata</strong>
       </Typography>,
       <Typography variant="body1" gutterBottom>
-        Text
+        Strata are optional, but provide a framework to focus effort and minimize variability. Each stratum is homogeneous within, but distinct from, others.
+      </Typography>,
+      <Typography variant="body1" gutterBottom>
+        For example, a survey could have two strata: low elevation, and high elevation. A sampling site may be classified under one stratum, or the other, but not both. Within the low elevation stratum, all sampling sites occur at low elevation - therefore homogenous within the stratum - but distinct from sampling sites that occur in the high-elevation stratum.
       </Typography>
     ],
     markdownType: EnumMarkdownTypes[SupportPageView.FOUNDATION]?.[1]
   },
   {
-    label: 'Techniques',
+    label: 'Managing Techniques',
     description: [
       <Typography variant="body1" gutterBottom>
         <strong>Techniques</strong>
       </Typography>,
       <Typography variant="body1" gutterBottom>
-        TEXT
+        A technique is a combination of a sampling method, method attributes, attractants and the detection distance for this methodology. Surveys may have a number of techniques applied at sampling sites depending on the number of combinations of attractants, attributes, and detection distances applied to a specific methodology. 
+      </Typography>, 
+      <Typography variant="body1" gutterBottom>
+        For example, a biologist may be using Angling as their methodology, but at a single site they will attempt Angling with Worms, and Reflective Materials, and then they may also attempt Angling with Maggots and no reflective lures. These two distinct combinations would each be classified as their own technique. By classifying each as their own technique applied at a single sampling site, the success of each technique can be noted by relating which observations were made using which technique. 
       </Typography>
     ],
     markdownType: EnumMarkdownTypes[SupportPageView.FOUNDATION]?.[2]
   },
   {
-    label: 'Sampling Sites',
+    label: 'Managing Sampling Sites',
     description: [
       <Typography variant="body1" gutterBottom>
         <strong>Sampling Sites</strong>
       </Typography>,
       <Typography variant="body1" gutterBottom>
-        Insert text
+        Insert text. MENTION SAMPLING PERIODS 
       </Typography>
     ],
     markdownType: EnumMarkdownTypes[SupportPageView.FOUNDATION]?.[3]
