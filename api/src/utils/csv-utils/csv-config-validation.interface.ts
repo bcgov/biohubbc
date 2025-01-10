4d24ecaf72d9d45c7e7d24474adaf9cbffa1d2dd
Mac Deluca <Mac.Deluca@quartech.com>
2025-01-09 16:58:55 -0800

    fix: resolved conflict with dev

--- api/src/utils/csv-utils/csv-config-validation.interface.ts
+++ api/src/utils/csv-utils/csv-config-validation.interface.ts
@@ -8,10 +8,6 @@ export const CSV_ERROR_MESSAGE =
  *  1. Allow or disallow duplicate CSV rows
  *    - Similar to a DB unique constraint? ie: ['NAME', 'AGE']
  *  2. Support CSVWarnings
-<<<<<<< HEAD
- *  3. Support CSVRowValidation? ie: Validate the entire row before / after the cell validation
-=======
->>>>>>> md-csv-row-validation
  */
 export interface CSVConfig<THeader extends Uppercase<string> = Uppercase<string>> {
   /**
