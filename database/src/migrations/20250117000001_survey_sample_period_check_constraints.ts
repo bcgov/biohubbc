aac330348af7499b5c9f1515d0b23aaea0924f73
Nick Phura <nickphura@gmail.com>
2025-01-17 14:39:13 -0800

    Update constraint

--- database/src/migrations/20250117000001_survey_sample_period_check_constraints.ts
+++ database/src/migrations/20250117000001_survey_sample_period_check_constraints.ts
@@ -14,9 +14,11 @@ export async function up(knex: Knex): Promise<void> {
     SET SEARCH_PATH=biohub;
 
     -- Asserts that start_date and end_date are either both null or both not null
-    ALTER TABLE survey_sample_period ADD CONSTRAINT check_neither_or_both_start_and_end_date_provided CHECK (
+    ALTER TABLE survey_sample_period ADD CONSTRAINT check_start_and_end_date_requirement CHECK (
       NUM_NONNULLS(start_date, end_date) IN (0, 2)
     );
+
+    COMMENT ON CONSTRAINT check_start_and_end_date_requirement ON survey_sample_period IS 'Checks that start_date and end_date are either both null or both not null.';
 
     /*
      * Asserts that the survey_sample_period record meets the minimum data requirements for the survey_sample_period
@@ -28,6 +30,8 @@ export async function up(knex: Knex): Promise<void> {
     ALTER TABLE survey_sample_period ADD CONSTRAINT check_minimum_data_requirement CHECK (
       NUM_NONNULLS(survey_sample_site_id, method_technique_id, start_date) >= 1
     );
+
+    COMMENT ON CONSTRAINT check_minimum_data_requirement ON survey_sample_period IS 'Checks that at least one of the following items is not null: survey_sample_site_id, method_technique_id, start_date + end_date. Note: This constraint only checks start_date because the check_start_and_end_date_requirement constraint already ensures that start_date and end_date are either both null or both not null.';
   `);
 }
 
