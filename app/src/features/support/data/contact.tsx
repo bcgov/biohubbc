4e9db8a3c83c32723ca77188c64cb24d9a7aa91c
Meijer <Annika.Meijer@gov.bc.ca>
2025-01-29 10:49:19 -0800

    contact and standards keys

--- app/src/features/support/data/contact.tsx
+++ app/src/features/support/data/contact.tsx
@@ -3,7 +3,7 @@ const contact = [
   {
     label: 'Contact Support',
     description: [
-      <Typography variant="body1" gutterBottom>
+      <Typography variant="body1" gutterBottom key='contact1'>
         For technical support or questions about this application, please email{' '}
         <a href="mailto:spi_mail@gov.bc.ca">spi_mail@gov.bc.ca</a>.
       </Typography>
