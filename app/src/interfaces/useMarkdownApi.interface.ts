392f647f5b24ba906e69e1d473bb4ca850d0b207
Macgregor Aubertin-Young <macgregor.aubertin-young@gov.bc.ca>
2025-02-12 14:26:16 -0800

    remove migration

--- app/src/interfaces/useMarkdownApi.interface.ts
+++ app/src/interfaces/useMarkdownApi.interface.ts
@@ -29,22 +29,3 @@ export enum MarkdownTypeNameEnum {
   SURVEY_METADATA = 'Survey Metadata',
   OBSERVATIONS = 'Observations'
 }
-
-export enum MarkdownTypeSupportNameEnum {
-  SPI = 'SPI Data',
-  PROJECT_ROLES = 'Role Based Security',
-  PROJECT_COMPONENTS = 'Project Components',
-  SURVEY_METADATA = 'Survey Metadata Support',
-  SURVEY_ATTACHMENTS = 'Survey Attachments',
-  BLOCKS = 'Support Blocks',
-  STRATA = 'Support Strata',
-  TECHNIQUES = 'Support Techniques',
-  SAMPLING_SITES = 'Support Sites',
-  ITIS = 'ITIS Standards',
-  ANIMAL_ENTITY = 'Animal Entity',
-  ANIMAL_EVENT = 'Animal Event',
-  ANIMAL_BULK = 'Animal Bulk Upload',
-  TELEMETRY_MANUAL = 'Telemetry Manual Upload',
-  TELEMETRY_AUTO = 'Telemetry Automated',
-  OBSERVATIONS = 'Observation Data Load'
-}
