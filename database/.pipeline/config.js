ff05de617082356119df710809a713ad6d4c1e01
Nick Phura <nickphura@gmail.com>
2025-02-12 16:40:39 -0800

    Temp: Remove database migrations (will run manually to import prod data) Temp: Increase database volume size (to handle prod data size)

--- database/.pipeline/config.js
+++ database/.pipeline/config.js
@@ -71,7 +71,7 @@ const phases = {
     nodeEnv: 'development',
     tz: config.timezone.db,
     dbSetupDockerfilePath: dbSetupDockerfilePath,
-    volumeCapacity: (isStaticDeployment && '3Gi') || '500Mi',
+    volumeCapacity: (isStaticDeployment && '3Gi') || '10Gi',
     cpuRequest: '50m',
     cpuLimit: '600m',
     memoryRequest: '100Mi',
