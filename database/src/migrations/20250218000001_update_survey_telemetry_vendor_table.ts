bcaddaaffbdacab8950ad603003885513e2755be
Oscar Inostroza <oscar.inostroza@quartech.com>
2025-02-18 19:08:41 -0700

    Keeping survey_id in attachment table, updated key import code to reflect data model changes

--- database/src/migrations/20250218000001_update_survey_telemetry_vendor_table.ts
+++ database/src/migrations/20250218000001_update_survey_telemetry_vendor_table.ts
@@ -3,7 +3,6 @@ import { Knex } from 'knex';
 /**
  * Update tables:
  * survey_telemetry_vendor_credential - add survey_id column and make survey_telemetry_credential_attachment_id nullable.
- * survey_telemetry_credential_attachment - remove survey_id column.
  *
  * @param {Knex} knex
  * @returns {Promise<void>}
@@ -14,7 +13,6 @@ export async function up(knex: Knex): Promise<void> {
 
     ALTER TABLE survey_telemetry_vendor_credential ADD COLUMN survey_id INTEGER NOT NULL, ALTER COLUMN survey_telemetry_credential_attachment_id DROP NOT NULL;
     COMMENT ON COLUMN survey_telemetry_vendor_credential.survey_id IS 'Foreign key to the survey table.';
-    ALTER TABLE survey_telemetry_credential_attachment DROP COLUMN survey_id;
 
     -- Add foreign key constraints
     ALTER TABLE survey_telemetry_vendor_credential 
