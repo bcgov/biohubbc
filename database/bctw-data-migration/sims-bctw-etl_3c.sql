7280278e2123834f33fa2a6039100eff869aa7a6
Nick Phura <nickphura@gmail.com>
2025-02-20 09:32:38 -0800

    note

---
+++ database/bctw-data-migration/sims-bctw-etl_3c.sql
@@ -0,0 +1,6 @@
+
+-- Tables to use:
+-- bctw.matched_sims_deployments
+-- bctw.valid_collar_deployment
+-- bctw.invalid_collar_deployment
+-- plus any other intermediate, etc tables that are needed.
