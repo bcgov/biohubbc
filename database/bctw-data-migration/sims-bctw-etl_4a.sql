3ac5c001ff18f484bcf3aad0df21179e612ddd01
Nick Phura <nickphura@gmail.com>
2025-02-20 12:35:31 -0800

    Add 4a

--- database/bctw-data-migration/sims-bctw-etl_4a.sql
+++ database/bctw-data-migration/sims-bctw-etl_4a.sql
@@ -1,75 +1,70 @@
---------------------------------------------------------------------------------------------------------------------
--- Insert matched and mismatched devices and deployments into SIMS tables
---------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------
+-- Combine and transform final mismatched and final matched tables
+--------------------------------------------------------------------------------------------------------------
 
--- Insert SIMS devices
-insert
-  into
-  biohub.device (
+-- TODO
+
+--------------------------------------------------------------------------------------------------------------
+-- Insert final data into SIMS
+--------------------------------------------------------------------------------------------------------------
+
+-- Insert data into device
+WITH w_insert_device AS (
+  INSERT INTO biohub.device (
     survey_id,
     serial,
-    model,
     device_make_id,
+    model,
     comment
-  )
-select
+  ) 
+  SELECT (
+    survey_id,
+    serial,
+    device_make_id,
+    model,
+    comment
+  ) 
+  FROM 
+    sims_bctw.final_device_deployment
+  RETURNING 
+    *
+),
+-- Insert data into deployment
+INSERT INTO biohub.deployment (
   survey_id,
-  serial,
-  model,
-  device_make_id,
-  comment
-from
-  sims_bctw.device;
-
--- Insert SIMS deployments
-insert
-  into
-    biohub.deployment (
-      survey_id,
-      critter_id,
-      device_id,
-      frequency,
-      frequency_unit_id,
-      attachment_start_date,
-      attachment_start_time,
-      attachment_end_date,
-      attachment_end_time,
-      critterbase_start_capture_id,
-      critterbase_end_capture_id,
-      critterbase_end_mortality_id
-  )
-    select
-      sims_bctw.deployment.survey_id,
-      sims_bctw.deployment.critter_id,
-      biohub.device.device_id,
-      frequency,
-      case
-        when frequency is not null then
-          coalesce(
-            frequency_unit,
-            (
-              select
-                      frequency_unit_id
-              from
-                      frequency_unit
-              where
-                      name = 'mhz'
-            )
-          )
-        else null
-      end as frequency_unit,
-      attachment_start_date,
-      attachment_start_time,
-      attachment_end_date,
-      attachment_end_time,
-      critterbase_start_capture_id,
-      critterbase_end_capture_id,
-      critterbase_end_mortality_id
-from
-      sims_bctw.deployment
-left join biohub.device
-    on
-      sims_bctw.deployment.survey_id = biohub.device.survey_id
-  and
-       sims_bctw.deployment.serial = biohub.device.serial
-returning *;
+  critter_id,
+  device_id,
+  frequency,
+  frequency_unit_id,
+  attachment_start_date,
+  attachment_start_time,
+  attachment_end_date,
+  attachment_end_time,
+  critterbase_start_capture_id,
+  critterbase_end_capture_id,
+  critterbase_end_mortality_id
+) 
+SELECT (
+  survey_id,
+  critter_id,
+  device_id,
+  frequency,
+  frequency_unit_id,
+  attachment_start_date,
+  attachment_start_time,
+  attachment_end_date,
+  attachment_end_time,
+  critterbase_start_capture_id,
+  critterbase_end_capture_id,
+  critterbase_end_mortality_id
+)
+FROM 
+  final_device_deployment
+INNER JOIN
+  w_insert_device
+ON
+  final_device_deployment.survey_id = w_insert_device.survey_id
+AND
+  final_device_deployment.serial = w_insert_device.serial
+AND
+  final_device_deployment.device_make_id = w_insert_device.device_make_id;
