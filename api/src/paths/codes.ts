286e15be33c82d517a7d2c9552d738db7658f566
Mac Deluca <Mac.Deluca@quartech.com>
2025-02-25 16:16:38 -0800

    fix: uncommented cache

--- api/src/paths/codes.ts
+++ api/src/paths/codes.ts
@@ -531,8 +531,8 @@ export function getAllCodes(): RequestHandler {
         throw new HTTP500('Failed to fetch codes');
       }
 
-      // // Allow browsers to cache this response for 300 seconds (5 minutes)
-      // res.setHeader('Cache-Control', 'private, max-age=300');
+      // Allow browsers to cache this response for 300 seconds (5 minutes)
+      res.setHeader('Cache-Control', 'private, max-age=300');
 
       return res.status(200).json(allCodeSets);
     } catch (error) {
