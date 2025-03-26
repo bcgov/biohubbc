a44c0d5f62e0094ac35a80c2af281eb385290d9c
Mac Deluca <Mac.Deluca@quartech.com>
2025-03-26 13:31:40 -0700

    chore: dropped unused components 4

--- app/src/interfaces/useBioHubApi.interface.ts
+++
@@ -1,6 +0,0 @@
-export type WarningSchema<DataType extends Record<string, unknown> = Record<string, unknown>> = {
-  name: string;
-  message: string;
-  data: DataType;
-  errors?: (string | Record<string, unknown>)[];
-};
