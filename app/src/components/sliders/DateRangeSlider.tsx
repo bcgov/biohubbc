481c92a9cfd4c286048a035fbcbc35252bbb55a0
Macgregor Aubertin-Young <108430771+mauberti-bc@users.noreply.github.com>
2025-05-12 09:26:58 -0700

    Add Date Range to Telemetry Map on Survey Page (#1547)
    * add date range to telemetry endpoints
    
    * add frontend slider
    
    * update tests & fix slider
    
    * fix tests
    
    * add date picker boxes
    
    * fix map bounds
    
    * skeleton loader for map
    
    * remove unused label prop
    
    * skip-ignore
    
    * ignore-skip

---
+++ app/src/components/sliders/DateRangeSlider.tsx
@@ -0,0 +1,46 @@
+import Slider from '@mui/material/Slider';
+import dayjs, { Dayjs } from 'dayjs';
+
+interface IDateRangeSliderProps {
+  value: [Dayjs, Dayjs];
+  onChange: (value: [Dayjs, Dayjs]) => void;
+  minDate: Dayjs;
+  maxDate: Dayjs;
+}
+
+/**
+ * Returns slider with values formatted as dates
+ *
+ * @param {IDateRangeSliderProps} props
+ * @returns {*}
+ */
+export const DateRangeSlider = (props: IDateRangeSliderProps) => {
+  const { value, onChange, minDate, maxDate } = props;
+
+  const minTimestamp = minDate.valueOf();
+  const maxTimestamp = maxDate.valueOf();
+
+  // Get the date in milliseconds to make it numeric for the slider
+  const sliderValue = [value[0].valueOf(), value[1].valueOf()];
+
+  const handleChange = (newValue: number[]) => {
+    if (Array.isArray(newValue)) {
+      onChange([dayjs(newValue[0]), dayjs(newValue[1])]);
+    }
+  };
+
+  return (
+    <Slider
+      min={minTimestamp}
+      max={maxTimestamp}
+      value={sliderValue}
+      onChange={(_, value) => {
+        if (Array.isArray(value)) {
+          handleChange(value);
+        }
+      }}
+      valueLabelDisplay="auto"
+      valueLabelFormat={(val) => dayjs(val).format('YYYY-MM-DD')}
+    />
+  );
+};
