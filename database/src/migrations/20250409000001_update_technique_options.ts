4aea60112a28ff6c355059ef2ee14346a515108a
Macgregor Aubertin-Young <macgregor.aubertin-young@gov.bc.ca>
2025-05-12 12:19:23 -0700

    misc cleanup

--- database/src/migrations/20250409000001_update_technique_options.ts
+++ database/src/migrations/20250409000001_update_technique_options.ts
@@ -1,23 +1,23 @@
 import { Knex } from 'knex';
 
 /**
- * UPDATES TO EXISTING CONCEPTS:
- *
  * - Adds pit reader station as a sampling method
  * - Adds camera trap and pit tag reader attributes
+ *
  * @export
  * @param {Knex} knex
  * @return {*}  {Promise<void>}
  */
 export async function up(knex: Knex): Promise<void> {
   await knex.raw(`--sql
+    SET SEARCH_PATH=biohub;
+
     ---------------------------------------------------------------------------------------------------
     ---------------------- Add pit tag as a sampling method option ------------------------------------
     ---------------------------------------------------------------------------------------------------
-    SET SEARCH_PATH=biohub;
 
     INSERT INTO method_lookup (name, description, record_effective_date)
-    VALUES ('Pit Reader Station','Recording an observation of a species through its interaction with a pit tag reader', now());
+    VALUES ('PIT Tag Reader', 'Detecting species using a PIT tag reader', now());
 
     --------------------------------------------------------------------------------------------------
     --------------------- Insert attributes for camera trap and pit readers --------------------------
@@ -25,9 +25,7 @@ export async function up(knex: Knex): Promise<void> {
 
     INSERT INTO technique_attribute_quantitative (name, description)
     VALUES 
-    ('Quiet Period Duration', 'The set minimum time span permitted between the end of a recording event and the triggering of a subsequent recording event.'),
-    ('Video Length per Trigger', 'The set minimum length of video time, in seconds, that a camera should record when triggered.'),
-    ('Trigger Timing','The set time span between automated regularly timed recording events.');
+    ('Quiet Period Duration', 'The delay time between consecutive images.');
 
     INSERT INTO method_lookup_attribute_quantitative (technique_attribute_quantitative_id, method_lookup_id, min, max, unit)
     VALUES
@@ -35,20 +33,6 @@ export async function up(knex: Knex): Promise<void> {
             (SELECT technique_attribute_quantitative_id FROM technique_attribute_quantitative WHERE name = 'Quiet Period Duration'),
             (SELECT method_lookup_id FROM method_lookup WHERE name = 'Camera trap'),
             0,
-            10000, 
-            'seconds'
-        ),
-        (
-            (SELECT technique_attribute_quantitative_id FROM technique_attribute_quantitative WHERE name = 'Video Length per Trigger'),
-            (SELECT method_lookup_id FROM method_lookup WHERE name = 'Camera trap'),
-            0, 
-            10000, 
-            'seconds'
-        ),
-         (
-            (SELECT technique_attribute_quantitative_id FROM technique_attribute_quantitative WHERE name = 'Trigger Timing'),
-            (SELECT method_lookup_id FROM method_lookup WHERE name = 'Camera trap'),
-            0, 
             10000, 
             'seconds'
         );
