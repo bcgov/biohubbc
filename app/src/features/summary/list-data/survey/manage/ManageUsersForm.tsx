409853f666d23161e7c69d0da6c97d80806e8ac3
Meijer <Annika.Meijer@gov.bc.ca>
2025-05-29 16:44:25 -0700

    front end design for users form

--- app/src/features/summary/list-data/survey/manage/ManageUsersForm.tsx
+++ app/src/features/summary/list-data/survey/manage/ManageUsersForm.tsx
@@ -1,9 +1,14 @@
+import { LoadingButton } from '@mui/lab';
 import Autocomplete from '@mui/material/Autocomplete';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
 import Divider from '@mui/material/Divider';
+import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import FormikErrorSnackbar from 'components/alert/FormikErrorSnackbar';
 import HorizontalSplitFormComponent from 'components/fields/HorizontalSplitFormComponent';
+import PageHeader from 'components/layout/PageHeader';
 import { SURVEY_ROLE } from 'constants/roles';
 import { CodesContext } from 'contexts/codesContext';
 import ParticipantsCollectionForm from 'features/collection/edit/participants/ParticipantsCollectionForm';
@@ -19,10 +24,14 @@ import { useContext, useMemo } from 'react';
  */
 const ManageUsersForm = ({
   handleSubmit,
-  formikRef
+  formikRef,
+  isSaving = false,
+  handleCancel = () => {}
 }: {
   handleSubmit: (formikData: any) => void;
   formikRef: React.RefObject<FormikProps<any>>;
+  isSaving?: boolean;
+  handleCancel?: () => void;
 }) => {
   const codesContext = useContext(CodesContext);
   const codes = codesContext.codesDataLoader.data;
@@ -52,50 +61,97 @@ const ManageUsersForm = ({
   };
 
   return (
-    <Formik
-      innerRef={formikRef}
-      initialValues={initialValues}
-      validateOnBlur={false}
-      validateOnChange={false}
-      enableReinitialize={true}
-      onSubmit={handleSubmit}>
-      {({ values, setFieldValue }) => (
-        <Stack gap={5}>
-          <FormikErrorSnackbar />
-          {/* Top half: Survey selection */}
-          <HorizontalSplitFormComponent
-            title="Surveys"
-            summary="Select the surveys you want to invite members to."
-            component={
-              <Autocomplete
-                multiple
-                id="selectedSurveys"
-                options={surveyOptions}
-                getOptionLabel={(option) => option.label}
-                value={surveyOptions.filter((option) => (values.selectedSurveys as number[]).includes(option.value))}
-                onChange={(_, newValue) =>
-                  setFieldValue(
-                    'selectedSurveys',
-                    newValue.map((option) => option.value)
-                  )
-                }
-                renderInput={(params) => (
-                  <TextField {...params} label="Select Surveys" placeholder="Search and select surveys" required />
-                )}
-                disableCloseOnSelect
-              />
-            }
-          />
-          {/* Bottom half: User/role selection */}
-          <HorizontalSplitFormComponent
-            title="Invite Members"
-            summary="Invite members to access your surveys. Any individual role you assign here will apply to each member in every survey you have selected."
-            component={<ParticipantsCollectionForm roles={codes?.survey_roles ?? []} />}
-          />
-          <Divider />
-        </Stack>
-      )}
-    </Formik>
+    <>
+      <PageHeader
+        title="Manage Users"
+        buttonJSX={
+          <>
+            <LoadingButton
+              loading={isSaving}
+              type="submit"
+              color="primary"
+              variant="contained"
+              onClick={() => formikRef.current?.submitForm()}
+              data-testid="submit-manage-users-button">
+              Save and Exit
+            </LoadingButton>
+            <Button disabled={isSaving} color="primary" variant="outlined" onClick={handleCancel}>
+              Cancel
+            </Button>
+          </>
+        }
+      />
+      <Container maxWidth="xl" sx={{ py: 3 }}>
+        <Paper sx={{ p: 5 }}>
+          <Formik
+            innerRef={formikRef}
+            initialValues={initialValues}
+            validateOnBlur={false}
+            validateOnChange={false}
+            enableReinitialize={true}
+            onSubmit={handleSubmit}>
+            {({ values, setFieldValue }) => (
+              <Stack gap={5}>
+                <FormikErrorSnackbar />
+                {/* Top half: Survey selection */}
+                <HorizontalSplitFormComponent
+                  title="Surveys"
+                  summary="Select the surveys you want to invite members to."
+                  component={
+                    <Autocomplete
+                      multiple
+                      id="selectedSurveys"
+                      options={surveyOptions}
+                      getOptionLabel={(option) => option.label}
+                      value={surveyOptions.filter((option) =>
+                        (values.selectedSurveys as number[]).includes(option.value)
+                      )}
+                      onChange={(_, newValue) =>
+                        setFieldValue(
+                          'selectedSurveys',
+                          newValue.map((option) => option.value)
+                        )
+                      }
+                      renderInput={(params) => (
+                        <TextField
+                          {...params}
+                          label="Select Surveys"
+                          placeholder="Search and select surveys"
+                          required
+                        />
+                      )}
+                      disableCloseOnSelect
+                    />
+                  }
+                />
+                <Divider />
+                {/* Bottom half: User/role selection */}
+                <HorizontalSplitFormComponent
+                  title="Invite Members"
+                  summary="Invite members to access your surveys. Any individual role you assign here will apply to each member in every survey you have selected."
+                  component={<ParticipantsCollectionForm roles={codes?.survey_roles ?? []} />}
+                />
+                <Divider />
+                <Stack mt={4} flexDirection="row" justifyContent="flex-end" gap={1}>
+                  <LoadingButton
+                    loading={isSaving}
+                    type="submit"
+                    color="primary"
+                    variant="contained"
+                    onClick={() => formikRef.current?.submitForm()}
+                    data-testid="submit-manage-users-button">
+                    Save and Exit
+                  </LoadingButton>
+                  <Button disabled={isSaving} color="primary" variant="outlined" onClick={handleCancel}>
+                    Cancel
+                  </Button>
+                </Stack>
+              </Stack>
+            )}
+          </Formik>
+        </Paper>
+      </Container>
+    </>
   );
 };
 
