ebeb9f2d3f43c33a4d22250b404ec3f6f20bbd98
Meijer <Annika.Meijer@gov.bc.ca>
2025-06-06 09:20:33 -0700

    tooltipping the deployments form

--- app/src/features/surveys/telemetry/manage/deployments/form/DeploymentForm.tsx
+++ app/src/features/surveys/telemetry/manage/deployments/form/DeploymentForm.tsx
@@ -5,6 +5,7 @@ import Divider from '@mui/material/Divider';
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import HelpButtonStack from 'components/buttons/HelpButtonStack';
 import HorizontalSplitFormComponent from 'components/fields/HorizontalSplitFormComponent';
 import {
   DeploymentDetailsForm,
@@ -153,10 +154,12 @@ export const DeploymentForm = (props: IDeploymentFormProps) => {
           <HorizontalSplitFormComponent
             title="End Date (optional)"
             summary="Enter information about when the deployment ended">
-            <DeploymentEndForm
-              captures={critterDataLoader.data?.captures ?? []}
-              mortalities={critterDataLoader.data?.mortality ?? []}
-            />
+            <HelpButtonStack helpText="A deployment must be ended prior to re-deploying the device.">
+              <DeploymentEndForm
+                captures={critterDataLoader.data?.captures ?? []}
+                mortalities={critterDataLoader.data?.mortality ?? []}
+              />
+            </HelpButtonStack>
           </HorizontalSplitFormComponent>
 
           <Divider />
--- app/src/features/surveys/telemetry/manage/deployments/form/timeline/DeploymentStartForm.tsx
+++ app/src/features/surveys/telemetry/manage/deployments/form/timeline/DeploymentStartForm.tsx
@@ -1,5 +1,6 @@
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import HelpButtonStack from 'components/buttons/HelpButtonStack';
 import AutocompleteField, { IAutocompleteFieldOption } from 'components/fields/AutocompleteField';
 import SingleDateField from 'components/fields/SingleDateField';
 import { TimeField } from 'components/fields/TimeField';
@@ -44,50 +45,54 @@ export const DeploymentStartForm = (props: IDeploymentStartFormProps) => {
   return (
     <Grid container spacing={3}>
       <Grid item xs={12}>
-        <AutocompleteField
-          name="critterbase_start_capture_id"
-          id="critterbase_start_capture_id"
-          label={'Initial capture event'}
-          options={captures.map((capture) => ({
-            value: capture.capture_id,
-            label: capture.capture_time
-              ? dayjs(`${capture.capture_date} ${capture.capture_time}`).format(DATE_FORMAT.LongDateTimeFormat)
-              : dayjs(`${capture.capture_date}`).format(DATE_FORMAT.MediumDateFormat)
-          }))}
-          onChange={(_: SyntheticEvent<Element, Event>, value: IAutocompleteFieldOption<string> | null) => {
-            // Get date of the capture to set attachment_start_date
-            if (value) {
-              const timestamp = dayjs(value.label);
-              const date = timestamp.format(DATE_FORMAT.ShortDateFormat);
-              const time = timestamp.format('HH:mm:ss');
+        <HelpButtonStack helpText="Capture events must be created on your animal profile prior to populating in this list.">
+          <AutocompleteField
+            name="critterbase_start_capture_id"
+            id="critterbase_start_capture_id"
+            label={'Initial capture event'}
+            options={captures.map((capture) => ({
+              value: capture.capture_id,
+              label: capture.capture_time
+                ? dayjs(`${capture.capture_date} ${capture.capture_time}`).format(DATE_FORMAT.LongDateTimeFormat)
+                : dayjs(`${capture.capture_date}`).format(DATE_FORMAT.MediumDateFormat)
+            }))}
+            onChange={(_: SyntheticEvent<Element, Event>, value: IAutocompleteFieldOption<string> | null) => {
+              // Get date of the capture to set attachment_start_date
+              if (value) {
+                const timestamp = dayjs(value.label);
+                const date = timestamp.format(DATE_FORMAT.ShortDateFormat);
+                const time = timestamp.format('HH:mm:ss');
 
-              setFieldValue('attachment_start_date', date);
-              setFieldValue('critterbase_start_capture_id', value.value);
-              // Set capture time if it exists on the selected capture
-              if (time) {
-                setFieldValue('attachment_start_time', time);
-              }
-            }
-          }}
-          required
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <Box sx={{ width: '100%' }} display="flex">
-          <SingleDateField
-            sx={{
-              '& .MuiOutlinedInput-root': {
-                borderTopRightRadius: 0,
-                borderBottomRightRadius: 0
+                setFieldValue('attachment_start_date', date);
+                setFieldValue('critterbase_start_capture_id', value.value);
+                // Set capture time if it exists on the selected capture
+                if (time) {
+                  setFieldValue('attachment_start_time', time);
+                }
               }
             }}
-            id="attachment_start_date"
-            name="attachment_start_date"
-            label="Start date"
-            required={values.attachment_start_time !== null}
+            required
           />
-          <TimeField id="attachment_start_time" name="attachment_start_time" label="Start time" required={false} />
-        </Box>
+        </HelpButtonStack>
+      </Grid>
+      <Grid item xs={12}>
+        <HelpButtonStack helpText="Optional: ammend the date and time to truncate the data range.">
+          <Box sx={{ width: '100%' }} display="flex">
+            <SingleDateField
+              sx={{
+                '& .MuiOutlinedInput-root': {
+                  borderTopRightRadius: 0,
+                  borderBottomRightRadius: 0
+                }
+              }}
+              id="attachment_start_date"
+              name="attachment_start_date"
+              label="Start date"
+              required={values.attachment_start_time !== null}
+            />
+            <TimeField id="attachment_start_time" name="attachment_start_time" label="Start time" required={false} />
+          </Box>
+        </HelpButtonStack>
       </Grid>
     </Grid>
   );
