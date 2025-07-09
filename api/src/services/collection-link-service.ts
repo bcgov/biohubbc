cf6db3f93491a92e2a623f6501da0829ce62980f
Andrew Thompson <andrew.thompson@gov.bc.ca>
2025-07-09 10:43:14 -0700

    collection link service

---
+++ api/src/services/collection-link-service.ts
@@ -0,0 +1,91 @@
+import { IDBConnection } from '../database/db';
+import { ICollectionLink, IPostCollectionLinkRequest, IPutCollectionLinkRequest } from '../models/collection-link';
+import { CollectionLinkRepository } from '../repositories/collection-link-repository';
+import { ApiPaginationOptions } from '../zod-schema/pagination';
+import { DBService } from './db-service';
+
+/**
+ * Service layer for collection links.
+ *
+ * @export
+ * @class CollectionLinkService
+ * @extends {DBService}
+ */
+export class CollectionLinkService extends DBService {
+  collectionLinkRepository: CollectionLinkRepository;
+
+  constructor(connection: IDBConnection) {
+    super(connection);
+
+    this.collectionLinkRepository = new CollectionLinkRepository(connection);
+  }
+
+  /**
+   * Get all links for a specific collection.
+   *
+   * @param {number} collectionId
+   * @param {ApiPaginationOptions} [pagination]
+   * @return {*}  {Promise<ICollectionLink[]>}
+   * @memberof CollectionLinkService
+   */
+  async getCollectionLinks(collectionId: number, pagination?: ApiPaginationOptions): Promise<ICollectionLink[]> {
+    return this.collectionLinkRepository.getCollectionLinks(collectionId, pagination);
+  }
+
+  /**
+   * Get the total count of links for a specific collection.
+   *
+   * @param {number} collectionId
+   * @return {*}  {Promise<number>}
+   * @memberof CollectionLinkService
+   */
+  async getCollectionLinksCount(collectionId: number): Promise<number> {
+    return this.collectionLinkRepository.getCollectionLinksCount(collectionId);
+  }
+
+  /**
+   * Create a new collection link.
+   *
+   * @param {number} collectionId
+   * @param {IPostCollectionLinkRequest} linkData
+   * @param {number} systemUserId
+   * @return {*}  {Promise<ICollectionLink>}
+   * @memberof CollectionLinkService
+   */
+  async createCollectionLink(
+    collectionId: number,
+    linkData: IPostCollectionLinkRequest,
+    systemUserId: number
+  ): Promise<ICollectionLink> {
+    return this.collectionLinkRepository.createCollectionLink(collectionId, linkData, systemUserId);
+  }
+
+  /**
+   * Update an existing collection link.
+   *
+   * @param {number} collectionId
+   * @param {number} linkId
+   * @param {IPutCollectionLinkRequest} linkData
+   * @return {*}  {Promise<ICollectionLink>}
+   * @memberof CollectionLinkService
+   */
+  async updateCollectionLink(
+    collectionId: number,
+    linkId: number,
+    linkData: IPutCollectionLinkRequest
+  ): Promise<ICollectionLink> {
+    return this.collectionLinkRepository.updateCollectionLink(collectionId, linkId, linkData);
+  }
+
+  /**
+   * Delete a collection link.
+   *
+   * @param {number} collectionId
+   * @param {number} linkId
+   * @return {*}  {Promise<void>}
+   * @memberof CollectionLinkService
+   */
+  async deleteCollectionLink(collectionId: number, linkId: number): Promise<void> {
+    return this.collectionLinkRepository.deleteCollectionLink(collectionId, linkId);
+  }
+}
