3ae77e6c20c64dea2b056dfc63bbf475988242aa
Macgregor Aubertin-Young <108430771+mauberti-bc@users.noreply.github.com>
2026-01-22 13:00:36 -0800

    SIMSBIOHUB-848: Remove feature flag & codes data loader (#1592)
    remove feature flag & codes data loader

--- app/src/contexts/codesContext.tsx
+++ app/src/contexts/codesContext.tsx
@@ -1,7 +1,7 @@
 import { useBiohubApi } from 'hooks/useBioHubApi';
 import useDataLoader, { DataLoader } from 'hooks/useDataLoader';
 import { IGetAllCodeSetsResponse } from 'interfaces/useCodesApi.interface';
-import { createContext, PropsWithChildren, useMemo } from 'react';
+import { createContext, PropsWithChildren, useEffect, useMemo } from 'react';
 
 /**
  * Context object that stores information about codes
@@ -26,6 +26,10 @@ export const CodesContext = createContext<ICodesContext>({
 export const CodesContextProvider = (props: PropsWithChildren<Record<never, any>>) => {
   const biohubApi = useBiohubApi();
   const codesDataLoader = useDataLoader(biohubApi.codes.getAllCodeSets);
+
+  useEffect(() => {
+    codesDataLoader.load();
+  }, [codesDataLoader]);
 
   const codesContext: ICodesContext = useMemo(() => ({ codesDataLoader }), [codesDataLoader]);
 
