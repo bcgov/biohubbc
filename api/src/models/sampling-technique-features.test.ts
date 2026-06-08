1c8e07d6482be81b45136f83d4aa6cad95167d25
Dylan Rogowsky <dylan@oxd.com>
2026-06-08 15:23:36 -0600

    Adjust array values

--- api/src/models/sampling-technique-features.test.ts
+++ api/src/models/sampling-technique-features.test.ts
@@ -47,8 +47,8 @@ describe('Sampling Technique Features BioHub Integration', () => {
       expect(techniqueObj.properties.description).to.equal('Wildlife camera deployed for monitoring');
       expect(techniqueObj.properties.method_name).to.equal('code::method_lookup::5');
       expect(techniqueObj.properties.attractant).to.deep.equal([
-        { attractant_name: 'code::attractant_lookup::3' },
-        { attractant_name: 'code::attractant_lookup::7' }
+        'code::attractant_lookup::3',
+        'code::attractant_lookup::7'
       ]);
       expect(techniqueObj.properties.method_attribute).to.equal(
         'code::technique_attribute_qualitative::e0479006-f5ee-40cf-a33b-c1ffce393bf4'
